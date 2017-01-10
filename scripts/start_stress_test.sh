fdd1f0a09dd08fd5f6fba1c22607dba0b6d23777
Siva Kalva <sivakk@uber.com>
2017-01-10 13:17:01 -0800

    Stress Scripts

--- scripts/start_stress_test.sh
+++ scripts/start_stress_test.sh
@@ -15,11 +15,20 @@ port=9042
 # TODO: probably allow getting this from command line
 workflow_keyspace="workflow"
 
+DROP_UBER_MINIONS="$HOME/uber-minions"
+DROP_CMD=$DROP/cmd/stress/
+STRESS_LOG=stress_output.log
+
 # cmd/stress/stress -emitMetric=m3 -host="10.185.19.27,10.184.45.6,10.185.27.8,10.185.17.12,10.185.15.30"
 for host in 10.185.19.27
 do
   echo Installing schema on cassandra cluster via $host
-  ./cassandra/bin/cqlsh -f ./schema/drop_keyspace_stress_test.cql $host $port
-  ./cassandra/bin/cqlsh -f ./schema/keyspace_prod.cql $host $port
-  ./cassandra/bin/cqlsh -k $workflow_keyspace -f ./schema/workflow_test.cql $host $port
+  cqlsh --cqlversion=3.4.2 -f ./schema/drop_keyspace_stress_test.cql $host $port
+  cqlsh --cqlversion=3.4.2 -f ./schema/keyspace_prod.cql $host $port
+  cqlsh --cqlversion=3.4.2 -k $workflow_keyspace -f ./schema/workflow_test.cql $host $port
+  cqlsh --cqlversion=3.4.2 -k $workflow_keyspace -f ./schema/stress_schema_setup.cql $host $port
 done
+
+echo "Starting stress on"
+cd $DROP_CMD
+nohup ./stress -emitMetric=m3 -host="10.185.19.27,10.184.45.6,10.185.27.8,10.185.17.12,10.185.15.30" > $STRESS_LOG 2>&1 &
