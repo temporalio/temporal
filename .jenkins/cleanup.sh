c56b06ec260c015f97f78ebb66603df9f3700bd6
Tamer Eldeeb <tamer@uber.com>
2017-04-17 22:14:21 -0700

    Remove unused scripts (#139)
    Removes holdover scripts for Jenkins environment

--- .jenkins/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -xu
-
-bash scripts/cadence-stop-cassandra
--- .jenkins/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xu
-
-bash scripts/cadence-stop-cassandra
-bash scripts/cadence-start-cassandra
-bash scripts/cadence-setup-schema
-
-exit $?
