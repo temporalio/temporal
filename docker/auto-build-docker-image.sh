8b6694a16ca463ff70b831e5247a3bd472127328
Quanzheng Long <longquanzheng@users.noreply.github.com>
2018-04-04 13:07:03 -0700

    Add new members to receive docker build notifications (#654)

--- docker/auto-build-docker-image.sh
+++ docker/auto-build-docker-image.sh
@@ -6,5 +6,5 @@ cm_msg=`git log -1 --abbrev-commit`
 if [ "$last_cm_id" == "$curr_cm_id" ]; then
         echo "Last commit doesn't change."
 else
-        echo "Last commit changed. Start building..."  && docker build . -t ubercadence/server:master --build-arg git_branch=master && docker push ubercadence/server:master &&  mail -s "Docker image auto build succeeded HEAD: $curr_cm_id tag: master" longer@uber.com,maxim@uber.com,yiminc@uber.com,venkat@uber.com,mihneao@uber.com,samar@uber.com,madhurav@uber.com,boweixu@uber.com <<< "commit info: $cm_msg  " && echo $curr_cm_id > .last_cm_id.txt && echo "build succ!"
+        echo "Last commit changed. Start building..."  && docker build . -t ubercadence/server:master --build-arg git_branch=master && docker push ubercadence/server:master &&  mail -s "Docker image auto build succeeded HEAD: $curr_cm_id tag: master" longer@uber.com,maxim@uber.com,yiminc@uber.com,venkat@uber.com,krupapc@uber.com,samar@uber.com,wenquanx@uber.com,boweixu@uber.com,meiliang@uber.com,arthurg@uber.com,nathanbl@uber.com <<< "commit info: $cm_msg  " && echo $curr_cm_id > .last_cm_id.txt && echo "build succ!"
 fi
