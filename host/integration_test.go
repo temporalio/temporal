974e6cb74165a79a3eec035cbfc4f0db269a2a98
Bowei Xu <boweixu@uber.com>
2018-11-30 10:12:32 -0800

    Fix flaky TestVisibility (#1296)

--- host/integration_test.go
+++ host/integration_test.go
@@ -3229,13 +3229,20 @@ func (s *integrationSuite) TestVisibility() {
 	closedCount := 0
 	openCount := 0
 
-	resp, err3 := s.engine.ListClosedWorkflowExecutions(createContext(), &workflow.ListClosedWorkflowExecutionsRequest{
-		Domain:          common.StringPtr(s.domainName),
-		MaximumPageSize: common.Int32Ptr(100),
-		StartTimeFilter: startFilter,
-	})
-	s.Nil(err3)
-	closedCount = len(resp.Executions)
+	for i := 0; i < 10; i++ {
+		resp, err3 := s.engine.ListClosedWorkflowExecutions(createContext(), &workflow.ListClosedWorkflowExecutionsRequest{
+			Domain:          common.StringPtr(s.domainName),
+			MaximumPageSize: common.Int32Ptr(100),
+			StartTimeFilter: startFilter,
+		})
+		s.Nil(err3)
+		closedCount = len(resp.Executions)
+		if closedCount == 1 {
+			break
+		}
+		s.logger.Info("Closed WorkflowExecution is not yet visible")
+		time.Sleep(100 * time.Millisecond)
+	}
 	s.Equal(1, closedCount)
 
 	for i := 0; i < 10; i++ {
