467600467a8cbe9a1a848665980084cb13213dbd
Quanzheng Long <longquanzheng@users.noreply.github.com>
2018-12-17 12:46:58 -0800

    Fix eventsV2 detecting corrupted history too aggressive (#1338)

--- common/persistence/historyV2Store.go
+++ common/persistence/historyV2Store.go
@@ -333,13 +333,13 @@ func (m *historyV2ManagerImpl) readHistoryBranch(byBatch bool, request *ReadHist
 			continue
 		}
 
-		if *fe.EventId == token.LastEventID {
+		if *fe.EventId <= token.LastEventID {
 			// we could see it because of batch with smaller txn_id
 			logger.Infof("Stale event batch with eventID: %v", *fe.EventId)
 			continue
 		}
 		if *fe.EventId != token.LastEventID+1 {
-			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el)
+			logger.Errorf("Corrupted incontinouous event batch, %v, %v, %v, %v, %v, %v", *fe.Version, *le.Version, *fe.EventId, *le.EventId, el, token.LastEventID)
 			return nil, nil, nil, 0, 0, &workflow.InternalServiceError{
 				Message: fmt.Sprintf("corrupted history event batch, eventID is not continouous"),
 			}
