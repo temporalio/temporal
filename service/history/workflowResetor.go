09d90a241dd3d61318b553ae96b1e3639d38cee4
wxing1292 <wxing1292@users.noreply.github.com>
2019-03-06 12:50:25 -0800

    Fix workflow reset input validation (#1514)

--- service/history/workflowResetor.go
+++ service/history/workflowResetor.go
@@ -73,6 +73,12 @@ func (w *workflowResetorImpl) ResetWorkflowExecution(ctx context.Context, resetR
 		}
 		return
 	}
+	if request.GetDecisionFinishEventId() <= common.FirstEventID {
+		retError = &workflow.BadRequestError{
+			Message: fmt.Sprintf("Decision finish ID must be > 1."),
+		}
+		return
+	}
 	baseExecution := workflow.WorkflowExecution{
 		WorkflowId: request.WorkflowExecution.WorkflowId,
 		RunId:      request.WorkflowExecution.RunId,
