26feb8a1cdd07afccbbbd0d35e68235e923a0e26
wxing1292 <wxing1292@users.noreply.github.com>
2019-05-08 16:07:51 -0700

    Add paging iterator to collection (#1467)

---
+++ common/collection/pagingIterator.go
@@ -0,0 +1,104 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package collection
+
+type (
+	// PaginationFn is the function which get a page of results
+	PaginationFn func(paginationToken []byte) ([]interface{}, []byte, error)
+
+	// PagingIteratorImpl is the implementation of PagingIterator
+	PagingIteratorImpl struct {
+		paginationFn      PaginationFn
+		pageToken         []byte
+		pageErr           error
+		pageItems         []interface{}
+		nextPageItemIndex int
+	}
+)
+
+// NewPagingIterator create a new paging iterator
+func NewPagingIterator(paginationFn PaginationFn) Iterator {
+	iter := &PagingIteratorImpl{
+		paginationFn:      paginationFn,
+		pageToken:         nil,
+		pageErr:           nil,
+		pageItems:         nil,
+		nextPageItemIndex: 0,
+	}
+	iter.getNextPage() // this will initialize the paging iterator
+	return iter
+}
+
+// HasNext return whether has next item or err
+func (iter *PagingIteratorImpl) HasNext() bool {
+	// pagination encounters error
+	if iter.pageErr != nil {
+		return true
+	}
+
+	// still have local cached item to return
+	if iter.nextPageItemIndex < len(iter.pageItems) {
+		return true
+	}
+
+	if len(iter.pageToken) != 0 {
+		iter.getNextPage()
+		return iter.HasNext()
+	}
+
+	return false
+}
+
+// Next return next item or err
+func (iter *PagingIteratorImpl) Next() (interface{}, error) {
+	if !iter.HasNext() {
+		panic("HistoryEventIterator Next() called without checking HasNext()")
+	}
+
+	if iter.pageErr != nil {
+		err := iter.pageErr
+		iter.pageErr = nil
+		return nil, err
+	}
+
+	// we have cached events
+	if iter.nextPageItemIndex < len(iter.pageItems) {
+		index := iter.nextPageItemIndex
+		iter.nextPageItemIndex++
+		return iter.pageItems[index], nil
+	}
+
+	panic("HistoryEventIterator Next() should return either a history event or a err")
+}
+
+func (iter *PagingIteratorImpl) getNextPage() {
+	items, token, err := iter.paginationFn(iter.pageToken)
+	if err == nil {
+		iter.pageItems = items
+		iter.pageToken = token
+		iter.pageErr = nil
+	} else {
+		iter.pageItems = nil
+		iter.pageToken = nil
+		iter.pageErr = err
+	}
+	iter.nextPageItemIndex = 0
+}
---
+++ common/collection/pagingIterator_test.go
@@ -0,0 +1,166 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package collection
+
+import (
+	"errors"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type (
+	pagingIteratorSuite struct {
+		suite.Suite
+	}
+)
+
+func TestPagingIteratorSuite(t *testing.T) {
+	s := new(pagingIteratorSuite)
+	suite.Run(t, s)
+}
+
+func (s *pagingIteratorSuite) SetupSuite() {
+	if testing.Verbose() {
+		log.SetOutput(os.Stdout)
+	}
+
+}
+
+func (s *pagingIteratorSuite) TearDownSuite() {
+
+}
+
+func (s *pagingIteratorSuite) SetupTest() {
+
+}
+
+func (s *pagingIteratorSuite) TearDownTest() {
+
+}
+
+func (s *pagingIteratorSuite) TestIteration_NoErr() {
+	phase := 0
+	outputs := [][]interface{}{
+		[]interface{}{1, 2, 3, 4, 5},
+		[]interface{}{},
+		[]interface{}{6},
+		[]interface{}{},
+	}
+	tokens := [][]byte{
+		[]byte("some random token 1"),
+		[]byte("some random token 2"),
+		[]byte("some random token 3"),
+		[]byte(nil),
+	}
+	pagingFn := func(token []byte) ([]interface{}, []byte, error) {
+		switch phase {
+		case 0:
+			s.Equal(0, len(token))
+			defer func() { phase++ }()
+			return outputs[phase], tokens[phase], nil
+		case 1:
+			s.Equal(tokens[0], token)
+			defer func() { phase++ }()
+			return outputs[phase], tokens[phase], nil
+		case 2:
+			s.Equal(tokens[1], token)
+			defer func() { phase++ }()
+			return outputs[phase], tokens[phase], nil
+		case 3:
+			s.Equal(tokens[2], token)
+			defer func() { phase++ }()
+			return outputs[phase], tokens[phase], nil
+		default:
+			panic("should not reach here during test")
+		}
+	}
+
+	result := []int{}
+	ite := NewPagingIterator(pagingFn)
+	for ite.HasNext() {
+		item, err := ite.Next()
+		s.Nil(err)
+		num, ok := item.(int)
+		s.True(ok)
+		result = append(result, num)
+	}
+	s.Equal([]int{1, 2, 3, 4, 5, 6}, result)
+}
+
+func (s *pagingIteratorSuite) TestIteration_Err_Beginging() {
+	phase := 0
+	ite := NewPagingIterator(func(token []byte) ([]interface{}, []byte, error) {
+		switch phase {
+		case 0:
+			defer func() { phase++ }()
+			return nil, nil, errors.New("some random error")
+		default:
+			panic("should not reach here during test")
+		}
+	})
+
+	s.True(ite.HasNext())
+	item, err := ite.Next()
+	s.Nil(item)
+	s.NotNil(err)
+	s.False(ite.HasNext())
+}
+
+func (s *pagingIteratorSuite) TestIteration_Err_NotBegining() {
+
+	phase := 0
+	outputs := [][]interface{}{
+		[]interface{}{1, 2, 3, 4, 5},
+	}
+	tokens := [][]byte{
+		[]byte("some random token 1"),
+	}
+	pagingFn := func(token []byte) ([]interface{}, []byte, error) {
+		switch phase {
+		case 0:
+			s.Equal(0, len(token))
+			defer func() { phase++ }()
+			return outputs[phase], tokens[phase], nil
+		case 1:
+			s.Equal(tokens[0], token)
+			defer func() { phase++ }()
+			return nil, nil, errors.New("some random error")
+		default:
+			panic("should not reach here during test")
+		}
+	}
+
+	result := []int{}
+	ite := NewPagingIterator(pagingFn)
+	for ite.HasNext() {
+		item, err := ite.Next()
+		if err != nil {
+			break
+		}
+		num, ok := item.(int)
+		s.True(ok)
+		result = append(result, num)
+	}
+	s.Equal([]int{1, 2, 3, 4, 5}, result)
+}
