3f6078d3794913e4a8adf553aa19a0f12040bc2b
Bowei Xu <boweixu@uber.com>
2019-05-28 17:42:25 -0700

    Fix dynamic config GetMapProperty log duplicates (#1920)
    * Fix dynamic config GetMapProperty log duplicates
    
    * More comment

--- common/service/dynamicconfig/nopClient.go
+++ common/service/dynamicconfig/nopClient.go
@@ -27,24 +27,7 @@ import (
 	"github.com/uber/cadence/common/log"
 )
 
-// Client allows fetching values from a dynamic configuration system NOTE: This does not have async
-// options right now. In the interest of keeping it minimal, we can add when requirement arises.
-type Client interface {
-	GetValue(name Key, defaultValue interface{}) (interface{}, error)
-	GetValueWithFilters(name Key, filters map[Filter]interface{}, defaultValue interface{}) (interface{}, error)
-
-	GetIntValue(name Key, filters map[Filter]interface{}, defaultValue int) (int, error)
-	GetFloatValue(name Key, filters map[Filter]interface{}, defaultValue float64) (float64, error)
-	GetBoolValue(name Key, filters map[Filter]interface{}, defaultValue bool) (bool, error)
-	GetStringValue(name Key, filters map[Filter]interface{}, defaultValue string) (string, error)
-	GetMapValue(
-		name Key, filters map[Filter]interface{}, defaultValue map[string]interface{},
-	) (map[string]interface{}, error)
-	GetDurationValue(
-		name Key, filters map[Filter]interface{}, defaultValue time.Duration,
-	) (time.Duration, error)
-}
-
+// nopClient is a dummy implements of dynamicconfig Client interface, all operations will always return default values.
 type nopClient struct{}
 
 func (mc *nopClient) GetValue(name Key, defaultValue interface{}) (interface{}, error) {
