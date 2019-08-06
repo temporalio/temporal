d889f2d5f5403610e6f831c170a7611c46dea2d1
Yichao Yang <ycyang@uber.com>
2019-08-06 14:45:03 -0700

    update cli version (#2352)

--- tools/cli/app.go
+++ tools/cli/app.go
@@ -27,7 +27,7 @@ import (
 const (
 	// Version is the controlled version string. It should be updated every time
 	// before we release a new version.
-	Version = "0.7.0"
+	Version = "0.8.0"
 )
 
 // SetFactory is used to set the ClientFactory global
