e9e1ef9c62c8cf54e4fc17f9a2a606bbf543f086
Yichao Yang <ycyang@uber.com>
2019-10-29 13:29:29 -0700

    Update supported client version (#2741)

--- service/frontend/versionchecker.go
+++ service/frontend/versionchecker.go
@@ -30,9 +30,9 @@ import (
 )
 
 var versionSupported = map[string]version.Constraints{
-	"uber-go":   mustNewConstraint("<1.4"),
-	"uber-java": mustNewConstraint("<1.4"),
-	"cli":       mustNewConstraint("<1.4"),
+	"uber-go":   mustNewConstraint("<1.5"),
+	"uber-java": mustNewConstraint("<1.5"),
+	"cli":       mustNewConstraint("<1.5"),
 }
 
 type (
