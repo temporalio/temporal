7dc26478a17de4d2c7d3caf252fdcd5690c851e6
Samar Abbas <samar@temporal.io>
2019-11-11 09:36:06 -0800

    fix gomock generation

--- common/service/dynamicconfig/clientInterface.go
+++ common/service/dynamicconfig/clientInterface.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//go:generate mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE -source $GOFILE -destination clientInterface_mock.go -self_package github.com/uber/cadence/common/service/dynamicconfig
+//go:generate mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE -source $GOFILE -destination clientInterface_mock.go -self_package github.com/temporalio/temporal/common/service/dynamicconfig
 
 package dynamicconfig
 
