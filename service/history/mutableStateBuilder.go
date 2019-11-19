c6112ac58aa321cd9b1afebffe212090d81777ae
Yu Xia <yx@uber.com>
2019-11-18 17:37:44 -0800

    Fix replicate ActivityTaskCancelRequested event if activity info not found (#2838)
    Fix replicate ActivityTaskCancelRequested event if activity info not found

--- service/history/mutableStateBuilder.go
+++ service/history/mutableStateBuilder.go
@@ -2347,7 +2347,9 @@ func (e *mutableStateBuilder) ReplicateActivityTaskCancelRequestedEvent(
 	activityID := attributes.GetActivityId()
 	ai, ok := e.GetActivityByActivityID(activityID)
 	if !ok {
-		return ErrMissingActivityInfo
+		// On active side, if the ActivityTaskCancelRequested is invalid, it will created a RequestCancelActivityTaskFailed
+		// Passive will rely on active side logic
+		return nil
 	}
 
 	ai.Version = event.GetVersion()
