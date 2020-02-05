b38329b68aa90b2aa44ed88dd4e5d72935666349
Shawn Hathaway <shawnhath@gmail.com>
2020-02-05 14:20:39 -0800

    ClusterMembership Schema - Mysql

--- schema/mysql/v57/cadence/schema.sql
+++ schema/mysql/v57/cadence/schema.sql
@@ -254,10 +254,19 @@ CREATE TABLE queue_metadata (
   PRIMARY KEY(queue_type)
 );
 
-
 CREATE TABLE cluster_metadata (
   metadata_partition        INT NOT NULL,
   immutable_data            BLOB NOT NULL,
   immutable_data_encoding   VARCHAR(16) NOT NULL,
   PRIMARY KEY(metadata_partition)
+);
+
+CREATE TABLE cluster_membership
+(
+    host_id              BINARY(16) NOT NULL,
+    rpc_address          VARCHAR(15) NOT NULL,
+    session_start        TIMESTAMP NOT NULL,
+    last_heartbeat       TIMESTAMP NOT NULL,
+    record_expiry        TIMESTAMP NOT NULL,
+    PRIMARY KEY (host_id)
 );
--- schema/mysql/v57/cadence/versioned/v0.4/cluster_metadata.sql
+++ schema/mysql/v57/cadence/versioned/v0.4/cluster_metadata.sql
@@ -4,3 +4,13 @@ CREATE TABLE cluster_metadata (
   immutable_data_encoding   VARCHAR(16) NOT NULL,
   PRIMARY KEY(metadata_partition)
 );
+
+CREATE TABLE cluster_membership
+(
+    host_id              BINARY(16) NOT NULL,
+    rpc_address          VARCHAR(15) NOT NULL,
+    session_start        TIMESTAMP NOT NULL,
+    last_heartbeat       TIMESTAMP NOT NULL,
+    record_expiry        TIMESTAMP NOT NULL,
+    PRIMARY KEY (host_id)
+);
