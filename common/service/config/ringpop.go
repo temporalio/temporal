16bcc6b2ac474899a7d949efae82cc9c23b831fb
Shawn Hathaway <shawnhath@gmail.com>
2020-02-08 00:27:40 -0800

    Cleanup RingpopFactory and move it to its own package
    Moving necessity is due to cycles caused by bringing in persistence.

--- common/service/config/ringpop.go
+++ common/service/config/ringpop.go
@@ -18,44 +18,27 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package config
+package ringpop
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"net"
 	"reflect"
-	"strings"
 	"sync"
 	"time"
 
+	"github.com/temporalio/temporal/common/service/config"
+
+	"github.com/temporalio/temporal/common/persistence"
+
 	"github.com/uber/ringpop-go"
-	"github.com/uber/ringpop-go/discovery"
-	"github.com/uber/ringpop-go/discovery/jsonfile"
-	"github.com/uber/ringpop-go/discovery/statichosts"
-	"github.com/uber/ringpop-go/swim"
 	tcg "github.com/uber/tchannel-go"
 	"go.uber.org/yarpc"
 	"go.uber.org/yarpc/transport/tchannel"
 
 	"github.com/temporalio/temporal/common/log"
-	"github.com/temporalio/temporal/common/log/tag"
 	"github.com/temporalio/temporal/common/membership"
-)
-
-const (
-	// BootstrapModeNone represents a bootstrap mode set to nothing or invalid
-	BootstrapModeNone BootstrapMode = iota
-	// BootstrapModeFile represents a file-based bootstrap mode
-	BootstrapModeFile
-	// BootstrapModeHosts represents a list of hosts passed in the configuration
-	BootstrapModeHosts
-	// BootstrapModeCustom represents a custom bootstrap mode
-	BootstrapModeCustom
-	// BootstrapModeDNS represents a list of hosts passed in the configuration
-	// to be resolved, and the resulting addresses are used for bootstrap
-	BootstrapModeDNS
 )
 
 const (
@@ -64,7 +47,7 @@ const (
 
 // RingpopFactory implements the RingpopFactory interface
 type RingpopFactory struct {
-	config         *Ringpop
+	config         *config.Ringpop
 	dispatcher     *yarpc.Dispatcher
 	serviceName    string
 	servicePortMap map[string]int
@@ -73,103 +56,54 @@ type RingpopFactory struct {
 	sync.Mutex
 	ringPop           *membership.RingPop
 	membershipMonitor membership.Monitor
+	metadataManager   persistence.ClusterMetadataManager
 }
 
-// NewFactory builds a ringpop factory conforming
+// NewRingpopFactory builds a ringpop factory conforming
 // to the underlying configuration
-func (rpConfig *Ringpop) NewFactory(
+func NewRingpopFactory(
+	rpConfig *config.Ringpop,
 	dispatcher *yarpc.Dispatcher,
 	serviceName string,
 	servicePortMap map[string]int,
 	logger log.Logger,
+	metadataManager persistence.ClusterMetadataManager,
 ) (*RingpopFactory, error) {
-
-	return newRingpopFactory(rpConfig, dispatcher, serviceName, servicePortMap, logger)
+	return newRingpopFactory(rpConfig, dispatcher, serviceName, servicePortMap, logger, metadataManager)
 }
 
-func (rpConfig *Ringpop) validate() error {
+func validateRingpopConfig(rpConfig *config.Ringpop) error {
 	if len(rpConfig.Name) == 0 {
 		return fmt.Errorf("ringpop config missing `name` param")
 	}
-	return validateBootstrapMode(rpConfig)
-}
-
-// UnmarshalYAML is called by the yaml package to convert
-// the config YAML into a BootstrapMode.
-func (m *BootstrapMode) UnmarshalYAML(
-	unmarshal func(interface{}) error,
-) error {
-
-	var s string
-	if err := unmarshal(&s); err != nil {
-		return err
-	}
-	var err error
-	*m, err = parseBootstrapMode(s)
-	return err
-}
-
-// parseBootstrapMode reads a string value and returns a bootstrap mode.
-func parseBootstrapMode(
-	mode string,
-) (BootstrapMode, error) {
-
-	switch strings.ToLower(mode) {
-	case "hosts":
-		return BootstrapModeHosts, nil
-	case "file":
-		return BootstrapModeFile, nil
-	case "custom":
-		return BootstrapModeCustom, nil
-	case "dns":
-		return BootstrapModeDNS, nil
-	}
-	return BootstrapModeNone, errors.New("invalid or no ringpop bootstrap mode")
-}
-
-func validateBootstrapMode(
-	rpConfig *Ringpop,
-) error {
-
-	switch rpConfig.BootstrapMode {
-	case BootstrapModeFile:
-		if len(rpConfig.BootstrapFile) == 0 {
-			return fmt.Errorf("ringpop config missing bootstrap file param")
-		}
-	case BootstrapModeHosts, BootstrapModeDNS:
-		if len(rpConfig.BootstrapHosts) == 0 {
-			return fmt.Errorf("ringpop config missing boostrap hosts param")
-		}
-	case BootstrapModeCustom:
-		if rpConfig.DiscoveryProvider == nil {
-			return fmt.Errorf("ringpop bootstrapMode is set to custom but discoveryProvider is nil")
-		}
-	default:
-		return fmt.Errorf("ringpop config with unknown boostrap mode")
+	if rpConfig.BroadcastAddress != "" && net.ParseIP(rpConfig.BroadcastAddress) == nil {
+		return fmt.Errorf("ringpop config malformed `broadcastAddress` param")
 	}
 	return nil
 }
 
 func newRingpopFactory(
-	rpConfig *Ringpop,
+	rpConfig *config.Ringpop,
 	dispatcher *yarpc.Dispatcher,
 	serviceName string,
 	servicePortMap map[string]int,
 	logger log.Logger,
+	metadataManager persistence.ClusterMetadataManager,
 ) (*RingpopFactory, error) {
 
-	if err := rpConfig.validate(); err != nil {
+	if err := validateRingpopConfig(rpConfig); err != nil {
 		return nil, err
 	}
 	if rpConfig.MaxJoinDuration == 0 {
 		rpConfig.MaxJoinDuration = defaultMaxJoinDuration
 	}
 	return &RingpopFactory{
-		config:         rpConfig,
-		dispatcher:     dispatcher,
-		serviceName:    serviceName,
-		servicePortMap: servicePortMap,
-		logger:         logger,
+		config:          rpConfig,
+		dispatcher:      dispatcher,
+		serviceName:     serviceName,
+		servicePortMap:  servicePortMap,
+		logger:          logger,
+		metadataManager: metadataManager,
 	}, nil
 }
 
@@ -201,7 +135,9 @@ func (factory *RingpopFactory) createMembership() (membership.Monitor, error) {
 		return nil, fmt.Errorf("ringpop creation failed: %v", err)
 	}
 
-	membershipMonitor := membership.NewRingpopMonitor(factory.serviceName, factory.servicePortMap, rp, factory.logger)
+	membershipMonitor := membership.NewRingpopMonitor(factory.serviceName,
+		factory.servicePortMap, rp, factory.logger, factory.metadataManager, factory.broadcastAddressResolver)
+
 	return membershipMonitor, nil
 }
 
@@ -226,7 +162,7 @@ func (factory *RingpopFactory) broadcastAddressResolver() (string, error) {
 		return "", err
 	}
 
-	return membership.BuildBroadcastHostPort(ch.PeerInfo(), factory.config.broadcastAddress)
+	return membership.BuildBroadcastHostPort(ch.PeerInfo(), factory.config.BroadcastAddress)
 }
 
 func (factory *RingpopFactory) createRingpop() (*membership.RingPop, error) {
@@ -242,15 +178,7 @@ func (factory *RingpopFactory) createRingpop() (*membership.RingPop, error) {
 		return nil, err
 	}
 
-	discoveryProvider, err := newDiscoveryProvider(factory.config, factory.logger)
-	if err != nil {
-		return nil, err
-	}
-	bootstrapOpts := &swim.BootstrapOptions{
-		MaxJoinDuration:  factory.config.MaxJoinDuration,
-		DiscoverProvider: discoveryProvider,
-	}
-	return membership.NewRingPop(rp, bootstrapOpts, factory.logger), nil
+	return membership.NewRingPop(rp, factory.config.MaxJoinDuration, factory.logger), nil
 }
 
 func (factory *RingpopFactory) getChannel(
@@ -265,86 +193,4 @@ func (factory *RingpopFactory) getChannel(
 		return nil, errors.New("unable to get tchannel out of the dispatcher")
 	}
 	return ch, nil
-}
-
-type dnsHostResolver interface {
-	LookupHost(ctx context.Context, host string) (addrs []string, err error)
-}
-
-type dnsProvider struct {
-	UnresolvedHosts []string
-	Resolver        dnsHostResolver
-	Logger          log.Logger
-}
-
-func newDNSProvider(
-	hosts []string,
-	resolver dnsHostResolver,
-	logger log.Logger,
-) *dnsProvider {
-
-	set := map[string]struct{}{}
-	for _, hostport := range hosts {
-		set[hostport] = struct{}{}
-	}
-
-	var keys []string
-	for key := range set {
-		keys = append(keys, key)
-	}
-	return &dnsProvider{
-		UnresolvedHosts: keys,
-		Resolver:        resolver,
-		Logger:          logger,
-	}
-}
-
-func (provider *dnsProvider) Hosts() ([]string, error) {
-	var results []string
-	resolvedHosts := map[string][]string{}
-	for _, hostPort := range provider.UnresolvedHosts {
-		host, port, err := net.SplitHostPort(hostPort)
-		if err != nil {
-			provider.Logger.Warn("could not split host and port", tag.Address(hostPort), tag.Error(err))
-			continue
-		}
-
-		resolved, exists := resolvedHosts[host]
-		if !exists {
-			resolved, err = provider.Resolver.LookupHost(context.Background(), host)
-			if err != nil {
-				provider.Logger.Warn("could not resolve host", tag.Address(host), tag.Error(err))
-				continue
-			}
-			resolvedHosts[host] = resolved
-		}
-		for _, r := range resolved {
-			results = append(results, net.JoinHostPort(r, port))
-		}
-	}
-	if len(results) == 0 {
-		return nil, errors.New("no hosts found, and bootstrap requires at least one")
-	}
-	return results, nil
-}
-
-func newDiscoveryProvider(
-	cfg *Ringpop,
-	logger log.Logger,
-) (discovery.DiscoverProvider, error) {
-
-	if cfg.DiscoveryProvider != nil {
-		// custom discovery provider takes first precedence
-		return cfg.DiscoveryProvider, nil
-	}
-
-	switch cfg.BootstrapMode {
-	case BootstrapModeHosts:
-		return statichosts.New(cfg.BootstrapHosts...), nil
-	case BootstrapModeFile:
-		return jsonfile.New(cfg.BootstrapFile), nil
-	case BootstrapModeDNS:
-		return newDNSProvider(cfg.BootstrapHosts, net.DefaultResolver, logger), nil
-	}
-	return nil, fmt.Errorf("unknown bootstrap mode")
 }
--- common/service/config/ringpop_test.go
+++ common/service/config/ringpop_test.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package config
+package ringpop
 
 import (
 	"context"
@@ -26,9 +26,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/temporalio/temporal/common/service/config"
+
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-	"github.com/uber/ringpop-go/discovery/statichosts"
 	"gopkg.in/yaml.v2"
 
 	"github.com/temporalio/temporal/common/log/loggerimpl"
@@ -48,45 +49,15 @@ func (s *RingpopSuite) SetupTest() {
 }
 
 func (s *RingpopSuite) TestHostsMode() {
-	var cfg Ringpop
+	var cfg config.Ringpop
 	err := yaml.Unmarshal([]byte(getHostsConfig()), &cfg)
 	s.Nil(err)
 	s.Equal("test", cfg.Name)
-	s.Equal(BootstrapModeHosts, cfg.BootstrapMode)
-	s.Equal([]string{"127.0.0.1:1111"}, cfg.BootstrapHosts)
+	s.Equal("1.2.3.4", cfg.BroadcastAddress)
 	s.Equal(time.Second*30, cfg.MaxJoinDuration)
-	err = cfg.validate()
+	err = validateRingpopConfig(&cfg)
 	s.Nil(err)
-	f, err := cfg.NewFactory(nil, "test", nil, loggerimpl.NewNopLogger())
-	s.Nil(err)
-	s.NotNil(f)
-}
-
-func (s *RingpopSuite) TestFileMode() {
-	var cfg Ringpop
-	err := yaml.Unmarshal([]byte(getJSONConfig()), &cfg)
-	s.Nil(err)
-	s.Equal("test", cfg.Name)
-	s.Equal(BootstrapModeFile, cfg.BootstrapMode)
-	s.Equal("/tmp/file.json", cfg.BootstrapFile)
-	s.Equal(time.Second*30, cfg.MaxJoinDuration)
-	err = cfg.validate()
-	s.Nil(err)
-	f, err := cfg.NewFactory(nil, "test", nil, loggerimpl.NewNopLogger())
-	s.Nil(err)
-	s.NotNil(f)
-}
-
-func (s *RingpopSuite) TestCustomMode() {
-	var cfg Ringpop
-	err := yaml.Unmarshal([]byte(getCustomConfig()), &cfg)
-	s.Nil(err)
-	s.Equal("test", cfg.Name)
-	s.Equal(BootstrapModeCustom, cfg.BootstrapMode)
-	s.NotNil(cfg.validate())
-	cfg.DiscoveryProvider = statichosts.New("127.0.0.1")
-	s.Nil(cfg.validate())
-	f, err := cfg.NewFactory(nil, "test", nil, loggerimpl.NewNopLogger())
+	f, err := NewRingpopFactory(&cfg, nil, "test", nil, loggerimpl.NewNopLogger(), nil)
 	s.Nil(err)
 	s.NotNil(f)
 }
@@ -103,107 +74,17 @@ func (resolver *mockResolver) LookupHost(ctx context.Context, host string) ([]st
 	return addrs, nil
 }
 
-func (s *RingpopSuite) TestDNSMode() {
-	var cfg Ringpop
-	err := yaml.Unmarshal([]byte(getDNSConfig()), &cfg)
-	s.Nil(err)
-	s.Equal("test", cfg.Name)
-	s.Equal(BootstrapModeDNS, cfg.BootstrapMode)
-	s.Nil(cfg.validate())
-	logger := loggerimpl.NewNopLogger()
-	f, err := cfg.NewFactory(nil, "test", nil, logger)
-	s.Nil(err)
-	s.NotNil(f)
-
-	s.ElementsMatch(
-		[]string{
-			"example.net:1111",
-			"example.net:1112",
-			"unknown-duplicate.example.net:1111",
-			"unknown-duplicate.example.net:1111",
-			"badhostport",
-		},
-		cfg.BootstrapHosts,
-	)
-
-	provider := newDNSProvider(
-		cfg.BootstrapHosts,
-		&mockResolver{
-			Hosts: map[string][]string{"example.net": []string{"10.0.0.0", "10.0.0.1"}},
-		},
-		logger,
-	)
-	cfg.DiscoveryProvider = provider
-	s.ElementsMatch(
-		[]string{
-			"example.net:1111",
-			"example.net:1112",
-			"unknown-duplicate.example.net:1111",
-			"badhostport",
-		},
-		provider.UnresolvedHosts,
-		"duplicate entries should be removed",
-	)
-
-	hostports, err := cfg.DiscoveryProvider.Hosts()
-	s.Nil(err)
-	s.ElementsMatch(
-		[]string{
-			"10.0.0.0:1111", "10.0.0.1:1111",
-			"10.0.0.0:1112", "10.0.0.1:1112",
-		},
-		hostports,
-	)
-
-	cfg.DiscoveryProvider = newDNSProvider(
-		cfg.BootstrapHosts,
-		&mockResolver{Hosts: map[string][]string{}},
-		logger,
-	)
-	hostports, err = cfg.DiscoveryProvider.Hosts()
-	s.Nil(hostports)
-	s.NotNil(err, "error should be returned when no hosts")
-}
-
 func (s *RingpopSuite) TestInvalidConfig() {
-	var cfg Ringpop
-	s.NotNil(cfg.validate())
+	var cfg config.Ringpop
+	s.Error(validateRingpopConfig(&cfg))
 	cfg.Name = "test"
-	s.NotNil(cfg.validate())
-	cfg.BootstrapMode = BootstrapModeNone
-	s.NotNil(cfg.validate())
-	_, err := parseBootstrapMode("unknown")
-	s.NotNil(err)
-}
-
-func getJSONConfig() string {
-	return `name: "test"
-bootstrapMode: "file"
-bootstrapFile: "/tmp/file.json"
-maxJoinDuration: 30s`
+	s.NoError(validateRingpopConfig(&cfg))
+	cfg.BroadcastAddress = "sjhdfskdjhf"
+	s.Error(validateRingpopConfig(&cfg))
 }
 
 func getHostsConfig() string {
 	return `name: "test"
-bootstrapMode: "hosts"
-bootstrapHosts: ["127.0.0.1:1111"]
-maxJoinDuration: 30s`
-}
-
-func getCustomConfig() string {
-	return `name: "test"
-bootstrapMode: "custom"
-maxJoinDuration: 30s`
-}
-
-func getDNSConfig() string {
-	return `name: "test"
-bootstrapMode: "dns"
-bootstrapHosts:
-- example.net:1111
-- example.net:1112
-- unknown-duplicate.example.net:1111
-- unknown-duplicate.example.net:1111
-- badhostport
+broadcastAddress: "1.2.3.4"
 maxJoinDuration: 30s`
 }
