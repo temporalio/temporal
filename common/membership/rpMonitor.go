609e999c28f35dc4b2e7bef93ca8f3526c218200
Venkat <venkat@uber.com>
2020-03-24 16:22:49 -0700

    ringpop: add method to selfEvict from ring (#3132)

--- common/membership/rpMonitor.go
+++ common/membership/rpMonitor.go
@@ -115,6 +115,10 @@ func (rpo *ringpopMonitor) WhoAmI() (*HostInfo, error) {
 	return NewHostInfo(address, labels.AsMap()), nil
 }
 
+func (rpo *ringpopMonitor) EvictSelf() error {
+	return rpo.rp.SelfEvict()
+}
+
 func (rpo *ringpopMonitor) GetResolver(service string) (ServiceResolver, error) {
 	ring, found := rpo.rings[service]
 	if !found {
