198775c873d67f81fc763fe0ef3ad9c69470bbb8
Samar Abbas <samar@temporal.io>
2020-04-18 19:45:47 -0700

    fix imports

--- service/history/queueProcessor_test.go
+++ service/history/queueProcessor_test.go
@@ -32,9 +32,9 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
-	"github.com/uber/cadence/common/collection"
-	"github.com/uber/cadence/common/log"
-	"github.com/uber/cadence/common/log/loggerimpl"
+	"github.com/temporalio/temporal/common/collection"
+	"github.com/temporalio/temporal/common/log"
+	"github.com/temporalio/temporal/common/log/loggerimpl"
 )
 
 type (
