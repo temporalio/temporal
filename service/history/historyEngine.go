hanzoai/tasks

Add feature flags for RPC replication migration (#3216)

service/history/historyEngine.go
@@ -309,7 +309,9 @@ func (e *historyEngineImpl) Start() {
 	e.timerProcessor.Start()
 
 	clusterMetadata := e.shard.GetClusterMetadata()
-	if e.replicatorProcessor != nil && clusterMetadata.GetReplicationConsumerConfig().Type != sconfig.ReplicationConsumerTypeRPC {
+	if e.replicatorProcessor != nil &&
+		clusterMetadata.GetReplicationConsumerConfig().Type != sconfig.ReplicationConsumerTypeRPC &&
+		e.config.EnableKafkaReplication() {
 		e.replicatorProcessor.Start()
 	}
 

temporalio/temporal

service/history/historyEngine.go
@@ -355,7 +355,9 @@ func (e *historyEngineImpl) Start() {
 	e.timerProcessor.Start()
 
 	clusterMetadata := e.shard.GetClusterMetadata()
-	if e.replicatorProcessor != nil && clusterMetadata.GetReplicationConsumerConfig().Type != config.ReplicationConsumerTypeRPC {
+	if e.replicatorProcessor != nil &&
+		clusterMetadata.GetReplicationConsumerConfig().Type != config.ReplicationConsumerTypeRPC &&
+		e.config.EnableKafkaReplication() {
 		e.replicatorProcessor.Start()
 	}
 