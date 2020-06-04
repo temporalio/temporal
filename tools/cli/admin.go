8aa47f47e64a7e8a65c6f70bb9ffbe1ad12e3873
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2020-06-03 17:26:50 -0700

    Add consistency to cli admin command names (#427)
    Co-authored-by: Shawn Hathaway <shawn@temporal.io>

--- tools/cli/admin.go
+++ tools/cli/admin.go
@@ -74,7 +74,7 @@ func newAdminWorkflowCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "refresh-tasks",
+			Name:    "refresh_tasks",
 			Aliases: []string{"rt"},
 			Usage:   "Refreshes all the tasks of a workflow",
 			Flags: []cli.Flag{
@@ -138,7 +138,7 @@ func newAdminShardManagementCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "describe-task",
+			Name:    "describe_task",
 			Aliases: []string{"dt"},
 			Usage:   "Describe a task based on task Id, task type, shard Id and task visibility timestamp",
 			Flags: append(
@@ -172,7 +172,7 @@ func newAdminShardManagementCommands() []cli.Command {
 		},
 
 		{
-			Name:    "closeShard",
+			Name:    "close_shard",
 			Aliases: []string{"clsh"},
 			Usage:   "close a shard given a shard id",
 			Flags: []cli.Flag{
@@ -186,7 +186,7 @@ func newAdminShardManagementCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "removeTask",
+			Name:    "remove_task",
 			Aliases: []string{"rmtk"},
 			Usage:   "remove a task based on shardId, task type, taskId, and task visibility timestamp",
 			Flags: []cli.Flag{
@@ -269,7 +269,7 @@ func newAdminHistoryHostCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "getshard",
+			Name:    "get_shardid",
 			Aliases: []string{"gsh"},
 			Usage:   "Get shardId for a workflowId",
 			Flags: []cli.Flag{
@@ -319,7 +319,7 @@ func newAdminNamespaceCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "getnamespaceidorname",
+			Name:    "get_namespaceidorname",
 			Aliases: []string{"getdn"},
 			Usage:   "Get namespaceId or namespace",
 			Flags: append(getDBFlags(),
@@ -380,7 +380,7 @@ func newAdminKafkaCommands() []cli.Command {
 			},
 		},
 		{
-			Name:    "purgeTopic",
+			Name:    "purge_topic",
 			Aliases: []string{"purge"},
 			Usage:   "purge Kafka topic by consumer group",
 			Flags: []cli.Flag{
@@ -416,7 +416,7 @@ clusters:
 			},
 		},
 		{
-			Name:    "mergeDLQ",
+			Name:    "merge_dlq",
 			Aliases: []string{"mgdlq"},
 			Usage:   "Merge replication tasks to target topic(from input file or DLQ topic)",
 			Flags: []cli.Flag{
@@ -674,7 +674,7 @@ func newAdminTaskListCommands() []cli.Command {
 func newAdminClusterCommands() []cli.Command {
 	return []cli.Command{
 		{
-			Name:    "add-search-attr",
+			Name:    "add_search_attr",
 			Aliases: []string{"asa"},
 			Usage:   "whitelist search attribute",
 			Flags: []cli.Flag{
