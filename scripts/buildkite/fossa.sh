f333dff5e3bddb65d7bb6e2ca6146906354bf5b8
Liang Mei <meiliang86@gmail.com>
2020-06-03 17:57:33 -0700

    Fix fossa script (#3312)

--- scripts/buildkite/fossa.sh
+++ scripts/buildkite/fossa.sh
@@ -4,7 +4,6 @@ set -exo pipefail
 
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash -s -- -b ~/
 
-~/fossa init
 ~/fossa analyze
 
 # Capture the exit status
