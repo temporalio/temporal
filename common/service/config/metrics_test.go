d3f789987903569d3600adf5a4d6bae3ba33c6ad
Alex Shtin <alex@shtin.com>
2020-06-15 17:54:13 -0700

    Fix tests.

--- common/service/config/metrics_test.go
+++ common/service/config/metrics_test.go
@@ -62,7 +62,7 @@ func (s *MetricsSuite) TestStatsd() {
 }
 
 func (s *MetricsSuite) TestM3() {
-	m3 := &m3.Config{
+	m3 := &m3.Configuration{
 		HostPort: "127.0.0.1:8125",
 		Service:  "testM3",
 		Env:      "devel",
@@ -74,7 +74,7 @@ func (s *MetricsSuite) TestM3() {
 }
 
 func (s *MetricsSuite) TestPrometheus() {
-	prom := &prometheus.Config{
+	prom := &prometheus.Configuration{
 		OnError:       "panic",
 		TimerType:     "histogram",
 		ListenAddress: "127.0.0.1:0",
