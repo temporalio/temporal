c27734c67eff5326c45c360edf77003fac928024
Yu Xia <yx@uber.com>
2020-07-06 14:56:34 -0700

    Fix start workflow execution expiration time (#3371)

---
+++ common/util_test.go
@@ -0,0 +1,68 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package common
+
+import (
+	"testing"
+	"time"
+
+	"github.com/pborman/uuid"
+	"github.com/stretchr/testify/require"
+
+	workflow "github.com/uber/cadence/.gen/go/shared"
+)
+
+func TestCreateHistoryStartWorkflowRequest_ExpirationTimeWithCron(t *testing.T) {
+	domainId := uuid.New()
+	request := &workflow.StartWorkflowExecutionRequest{
+		RetryPolicy:                         &workflow.RetryPolicy{
+			InitialIntervalInSeconds:    Int32Ptr(60),
+			ExpirationIntervalInSeconds: Int32Ptr(60),
+		},
+		CronSchedule:                        StringPtr("@every 300s"),
+	}
+	now := time.Now()
+	startRequest := CreateHistoryStartWorkflowRequest(domainId, request)
+
+	expirationTime := startRequest.GetExpirationTimestamp()
+	require.NotNil(t, expirationTime)
+	require.True(t, time.Unix(0, expirationTime).Sub(now) > 60 * time.Second)
+}
+
+func TestCreateHistoryStartWorkflowRequest_ExpirationTimeWithoutCron(t *testing.T) {
+	domainId := uuid.New()
+	request := &workflow.StartWorkflowExecutionRequest{
+		RetryPolicy:                         &workflow.RetryPolicy{
+			InitialIntervalInSeconds:    Int32Ptr(60),
+			ExpirationIntervalInSeconds: Int32Ptr(60),
+		},
+	}
+	now := time.Now()
+	startRequest := CreateHistoryStartWorkflowRequest(domainId, request)
+
+	expirationTime := startRequest.GetExpirationTimestamp()
+	require.NotNil(t, expirationTime)
+	delta := time.Unix(0, expirationTime).Sub(now)
+	require.True(t, delta > 58 * time.Second)
+	require.True(t, delta < 62 * time.Second)
+}
--- service/history/task/timer_active_task_executor.go
+++ service/history/task/timer_active_task_executor.go
@@ -500,7 +500,7 @@ func (t *timerActiveTaskExecutor) executeWorkflowTimeoutTask(
 	}
 
 	startAttributes := startEvent.WorkflowExecutionStartedEventAttributes
-	continueAsnewAttributes := &workflow.ContinueAsNewWorkflowExecutionDecisionAttributes{
+	continueAsNewAttributes := &workflow.ContinueAsNewWorkflowExecutionDecisionAttributes{
 		WorkflowType:                        startAttributes.WorkflowType,
 		TaskList:                            startAttributes.TaskList,
 		Input:                               startAttributes.Input,
@@ -519,7 +519,7 @@ func (t *timerActiveTaskExecutor) executeWorkflowTimeoutTask(
 		mutableState,
 		eventBatchFirstEventID,
 		startAttributes.GetParentWorkflowDomain(),
-		continueAsnewAttributes,
+		continueAsNewAttributes,
 	)
 	if err != nil {
 		return err
