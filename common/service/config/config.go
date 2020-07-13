5e5accdb7b764766c6e91e997ca7bd713aa7219a
Manu Srivastava <manu@temporal.io>
2020-07-13 13:30:54 -0700

    cleanup

--- common/service/config/config.go
+++ common/service/config/config.go
@@ -244,16 +244,16 @@ type (
 
 	// CassandraStoreConsistency enables you to set the consistency settings for each Cassandra Persistence Store for Temporal
 	CassandraStoreConsistency struct {
-		// Default defines the consistency level for ALL stores. Each store can be overwritten with optional respective settings below.
+		// Default defines the consistency level for ALL stores.
 		// Defaults to LOCAL_QUORUM and LOCAL_SERIAL if not set
 		Default *CassandraConsistencySettings `yaml:"default"`
 	}
 
 	// CassandraConsistencySettings sets the default consistency level for regular & serial queries to Cassandra.
 	CassandraConsistencySettings struct {
-		// Consistency sets the default consistency level. Values identical to gocql Consistency values. (default: LOCAL_QUORUM).
+		// Consistency sets the default consistency level. Values identical to gocql Consistency values. (defaults to LOCAL_QUORUM if not set).
 		Consistency string `yaml:"consistency"`
-		// SerialConsistency sets the consistency for the serial prtion of queries. Values identical to gocql SerialConsistency values. (default: LOCAL_SERIAL)
+		// SerialConsistency sets the consistency for the serial prtion of queries. Values identical to gocql SerialConsistency values. (defaults to LOCAL_SERIAL if not set)
 		SerialConsistency string `yaml:"serialConsistency"`
 	}
 
