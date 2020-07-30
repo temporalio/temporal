258fbb8348069e7337a050b04e11d07557db6e49
Alex Shtin <alex@shtin.com>
2020-07-30 00:39:04 -0700

    Fix unit test.

--- tools/cli/app_test.go
+++ tools/cli/app_test.go
@@ -592,8 +592,10 @@ func (s *cliAppSuite) TestObserveWorkflowWithID() {
 // TestParseTime tests the parsing of date argument in UTC and UnixNano formats
 func (s *cliAppSuite) TestParseTime() {
 	s.Equal("1978-08-22 00:00:00 +0000 UTC", parseTime("", time.Date(1978, 8, 22, 0, 0, 0, 0, time.UTC), time.Now()).String())
-	s.Equal("2018-06-07 15:04:05 +0000 +0000", parseTime("2018-06-07T15:04:05+00:00", time.Time{}, time.Now()).String())
-	s.Equal("2018-06-07 08:04:05 -0700 PDT", parseTime("1528383845000000000", time.Time{}, time.Now()).String())
+	s.Equal("2018-06-07T15:04:05+07:00", parseTime("2018-06-07T15:04:05+07:00", time.Time{}, time.Now()).Format(time.RFC3339))
+	expected, err := time.Parse(defaultDateTimeFormat, "2018-06-07T15:04:05+07:00")
+	s.NoError(err)
+	s.Equal(expected.Local(), parseTime("1528358645000000000", time.Time{}, time.Now().Local()))
 }
 
 // TestParseTimeDateRange tests the parsing of date argument in time range format, N<duration>
