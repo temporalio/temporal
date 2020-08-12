fc2e1673ad87332dea35574018701589c4f49f22
Yu Xia <yx@uber.com>
2020-08-12 15:41:49 -0700

    Fix shard ID filter (#3451)
    * Fix shard ID fitler

--- common/service/dynamicconfig/constants.go
+++ common/service/dynamicconfig/constants.go
@@ -903,7 +903,7 @@ const (
 type Filter int
 
 func (f Filter) String() string {
-	if f <= unknownFilter || f > TaskType {
+	if f <= unknownFilter || f > ShardID {
 		return filters[unknownFilter]
 	}
 	return filters[f]
