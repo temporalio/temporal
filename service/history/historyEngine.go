1e7b59e007e388d6ea3de2ac687e676a50a25949
Alex Shtin <alex@shtin.com>
2020-09-19 12:15:45 -0700

    Extract workflowMutableStateToJSON to separate file.

--- service/history/historyEngine.go
+++ service/history/historyEngine.go
@@ -29,14 +29,10 @@ package history
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
-	"github.com/gogo/protobuf/jsonpb"
-	"github.com/gogo/protobuf/proto"
 	"github.com/pborman/uuid"
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
@@ -1086,7 +1082,7 @@ func (e *historyEngineImpl) DescribeMutableState(
 	if cacheHit && cacheCtx.(*workflowExecutionContextImpl).mutableState != nil {
 		msb := cacheCtx.(*workflowExecutionContextImpl).mutableState
 		wms := msb.CopyToPersistence()
-		response.CacheMutableState = e.workflowMutableStateToJSON(wms)
+		response.CacheMutableState = workflowMutableStateToJSON(wms)
 	}
 
 	msb, err := dbCtx.loadWorkflowExecution()
@@ -1095,7 +1091,7 @@ func (e *historyEngineImpl) DescribeMutableState(
 		return response, nil
 	}
 	wms := msb.CopyToPersistence()
-	response.DatabaseMutableState = e.workflowMutableStateToJSON(wms)
+	response.DatabaseMutableState = workflowMutableStateToJSON(wms)
 
 	currentBranchToken := wms.ExecutionInfo.EventBranchToken
 	if wms.VersionHistories != nil {
@@ -1118,132 +1114,6 @@ func (e *historyEngineImpl) DescribeMutableState(
 	response.BranchId = branchInfo.BranchId
 
 	return response, nil
-}
-
-func (e *historyEngineImpl) workflowMutableStateToJSON(wms *persistence.WorkflowMutableState) string {
-	// TODO: call @alexshtin before you dig!
-	// This is a _temporarry_ workaround to make JSON serialization work.
-	// This code will be removed soon and MutableState will be serialized to proto.
-
-	jsonMarshal := func(v interface{}) string {
-		if pb, ok := v.(proto.Message); ok {
-			m := jsonpb.Marshaler{}
-			var buf bytes.Buffer
-			err := m.Marshal(&buf, pb)
-			if err != nil {
-				return fmt.Sprintf(`{"error": "%s"}`, err.Error())
-			}
-			return string(buf.Bytes())
-		}
-
-		b, err := json.Marshal(v)
-		if err != nil {
-			return fmt.Sprintf(`{"error": "%s"}`, err.Error())
-		}
-		return string(b)
-	}
-
-	var i int
-	var sb strings.Builder
-	sb.WriteString("{")
-
-	// ActivityInfos
-	sb.WriteString(`"ActivityInfos":`)
-	sb.WriteString("{")
-	i = 1
-	for key, item := range wms.ActivityInfos {
-		sb.WriteString(fmt.Sprintf(`"%d":%s`, key, jsonMarshal(item)))
-		if i != len(wms.ActivityInfos) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("},")
-
-	// TimerInfos
-	sb.WriteString(`"TimerInfos":`)
-	sb.WriteString("{")
-	i = 1
-	for key, item := range wms.TimerInfos {
-		sb.WriteString(fmt.Sprintf(`"%s":%s`, key, jsonMarshal(item)))
-		if i != len(wms.TimerInfos) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("},")
-
-	// ChildExecutionInfos
-	sb.WriteString(`"ChildExecutionInfos":`)
-	sb.WriteString("{")
-	i = 1
-	for key, item := range wms.ChildExecutionInfos {
-		sb.WriteString(fmt.Sprintf(`"%d":%s`, key, jsonMarshal(item)))
-		if i != len(wms.ChildExecutionInfos) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("},")
-
-	// RequestCancelInfos
-	sb.WriteString(`"RequestCancelInfos":`)
-	sb.WriteString("{")
-	i = 1
-	for key, item := range wms.RequestCancelInfos {
-		sb.WriteString(fmt.Sprintf(`"%d":%s`, key, jsonMarshal(item)))
-		if i != len(wms.RequestCancelInfos) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("},")
-
-	// SignalInfos
-	sb.WriteString(`"SignalInfos":`)
-	sb.WriteString("{")
-	i = 1
-	for key, item := range wms.SignalInfos {
-		sb.WriteString(fmt.Sprintf(`"%d":%s`, key, jsonMarshal(item)))
-		if i != len(wms.SignalInfos) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("},")
-
-	// SignalRequestedIDs
-	sb.WriteString(fmt.Sprintf(`"SignalRequestedIds":%s,`, jsonMarshal(wms.SignalRequestedIDs)))
-
-	// ExecutionInfo
-	sb.WriteString(fmt.Sprintf(`"ExecutionInfo":%s,`, jsonMarshal(wms.ExecutionInfo)))
-
-	// ExecutionStats
-	sb.WriteString(fmt.Sprintf(`"ExecutionStats":%s,`, jsonMarshal(wms.ExecutionStats)))
-
-	// BufferedEvents
-	sb.WriteString(`"BufferedEvents":`)
-	sb.WriteString("[")
-	i = 1
-	for _, item := range wms.BufferedEvents {
-		sb.WriteString(jsonMarshal(item))
-		if i != len(wms.BufferedEvents) {
-			sb.WriteString(",")
-		}
-		i++
-	}
-	sb.WriteString("],")
-
-	// VersionHistories
-	sb.WriteString(fmt.Sprintf(`"VersionHistories":%s,`, jsonMarshal(wms.VersionHistories)))
-
-	// Checksum
-	sb.WriteString(fmt.Sprintf(`"Checksum":%s`, jsonMarshal(wms.Checksum)))
-
-	// Close root object
-	sb.WriteString("}")
-
-	return sb.String()
 }
 
 // ResetStickyTaskQueue reset the volatile information in mutable state of a given workflow.
