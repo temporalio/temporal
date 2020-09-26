hanzoai/tasks

[SQL]Fix upsert SQL template for Postgres plugin (#3498)

common/persistence/persistence-tests/executionManagerTest.go
@@ -724,6 +724,107 @@ func (s *ExecutionManagerSuite) TestCreateWorkflowExecutionBrandNew() {
 	s.Equal(p.WorkflowStateRunning, alreadyStartedErr.State)
 }
 
+// TestUpsertWorkflowActivity test
+func (s *ExecutionManagerSuite) TestUpsertWorkflowActivity() {
+	domainID := uuid.New()
+	workflowID := "create-workflow-test-with-upsert-activity"
+	workflowExecution := gen.WorkflowExecution{
+		WorkflowId: common.StringPtr(workflowID),
+		RunId:      common.StringPtr(uuid.New()),
+	}
+	tasklist := "some random tasklist"
+	workflowType := "some random workflow type"
+	workflowTimeout := int32(10)
+	decisionTimeout := int32(14)
+	lastProcessedEventID := int64(0)
+	nextEventID := int64(3)
+	csum := s.newRandomChecksum()
+
+	// create and update a workflow to make it completed
+	req := &p.CreateWorkflowExecutionRequest{
+		NewWorkflowSnapshot: p.WorkflowSnapshot{
+			ExecutionInfo: &p.WorkflowExecutionInfo{
+				CreateRequestID:             uuid.New(),
+				DomainID:                    domainID,
+				WorkflowID:                  workflowExecution.GetWorkflowId(),
+				RunID:                       workflowExecution.GetRunId(),
+				TaskList:                    tasklist,
+				WorkflowTypeName:            workflowType,
+				WorkflowTimeout:             workflowTimeout,
+				DecisionStartToCloseTimeout: decisionTimeout,
+				NextEventID:                 nextEventID,
+				LastProcessedEvent:          lastProcessedEventID,
+				State:                       p.WorkflowStateRunning,
+				CloseStatus:                 p.WorkflowCloseStatusNone,
+			},
+			ExecutionStats: &p.ExecutionStats{},
+			Checksum:       csum,
+		},
+		RangeID: s.ShardInfo.RangeID,
+		Mode:    p.CreateWorkflowModeBrandNew,
+	}
+	_, err := s.ExecutionManager.CreateWorkflowExecution(req)
+	s.Nil(err)
+	currentRunID, err := s.GetCurrentWorkflowRunID(domainID, workflowID)
+	s.Nil(err)
+	s.Equal(workflowExecution.GetRunId(), currentRunID)
+
+	info, err := s.GetWorkflowExecutionInfo(domainID, workflowExecution)
+	s.Nil(err)
+	s.assertChecksumsEqual(csum, info.Checksum)
+	s.Equal(0, len(info.ActivityInfos))
+
+	// insert a new activity
+	updatedInfo := copyWorkflowExecutionInfo(info.ExecutionInfo)
+	updateStats := copyExecutionStats(info.ExecutionStats)
+	_, err = s.ExecutionManager.UpdateWorkflowExecution(&p.UpdateWorkflowExecutionRequest{
+		UpdateWorkflowMutation: p.WorkflowMutation{
+			ExecutionInfo:  updatedInfo,
+			ExecutionStats: updateStats,
+			Condition:      nextEventID,
+			Checksum:       csum,
+			UpsertActivityInfos: []*p.ActivityInfo{
+				&p.ActivityInfo{
+					Version: 0,
+					ScheduleID: 100,
+					TaskList: "test-activity-tasktlist-1",
+				},
+			},
+		},
+		RangeID: s.ShardInfo.RangeID,
+		Mode:    p.UpdateWorkflowModeUpdateCurrent,
+	})
+	s.Nil(err)
+
+	info2, err := s.GetWorkflowExecutionInfo(domainID, workflowExecution)
+	s.Nil(err)
+	s.Equal(1, len(info2.ActivityInfos))
+	s.Equal("test-activity-tasktlist-1", info2.ActivityInfos[100].TaskList)
+
+	// upsert the previous activity
+	_, err = s.ExecutionManager.UpdateWorkflowExecution(&p.UpdateWorkflowExecutionRequest{
+		UpdateWorkflowMutation: p.WorkflowMutation{
+			ExecutionInfo:  updatedInfo,
+			ExecutionStats: updateStats,
+			Condition:      nextEventID,
+			Checksum:       csum,
+			UpsertActivityInfos: []*p.ActivityInfo{
+				&p.ActivityInfo{
+					Version: 0,
+					ScheduleID: 100,
+					TaskList: "test-activity-tasktlist-2",
+				},
+			},
+		},
+		RangeID: s.ShardInfo.RangeID,
+		Mode:    p.UpdateWorkflowModeUpdateCurrent,
+	})
+	info3, err := s.GetWorkflowExecutionInfo(domainID, workflowExecution)
+	s.Nil(err)
+	s.Equal(1, len(info3.ActivityInfos))
+	s.Equal("test-activity-tasktlist-2", info3.ActivityInfos[100].TaskList)
+}
+
 // TestCreateWorkflowExecutionRunIDReuseWithReplication test
 func (s *ExecutionManagerSuite) TestCreateWorkflowExecutionRunIDReuseWithReplication() {
 	domainID := uuid.New()
@@ -1671,7 +1772,8 @@ func (s *ExecutionManagerSuite) TestDeleteWorkflow() {
 // TestDeleteCurrentWorkflow test
 func (s *ExecutionManagerSuite) TestDeleteCurrentWorkflow() {
 	if s.ExecutionManager.GetName() != "cassandra" {
-		s.T().Skip("this test is only applicable for cassandra (uses TTL based deletes)")
+		//"this test is only applicable for cassandra (uses TTL based deletes)"
+		return
 	}
 	domainID := "54d15308-e20e-4b91-a00f-a518a3892790"
 	workflowExecution := gen.WorkflowExecution{

common/persistence/sql/sqlplugin/postgres/execution_maps.go
@@ -39,6 +39,8 @@ run_id = $4`
 	// %[2]v is the columns of the value struct (i.e. no primary key columns), comma separated
 	// %[3]v should be %[2]v with colons prepended.
 	// i.e. %[3]v = ",".join(":" + s for s in %[2]v)
+	// %[5]v should be %[2]v with "excluded." prepended.
+	// i.e. %[5]v = ",".join("excluded." + s for s in %[2]v)
 	// So that this query can be used with BindNamed
 	// %[4]v should be the name of the key associated with the map
 	// e.g. for ActivityInfo it is "schedule_id"
@@ -46,12 +48,9 @@ run_id = $4`
 (shard_id, domain_id, workflow_id, run_id, %[4]v, %[2]v)
 VALUES
 (:shard_id, :domain_id, :workflow_id, :run_id, :%[4]v, %[3]v)
-ON CONFLICT (shard_id, domain_id, workflow_id, run_id, %[4]v) DO UPDATE 
-  SET shard_id = excluded.shard_id,
-      domain_id = excluded.domain_id,
-      workflow_id = excluded.workflow_id,
-	  run_id = excluded.run_id,
-      %[4]v = excluded.%[4]v `
+ON CONFLICT (shard_id, domain_id, workflow_id, run_id, %[4]v) DO UPDATE
+	SET (shard_id, domain_id, workflow_id, run_id, %[4]v, %[2]v)
+  	  = (excluded.shard_id, excluded.domain_id, excluded.workflow_id, excluded.run_id, excluded.%[4]v, %[5]v)`
 
 	// %[2]v is the name of the key
 	deleteKeyInMapQueryTemplate = `DELETE FROM %[1]v
@@ -121,7 +120,10 @@ func makeSetKeyInMapQry(tableName string, nonPrimaryKeyColumns []string, mapKeyN
 		strings.Join(stringMap(nonPrimaryKeyColumns, func(x string) string {
 			return ":" + x
 		}), ","),
-		mapKeyName)
+		mapKeyName,
+		strings.Join(stringMap(nonPrimaryKeyColumns, func(x string) string {
+			return "excluded." + x
+		}), ","))
 }
 
 func makeDeleteKeyInMapQry(tableName string, mapKeyName string) string {

temporalio/temporal

common/persistence/persistence-tests/executionManagerTest.go
@@ -751,6 +751,109 @@ func (s *ExecutionManagerSuite) TestCreateWorkflowExecutionBrandNew() {
 	s.Equal(enumsspb.WORKFLOW_EXECUTION_STATE_RUNNING, alreadyStartedErr.State)
 }
 
+// TestUpsertWorkflowActivity test
+func (s *ExecutionManagerSuite) TestUpsertWorkflowActivity() {
+	namespaceID := uuid.New()
+	workflowID := "create-workflow-test-with-upsert-activity"
+	workflowExecution := commonpb.WorkflowExecution{
+		WorkflowId: workflowID,
+		RunId:      uuid.New(),
+	}
+	taskqueue := "some random tasklist"
+	workflowType := "some random workflow type"
+	workflowTimeout := timestamp.DurationFromSeconds(10)
+	workflowTaskTimeout := timestamp.DurationFromSeconds(14)
+	lastProcessedEventID := int64(0)
+	nextEventID := int64(3)
+	csum := s.newRandomChecksum()
+
+	// create and update a workflow to make it completed
+	req := &p.CreateWorkflowExecutionRequest{
+		NewWorkflowSnapshot: p.WorkflowSnapshot{
+			ExecutionInfo: &p.WorkflowExecutionInfo{
+				NamespaceId:                namespaceID,
+				WorkflowId:                 workflowExecution.GetWorkflowId(),
+				TaskQueue:                  taskqueue,
+				WorkflowTypeName:           workflowType,
+				WorkflowRunTimeout:         workflowTimeout,
+				DefaultWorkflowTaskTimeout: workflowTaskTimeout,
+				ExecutionState: &persistenceblobs.WorkflowExecutionState{
+					RunId:           workflowExecution.GetRunId(),
+					CreateRequestId: uuid.New(),
+					State:           enumsspb.WORKFLOW_EXECUTION_STATE_RUNNING,
+					Status:          enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING,
+				},
+				LastFirstEventId:   common.FirstEventID,
+				NextEventId:        nextEventID,
+				LastProcessedEvent: lastProcessedEventID,
+			},
+			ExecutionStats: &persistenceblobs.ExecutionStats{},
+		},
+		RangeID: s.ShardInfo.GetRangeId(),
+		Mode:    p.CreateWorkflowModeBrandNew,
+	}
+	_, err := s.ExecutionManager.CreateWorkflowExecution(req)
+	s.Nil(err)
+	currentRunID, err := s.GetCurrentWorkflowRunID(namespaceID, workflowID)
+	s.Nil(err)
+	s.Equal(workflowExecution.GetRunId(), currentRunID)
+
+	info, err := s.GetWorkflowExecutionInfo(namespaceID, workflowExecution)
+	s.Nil(err)
+	s.assertChecksumsEqual(csum, info.Checksum)
+	s.Equal(0, len(info.ActivityInfos))
+
+	// insert a new activity
+	updatedInfo := copyWorkflowExecutionInfo(info.ExecutionInfo)
+	updateStats := copyExecutionStats(info.ExecutionStats)
+	_, err = s.ExecutionManager.UpdateWorkflowExecution(&p.UpdateWorkflowExecutionRequest{
+		UpdateWorkflowMutation: p.WorkflowMutation{
+			ExecutionInfo:  updatedInfo,
+			ExecutionStats: updateStats,
+			Condition:      nextEventID,
+			Checksum:       csum,
+			UpsertActivityInfos: []*persistenceblobs.ActivityInfo{
+				{
+					Version:    0,
+					ScheduleId: 100,
+					TaskQueue:  "test-activity-tasktlist-1",
+				},
+			},
+		},
+		RangeID: s.ShardInfo.RangeId,
+		Mode:    p.UpdateWorkflowModeUpdateCurrent,
+	})
+	s.Nil(err)
+
+	info2, err := s.GetWorkflowExecutionInfo(namespaceID, workflowExecution)
+	s.Nil(err)
+	s.Equal(1, len(info2.ActivityInfos))
+	s.Equal("test-activity-tasktlist-1", info2.ActivityInfos[100].TaskQueue)
+
+	// upsert the previous activity
+	_, err = s.ExecutionManager.UpdateWorkflowExecution(&p.UpdateWorkflowExecutionRequest{
+		UpdateWorkflowMutation: p.WorkflowMutation{
+			ExecutionInfo:  updatedInfo,
+			ExecutionStats: updateStats,
+			Condition:      nextEventID,
+			Checksum:       csum,
+			UpsertActivityInfos: []*persistenceblobs.ActivityInfo{
+				{
+					Version:    0,
+					ScheduleId: 100,
+					TaskQueue:  "test-activity-tasktlist-2",
+				},
+			},
+		},
+		RangeID: s.ShardInfo.RangeId,
+		Mode:    p.UpdateWorkflowModeUpdateCurrent,
+	})
+	info3, err := s.GetWorkflowExecutionInfo(namespaceID, workflowExecution)
+	s.Nil(err)
+	s.Equal(1, len(info3.ActivityInfos))
+	s.Equal("test-activity-tasktlist-2", info3.ActivityInfos[100].TaskQueue)
+}
+
 // TestCreateWorkflowExecutionRunIDReuseWithoutReplication test
 func (s *ExecutionManagerSuite) TestCreateWorkflowExecutionRunIDReuseWithoutReplication() {
 	namespaceID := uuid.New()
@@ -1385,7 +1488,8 @@ func (s *ExecutionManagerSuite) TestDeleteWorkflow() {
 // TestDeleteCurrentWorkflow test
 func (s *ExecutionManagerSuite) TestDeleteCurrentWorkflow() {
 	if s.ExecutionManager.GetName() != "cassandra" {
-		s.T().Skip("this test is only applicable for cassandra (uses TTL based deletes)")
+		// "this test is only applicable for cassandra (uses TTL based deletes)"
+		return
 	}
 	namespaceID := "54d15308-e20e-4b91-a00f-a518a3892790"
 	workflowExecution := commonpb.WorkflowExecution{

common/persistence/sql/sqlplugin/postgres/execution_maps.go
@@ -43,6 +43,8 @@ run_id = $4`
 	// %[2]v is the columns of the value struct (i.e. no primary key columns), comma separated
 	// %[3]v should be %[2]v with colons prepended.
 	// i.e. %[3]v = ",".join(":" + s for s in %[2]v)
+	// %[5]v should be %[2]v with "excluded." prepended.
+	// i.e. %[5]v = ",".join("excluded." + s for s in %[2]v)
 	// So that this query can be used with BindNamed
 	// %[4]v should be the name of the key associated with the map
 	// e.g. for ActivityInfo it is "schedule_id"
@@ -50,12 +52,9 @@ run_id = $4`
 (shard_id, namespace_id, workflow_id, run_id, %[4]v, %[2]v)
 VALUES
 (:shard_id, :namespace_id, :workflow_id, :run_id, :%[4]v, %[3]v)
-ON CONFLICT (shard_id, namespace_id, workflow_id, run_id, %[4]v) DO UPDATE 
-  SET shard_id = excluded.shard_id,
-      namespace_id = excluded.namespace_id,
-      workflow_id = excluded.workflow_id,
-	  run_id = excluded.run_id,
-      %[4]v = excluded.%[4]v `
+ON CONFLICT (shard_id, namespace_id, workflow_id, run_id, %[4]v) DO UPDATE
+	SET (shard_id, namespace_id, workflow_id, run_id, %[4]v, %[2]v)
+  	  = (excluded.shard_id, excluded.namespace_id, excluded.workflow_id, excluded.run_id, excluded.%[4]v, %[5]v)`
 
 	// %[2]v is the name of the key
 	deleteKeyInMapQueryTemplate = `DELETE FROM %[1]v
@@ -125,7 +124,10 @@ func makeSetKeyInMapQry(tableName string, nonPrimaryKeyColumns []string, mapKeyN
 		strings.Join(stringMap(nonPrimaryKeyColumns, func(x string) string {
 			return ":" + x
 		}), ","),
-		mapKeyName)
+		mapKeyName,
+		strings.Join(stringMap(nonPrimaryKeyColumns, func(x string) string {
+			return "excluded." + x
+		}), ","))
 }
 
 func makeDeleteKeyInMapQry(tableName string, mapKeyName string) string {