hanzoai/tasks

Update replication path to use lazy retry on service busy error (#3376)

service/history/historyEngine.go
@@ -36,6 +36,7 @@ import (
 	m "github.com/uber/cadence/.gen/go/matching"
 	r "github.com/uber/cadence/.gen/go/replicator"
 	workflow "github.com/uber/cadence/.gen/go/shared"
+	"github.com/uber/cadence/client/admin"
 	hc "github.com/uber/cadence/client/history"
 	"github.com/uber/cadence/client/matching"
 	"github.com/uber/cadence/common"
@@ -287,11 +288,25 @@ func NewEngineWithShardContext(
 	replicationTaskExecutors := make(map[string]replication.TaskExecutor)
 	for _, replicationTaskFetcher := range replicationTaskFetchers.GetFetchers() {
 		sourceCluster := replicationTaskFetcher.GetSourceCluster()
+		// Intentionally use the raw client to create its own retry policy
+		adminClient := shard.GetService().GetClientBean().GetRemoteAdminClient(sourceCluster)
+		adminRetryableClient := admin.NewRetryableClient(
+			adminClient,
+			common.CreateReplicationServiceBusyRetryPolicy(),
+			common.IsServiceBusyError,
+		)
+		// Intentionally use the raw client to create its own retry policy
+		historyClient := shard.GetService().GetClientBean().GetHistoryClient()
+		historyRetryableClient := hc.NewRetryableClient(
+			historyClient,
+			common.CreateReplicationServiceBusyRetryPolicy(),
+			common.IsServiceBusyError,
+		)
 		nDCHistoryResender := xdc.NewNDCHistoryResender(
 			shard.GetDomainCache(),
-			shard.GetService().GetClientBean().GetRemoteAdminClient(sourceCluster),
+			adminRetryableClient,
 			func(ctx context.Context, request *h.ReplicateEventsV2Request) error {
-				return shard.GetService().GetHistoryClient().ReplicateEventsV2(ctx, request)
+				return historyRetryableClient.ReplicateEventsV2(ctx, request)
 			},
 			shard.GetService().GetPayloadSerializer(),
 			nil,
@@ -300,9 +315,9 @@ func NewEngineWithShardContext(
 		historyRereplicator := xdc.NewHistoryRereplicator(
 			currentClusterName,
 			shard.GetDomainCache(),
-			shard.GetService().GetClientBean().GetRemoteAdminClient(sourceCluster),
+			adminRetryableClient,
 			func(ctx context.Context, request *h.ReplicateRawEventsRequest) error {
-				return shard.GetService().GetHistoryClient().ReplicateRawEvents(ctx, request)
+				return historyRetryableClient.ReplicateRawEvents(ctx, request)
 			},
 			shard.GetService().GetPayloadSerializer(),
 			replicationTimeout,

temporalio/temporal

service/history/historyEngine.go
@@ -50,6 +50,7 @@ import (
 	"go.temporal.io/server/api/persistenceblobs/v1"
 	replicationspb "go.temporal.io/server/api/replication/v1"
 	workflowspb "go.temporal.io/server/api/workflow/v1"
+	"go.temporal.io/server/client/admin"
 	"go.temporal.io/server/client/history"
 	"go.temporal.io/server/client/matching"
 	"go.temporal.io/server/common"
@@ -293,11 +294,26 @@ func NewEngineWithShardContext(
 	replicationTaskExecutors := make(map[string]replicationTaskExecutor)
 	for _, replicationTaskFetcher := range replicationTaskFetchers.GetFetchers() {
 		sourceCluster := replicationTaskFetcher.GetSourceCluster()
+		// Intentionally use the raw client to create its own retry policy
+		adminClient := shard.GetService().GetClientBean().GetRemoteAdminClient(sourceCluster)
+		adminRetryableClient := admin.NewRetryableClient(
+			adminClient,
+			common.CreateReplicationServiceBusyRetryPolicy(),
+			common.IsResourceExhausted,
+		)
+		// Intentionally use the raw client to create its own retry policy
+		historyClient := shard.GetService().GetClientBean().GetHistoryClient()
+		historyRetryableClient := history.NewRetryableClient(
+			historyClient,
+			common.CreateReplicationServiceBusyRetryPolicy(),
+			common.IsResourceExhausted,
+		)
 		nDCHistoryResender := xdc.NewNDCHistoryResender(
 			shard.GetNamespaceCache(),
-			shard.GetService().GetClientBean().GetRemoteAdminClient(currentClusterName),
+			adminRetryableClient,
 			func(ctx context.Context, request *historyservice.ReplicateEventsV2Request) error {
-				return historyEngImpl.ReplicateEventsV2(ctx, request)
+				_, err := historyRetryableClient.ReplicateEventsV2(ctx, request)
+				return err
 			},
 			shard.GetService().GetPayloadSerializer(),
 			shard.GetConfig().StandbyTaskReReplicationContextTimeout,