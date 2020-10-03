bf584eaa0b53cdd3425e25b0db1d8e995a45d6a1
wxing1292 <wxing1292@users.noreply.github.com>
2020-10-02 17:13:55 -0700

    Update common/shuffle/shuffle.go
    Co-authored-by: Alex Shtin <alex@shtin.com>

--- common/shuffle/shuffle.go
+++ common/shuffle/shuffle.go
@@ -17,14 +17,8 @@ func Bytes(slice []byte) []byte {
 	result := make([]byte, len(slice))
 	copy(result, slice)
 
-LoopSwap:
-	for i := 0; i < length; i++ {
-		index := rand.Intn(i + 1)
-		if i == index {
-			continue LoopSwap
-		}
-
-		result[i], result[index] = result[index], result[i]
-	}
+	rand.Shuffle(len(result), func(i, j int) {
+		result[i], result[j] = result[j], result[i]
+	})
 	return result
 }
