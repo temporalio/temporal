b121a080cc9b29b8881662b16d90b2ef8157fe0f
Alex Shtin <alex@shtin.com>
2020-10-12 22:33:11 -0700

    Fix --services flag format

--- docker/start-temporal.sh
+++ docker/start-temporal.sh
@@ -4,4 +4,4 @@ set -ex
 
 dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/config/docker.yaml
 
-exec temporal-server --root $TEMPORAL_HOME --env docker start --services=$SERVICES
+exec temporal-server --root $TEMPORAL_HOME --env docker start $SERVICES
