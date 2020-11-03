8ddc17a00473217cf11f2fc6f84f5b30ac6ff42b
wenquan xing <wenquan.xing@gmail.com>
2020-11-03 12:54:22 -0800

    debug reset

--- host/resetworkflow_test.go
+++ host/resetworkflow_test.go
@@ -204,13 +204,11 @@ func (s *integrationSuite) TestResetWorkflow() {
 		MaximumPageSize: 1000,
 	})
 	s.NoError(err)
-	fmt.Println("#######")
 	prettyPrint := func(input interface{}) string {
 		binary, _ := json.Marshal(input)
 		return string(binary)
 	}
 	fmt.Printf("%v\n", prettyPrint(historyResponse.History))
-	fmt.Println("#######")
 	fmt.Println("###### @@ ######")
 
 	_, err = poller.PollAndProcessWorkflowTask(true, false)
