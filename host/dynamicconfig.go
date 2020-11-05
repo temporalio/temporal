13278d2edd4023129f1acc7bae564136da25d549
wenquan xing <wenquan.xing@gmail.com>
2020-11-05 11:48:39 -0800

    Update NDC default config

--- host/dynamicconfig.go
+++ host/dynamicconfig.go
@@ -47,10 +47,10 @@ var (
 		dynamicconfig.ReplicationTaskFetcherAggregationInterval:     200 * time.Millisecond,
 		dynamicconfig.ReplicationTaskFetcherErrorRetryWait:          50 * time.Millisecond,
 		dynamicconfig.ReplicationTaskProcessorErrorRetryWait:        time.Millisecond,
-		dynamicconfig.FrontendEnableRPCReplication:                  true,
-		dynamicconfig.HistoryEnableRPCReplication:                   true,
-		dynamicconfig.HistoryEnableKafkaReplication:                 false,
-		dynamicconfig.WorkerEnableRPCReplication:                    true,
+		dynamicconfig.FrontendEnableRPCReplication:                  false,
+		dynamicconfig.HistoryEnableRPCReplication:                   false,
+		dynamicconfig.HistoryEnableKafkaReplication:                 true,
+		dynamicconfig.WorkerEnableRPCReplication:                    false,
 	}
 )
 
