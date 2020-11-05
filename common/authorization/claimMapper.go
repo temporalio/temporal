eb25b69df547a9a01a8cc7bf6db0d12909f96b7d
Sergey Bykov <gh@sbykov.com>
2020-11-05 10:05:42 -0800

    Add ClaimMapper interface and a no-op mapper

---
+++ common/authorization/claimMapper.go
@@ -0,0 +1,51 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package authorization
+
+import (
+	"crypto/x509/pkix"
+)
+
+// Authentication information from subjects JWT token or/and mTLS certificate
+type AuthInfo struct {
+	authToken  string
+	tlsSubject *pkix.Name
+}
+
+// Converts authorization info of a subject into Temporal claims (permissions) for authorization
+type ClaimMapper interface {
+	GetClaims(authInfo AuthInfo) (*Claims, error)
+}
+
+// No-op claim mapper that gives system level admin permission to everybody
+type noopClaimMapper struct{}
+
+func NewNoopClaimMapper() ClaimMapper {
+	return &noopClaimMapper{}
+}
+
+func (a *noopClaimMapper) GetClaims(_ AuthInfo) (*Claims, error) {
+	return &Claims{system: RoleAdmin}, nil
+}
