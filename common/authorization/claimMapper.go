e7596935e2a846d0c3f31ee017f67be951093453
Sergey Bykov <gh@sbykov.com>
2020-11-12 19:00:19 -0800

    Fix tests

--- common/authorization/claimMapper.go
+++ common/authorization/claimMapper.go
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+//go:generate mockgen -copyright_file ../../LICENSE -package $GOPACKAGE -source $GOFILE -destination claim_mapper_mock.go -self_package go.temporal.io/server/common/authorization
+
 package authorization
 
 import (
