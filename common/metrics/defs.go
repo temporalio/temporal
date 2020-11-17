a9eed10c29c7261a9eb2b0ef00c80d7e64d09aaf
wxing1292 <wxing1292@users.noreply.github.com>
2020-11-16 16:09:47 -0800

    Refactor replication task executor (#1001)
    * Refactor replication task executor
    * Rewrite some tests

--- common/metrics/defs.go
+++ common/metrics/defs.go
@@ -1025,8 +1025,6 @@ const (
 	HistoryReplicationTaskScope
 	// HistoryMetadataReplicationTaskScope is the scope used by history metadata task replication processing
 	HistoryMetadataReplicationTaskScope
-	// HistoryReplicationV2TaskScope is the scope used by history task replication processing
-	HistoryReplicationV2TaskScope
 	// SyncShardTaskScope is the scope used by sync shrad information processing
 	SyncShardTaskScope
 	// SyncActivityTaskScope is the scope used by sync activity information processing
@@ -1520,7 +1518,6 @@ var ScopeDefs = map[ServiceIdx]map[int]scopeDefinition{
 		NamespaceReplicationTaskScope:          {operation: "NamespaceReplicationTask"},
 		HistoryReplicationTaskScope:            {operation: "HistoryReplicationTask"},
 		HistoryMetadataReplicationTaskScope:    {operation: "HistoryMetadataReplicationTask"},
-		HistoryReplicationV2TaskScope:          {operation: "HistoryReplicationV2Task"},
 		SyncShardTaskScope:                     {operation: "SyncShardTask"},
 		SyncActivityTaskScope:                  {operation: "SyncActivityTask"},
 		ESProcessorScope:                       {operation: "ESProcessor"},
