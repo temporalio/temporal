ad093b3647b097c3a4ae91436e7747f1f680da96
Sergey Bykov <8248806+sergeybykov@users.noreply.github.com>
2020-12-10 08:18:52 -0800

    Add explicit TLS settings for system workers while also supporting legacy config (#1059)

--- common/rpc/encryption/localStoreCertProvider.go
+++ common/rpc/encryption/localStoreCertProvider.go
@@ -37,15 +37,21 @@ import (
 )
 
 var _ CertProvider = (*localStoreCertProvider)(nil)
+var _ ClientCertProvider = (*localStoreCertProvider)(nil)
 
 type localStoreCertProvider struct {
 	sync.RWMutex
 
-	tlsSettings *config.GroupTLS
+	tlsSettings       *config.GroupTLS
+	workerTLSSettings *config.WorkerTLS
 
 	serverCert *tls.Certificate
+	clientCert *tls.Certificate
 	clientCAs  *x509.CertPool
 	serverCAs  *x509.CertPool
+
+	isLegacyWorkerConfig bool
+	legacyWorkerSettings *config.ClientTLS
 }
 
 func (s *localStoreCertProvider) GetSettings() *config.GroupTLS {
@@ -53,63 +59,8 @@ func (s *localStoreCertProvider) GetSettings() *config.GroupTLS {
 }
 
 func (s *localStoreCertProvider) FetchServerCertificate() (*tls.Certificate, error) {
-	if s.tlsSettings.Server.CertFile == "" && s.tlsSettings.Server.CertData == "" {
-		return nil, nil
-	}
-
-	if s.tlsSettings.Server.CertFile != "" && s.tlsSettings.Server.CertData != "" {
-		return nil, errors.New("Cannot specify both certFile and certData properties")
-	}
-
-	s.RLock()
-	if s.serverCert != nil {
-		defer s.RUnlock()
-		return s.serverCert, nil
-	}
-
-	s.RUnlock()
-	s.Lock()
-	defer s.Unlock()
-
-	if s.serverCert != nil {
-		return s.serverCert, nil
-	}
-
-	var certBytes []byte
-	var keyBytes []byte
-	var err error
-
-	if s.tlsSettings.Server.CertFile != "" {
-		certBytes, err = ioutil.ReadFile(s.tlsSettings.Server.CertFile)
-		if err != nil {
-			return nil, err
-		}
-	} else if s.tlsSettings.Server.CertData != "" {
-		certBytes, err = base64.StdEncoding.DecodeString(s.tlsSettings.Server.CertData)
-		if err != nil {
-			return nil, fmt.Errorf("TLS public certificate could not be decoded: %w", err)
-		}
-	}
-
-	if s.tlsSettings.Server.KeyFile != "" {
-		keyBytes, err = ioutil.ReadFile(s.tlsSettings.Server.KeyFile)
-		if err != nil {
-			return nil, err
-		}
-	} else if s.tlsSettings.Server.KeyData != "" {
-		keyBytes, err = base64.StdEncoding.DecodeString(s.tlsSettings.Server.KeyData)
-		if err != nil {
-			return nil, fmt.Errorf("TLS private key could not be decoded: %w", err)
-		}
-	}
-
-	serverCert, err := tls.X509KeyPair(certBytes, keyBytes)
-	if err != nil {
-		return nil, fmt.Errorf("loading server tls certificate failed: %v", err)
-	}
-
-	s.serverCert = &serverCert
-	return s.serverCert, nil
+	return s.FetchCertificate(&s.serverCert, s.tlsSettings.Server.CertFile, s.tlsSettings.Server.CertData,
+		s.tlsSettings.Server.KeyFile, s.tlsSettings.Server.KeyData)
 }
 
 func (s *localStoreCertProvider) FetchClientCAs() (*x509.CertPool, error) {
@@ -154,8 +105,12 @@ func (s *localStoreCertProvider) FetchClientCAs() (*x509.CertPool, error) {
 	return s.clientCAs, nil
 }
 
-func (s *localStoreCertProvider) FetchServerRootCAsForClient() (*x509.CertPool, error) {
-	if len(s.tlsSettings.Client.RootCAFiles) == 0 && len(s.tlsSettings.Client.RootCAData) == 0 {
+func (s *localStoreCertProvider) FetchServerRootCAsForClient(isWorker bool) (*x509.CertPool, error) {
+	clientSettings := s.getClientTLSSettings(isWorker)
+	rootCAFiles := clientSettings.RootCAFiles
+	rootCAData := clientSettings.RootCAData
+
+	if len(rootCAFiles) == 0 && len(rootCAData) == 0 {
 		return nil, nil
 	}
 
@@ -173,12 +128,12 @@ func (s *localStoreCertProvider) FetchServerRootCAsForClient() (*x509.CertPool, 
 		return s.serverCAs, nil
 	}
 
-	serverCAPoolFromFiles, err := buildCAPoolFromFiles(s.tlsSettings.Client.RootCAFiles)
+	serverCAPoolFromFiles, err := buildCAPoolFromFiles(rootCAFiles)
 	if err != nil {
 		return nil, err
 	}
 
-	serverCAPoolFromData, err := buildCAPoolFromData(s.tlsSettings.Client.RootCAData)
+	serverCAPoolFromData, err := buildCAPoolFromData(rootCAData)
 	if err != nil {
 		return nil, err
 	}
@@ -194,6 +149,105 @@ func (s *localStoreCertProvider) FetchServerRootCAsForClient() (*x509.CertPool, 
 	}
 
 	return s.serverCAs, nil
+}
+
+func (s *localStoreCertProvider) FetchClientCertificate(isWorker bool) (*tls.Certificate, error) {
+	if isWorker {
+		return s.fetchWorkerCertificate()
+	} else {
+		return s.FetchCertificate(&s.clientCert, s.tlsSettings.Server.CertFile, s.tlsSettings.Server.CertData,
+			s.tlsSettings.Server.KeyFile, s.tlsSettings.Server.KeyData)
+	}
+}
+
+func (s *localStoreCertProvider) fetchWorkerCertificate() (*tls.Certificate, error) {
+	if s.isLegacyWorkerConfig {
+		return s.FetchCertificate(&s.clientCert, s.tlsSettings.Server.CertFile, s.tlsSettings.Server.CertData,
+			s.tlsSettings.Server.KeyFile, s.tlsSettings.Server.KeyData)
+	} else {
+		return s.FetchCertificate(&s.clientCert, s.workerTLSSettings.CertFile, s.workerTLSSettings.CertData,
+			s.workerTLSSettings.KeyFile, s.workerTLSSettings.KeyData)
+	}
+}
+
+func (s *localStoreCertProvider) FetchCertificate(cachedCert **tls.Certificate,
+	certFile string, certData string,
+	keyFile string, keyData string) (*tls.Certificate, error) {
+	if certFile == "" && certData == "" {
+		return nil, nil
+	}
+
+	if certFile != "" && certData != "" {
+		return nil, errors.New("Cannot specify both certFile and certData properties")
+	}
+
+	s.RLock()
+	if *cachedCert != nil {
+		defer s.RUnlock()
+		return *cachedCert, nil
+	}
+
+	s.RUnlock()
+	s.Lock()
+	defer s.Unlock()
+
+	if *cachedCert != nil {
+		return *cachedCert, nil
+	}
+
+	var certBytes []byte
+	var keyBytes []byte
+	var err error
+
+	if certFile != "" {
+		certBytes, err = ioutil.ReadFile(certFile)
+		if err != nil {
+			return nil, err
+		}
+	} else if certData != "" {
+		certBytes, err = base64.StdEncoding.DecodeString(certData)
+		if err != nil {
+			return nil, fmt.Errorf("TLS public certificate could not be decoded: %w", err)
+		}
+	}
+
+	if keyFile != "" {
+		keyBytes, err = ioutil.ReadFile(keyFile)
+		if err != nil {
+			return nil, err
+		}
+	} else if keyData != "" {
+		keyBytes, err = base64.StdEncoding.DecodeString(keyData)
+		if err != nil {
+			return nil, fmt.Errorf("TLS private key could not be decoded: %w", err)
+		}
+	}
+
+	cert, err := tls.X509KeyPair(certBytes, keyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("loading tls certificate failed: %v", err)
+	}
+
+	*cachedCert = &cert
+	return *cachedCert, nil
+}
+
+func (s *localStoreCertProvider) ServerName(isWorker bool) string {
+	return s.getClientTLSSettings(isWorker).ServerName
+}
+
+func (s *localStoreCertProvider) DisableHostVerification(isWorker bool) bool {
+	return s.getClientTLSSettings(isWorker).DisableHostVerification
+}
+
+func (s *localStoreCertProvider) getClientTLSSettings(isWorker bool) *config.ClientTLS {
+	if isWorker && s.workerTLSSettings != nil {
+		return &s.workerTLSSettings.Client // explicit system worker case
+	} else if isWorker {
+		return s.legacyWorkerSettings // legacy config case when we use Frontend.Client settings
+	} else {
+		return &s.tlsSettings.Client // internode client case
+	}
 }
 
 func buildCAPoolFromData(caData []string) (*x509.CertPool, error) {
