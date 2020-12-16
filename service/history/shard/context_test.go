257e22bcc91fa8f4717775522280977ad8b82013
Alex Shtin <alex@shtin.com>
2020-12-15 16:45:05 -0800

    Add AddTasks test

---
+++ service/history/shard/context_test.go
@@ -0,0 +1,119 @@
+package shard
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	enumsspb "go.temporal.io/server/api/enums/v1"
+	persistencespb "go.temporal.io/server/api/persistence/v1"
+	"go.temporal.io/server/common/cache"
+	"go.temporal.io/server/common/cluster"
+	"go.temporal.io/server/common/mocks"
+	"go.temporal.io/server/common/persistence"
+	"go.temporal.io/server/common/primitives/timestamp"
+	"go.temporal.io/server/common/resource"
+	"go.temporal.io/server/service/history/configs"
+)
+
+type (
+	contextSuite struct {
+		suite.Suite
+		*require.Assertions
+
+		controller   *gomock.Controller
+		shardContext Context
+
+		mockResource *resource.Test
+
+		namespaceID        string
+		mockNamespaceCache *cache.MockNamespaceCache
+		namespaceEntry     *cache.NamespaceCacheEntry
+
+		mockClusterMetadata  *cluster.MockMetadata
+		mockExecutionManager *mocks.ExecutionManager
+		mockHistoryEngine    *MockEngine
+	}
+)
+
+func TestShardContextSuite(t *testing.T) {
+	s := &contextSuite{}
+	suite.Run(t, s)
+}
+
+func (s *contextSuite) SetupTest() {
+	s.Assertions = require.New(s.T())
+
+	s.controller = gomock.NewController(s.T())
+
+	shardContext := NewTestContext(
+		s.controller,
+		&persistence.ShardInfoWithFailover{
+			ShardInfo: &persistencespb.ShardInfo{
+				ShardId:          0,
+				RangeId:          1,
+				TransferAckLevel: 0,
+			}},
+		configs.NewDynamicConfigForTest())
+	s.shardContext = shardContext
+
+	s.mockResource = shardContext.Resource
+
+	s.namespaceID = "namespace-Id"
+	s.namespaceEntry = cache.NewLocalNamespaceCacheEntryForTest(&persistencespb.NamespaceInfo{Id: s.namespaceID}, &persistencespb.NamespaceConfig{}, "", nil)
+	s.mockNamespaceCache = s.mockResource.NamespaceCache
+
+	s.mockClusterMetadata = s.mockResource.ClusterMetadata
+	s.mockExecutionManager = s.mockResource.ExecutionMgr
+	s.mockHistoryEngine = NewMockEngine(s.controller)
+	shardContext.engine = s.mockHistoryEngine
+}
+
+func (s *contextSuite) TearDownTest() {
+	s.controller.Finish()
+	s.mockResource.Finish(s.T())
+	s.mockExecutionManager.AssertExpectations(s.T())
+}
+
+func (s *contextSuite) TestAddTasks_Success() {
+	task := &persistencespb.TimerTaskInfo{
+		NamespaceId:     s.namespaceID,
+		WorkflowId:      "workflow-id",
+		RunId:           "run-id",
+		TaskType:        enumsspb.TASK_TYPE_VISIBILITY_DELETE_EXECUTION,
+		Version:         1,
+		EventId:         2,
+		ScheduleAttempt: 1,
+		TaskId:          12345,
+		VisibilityTime:  timestamp.TimeNowPtrUtc(),
+	}
+
+	visibilityTasks := []persistence.Task{&persistence.DeleteExecutionVisibilityTask{
+		// TaskID is set by shard
+		VisibilityTimestamp: s.shardContext.GetTimeSource().Now(),
+		Version:             task.GetVersion(),
+	}}
+	addTasksRequest := &persistence.AddTasksRequest{
+		NamespaceID: task.GetNamespaceId(),
+		WorkflowID:  task.GetWorkflowId(),
+		RunID:       task.GetRunId(),
+
+		TransferTasks:    nil,
+		TimerTasks:       nil,
+		ReplicationTasks: nil,
+		VisibilityTasks:  visibilityTasks,
+	}
+
+	s.mockNamespaceCache.EXPECT().GetNamespaceByID(s.namespaceID).Return(s.namespaceEntry, nil)
+	s.mockClusterMetadata.EXPECT().GetCurrentClusterName().Return(cluster.TestCurrentClusterName)
+	s.mockExecutionManager.On("AddTasks", addTasksRequest).Return(nil).Once()
+	s.mockHistoryEngine.EXPECT().NotifyNewTransferTasks(nil).Times(1)
+	s.mockHistoryEngine.EXPECT().NotifyNewTimerTasks(nil).Times(1)
+	s.mockHistoryEngine.EXPECT().NotifyNewReplicationTasks(nil).Times(1)
+	s.mockHistoryEngine.EXPECT().NotifyNewVisibilityTasks(visibilityTasks).Times(1)
+
+	err := s.shardContext.AddTasks(addTasksRequest)
+	s.NoError(err)
+}
