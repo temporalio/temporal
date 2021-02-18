ef0fa678c1211d4fe198eaa416e48b170df60601
wenquan xing <wenquan.xing@gmail.com>
2021-02-18 13:19:55 -0800

    Patch 1.4.4

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -38,8 +38,8 @@ const (
 	ClientNameJavaSDK = "temporal-java"
 	ClientNameCLI     = "temporal-cli"
 
-	ServerVersion = "1.4.2"
-	CLIVersion    = "1.4.2"
+	ServerVersion = "1.4.4"
+	CLIVersion    = "1.4.4"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
