698d8a7ff8aec763256ed030e72580327f83ed98
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-03-22 19:09:53 -0700

    Adding image for debugging and traffic shaping (#1394)

---
+++ docker/debug-configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+# Adding artificial delay for all network calls.
+# In order to verify that there is a delay, you can login to the container and run:
+# > tc qdisc show dev eth0
+# To change the delay inside of the running container, run:
+# > tc qdisc del dev eth0 root && tc qdisc add dev eth0 root netem delay 100ms
+#
+# Read more about what's possible with tc here:
+# https://www.badunetworks.com/traffic-shaping-with-tc/
+if [ ! -z "$TEMPORAL_DBG_NETWORK_DELAY_MS" ]; then
+	tc qdisc add dev eth0 root netem delay "${TEMPORAL_DBG_NETWORK_DELAY_MS}ms" 
+fi
