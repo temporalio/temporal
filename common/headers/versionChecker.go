9ddb2d19206917df93b00057f5bc49a594894316
Alex Shtin <alex@shtin.com>
2021-04-15 09:21:57 -0700

    Release v1.8.2

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -38,8 +38,8 @@ const (
 	ClientNameJavaSDK = "temporal-java"
 	ClientNameCLI     = "temporal-cli"
 
-	ServerVersion = "1.8.1"
-	CLIVersion    = "1.8.1"
+	ServerVersion = "1.8.2"
+	CLIVersion    = "1.8.2"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
