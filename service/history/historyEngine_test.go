c98f14e740d2a70e2ada498d272f6d7e8fbd4c4f
wenquan xing <wenquan.xing@gmail.com>
2021-04-15 20:47:03 -0700

    fix tests

--- service/history/historyEngine_test.go
+++ service/history/historyEngine_test.go
@@ -5272,10 +5272,12 @@ func newMutableStateBuilderWithVersionHistoriesForTest(
 
 func createMutableState(ms mutableState) *persistencespb.WorkflowMutableState {
 	builder := ms.(*mutableStateBuilder)
-	builder.FlushBufferedEvents()
+	_, _, _ = builder.CloseTransactionAsMutation(time.Now().UTC(), transactionPolicyActive)
+
 	info := copyWorkflowExecutionInfo(builder.executionInfo)
 	state := copyWorkflowExecutionState(builder.executionState)
 	info.ExecutionStats = &persistencespb.ExecutionStats{}
+	info.VersionHistories = versionhistory.CopyVersionHistories(builder.executionInfo.VersionHistories)
 
 	activityInfos := make(map[int64]*persistencespb.ActivityInfo)
 	for id, info := range builder.pendingActivityInfoIDs {
@@ -5296,12 +5298,6 @@ func createMutableState(ms mutableState) *persistencespb.WorkflowMutableState {
 	childInfos := make(map[int64]*persistencespb.ChildExecutionInfo)
 	for id, info := range builder.pendingChildExecutionInfoIDs {
 		childInfos[id] = copyChildInfo(info)
-	}
-
-	// FlushBuffer will also be called within the CloseTransactionAsMutation
-	_, _, _ = builder.CloseTransactionAsMutation(time.Now().UTC(), transactionPolicyActive)
-	if builder.executionInfo.VersionHistories != nil {
-		info.VersionHistories = versionhistory.CopyVersionHistories(builder.executionInfo.VersionHistories)
 	}
 
 	return &persistencespb.WorkflowMutableState{
@@ -5343,6 +5339,7 @@ func copyWorkflowExecutionInfo(sourceInfo *persistencespb.WorkflowExecutionInfo)
 		WorkflowRunTimeout:                sourceInfo.WorkflowRunTimeout,
 		DefaultWorkflowTaskTimeout:        sourceInfo.DefaultWorkflowTaskTimeout,
 		LastFirstEventId:                  sourceInfo.LastFirstEventId,
+		LastFirstEventTxnId:               sourceInfo.LastFirstEventTxnId,
 		LastEventTaskId:                   sourceInfo.LastEventTaskId,
 		LastProcessedEvent:                sourceInfo.LastProcessedEvent,
 		StartTime:                         sourceInfo.StartTime,
