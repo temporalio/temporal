571c9b3cc8bed39842b04c3149accb6ff276f709
Alex Shtin <alex@shtin.com>
2021-04-27 17:24:50 -0700

    Unexport variables

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export BIND_ON_IP="${BIND_ON_IP:-$(hostname -arg)}"
+export BIND_ON_IP="${BIND_ON_IP:-$(hostname -i)}"
 
 # tctl
 if [[ "BIND_ON_IP" =~ ":" ]]
@@ -11,26 +11,6 @@ then  # ipv6
 else  # ipv4
     export TEMPORAL_CLI_ADDRESS="${BIND_ON_IP}:7233"
 fi
-
-export DB="${DB:-cassandra}"
-
-# Cassandra
-export KEYSPACE="${KEYSPACE:-temporal}"
-export VISIBILITY_KEYSPACE="${VISIBILITY_KEYSPACE:-temporal_visibility}"
-export CASSANDRA_PORT="${CASSANDRA_PORT:-9042}"
-
-# MySQL
-export DBNAME="${DBNAME:-temporal}"
-export VISIBILITY_DBNAME="${VISIBILITY_DBNAME:-temporal_visibility}"
-export DB_PORT=${DB_PORT:-3306}
-
-# Elasticsearch
-export ENABLE_ES="${ENABLE_ES:-false}"
-export ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS="${ES_SCHEMA_SETUP_TIMEOUT_IN_SECONDS:-0}"
-export ES_PORT="${ES_PORT:-9200}"
-export ES_VERSION="${ES_VERSION:-v6}"
-export ES_SCHEME="${ES_SCHEME:-http}"
-export ES_VIS_INDEX="${ES_VIS_INDEX:-temporal-visibility-dev}"
 
 dockerize -template ./config/config_template.yaml:./config/docker.yaml
 
