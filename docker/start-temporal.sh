adb8263fdc6f8b03acc5da6ae6a6ba2fb196bf05
Alex Shtin <alex@shtin.com>
2021-05-06 00:42:45 -0700

    Fix service list

--- docker/start-temporal.sh
+++ docker/start-temporal.sh
@@ -3,13 +3,13 @@
 set -eu -o pipefail
 
 SERVICES="${SERVICES:-}"
-SERVICE_ARGS="${SERVICE_ARGS:-}"
+SERVICE_FLAGS="${SERVICE_FLAGS:-}"
 
-if [ -n "${SERVICES}" ]; then
-    # Convert semicolon (or comma for backward compatibility) separated string (i.e. "history:matching")
-    # to valid flags list (i.e. "--service=history --service=matching").
-    IFS=':,' read -ra SERVICE_LIST <<< "${SERVICES}"
-    SERVICE_ARGS=$(printf -- "--service=%s " "${SERVICE_LIST[@]}")
+if [ -z "${SERVICE_FLAGS}" ] && [ -n "${SERVICES}" ]; then
+    # Convert semicolon (or comma, for backward compatibility) separated string (i.e. "history:matching")
+    # to valid flag list (i.e. "--service=history --service=matching").
+    IFS=':,' read -ra SERVICE_FLAGS <<< "${SERVICES}"
+    for i in "${!SERVICE_FLAGS[@]}"; do SERVICE_FLAGS[$i]="--service=${SERVICE_FLAGS[$i]}"; done
 fi
 
-exec temporal-server --env docker start "${SERVICE_ARGS[@]}"
+exec temporal-server --env docker start "${SERVICE_FLAGS[@]}"
