62c250671eb14ba7ee59ec4e478110f3c6f17b69
Alex Shtin <alex@shtin.com>
2021-06-02 13:50:22 -0700

    Add reindexCustomFields param to reindex script

--- schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
+++ schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
@@ -7,12 +7,13 @@ ES_SERVER="${ES_SERVER:-127.0.0.1}"
 ES_PORT="${ES_PORT:-9200}"
 ES_USER="${ES_USER:-}"
 ES_PWD="${ES_PWD:-}"
+REINDEX_CUSTOM_FIELDS="${REINDEX_CUSTOM_FIELDS:-true}"
 
 DIR_NAME=$(dirname "$0")
+# Convert multiline script to single line and remove repeated spaces.
 REINDEX_SCRIPT=$(tr --delete "\n" < "${DIR_NAME}/reindex.painless" | tr --squeeze-repeats " ")
-export REINDEX_SCRIPT
-REINDEX_JSON=$(envsubst < "${DIR_NAME}/reindex.json")
-
+# Substitute envs in reindex.json (envsubst is not available in alpine by default).
+REINDEX_JSON=$(sed -e "s/\${REINDEX_SCRIPT}/${REINDEX_SCRIPT}/" -e "s/\${REINDEX_CUSTOM_FIELDS}/${REINDEX_CUSTOM_FIELDS}/" "${DIR_NAME}/reindex.json")
 echo "${REINDEX_JSON}" | jq
 
 ES_URL="${ES_SCHEME}://${ES_SERVER}:${ES_PORT}/_reindex"
