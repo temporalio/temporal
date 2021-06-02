2fa1a3a04aa002a66614236f70b5996925e645fe
Alex Shtin <alex@shtin.com>
2021-06-02 10:28:26 -0700

    Use env in reindex.sh

--- schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
+++ schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
@@ -2,6 +2,12 @@
 
 set -eu -o pipefail
 
+ES_SCHEME="${ES_SCHEME:-http}"
+ES_SERVER="${ES_SERVER:-127.0.0.1}"
+ES_PORT="${ES_PORT:-9200}"
+ES_USER="${ES_USER:-}"
+ES_PWD="${ES_PWD:-}"
+
 DIR_NAME=$(dirname "$0")
 REINDEX_SCRIPT=$(tr --delete "\n" < "${DIR_NAME}/reindex.painless" | tr --squeeze-repeats " ")
 export REINDEX_SCRIPT
@@ -9,5 +15,6 @@ REINDEX_JSON=$(envsubst < "${DIR_NAME}/reindex.json")
 
 echo "${REINDEX_JSON}" | jq
 
-curl -X POST "http://127.0.0.1:9200/_reindex" -H "Content-Type: application/json" --data-binary "${REINDEX_JSON}" --write-out "\n" | jq
+ES_URL="${ES_SCHEME}://${ES_SERVER}:${ES_PORT}/_reindex"
+curl --user "${ES_USER}":"${ES_PWD}" -X POST "${ES_URL}" -H "Content-Type: application/json" --data-binary "${REINDEX_JSON}" --write-out "\n" | jq
 
