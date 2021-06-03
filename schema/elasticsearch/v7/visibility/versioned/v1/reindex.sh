efb4990353d0ade529499ffaa192d23549e89518
Alex Shtin <alex@shtin.com>
2021-06-03 12:53:41 -0700

    Add args for index names

--- schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
+++ schema/elasticsearch/v7/visibility/versioned/v1/reindex.sh
@@ -7,13 +7,22 @@ ES_SERVER="${ES_SERVER:-127.0.0.1}"
 ES_PORT="${ES_PORT:-9200}"
 ES_USER="${ES_USER:-}"
 ES_PWD="${ES_PWD:-}"
+ES_VIS_INDEX_V0="${ES_VIS_INDEX_V0:-temporal-visibility-dev}"
+ES_VIS_INDEX_V1="${ES_VIS_INDEX_V1:-temporal-visibility-v1-dev}"
+
 REINDEX_CUSTOM_FIELDS="${REINDEX_CUSTOM_FIELDS:-true}"
 
 DIR_NAME=$(dirname "$0")
 # Convert multiline script to single line and remove repeated spaces.
 REINDEX_SCRIPT=$(tr --delete "\n" < "${DIR_NAME}/reindex.painless" | tr --squeeze-repeats " ")
 # Substitute envs in reindex.json (envsubst is not available in alpine by default).
-REINDEX_JSON=$(sed -e "s/\${REINDEX_SCRIPT}/${REINDEX_SCRIPT}/" -e "s/\${REINDEX_CUSTOM_FIELDS}/${REINDEX_CUSTOM_FIELDS}/" "${DIR_NAME}/reindex.json")
+REINDEX_JSON=$(sed \
+  -e "s/\${REINDEX_SCRIPT}/${REINDEX_SCRIPT}/" \
+  -e "s/\${ES_VIS_INDEX_V0}/${ES_VIS_INDEX_V0}/" \
+  -e "s/\${ES_VIS_INDEX_V1}/${ES_VIS_INDEX_V1}/" \
+  -e "s/\${REINDEX_CUSTOM_FIELDS}/${REINDEX_CUSTOM_FIELDS}/" \
+  "${DIR_NAME}/reindex.json")
+
 echo "${REINDEX_JSON}" | jq
 
 ES_URL="${ES_SCHEME}://${ES_SERVER}:${ES_PORT}/_reindex"
