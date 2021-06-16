26ac39b081118988ca14a86e61daa3ab785366d8
Yimin Chen <yimin.chen@live.com>
2021-06-15 21:09:29 -0700

    Add mocks for data store interfaces (#1640)

--- common/persistence/persistenceInterface.go
+++ common/persistence/persistenceInterface.go
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+//go:generate mockgen -copyright_file ../../LICENSE -package mock -source $GOFILE -destination mock/store_mock.go -aux_files go.temporal.io/server/common/persistence=dataInterfaces.go
+
 package persistence
 
 import (
