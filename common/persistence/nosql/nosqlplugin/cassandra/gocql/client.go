27ac4509bd6994c09a75b3b2a4a1f9d4cf69399b
mastermanu <manu@temporal.io>
2021-06-17 16:38:48 -0700

    Sets Reconnection policy on gocql.ClusterObject (#1652)
    Enabling a Reconnection policy will enable better resiliency in the event that Cassandra nodes happen to churn for a given cluster. We have seen situations where nodes churned, and the only way to recover Temporal was by initiating a reboot. This should address this.
    
    Validated change in a deployed test environment. This is a low-risk change.

--- common/persistence/nosql/nosqlplugin/cassandra/gocql/client.go
+++ common/persistence/nosql/nosqlplugin/cassandra/gocql/client.go
@@ -150,6 +150,12 @@ func NewCassandraCluster(
 	cluster.Consistency = cfg.Consistency.GetConsistency()
 	cluster.SerialConsistency = cfg.Consistency.GetSerialConsistency()
 
+	cluster.ReconnectionPolicy = &gocql.ExponentialReconnectionPolicy{
+		MaxRetries:      30,
+		InitialInterval: time.Second,
+		MaxInterval:     10 * time.Second,
+	}
+
 	cluster.PoolConfig.HostSelectionPolicy = gocql.TokenAwareHostPolicy(gocql.RoundRobinHostPolicy())
 	return cluster, nil
 }
