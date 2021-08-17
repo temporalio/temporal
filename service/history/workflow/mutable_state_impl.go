06ddb22537fff8fbd506677de4182aecbceeba14
wenquan xing <wenquan.xing@gmail.com>
2021-08-16 18:49:04 -0700

    Fix execution time not set correctly during continue as new

--- service/history/workflow/mutable_state_impl.go
+++ service/history/workflow/mutable_state_impl.go
@@ -1351,8 +1351,6 @@ func (e *MutableStateImpl) AddWorkflowExecutionStartedEvent(
 		return nil, e.createInternalServerError(opTag)
 	}
 
-	e.executionInfo.ExecutionTime = timestamp.TimePtr(e.executionInfo.StartTime.Add(timestamp.DurationValue(startRequest.GetFirstWorkflowTaskBackoff())))
-
 	event := e.hBuilder.AddWorkflowExecutionStartedEvent(
 		*e.executionInfo.StartTime,
 		startRequest,
@@ -1437,6 +1435,10 @@ func (e *MutableStateImpl) ReplicateWorkflowExecutionStartedEvent(
 	} else {
 		e.executionInfo.InitiatedId = common.EmptyEventID
 	}
+
+	e.executionInfo.ExecutionTime = timestamp.TimePtr(
+		e.executionInfo.StartTime.Add(timestamp.DurationValue(event.GetFirstWorkflowTaskBackoff())),
+	)
 
 	e.executionInfo.Attempt = event.GetAttempt()
 	if !timestamp.TimeValue(event.GetWorkflowExecutionExpirationTime()).IsZero() {
