0781bb48ee53ecebe199abd219832789fbc82c29
Jacob LeGrone <git@jacob.work>
2021-09-23 00:49:46 -0400

    Export function to create namespaces in SQLite

--- schema/sqlite/setup.go
+++ schema/sqlite/setup.go
@@ -26,14 +26,19 @@ package sqlite
 
 import (
 	"bytes"
+	"context"
 	_ "embed"
 	"fmt"
 	"io"
 
+	"go.temporal.io/api/enums/v1"
+	"go.temporal.io/server/api/persistence/v1"
 	"go.temporal.io/server/common/config"
 	p "go.temporal.io/server/common/persistence"
+	"go.temporal.io/server/common/persistence/serialization"
 	"go.temporal.io/server/common/persistence/sql"
 	"go.temporal.io/server/common/persistence/sql/sqlplugin"
+	"go.temporal.io/server/common/primitives"
 	"go.temporal.io/server/common/resolver"
 )
 
@@ -44,6 +49,7 @@ var (
 	visibilitySchema []byte
 )
 
+// SetupSchema initializes the SQLite schema in an empty database.
 func SetupSchema(cfg *config.SQL) error {
 	db, err := sql.NewSQLAdminDB(sqlplugin.DbKindUnknown, cfg, resolver.NewNoopResolver())
 	if err != nil {
@@ -71,6 +77,86 @@ func SetupSchema(cfg *config.SQL) error {
 		if err = db.Exec(stmt); err != nil {
 			return fmt.Errorf("error executing statement %q: %w", stmt, err)
 		}
+	}
+
+	return nil
+}
+
+type NamespaceConfig struct {
+	// Low level representation of a Namespace used by Temporal persistence drivers.
+	Detail *persistence.NamespaceDetail
+	// Global Namespaces provide support for replication of Workflow execution across clusters.
+	IsGlobal bool
+}
+
+// CreateNamespaces creates namespaces in the target database without the need to have a running Temporal server.
+//
+// This exists primarily as a workaround for https://github.com/temporalio/temporal/issues/1336. Namespaces should
+// typically be created through the Temporal API either via `tctl` or an SDK client.
+//
+// Attempting to create a namespace that already exists will be a no-op.
+func CreateNamespaces(cfg *config.SQL, namespaces ...*NamespaceConfig) error {
+	db, err := sql.NewSQLDB(sqlplugin.DbKindUnknown, cfg, resolver.NewNoopResolver())
+	if err != nil {
+		return fmt.Errorf("unable to create SQLite admin DB: %w", err)
+	}
+	defer func() { _ = db.Close() }()
+
+	for _, ns := range namespaces {
+		if err := createNamespaceIfNotExists(db, ns); err != nil {
+			return fmt.Errorf("error creating namespace %q: %w", ns, err)
+		}
+	}
+
+	return nil
+}
+
+// NewNamespaceConfig initializes a NamespaceConfig with the field values needed to pre-register
+// the namespace via the CreateNamespaces function.
+func NewNamespaceConfig(namespace string, global bool) *NamespaceConfig {
+	detail := persistence.NamespaceDetail{
+		Info: &persistence.NamespaceInfo{
+			Id:    primitives.NewUUID().String(),
+			State: enums.NAMESPACE_STATE_REGISTERED,
+			Name:  namespace,
+		},
+		Config:            &persistence.NamespaceConfig{},
+		ReplicationConfig: &persistence.NamespaceReplicationConfig{},
+	}
+	return &NamespaceConfig{
+		Detail:   &detail,
+		IsGlobal: global,
+	}
+}
+
+func createNamespaceIfNotExists(db sqlplugin.DB, namespace *NamespaceConfig) error {
+	var (
+		name = namespace.Detail.GetInfo().GetName()
+		id   = primitives.MustParseUUID(namespace.Detail.GetInfo().GetId())
+	)
+
+	// Return early if namespace already exists
+	rows, err := db.SelectFromNamespace(context.Background(), sqlplugin.NamespaceFilter{
+		Name: &name,
+	})
+	if err == nil && len(rows) > 0 {
+		return nil
+	}
+
+	d, err := serialization.NewSerializer().NamespaceDetailToBlob(namespace.Detail, enums.ENCODING_TYPE_PROTO3)
+	if err != nil {
+		return err
+	}
+
+	if _, err := db.InsertIntoNamespace(context.Background(), &sqlplugin.NamespaceRow{
+		ID:                  id,
+		Name:                name,
+		Data:                d.GetData(),
+		DataEncoding:        d.GetEncodingType().String(),
+		IsGlobal:            namespace.IsGlobal,
+		NotificationVersion: 0,
+	}); err != nil {
+		return err
 	}
 
 	return nil
