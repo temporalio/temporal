91fce620fd75a08389c27895d6c54457a0a2f0f5
Mikhail Gryzykhin <mikhail@temporal.io>
2021-10-09 02:48:38 -0700

    Use uber/fx to init worker service

---
+++ service/worker/fx.go
@@ -0,0 +1,93 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package worker
+
+import (
+	"context"
+
+	"go.uber.org/fx"
+
+	"go.temporal.io/server/common"
+	"go.temporal.io/server/common/dynamicconfig"
+	"go.temporal.io/server/common/log"
+	persistenceClient "go.temporal.io/server/common/persistence/client"
+	esclient "go.temporal.io/server/common/persistence/visibility/store/elasticsearch/client"
+	"go.temporal.io/server/common/resource"
+	"go.temporal.io/server/service"
+)
+
+var Module = fx.Options(
+	resource.Module,
+	fx.Provide(ParamsExpandProvider),
+	fx.Provide(dynamicconfig.NewCollection),
+	fx.Provide(ThrottledLoggerRpsFnProvider),
+	fx.Provide(NewConfig),
+	fx.Provide(PersistenceMaxQpsProvider),
+	fx.Provide(NewService),
+	fx.Invoke(ServiceLifetimeHooks),
+)
+
+func ParamsExpandProvider(params *resource.BootstrapParams) (
+	log.Logger,
+	esclient.Client,
+	common.RPCFactory,
+	dynamicconfig.Client,
+) {
+	return params.Logger, params.ESClient, params.RPCFactory, params.DynamicConfigClient
+}
+
+
+func ThrottledLoggerRpsFnProvider(serviceConfig *Config) resource.ThrottledLoggerRpsFn {
+	return func() float64 { return float64(serviceConfig.ThrottledLogRPS()) }
+}
+func PersistenceMaxQpsProvider(
+	serviceConfig *Config,
+) persistenceClient.PersistenceMaxQps {
+	return service.PersistenceMaxQpsFn(serviceConfig.PersistenceMaxQPS, serviceConfig.PersistenceGlobalMaxQPS)
+}
+
+func ServiceLifetimeHooks(
+	lc fx.Lifecycle,
+	svcStoppedCh chan struct{},
+	svc *Service,
+) {
+	lc.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				go func(svc common.Daemon, svcStoppedCh chan<- struct{}) {
+					// Start is blocked until Stop() is called.
+					svc.Start()
+					close(svcStoppedCh)
+				}(svc, svcStoppedCh)
+
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				svc.Stop()
+				return nil
+			},
+		},
+	)
+}
--- service/worker/service.go
+++ service/worker/service.go
@@ -75,32 +75,20 @@ type (
 	}
 )
 
-// NewService builds a new worker service
 func NewService(
-	params *resource.BootstrapParams,
-) (*Service, error) {
-
-	serviceConfig := NewConfig(params)
-
-	serviceResource, err := resource.New(
-		params,
-		common.WorkerServiceName,
-		serviceConfig.PersistenceMaxQPS,
-		serviceConfig.PersistenceGlobalMaxQPS,
-		serviceConfig.ThrottledLogRPS,
-	)
-	if err != nil {
-		return nil, err
-	}
-
+	serviceResource resource.Resource,
+	serviceConfig *Config,
+	sdkClient sdkclient.Client,
+	esClient esclient.Client,
+) *Service {
 	return &Service{
 		Resource:  serviceResource,
 		status:    common.DaemonStatusInitialized,
 		config:    serviceConfig,
-		sdkClient: params.SdkClient,
-		esClient:  params.ESClient,
+		sdkClient: sdkClient,
+		esClient:  esClient,
 		stopC:     make(chan struct{}),
-	}, nil
+	}
 }
 
 // NewConfig builds the new Config for worker service
