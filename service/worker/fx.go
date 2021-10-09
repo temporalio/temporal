a3649987ec9eef3f9a1e96a3037b6835c9a0257e
Mikhail Gryzykhin <12602502+Ardagan@users.noreply.github.com>
2021-10-08 17:24:34 -0700

    Use uber/fx to init worker service (#2032)

---
+++ service/worker/fx.go
@@ -0,0 +1,72 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package worker
+
+import (
+	"context"
+
+	sdkclient "go.temporal.io/sdk/client"
+	"go.temporal.io/server/common"
+	esclient "go.temporal.io/server/common/persistence/visibility/store/elasticsearch/client"
+	"go.temporal.io/server/common/resource"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Options(
+	resource.Module,
+	fx.Provide(ParamsExpandProvider), // BootstrapParams should be deprecated
+	fx.Provide(NewService),
+	fx.Invoke(ServiceLifetimeHooks),
+)
+
+func ParamsExpandProvider(params *resource.BootstrapParams) (sdkclient.Client, esclient.Client) {
+	return params.SdkClient,
+		params.ESClient
+}
+
+func ServiceLifetimeHooks(
+	lc fx.Lifecycle,
+	svcStoppedCh chan struct{},
+	svc *Service,
+) {
+	lc.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				go func(svc common.Daemon, svcStoppedCh chan<- struct{}) {
+					// Start is blocked until Stop() is called.
+					svc.Start()
+					close(svcStoppedCh)
+				}(svc, svcStoppedCh)
+
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				svc.Stop()
+				return nil
+			},
+		},
+	)
+
+}
