aee485d48220bcdb5c2b897df7629d6b07d642ee
yiminc <yiminc yimin.chen@live.com>
2021-10-11 19:46:51 -0700

    Refactor visibility manager

---
+++ common/persistence/visibility/visibility_manager_impl.go
@@ -0,0 +1,327 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package visibility
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	commonpb "go.temporal.io/api/common/v1"
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/serviceerror"
+	workflowpb "go.temporal.io/api/workflow/v1"
+
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/persistence/visibility/manager"
+	"go.temporal.io/server/common/persistence/visibility/store"
+)
+
+type (
+	// visibilityManagerImpl is responsible for:
+	//  - convert request (serialized some fields),
+	//  - call underlying store (standard or advanced),
+	//  - convert response.
+	visibilityManagerImpl struct {
+		store  store.VisibilityStore
+		logger log.Logger
+	}
+)
+
+const (
+	// MemoEncoding is default encoding for visibility memo.
+	MemoEncoding = enumspb.ENCODING_TYPE_PROTO3
+)
+
+var _ manager.VisibilityManager = (*visibilityManagerImpl)(nil)
+
+func newVisibilityManagerImpl(
+	store store.VisibilityStore,
+	logger log.Logger,
+) *visibilityManagerImpl {
+	return &visibilityManagerImpl{
+		store:  store,
+		logger: logger,
+	}
+}
+
+func (p *visibilityManagerImpl) Close() {
+	p.store.Close()
+}
+
+func (p *visibilityManagerImpl) GetName() string {
+	return p.store.GetName()
+}
+
+func (p *visibilityManagerImpl) RecordWorkflowExecutionStarted(request *manager.RecordWorkflowExecutionStartedRequest) error {
+	requestBase, err := p.newInternalVisibilityRequestBase(request.VisibilityRequestBase)
+	if err != nil {
+		return err
+	}
+	req := &store.InternalRecordWorkflowExecutionStartedRequest{
+		InternalVisibilityRequestBase: requestBase,
+	}
+	return p.store.RecordWorkflowExecutionStarted(req)
+}
+
+func (p *visibilityManagerImpl) RecordWorkflowExecutionClosed(request *manager.RecordWorkflowExecutionClosedRequest) error {
+	requestBase, err := p.newInternalVisibilityRequestBase(request.VisibilityRequestBase)
+	if err != nil {
+		return err
+	}
+	req := &store.InternalRecordWorkflowExecutionClosedRequest{
+		InternalVisibilityRequestBase: requestBase,
+		CloseTime:                     request.CloseTime,
+		HistoryLength:                 request.HistoryLength,
+		Retention:                     request.Retention,
+	}
+	return p.store.RecordWorkflowExecutionClosed(req)
+}
+
+func (p *visibilityManagerImpl) UpsertWorkflowExecution(request *manager.UpsertWorkflowExecutionRequest) error {
+	requestBase, err := p.newInternalVisibilityRequestBase(request.VisibilityRequestBase)
+	if err != nil {
+		return err
+	}
+	req := &store.InternalUpsertWorkflowExecutionRequest{
+		InternalVisibilityRequestBase: requestBase,
+	}
+	return p.store.UpsertWorkflowExecution(req)
+}
+
+func (p *visibilityManagerImpl) DeleteWorkflowExecution(request *manager.VisibilityDeleteWorkflowExecutionRequest) error {
+	return p.store.DeleteWorkflowExecution(request)
+}
+
+func (p *visibilityManagerImpl) ListOpenWorkflowExecutions(request *manager.ListWorkflowExecutionsRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListOpenWorkflowExecutions(request)
+	if err != nil {
+		return nil, err
+	}
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListClosedWorkflowExecutions(request *manager.ListWorkflowExecutionsRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListClosedWorkflowExecutions(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListOpenWorkflowExecutionsByType(request *manager.ListWorkflowExecutionsByTypeRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListOpenWorkflowExecutionsByType(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListClosedWorkflowExecutionsByType(request *manager.ListWorkflowExecutionsByTypeRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListClosedWorkflowExecutionsByType(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListOpenWorkflowExecutionsByWorkflowID(request *manager.ListWorkflowExecutionsByWorkflowIDRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListOpenWorkflowExecutionsByWorkflowID(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListClosedWorkflowExecutionsByWorkflowID(request *manager.ListWorkflowExecutionsByWorkflowIDRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListClosedWorkflowExecutionsByWorkflowID(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListClosedWorkflowExecutionsByStatus(request *manager.ListClosedWorkflowExecutionsByStatusRequest) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListClosedWorkflowExecutionsByStatus(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ListWorkflowExecutions(request *manager.ListWorkflowExecutionsRequestV2) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ListWorkflowExecutions(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) ScanWorkflowExecutions(request *manager.ListWorkflowExecutionsRequestV2) (*manager.ListWorkflowExecutionsResponse, error) {
+	response, err := p.store.ScanWorkflowExecutions(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.convertInternalListResponse(response)
+}
+
+func (p *visibilityManagerImpl) CountWorkflowExecutions(request *manager.CountWorkflowExecutionsRequest) (*manager.CountWorkflowExecutionsResponse, error) {
+	response, err := p.store.CountWorkflowExecutions(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, err
+}
+
+func (p *visibilityManagerImpl) newInternalVisibilityRequestBase(request *manager.VisibilityRequestBase) (*store.InternalVisibilityRequestBase, error) {
+	if request == nil {
+		return nil, nil
+	}
+	memoBlob, err := p.serializeMemo(request.Memo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &store.InternalVisibilityRequestBase{
+		NamespaceID:          request.NamespaceID,
+		WorkflowID:           request.Execution.GetWorkflowId(),
+		RunID:                request.Execution.GetRunId(),
+		WorkflowTypeName:     request.WorkflowTypeName,
+		StartTime:            request.StartTime,
+		Status:               request.Status,
+		ExecutionTime:        request.ExecutionTime,
+		StateTransitionCount: request.StateTransitionCount,
+		TaskID:               request.TaskID,
+		ShardID:              request.ShardID,
+		TaskQueue:            request.TaskQueue,
+		Memo:                 memoBlob,
+		SearchAttributes:     request.SearchAttributes,
+	}, nil
+}
+
+func (p *visibilityManagerImpl) convertInternalListResponse(internalResponse *store.InternalListWorkflowExecutionsResponse) (*manager.ListWorkflowExecutionsResponse, error) {
+	if internalResponse == nil {
+		return nil, nil
+	}
+
+	resp := &manager.ListWorkflowExecutionsResponse{}
+	resp.Executions = make([]*workflowpb.WorkflowExecutionInfo, len(internalResponse.Executions))
+	for i, execution := range internalResponse.Executions {
+		var err error
+		resp.Executions[i], err = p.convertInternalWorkflowExecutionInfo(execution)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	resp.NextPageToken = internalResponse.NextPageToken
+	return resp, nil
+}
+
+func (p *visibilityManagerImpl) convertInternalWorkflowExecutionInfo(internalExecution *store.InternalWorkflowExecutionInfo) (*workflowpb.WorkflowExecutionInfo, error) {
+	if internalExecution == nil {
+		return nil, nil
+	}
+	memo, err := p.deserializeMemo(internalExecution.Memo)
+	if err != nil {
+		return nil, err
+	}
+
+	executionInfo := &workflowpb.WorkflowExecutionInfo{
+		Execution: &commonpb.WorkflowExecution{
+			WorkflowId: internalExecution.WorkflowID,
+			RunId:      internalExecution.RunID,
+		},
+		Type: &commonpb.WorkflowType{
+			Name: internalExecution.TypeName,
+		},
+		StartTime:            &internalExecution.StartTime,
+		ExecutionTime:        &internalExecution.ExecutionTime,
+		Memo:                 memo,
+		SearchAttributes:     internalExecution.SearchAttributes,
+		TaskQueue:            internalExecution.TaskQueue,
+		Status:               internalExecution.Status,
+		StateTransitionCount: internalExecution.StateTransitionCount,
+	}
+
+	// for close records
+	if internalExecution.Status != enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING {
+		executionInfo.CloseTime = &internalExecution.CloseTime
+		executionInfo.HistoryLength = internalExecution.HistoryLength
+	}
+
+	// Workflows created before 1.11 have ExecutionTime set to Unix epoch zero time (1/1/1970) for non-cron/non-retry case.
+	// Use StartTime as ExecutionTime for this case (if there was a backoff it must be set).
+	// Remove this "if" block when ExecutionTime field has actual correct value (added 6/9/21).
+	// Affects only non-advanced visibility.
+	if !executionInfo.ExecutionTime.After(time.Unix(0, 0)) {
+		executionInfo.ExecutionTime = executionInfo.StartTime
+	}
+
+	return executionInfo, nil
+}
+func (p *visibilityManagerImpl) deserializeMemo(data *commonpb.DataBlob) (*commonpb.Memo, error) {
+	if data == nil || len(data.Data) == 0 {
+		return &commonpb.Memo{}, nil
+	}
+
+	var ()
+	switch data.EncodingType {
+	case enumspb.ENCODING_TYPE_PROTO3:
+		memo := &commonpb.Memo{}
+		err := proto.Unmarshal(data.Data, memo)
+		if err != nil {
+			return nil, serviceerror.NewInternal(fmt.Sprintf("Unable to deserialize memo from data blob: %v", err))
+		}
+		return memo, nil
+	default:
+		return nil, serviceerror.NewInternal(fmt.Sprintf("Invalid memo encoding in database: %s", data.GetEncodingType().String()))
+	}
+}
+
+func (p *visibilityManagerImpl) serializeMemo(memo *commonpb.Memo) (*commonpb.DataBlob, error) {
+	if memo == nil {
+		memo = &commonpb.Memo{}
+	}
+
+	data, err := proto.Marshal(memo)
+	if err != nil {
+		return nil, serviceerror.NewInternal(fmt.Sprintf("Unable to serialize memo to data blob: %v", err))
+	}
+
+	return &commonpb.DataBlob{
+		Data:         data,
+		EncodingType: MemoEncoding,
+	}, nil
+}
--- common/persistence/visibility/visibility_manager_test.go
+++ common/persistence/visibility/visibility_manager_test.go
@@ -34,8 +34,8 @@ import (
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/serviceerror"
-
 	"go.temporal.io/server/common/dynamicconfig"
+
 	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/persistence"
@@ -99,7 +99,7 @@ func (s *VisibilityManagerSuite) TestRecordWorkflowExecutionStarted() {
 		},
 	}
 	s.visibilityStore.EXPECT().RecordWorkflowExecutionStarted(gomock.Any()).Return(nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceRecordWorkflowExecutionStartedScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceRecordWorkflowExecutionStartedScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	s.NoError(s.visibilityManager.RecordWorkflowExecutionStarted(request))
 
 	// no remaining tokens
@@ -120,7 +120,7 @@ func (s *VisibilityManagerSuite) TestRecordWorkflowExecutionClosed() {
 	}
 
 	s.visibilityStore.EXPECT().RecordWorkflowExecutionClosed(gomock.Any()).Return(nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceRecordWorkflowExecutionClosedScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceRecordWorkflowExecutionClosedScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	s.NoError(s.visibilityManager.RecordWorkflowExecutionClosed(request))
 
 	err := s.visibilityManager.RecordWorkflowExecutionClosed(request)
@@ -134,7 +134,7 @@ func (s *VisibilityManagerSuite) TestListOpenWorkflowExecutions() {
 		Namespace:   testNamespace,
 	}
 	s.visibilityStore.EXPECT().ListOpenWorkflowExecutions(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListOpenWorkflowExecutions(request)
 	s.NoError(err)
 
@@ -152,7 +152,7 @@ func (s *VisibilityManagerSuite) TestListClosedWorkflowExecutions() {
 		Namespace:   testNamespace,
 	}
 	s.visibilityStore.EXPECT().ListClosedWorkflowExecutions(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListClosedWorkflowExecutions(request)
 	s.NoError(err)
 
@@ -174,7 +174,7 @@ func (s *VisibilityManagerSuite) TestListOpenWorkflowExecutionsByType() {
 		WorkflowTypeName:              testWorkflowTypeName,
 	}
 	s.visibilityStore.EXPECT().ListOpenWorkflowExecutionsByType(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsByTypeScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsByTypeScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListOpenWorkflowExecutionsByType(request)
 	s.NoError(err)
 
@@ -196,7 +196,7 @@ func (s *VisibilityManagerSuite) TestListClosedWorkflowExecutionsByType() {
 		WorkflowTypeName:              testWorkflowTypeName,
 	}
 	s.visibilityStore.EXPECT().ListClosedWorkflowExecutionsByType(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByTypeScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByTypeScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListClosedWorkflowExecutionsByType(request)
 	s.NoError(err)
 
@@ -218,7 +218,7 @@ func (s *VisibilityManagerSuite) TestListOpenWorkflowExecutionsByWorkflowID() {
 		WorkflowID:                    testWorkflowExecution.GetWorkflowId(),
 	}
 	s.visibilityStore.EXPECT().ListOpenWorkflowExecutionsByWorkflowID(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsByWorkflowIDScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListOpenWorkflowExecutionsByWorkflowIDScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListOpenWorkflowExecutionsByWorkflowID(request)
 	s.NoError(err)
 
@@ -240,7 +240,7 @@ func (s *VisibilityManagerSuite) TestListClosedWorkflowExecutionsByWorkflowID() 
 		WorkflowID:                    testWorkflowExecution.GetWorkflowId(),
 	}
 	s.visibilityStore.EXPECT().ListClosedWorkflowExecutionsByWorkflowID(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByWorkflowIDScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByWorkflowIDScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListClosedWorkflowExecutionsByWorkflowID(request)
 	s.NoError(err)
 
@@ -262,7 +262,7 @@ func (s *VisibilityManagerSuite) TestListClosedWorkflowExecutionsByStatus() {
 		Status:                        enumspb.WORKFLOW_EXECUTION_STATUS_FAILED,
 	}
 	s.visibilityStore.EXPECT().ListClosedWorkflowExecutionsByStatus(gomock.Any()).Return(nil, nil)
-	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByStatusScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History))
+	s.metricClient.EXPECT().Scope(metrics.VisibilityPersistenceListClosedWorkflowExecutionsByStatusScope, metrics.StandardVisibilityTypeTag()).Return(metrics.NoopScope(metrics.History)).Times(2)
 	_, err := s.visibilityManager.ListClosedWorkflowExecutionsByStatus(request)
 	s.NoError(err)
 
