68a2641736af2464ecee3879a7b86237131f4d40
wenquan xing <wenquan.xing@gmail.com>
2021-10-13 16:14:43 -0700

    Make shard info to 0 unix nano time, instead of default time.Time

--- service/history/transferQueueActiveTaskExecutor.go
+++ service/history/transferQueueActiveTaskExecutor.go
@@ -38,6 +38,7 @@ import (
 	taskqueuepb "go.temporal.io/api/taskqueue/v1"
 	workflowpb "go.temporal.io/api/workflow/v1"
 	"go.temporal.io/api/workflowservice/v1"
+
 	enumsspb "go.temporal.io/server/api/enums/v1"
 	"go.temporal.io/server/api/historyservice/v1"
 	persistencespb "go.temporal.io/server/api/persistence/v1"
