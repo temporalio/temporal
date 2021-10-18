47b746d16c240a4bd066934a1ed4465e8ebb7cb0
Sergey Bykov <sergey@temporal.io>
2021-10-18 14:46:55 -0700

    Add validation of TLS config to NewTLSConfigProviderFromConfig()

--- common/rpc/encryption/tlsFactory.go
+++ common/rpc/encryption/tlsFactory.go
@@ -27,6 +27,8 @@ package encryption
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/uber-go/tally"
@@ -89,8 +91,97 @@ func NewTLSConfigProviderFromConfig(
 	certProviderFactory CertProviderFactory,
 ) (TLSConfigProvider, error) {
 
+	if err := validateRootTLS(&encryptionSettings); err != nil {
+		return nil, err
+	}
 	if certProviderFactory == nil {
 		certProviderFactory = NewLocalStoreCertProvider
 	}
 	return NewLocalStoreTlsProvider(&encryptionSettings, scope, logger, certProviderFactory)
+}
+
+func validateRootTLS(cfg *config.RootTLS) error {
+	if err := validateGroupTLS(&cfg.Internode); err != nil {
+		return err
+	}
+	if err := validateGroupTLS(&cfg.Frontend); err != nil {
+		return err
+	}
+	if err := validateWorkerTLS(&cfg.SystemWorker); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateGroupTLS(cfg *config.GroupTLS) error {
+	if err := validateServerTLS(&cfg.Server); err != nil {
+		return err
+	}
+	if err := validateClientTLS(&cfg.Client); err != nil {
+		return err
+	}
+	for host, hostConfig := range cfg.PerHostOverrides {
+
+		if strings.TrimSpace(host) == "" {
+			return fmt.Errorf("host name cannot be empty string")
+		}
+		if err := validateServerTLS(&hostConfig); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func validateWorkerTLS(cfg *config.WorkerTLS) error {
+	if cfg.CertFile != "" && cfg.CertData != "" {
+		return fmt.Errorf("cannot specify CertFile and CertData at the same time")
+	}
+	if cfg.KeyFile != "" && cfg.KeyData != "" {
+		return fmt.Errorf("cannot specify KeyFile and KeyData at the same time")
+	}
+	if err := validateClientTLS(&cfg.Client); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateServerTLS(cfg *config.ServerTLS) error {
+	if cfg.CertFile != "" && cfg.CertData != "" {
+		return fmt.Errorf("cannot specify CertFile and CertData at the same time")
+	}
+	if cfg.KeyFile != "" && cfg.KeyData != "" {
+		return fmt.Errorf("cannot specify KeyFile and KeyData at the same time")
+	}
+	if err := validateCAs(cfg.ClientCAData); err != nil {
+		return fmt.Errorf("invalid ServerTLS.ClientCAData: %w", err)
+	}
+	if err := validateCAs(cfg.ClientCAFiles); err != nil {
+		return fmt.Errorf("invalid ServerTLS.ClientCAFiles: %w", err)
+	}
+	if len(cfg.ClientCAFiles) > 0 && len(cfg.ClientCAData) > 0 {
+		return fmt.Errorf("cannot specify ClientCAFiles and ClientCAData at the same time")
+	}
+	return nil
+}
+
+func validateClientTLS(cfg *config.ClientTLS) error {
+	if err := validateCAs(cfg.RootCAData); err != nil {
+		return fmt.Errorf("invalid ClientTLS.RootCAData: %w", err)
+	}
+	if err := validateCAs(cfg.RootCAFiles); err != nil {
+		return fmt.Errorf("invalid ClientTLS.RootCAFiles: %w", err)
+	}
+	if len(cfg.RootCAData) > 0 && len(cfg.RootCAFiles) > 0 {
+		return fmt.Errorf("cannot specify RootCAFiles and RootCAData at the same time")
+	}
+	return nil
+}
+
+func validateCAs(cas []string) error {
+	for _, ca := range cas {
+		if strings.TrimSpace(ca) == "" {
+			return fmt.Errorf("CA cannot be empty string")
+		}
+	}
+	return nil
 }
