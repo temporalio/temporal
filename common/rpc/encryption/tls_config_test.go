92081a5a8e9fac637c39330a3f8634a9dc1fb5e8
Sergey Bykov <8248806+sergeybykov@users.noreply.github.com>
2021-10-18 16:20:55 -0700

    Handle case of empty string CAs in TLS config (#2063)
    * Don't set callback in case of disabled TLS
    
    * Treat empty string CAs as not configured
    
    * Add validation of TLS config to NewTLSConfigProviderFromConfig()
    
    * Revert "Treat empty string CAs as not configured"
    
    This reverts commit 52e7bc57942d8ed0c2351b756e52cdbb4785b536.
    
    * Revert "Don't set callback in case of disabled TLS"
    
    This reverts commit b71651e656fad1148f1e5e5d8564fbe6fc436a0f.
    
    * Add tests for TLS config validation

--- common/rpc/encryption/tls_config_test.go
+++ common/rpc/encryption/tls_config_test.go
@@ -83,3 +83,170 @@ func (s *tlsConfigTest) TestIsSystemWorker() {
 	cfg = &config.RootTLS{SystemWorker: config.WorkerTLS{Client: config.ClientTLS{ForceTLS: false}}}
 	s.False(isSystemWorker(cfg))
 }
+
+func (s *tlsConfigTest) TestCertFileAndData() {
+	s.testGroupTLS(s.testCertFileAndData)
+}
+
+func (s *tlsConfigTest) TestKeyFileAndData() {
+	s.testGroupTLS(s.testKeyFileAndData)
+}
+
+func (s *tlsConfigTest) TestClientCAData() {
+	s.testGroupTLS(s.testClientCAData)
+}
+
+func (s *tlsConfigTest) TestClientCAFiles() {
+	s.testGroupTLS(s.testClientCAFiles)
+}
+
+func (s *tlsConfigTest) TestRootCAData() {
+	s.testGroupTLS(s.testRootCAData)
+}
+
+func (s *tlsConfigTest) TestRootCAFiles() {
+	s.testGroupTLS(s.testRootCAFiles)
+}
+
+func (s *tlsConfigTest) testGroupTLS(f func(*config.RootTLS, *config.GroupTLS)) {
+
+	cfg := &config.RootTLS{Internode: config.GroupTLS{}}
+	f(cfg, &cfg.Internode)
+	cfg = &config.RootTLS{Frontend: config.GroupTLS{}}
+	f(cfg, &cfg.Frontend)
+}
+
+func (s *tlsConfigTest) testClientTLS(f func(*config.RootTLS, *config.ClientTLS)) {
+
+	cfg := &config.RootTLS{Internode: config.GroupTLS{}}
+	f(cfg, &cfg.Internode.Client)
+	cfg = &config.RootTLS{Frontend: config.GroupTLS{}}
+	f(cfg, &cfg.Frontend.Client)
+}
+
+func (s *tlsConfigTest) testServerTLS(f func(*config.RootTLS, *config.ServerTLS)) {
+
+	cfg := &config.RootTLS{Internode: config.GroupTLS{}}
+	f(cfg, &cfg.Internode.Server)
+	cfg = &config.RootTLS{Frontend: config.GroupTLS{}}
+	f(cfg, &cfg.Frontend.Server)
+}
+
+func (s *tlsConfigTest) testCertFileAndData(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Server = config.ServerTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{CertFile: "foo"}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{CertData: "bar"}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{CertFile: "foo", CertData: "bar"}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) testKeyFileAndData(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Server = config.ServerTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{KeyFile: "foo"}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{KeyData: "bar"}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{KeyFile: "foo", KeyData: "bar"}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) testClientCAData(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Server = config.ServerTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAData: []string{}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAData: []string{"foo"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAData: []string{"foo", "bar"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAData: []string{"foo", " "}}
+	s.Error(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAData: []string{""}}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) testClientCAFiles(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Server = config.ServerTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAFiles: []string{}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAFiles: []string{"foo"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAFiles: []string{"foo", "bar"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAFiles: []string{"foo", " "}}
+	s.Error(validateRootTLS(cfg))
+	group.Server = config.ServerTLS{ClientCAFiles: []string{""}}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) testRootCAData(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Client = config.ClientTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAData: []string{}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAData: []string{"foo"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAData: []string{"foo", "bar"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAData: []string{"foo", " "}}
+	s.Error(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAData: []string{""}}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) testRootCAFiles(cfg *config.RootTLS, group *config.GroupTLS) {
+
+	group.Client = config.ClientTLS{}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAFiles: []string{}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAFiles: []string{"foo"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAFiles: []string{"foo", "bar"}}
+	s.Nil(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAFiles: []string{"foo", " "}}
+	s.Error(validateRootTLS(cfg))
+	group.Client = config.ClientTLS{RootCAFiles: []string{""}}
+	s.Error(validateRootTLS(cfg))
+}
+
+func (s *tlsConfigTest) TestSystemWorkerTLSConfig() {
+	cfg := &config.RootTLS{}
+	cfg.SystemWorker = config.WorkerTLS{}
+	s.Nil(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{CertFile: "foo"}
+	s.Nil(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{CertData: "bar"}
+	s.Nil(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{CertFile: "foo", CertData: "bar"}
+	s.Error(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{KeyFile: "foo"}
+	s.Nil(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{KeyData: "bar"}
+	s.Nil(validateRootTLS(cfg))
+	cfg.SystemWorker = config.WorkerTLS{KeyFile: "foo", KeyData: "bar"}
+	s.Error(validateRootTLS(cfg))
+
+	cfg.SystemWorker = config.WorkerTLS{Client: config.ClientTLS{}}
+	client := &cfg.SystemWorker.Client
+	client.RootCAData = []string{}
+	s.Nil(validateRootTLS(cfg))
+	client.RootCAData = []string{"foo"}
+	s.Nil(validateRootTLS(cfg))
+	client.RootCAData = []string{"foo", "bar"}
+	s.Nil(validateRootTLS(cfg))
+	client.RootCAData = []string{"foo", " "}
+	s.Error(validateRootTLS(cfg))
+	client.RootCAData = []string{""}
+	s.Error(validateRootTLS(cfg))
+}
