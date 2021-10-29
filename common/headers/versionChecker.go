e5772b78bdc30d90b282f5c7cc5ee948f463c485
Alex Shtin <alex@shtin.com>
2021-10-29 09:30:25 -0700

    Bump version to 1.12.4

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -38,8 +38,8 @@ const (
 	ClientNameJavaSDK = "temporal-java"
 	ClientNameCLI     = "temporal-cli"
 
-	ServerVersion = "1.12.3"
-	CLIVersion    = "1.12.3"
+	ServerVersion = "1.12.4"
+	CLIVersion    = "1.12.4"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
