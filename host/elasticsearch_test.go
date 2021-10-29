fc422185f19bfd9737ada9dadf4b1cbd4c48fa8f
Alex Shtin <alex@temporal.io>
2021-10-28 23:18:45 -0700

    Limit Elasticsearch document _id to 512 bytes (#2112)

--- host/elasticsearch_test.go
+++ host/elasticsearch_test.go
@@ -53,6 +53,7 @@ import (
 	workflowpb "go.temporal.io/api/workflow/v1"
 	"go.temporal.io/api/workflowservice/v1"
 
+	"go.temporal.io/server/common/config"
 	"go.temporal.io/server/common/log/tag"
 	"go.temporal.io/server/common/payload"
 	"go.temporal.io/server/common/payloads"
@@ -1376,6 +1377,24 @@ func (s *elasticsearchIntegrationSuite) TestUpsertWorkflowExecution_InvalidKey()
 	failedEventAttr := workflowTaskFailedEvent.GetWorkflowTaskFailedEventAttributes()
 	s.Equal(enumspb.WORKFLOW_TASK_FAILED_CAUSE_BAD_SEARCH_ATTRIBUTES, failedEventAttr.GetCause())
 	s.NotNil(failedEventAttr.GetFailure())
+}
+
+func (s *elasticsearchIntegrationSuite) Test_LongWorkflowID() {
+	if s.testClusterConfig.Persistence.StoreType == config.StoreTypeSQL {
+		// TODO: remove this when workflow_id field size is increased from varchar(255) in SQL schema.
+		return
+	}
+
+	id := strings.Repeat("a", 1000)
+	wt := "es-integration-long-workflow-id-test-type"
+	tl := "es-integration-long-workflow-id-test-taskqueue"
+	request := s.createStartWorkflowExecutionRequest(id, wt, tl)
+
+	we, err := s.engine.StartWorkflowExecution(NewContext(), request)
+	s.NoError(err)
+
+	query := fmt.Sprintf(`WorkflowId = "%s"`, id)
+	s.testHelperForReadOnce(we.GetRunId(), query, false)
 }
 
 func (s *elasticsearchIntegrationSuite) putIndexSettings(indexName string, maxResultWindowSize int) {
