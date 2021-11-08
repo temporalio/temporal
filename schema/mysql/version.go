0d80301dd4150809d704a3229d6aab5605e21717
yux0 <yu@temporal.io>
2021-11-07 20:32:51 -0800

    Add new table for cluster metadata info and update cluster metadata struct

--- schema/mysql/version.go
+++ schema/mysql/version.go
@@ -24,10 +24,10 @@
 
 package mysql
 
-// NOTE: whenever there is a new data base schema update, plz update the following versions
+// NOTE: whenever there is a new database schema update, plz update the following versions
 
 // Version is the MySQL database release version
-const Version = "1.6"
+const Version = "1.7"
 
 // VisibilityVersion is the MySQL visibility database release version
 const VisibilityVersion = "1.1"
