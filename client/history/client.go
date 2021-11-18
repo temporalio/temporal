309e26bb220f87e6e7d8513637a3284a20297a02
yiminc <yimin.chen@live.com>
2021-11-17 19:28:43 -0800

    Add fan out to history client for GetReplicationStatus

--- client/history/client.go
+++ client/history/client.go
@@ -1053,20 +1053,44 @@ func (c *clientImpl) GetReplicationStatus(
 	request *historyservice.GetReplicationStatusRequest,
 	opts ...grpc.CallOption,
 ) (*historyservice.GetReplicationStatusResponse, error) {
-	// TODO: fan out this request to all history services
-	client, err := c.getClientForShardID(1)
-	var response *historyservice.GetReplicationStatusResponse
-	op := func(ctx context.Context, client historyservice.HistoryServiceClient) error {
-		var err error
-		ctx, cancel := c.createContext(ctx)
-		defer cancel()
-		response, err = client.GetReplicationStatus(ctx, request, opts...)
-		return err
-	}
-	err = c.executeWithRedirect(ctx, client, op)
+	clients, err := c.clients.GetAllClients()
 	if err != nil {
 		return nil, err
 	}
+
+	respChan := make(chan *historyservice.GetReplicationStatusResponse, len(clients))
+	errChan := make(chan error, 1)
+	var wg sync.WaitGroup
+	wg.Add(len(clients))
+	for _, client := range clients {
+		historyClient := client.(historyservice.HistoryServiceClient)
+		go func(client historyservice.HistoryServiceClient) {
+			defer wg.Done()
+			resp, err := historyClient.GetReplicationStatus(ctx, request, opts...)
+			if err != nil {
+				select {
+				case errChan <- err:
+				default:
+				}
+			} else {
+				respChan <- resp
+			}
+		}(historyClient)
+	}
+	wg.Wait()
+	close(respChan)
+	close(errChan)
+
+	response := &historyservice.GetReplicationStatusResponse{}
+	for resp := range respChan {
+		response.Shards = append(response.Shards, resp.Shards...)
+	}
+
+	if len(errChan) > 0 {
+		err = <-errChan
+		return response, err
+	}
+
 	return response, nil
 }
 
