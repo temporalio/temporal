temporalio/temporal

commit 1c52e90b657367d71e4cae4665549011126a927c

Remove unnecessary start version from current execution table (#2236)

schema/mysql/v57/temporal/schema.sql
@@ -52,7 +52,6 @@ CREATE TABLE current_executions(
   create_request_id VARCHAR(64) NOT NULL,
   state INT NOT NULL,
   status INT NOT NULL,
-  start_version BIGINT NOT NULL,
   last_write_version BIGINT NOT NULL,
   PRIMARY KEY (shard_id, namespace_id, workflow_id)
 );

schema/mysql/v57/temporal/versioned/v1.7/no_start_version.sql
@@ -1,0 +1,1 @@
+ALTER TABLE current_executions DROP start_version;

schema/postgresql/v96/temporal/schema.sql
@@ -52,7 +52,6 @@ CREATE TABLE current_executions(
   create_request_id VARCHAR(64) NOT NULL,
   state INTEGER NOT NULL,
   status INTEGER NOT NULL,
-  start_version BIGINT NOT NULL,
   last_write_version BIGINT NOT NULL,
   PRIMARY KEY (shard_id, namespace_id, workflow_id)
 );

schema/postgresql/v96/temporal/versioned/v1.7/no_start_version.sql
@@ -1,0 +1,1 @@
+ALTER TABLE current_executions DROP start_version;

schema/sqlite/v3/temporal/schema.sql
@@ -52,7 +52,6 @@ CREATE TABLE current_executions(
 	create_request_id VARCHAR(64) NOT NULL,
 	state INT NOT NULL,
 	status INT NOT NULL,
-	start_version BIGINT NOT NULL,
 	last_write_version BIGINT NOT NULL,
 	PRIMARY KEY (shard_id, namespace_id, workflow_id)
 );

common/persistence/execution_manager.go
@@ -431,7 +431,6 @@ func (m *executionManagerImpl) SerializeWorkflowMutation(
 		Condition:       input.Condition,
 		DBRecordVersion: input.DBRecordVersion,
 		NextEventID:     input.NextEventID,
-		StartVersion:    input.ExecutionInfo.StartVersion,
 	}
 
 	result.ExecutionInfo, err = m.serializer.WorkflowExecutionInfoToBlob(input.ExecutionInfo, enumspb.ENCODING_TYPE_PROTO3)
@@ -542,8 +541,6 @@ func (m *executionManagerImpl) SerializeWorkflowSnapshot(
 		Condition:       input.Condition,
 		DBRecordVersion: input.DBRecordVersion,
 		NextEventID:     input.NextEventID,
-
-		StartVersion: input.ExecutionInfo.StartVersion,
 	}
 
 	result.ExecutionInfo, err = m.serializer.WorkflowExecutionInfoToBlob(input.ExecutionInfo, enumspb.ENCODING_TYPE_PROTO3)

common/persistence/sql/execution.go
@@ -207,8 +207,8 @@ func (m *sqlExecutionStore) createWorkflowExecutionTx(
 		newWorkflow.ExecutionState.State,
 		newWorkflow.ExecutionState.Status,
 		newWorkflow.ExecutionState.CreateRequestId,
-		newWorkflow.StartVersion,
-		lastWriteVersion); err != nil {
+		lastWriteVersion,
+	); err != nil {
 		return nil, err
 	}
 
@@ -410,7 +410,6 @@ func (m *sqlExecutionStore) updateWorkflowExecutionTx(
 				newWorkflow.ExecutionState.CreateRequestId,
 				newWorkflow.ExecutionState.State,
 				newWorkflow.ExecutionState.Status,
-				newWorkflow.StartVersion,
 				lastWriteVersion,
 			); err != nil {
 				return serviceerror.NewUnavailable(fmt.Sprintf("UpdateWorkflowExecution: failed to continue as new current execution. Error: %v", err))
@@ -428,7 +427,6 @@ func (m *sqlExecutionStore) updateWorkflowExecutionTx(
 				updateWorkflow.ExecutionState.CreateRequestId,
 				updateWorkflow.ExecutionState.State,
 				updateWorkflow.ExecutionState.Status,
-				updateWorkflow.StartVersion,
 				lastWriteVersion,
 			); err != nil {
 				return serviceerror.NewUnavailable(fmt.Sprintf("UpdateWorkflowExecution: failed to update current execution. Error: %v", err))
@@ -520,11 +518,9 @@ func (m *sqlExecutionStore) conflictResolveWorkflowExecutionTx(
 	case p.ConflictResolveWorkflowModeUpdateCurrent:
 		executionState := resetWorkflow.ExecutionState
 		lastWriteVersion := resetWorkflow.LastWriteVersion
-		startVersion := resetWorkflow.StartVersion
 		if newWorkflow != nil {
 			executionState = newWorkflow.ExecutionState
 			lastWriteVersion = newWorkflow.LastWriteVersion
-			startVersion = newWorkflow.StartVersion
 		}
 		runID := primitives.MustParseUUID(executionState.RunId)
 		createRequestID := executionState.CreateRequestId
@@ -544,7 +540,6 @@ func (m *sqlExecutionStore) conflictResolveWorkflowExecutionTx(
 				createRequestID,
 				state,
 				status,
-				startVersion,
 				lastWriteVersion,
 			); err != nil {
 				return serviceerror.NewUnavailable(fmt.Sprintf("ConflictResolveWorkflowExecution. Failed to comare and swap the current record. Error: %v", err))
@@ -563,7 +558,6 @@ func (m *sqlExecutionStore) conflictResolveWorkflowExecutionTx(
 				createRequestID,
 				state,
 				status,
-				startVersion,
 				lastWriteVersion,
 			); err != nil {
 				return serviceerror.NewUnavailable(fmt.Sprintf("ConflictResolveWorkflowExecution. Failed to comare and swap the current record. Error: %v", err))

commit a1a237022a48f207071da52620702df28f21e8f9

Fix forward / backward compatibility issue (#2237)
* Fix forward / backward compatibility issue introduced by #2236

schema/mysql/v57/temporal/schema.sql
@@ -52,6 +52,7 @@ CREATE TABLE current_executions(
   create_request_id VARCHAR(64) NOT NULL,
   state INT NOT NULL,
   status INT NOT NULL,
+  start_version BIGINT NOT NULL DEFAULT 0,
   last_write_version BIGINT NOT NULL,
   PRIMARY KEY (shard_id, namespace_id, workflow_id)
 );

schema/mysql/v57/temporal/versioned/v1.7/no_start_version.sql
@@ -1,1 +1,1 @@
-ALTER TABLE current_executions DROP start_version;
+ALTER TABLE current_executions ALTER COLUMN start_version SET DEFAULT 0;

schema/postgresql/v96/temporal/schema.sql
@@ -52,6 +52,7 @@ CREATE TABLE current_executions(
   create_request_id VARCHAR(64) NOT NULL,
   state INTEGER NOT NULL,
   status INTEGER NOT NULL,
+  start_version BIGINT NOT NULL DEFAULT 0,
   last_write_version BIGINT NOT NULL,
   PRIMARY KEY (shard_id, namespace_id, workflow_id)
 );

schema/postgresql/v96/temporal/versioned/v1.7/no_start_version.sql
@@ -1,1 +1,1 @@
-ALTER TABLE current_executions DROP start_version;
+ALTER TABLE current_executions ALTER COLUMN start_version SET DEFAULT 0;