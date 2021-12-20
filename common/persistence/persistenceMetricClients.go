f9c1eba85d1d351e5d7729dff8cb3f2765b22e4e
Mikhail Gryzykhin <12602502+Ardagan@users.noreply.github.com>
2021-12-20 11:24:33 -0800

    Revert persistence rate limit error type (#2312) (#2315)
    * Correctly handle persistence rate limit error
    
    * revert error type change
    
    Co-authored-by: Yimin Chen <yimin.chen@live.com>

--- common/persistence/persistenceMetricClients.go
+++ common/persistence/persistenceMetricClients.go
@@ -1322,12 +1322,7 @@ func (p *metricEmitter) updateErrorMetric(scope int, err error) {
 		p.metricClient.IncCounter(scope, metrics.PersistenceFailures)
 
 	default:
-		if err == ErrPersistenceLimitExceeded {
-			p.metricClient.IncCounter(scope, metrics.PersistenceErrBusyCounter)
-			p.metricClient.IncCounter(scope, metrics.PersistenceFailures)
-		} else {
-			p.logger.Error("Operation failed with internal error.", tag.Error(err), tag.MetricScope(scope))
-			p.metricClient.IncCounter(scope, metrics.PersistenceFailures)
-		}
+		p.logger.Error("Operation failed with internal error.", tag.Error(err), tag.MetricScope(scope))
+		p.metricClient.IncCounter(scope, metrics.PersistenceFailures)
 	}
 }
