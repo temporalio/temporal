149d6e03ebcf1001a6441eaec6535636d4d7e149
Yimin Chen <yimin.chen@live.com>
2022-01-06 20:43:12 -0800

    Detecting stale cache when RecordChildExecutionCompleted

--- service/history/historyEngine.go
+++ service/history/historyEngine.go
@@ -2257,6 +2257,9 @@ func (e *historyEngineImpl) RecordChildExecutionCompleted(
 
 			// Check mutable state to make sure child execution is in pending child executions
 			ci, isRunning := mutableState.GetChildExecutionInfo(initiatedID)
+			if !isRunning && initiatedID >= mutableState.GetNextEventID() {
+				return nil, consts.ErrStaleState
+			}
 			if !isRunning || ci.StartedId == common.EmptyEventID {
 				return nil, serviceerror.NewNotFound("Pending child execution not found.")
 			}
