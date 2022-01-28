ade82d9b99037b3ffce7272f49c53eaf7c531cf4
Yimin Chen <yimin.chen@live.com>
2022-01-27 20:34:55 -0800

    Allow global namespace update from any cluster

--- common/namespace/handler.go
+++ common/namespace/handler.go
@@ -549,10 +549,6 @@ func (d *HandlerImpl) UpdateNamespace(
 	if configurationChanged && activeClusterChanged && isGlobalNamespace {
 		return nil, errCannotDoNamespaceFailoverAndUpdate
 	} else if configurationChanged || activeClusterChanged || needsNamespacePromotion {
-		if configurationChanged && isGlobalNamespace && !d.clusterMetadata.IsMasterCluster() {
-			return nil, errNotMasterCluster
-		}
-
 		// set the versions
 		if configurationChanged {
 			configVersion++
@@ -581,10 +577,6 @@ func (d *HandlerImpl) UpdateNamespace(
 		if err != nil {
 			return nil, err
 		}
-	} else if isGlobalNamespace && !d.clusterMetadata.IsMasterCluster() {
-		// although there is no attr updated, just prevent customer to use the non master cluster
-		// for update namespace, ever (except if customer want to do a namespace failover)
-		return nil, errNotMasterCluster
 	}
 
 	if isGlobalNamespace {
