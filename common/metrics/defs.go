24f6dc36c9b9438765a76baca3c22f4111c02301
Yichao Yang <yichao@temporal.io>
2022-02-16 10:41:41 -0800

    Emit matching task writer latency and throttle count (#2504)

--- common/metrics/defs.go
+++ common/metrics/defs.go
@@ -2118,6 +2118,8 @@ const (
 	LoadedTaskQueueGauge
 	TaskQueueStartedCounter
 	TaskQueueStoppedCounter
+	TaskWriteThrottlePerTaskQueueCounter
+	TaskWriteLatencyPerTaskQueue
 
 	NumMatchingMetrics
 )
@@ -2573,6 +2575,8 @@ var MetricDefs = map[ServiceIdx]map[int]metricDefinition{
 		LoadedTaskQueueGauge:                      NewGaugeDef("loaded_task_queue_count"),
 		TaskQueueStartedCounter:                   NewCounterDef("task_queue_started"),
 		TaskQueueStoppedCounter:                   NewCounterDef("task_queue_stopped"),
+		TaskWriteThrottlePerTaskQueueCounter:      NewRollupCounterDef("task_write_throttle_count_per_tl", "task_write_throttle_count"),
+		TaskWriteLatencyPerTaskQueue:              NewRollupTimerDef("task_write_latency_per_tl", "task_write_latency"),
 	},
 	Worker: {
 		ReplicatorMessages:                            NewCounterDef("replicator_messages"),
