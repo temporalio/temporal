2a6700db7439a6cfd4f0a3ec0d990bf894431ac3
Alex Shtin <alex@shtin.com>
2022-02-18 10:58:18 -0800

    Minor fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,10 +16,10 @@ fi
 dockerize -template /etc/temporal/config/config_template.yaml:/etc/temporal/config/docker.yaml
 
 # Automatically setup Temporal Server (databases, Elasticsearch, default namespace) if "autosetup" is passed as an argument.
-for arg in "$@" ; do [[ ${arg} == autosetup ]] && /etc/temporal/auto-setup.sh && break ; done
+for arg; do [[ ${arg} == autosetup ]] && /etc/temporal/auto-setup.sh && break ; done
 
 # Setup Temporal Server in development mode if "develop" is passed as an argument.
-for arg in "$@" ; do [[ ${arg} == develop ]] && /etc/temporal/setup-develop.sh && break ; done
+for arg; do [[ ${arg} == develop ]] && /etc/temporal/setup-develop.sh && break ; done
 
 # Run bash instead of Temporal Server if "bash" is passed as an argument (convenient to debug docker image).
 for arg; do [[ ${arg} == bash ]] && bash && exit 0 ; done
