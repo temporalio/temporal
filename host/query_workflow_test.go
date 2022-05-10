e8ea4e1fe2c35968b506e628d7820e30b7da49e5
Yimin Chen <yimin.chen@live.com>
2022-05-10 12:36:23 -0700

    Add integrationt est

--- host/query_workflow_test.go
+++ host/query_workflow_test.go
@@ -26,11 +26,15 @@ package host
 
 import (
 	"context"
+	"sync"
 	"sync/atomic"
 	"time"
 
 	sdkclient "go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/worker"
 	"go.temporal.io/sdk/workflow"
+	"go.temporal.io/server/service/history/consts"
 
 	"go.temporal.io/server/common/log/tag"
 )
@@ -137,4 +141,116 @@ func (s *clientIntegrationSuite) TestQueryWorkflow_Consistent_PiggybackQuery() {
 
 	// verify query sees all signals before it
 	s.Equal("pauseabc", queryResultStr)
+}
+
+func (s *clientIntegrationSuite) TestQueryWorkflow_QueryWhileBackoff() {
+	firstWorkflowDone := make(chan struct{}, 1)
+	workflowFn := func(ctx workflow.Context) (string, error) {
+		workflow.SetQueryHandler(ctx, "test", func() (string, error) {
+			return "should-reach-here", nil
+		})
+
+		// only for test: to notify the test is ready for a query
+		select {
+		case firstWorkflowDone <- struct{}{}:
+		default:
+		}
+
+		return "", temporal.NewApplicationError("retry-me", "test-error")
+	}
+
+	s.worker.RegisterWorkflow(workflowFn)
+
+	id := "test-query-before-backoff"
+	workflowOptions := sdkclient.StartWorkflowOptions{
+		ID:                 id,
+		TaskQueue:          s.taskQueue,
+		WorkflowRunTimeout: 20 * time.Second,
+		RetryPolicy: &temporal.RetryPolicy{
+			InitialInterval: 10 * time.Second,
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	workflowRun, err := s.sdkClient.ExecuteWorkflow(ctx, workflowOptions, workflowFn)
+	if err != nil {
+		s.Logger.Fatal("Start workflow failed with err", tag.Error(err))
+	}
+
+	s.NotNil(workflowRun)
+	s.True(workflowRun.GetRunID() != "")
+
+	// block until first workflow is done
+	select {
+	case <-firstWorkflowDone:
+		// unblock, no-op
+	case <-time.After(time.Second * 5):
+		s.Logger.Fatal("first workflow is not done in 5s")
+	}
+
+	_, err = s.sdkClient.QueryWorkflow(ctx, id, "", "test")
+	s.Error(err)
+	s.ErrorContains(err, consts.ErrWorkflowTaskNotScheduled.Error())
+}
+
+func (s *clientIntegrationSuite) TestQueryWorkflow_QueryBeforeStart() {
+	// stop the worker, so the workflow won't be started before query
+	s.worker.Stop()
+
+	workflowFn := func(ctx workflow.Context) (string, error) {
+		status := "initialized"
+		workflow.SetQueryHandler(ctx, "test", func() (string, error) {
+			return status, nil
+		})
+
+		status = "started"
+		workflow.Sleep(ctx, time.Hour)
+		return "", nil
+	}
+
+	id := "test-query-before-start"
+	workflowOptions := sdkclient.StartWorkflowOptions{
+		ID:                 id,
+		TaskQueue:          s.taskQueue,
+		WorkflowRunTimeout: 20 * time.Second,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	workflowRun, err := s.sdkClient.ExecuteWorkflow(ctx, workflowOptions, workflowFn)
+	if err != nil {
+		s.Logger.Fatal("Start workflow failed with err", tag.Error(err))
+	}
+
+	s.NotNil(workflowRun)
+	s.True(workflowRun.GetRunID() != "")
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		startTime := time.Now()
+		queryResult, err := s.sdkClient.QueryWorkflow(ctx, id, "", "test")
+		endTime := time.Now()
+		s.NoError(err)
+		var queryResultStr string
+		err = queryResult.Get(&queryResultStr)
+		s.NoError(err)
+
+		// verify query sees all signals before it
+		s.Equal("started", queryResultStr)
+
+		s.True(endTime.Sub(startTime) > time.Second)
+	}()
+
+	// delay 2s to start worker, this will block query for 2s
+	time.Sleep(time.Second * 2)
+	s.worker = worker.New(s.sdkClient, s.taskQueue, worker.Options{})
+	s.worker.RegisterWorkflow(workflowFn)
+	if err := s.worker.Start(); err != nil {
+		s.Logger.Fatal("Error when start worker", tag.Error(err))
+	}
+
+	// wait query
+	wg.Wait()
 }
