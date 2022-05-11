d789dfbb76158120576e6bffea3f59f5542c233e
wenquan xing <wenquan.xing@gmail.com>
2022-05-10 19:52:53 -0700

    Enforce timeout when updating shard

--- service/history/shard/context_impl.go
+++ service/history/shard/context_impl.go
@@ -79,6 +79,10 @@ const (
 	contextStateStopped
 )
 
+const (
+	shardIOTimeout = 5 * time.Second
+)
+
 type (
 	contextState int32
 
@@ -1123,7 +1127,9 @@ func (s *ContextImpl) renewRangeLocked(isStealing bool) error {
 		updatedShardInfo.StolenSinceRenew++
 	}
 
-	err := s.persistenceShardManager.UpdateShard(context.TODO(), &persistence.UpdateShardRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), shardIOTimeout)
+	defer cancel()
+	err := s.persistenceShardManager.UpdateShard(ctx, &persistence.UpdateShardRequest{
 		ShardInfo:       updatedShardInfo.ShardInfo,
 		PreviousRangeID: s.shardInfo.GetRangeId()})
 	if err != nil {
@@ -1173,7 +1179,9 @@ func (s *ContextImpl) updateShardInfoLocked() error {
 	updatedShardInfo := copyShardInfo(s.shardInfo)
 	s.emitShardInfoMetricsLogsLocked()
 
-	err = s.persistenceShardManager.UpdateShard(context.TODO(), &persistence.UpdateShardRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), shardIOTimeout)
+	defer cancel()
+	err = s.persistenceShardManager.UpdateShard(ctx, &persistence.UpdateShardRequest{
 		ShardInfo:       updatedShardInfo.ShardInfo,
 		PreviousRangeID: s.shardInfo.GetRangeId(),
 	})
@@ -1623,7 +1631,9 @@ func (s *ContextImpl) loadShardMetadata(ownershipChanged *bool) error {
 	s.rUnlock()
 
 	// We don't have any shardInfo yet, load it (outside of context rwlock)
-	resp, err := s.persistenceShardManager.GetOrCreateShard(context.TODO(), &persistence.GetOrCreateShardRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), shardIOTimeout)
+	defer cancel()
+	resp, err := s.persistenceShardManager.GetOrCreateShard(ctx, &persistence.GetOrCreateShardRequest{
 		ShardID:          s.shardID,
 		LifecycleContext: s.lifecycleCtx,
 	})
