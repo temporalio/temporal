b5613ca79fc42b24bf69aea6e649d4757bb3a773
David Reiss <david@temporal.io>
2022-06-01 01:07:24 -0700

    Add gomock mocks for SDK Client and Worker

---
+++ common/testing/mocksdk/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# These mocks need to be manually fixed up after generation because gomock
+# uses the types in the internal package instead of the public type aliases.
+mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE go.temporal.io/sdk/client Client | sed \
+	-e 's,internal,client,g' \
+	-e 's,client "go.temporal.io/sdk/client","go.temporal.io/sdk/client",' \
+	> client_mock.go
+mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE go.temporal.io/sdk/worker Worker | sed \
+	-e 's,internal.RegisterWorkflowOptions,workflow.RegisterOptions,g' \
+	-e 's,internal.RegisterActivityOptions,activity.RegisterOptions,g' \
+	-e 's,internal "go.temporal.io/sdk/internal",workflow "go.temporal.io/sdk/workflow"\
+	activity "go.temporal.io/sdk/activity",' \
+	> worker_mock.go
