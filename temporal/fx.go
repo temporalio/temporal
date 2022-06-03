b1fedf94facfee385b15f43c2056028e8f0c9f29
Matt McShane <matt.mcshane@temporal.io>
2022-06-03 15:37:48 -0400

    rebase fixups

--- temporal/fx.go
+++ temporal/fx.go
@@ -32,7 +32,6 @@ import (
 
 	"go.temporal.io/server/service/history/replication"
 
-	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/propagation"
