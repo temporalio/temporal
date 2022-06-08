e570cb62c65e6bf8cac83f925d8b144e0d3a3599
Alex Shtin <alex@shtin.com>
2022-06-08 11:34:15 -0700

    Remove extra namespace checks

--- service/frontend/workflowHandler.go
+++ service/frontend/workflowHandler.go
@@ -4270,9 +4270,6 @@ func (wh *WorkflowHandler) makeFakeContinuedAsNewEvent(
 func (wh *WorkflowHandler) validateNamespace(
 	namespace string,
 ) error {
-	if namespace == "" {
-		return errNamespaceNotSet
-	}
 	if err := wh.validateUTF8String(namespace); err != nil {
 		return err
 	}
