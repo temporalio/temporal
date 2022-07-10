3c8b3282d7d2e6a517f5568471799108a4bde8bd
Yimin Chen <yimin.chen@live.com>
2022-07-10 08:59:08 -0700

    Reorder frontend rate limiter

--- service/frontend/fx.go
+++ service/frontend/fx.go
@@ -165,9 +165,9 @@ func GrpcServerOptionsProvider(
 		metrics.NewServerMetricsContextInjectorInterceptor(),
 		telemetryInterceptor.Intercept,
 		namespaceValidatorInterceptor.Intercept,
-		rateLimitInterceptor.Intercept,
-		namespaceRateLimiterInterceptor.Intercept,
 		namespaceCountLimiterInterceptor.Intercept,
+		namespaceRateLimiterInterceptor.Intercept,
+		rateLimitInterceptor.Intercept,
 		authorization.NewAuthorizationInterceptor(
 			claimMapper,
 			authorizer,
