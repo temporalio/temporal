8c24825571a129ed9bb319dd0fc146bb160b1233
Yu Xia <yuhsia89@gmail.com>
2022-07-27 09:19:28 -0700

    Bring back the task complete loop (#3151)
    * Bring back the task complete loop

--- service/history/replication/task_processor_manager.go
+++ service/history/replication/task_processor_manager.go
@@ -28,11 +28,13 @@ import (
 	"context"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	"go.temporal.io/server/api/historyservice/v1"
 	"go.temporal.io/server/client"
 	"go.temporal.io/server/client/history"
 	"go.temporal.io/server/common"
+	"go.temporal.io/server/common/backoff"
 	"go.temporal.io/server/common/cluster"
 	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/log/tag"
@@ -131,6 +133,7 @@ func (r *taskProcessorManagerImpl) Start() {
 
 	// Listen to cluster metadata and dynamically update replication processor for remote clusters.
 	r.listenToClusterMetadataChange()
+	go r.completeReplicationTaskLoop()
 }
 
 func (r *taskProcessorManagerImpl) Stop() {
@@ -202,6 +205,30 @@ func (r *taskProcessorManagerImpl) handleClusterMetadataUpdate(
 			)
 			replicationTaskProcessor.Start()
 			r.taskProcessors[clusterName] = replicationTaskProcessor
+		}
+	}
+}
+
+func (r *taskProcessorManagerImpl) completeReplicationTaskLoop() {
+	shardID := r.shard.GetShardID()
+	cleanupTimer := time.NewTimer(backoff.JitDuration(
+		r.config.ReplicationTaskProcessorCleanupInterval(shardID),
+		r.config.ReplicationTaskProcessorCleanupJitterCoefficient(shardID),
+	))
+	defer cleanupTimer.Stop()
+	for {
+		select {
+		case <-cleanupTimer.C:
+			if err := r.cleanupReplicationTasks(); err != nil {
+				r.logger.Error("Failed to clean up replication messages.", tag.Error(err))
+				r.metricsClient.Scope(metrics.ReplicationTaskCleanupScope).IncCounter(metrics.ReplicationTaskCleanupFailure)
+			}
+			cleanupTimer.Reset(backoff.JitDuration(
+				r.config.ReplicationTaskProcessorCleanupInterval(shardID),
+				r.config.ReplicationTaskProcessorCleanupJitterCoefficient(shardID),
+			))
+		case <-r.shutdownChan:
+			return
 		}
 	}
 }
