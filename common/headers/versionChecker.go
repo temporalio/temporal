7f72106381e5ded2315b02681054045f589f836d
Yichao Yang <yichao@temporal.io>
2022-08-12 12:33:14 -0700

    Prepare 1.17.3 patch

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -44,7 +44,7 @@ const (
 	ClientNameCLI           = "temporal-cli"
 	ClientNameUI            = "temporal-ui"
 
-	ServerVersion = "1.17.2"
+	ServerVersion = "1.17.3"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
