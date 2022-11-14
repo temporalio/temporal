50936e1d5ee8528c1ba100079f32dfab0d56bf8b
Michael Snowden <MichaelSnowden@users.noreply.github.com>
2022-11-14 14:13:06 -0800

    Add a tool to limit the number of pending child workflows (#3575)

--- common/metrics/metric_defs.go
+++ common/metrics/metric_defs.go
@@ -1345,6 +1345,8 @@ var (
 	HistoryCount                                  = NewDimensionlessHistogramDef("history_count")
 	SearchAttributesSize                          = NewBytesHistogramDef("search_attributes_size")
 	MemoSize                                      = NewBytesHistogramDef("memo_size")
+	NumPendingChildWorkflowsHigh                  = NewCounterDef("num_pending_child_workflows_high")
+	NumPendingChildWorkflowsTooHigh               = NewCounterDef("num_pending_child_workflows_too_high")
 
 	// Frontend
 	AddSearchAttributesWorkflowSuccessCount  = NewCounterDef("add_search_attributes_workflow_success")
