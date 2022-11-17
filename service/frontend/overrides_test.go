d7e1cb43aba801b2755c2e825311211aada35901
Roey Berman <roey.berman@gmail.com>
2022-11-16 16:30:10 -0800

    Disable eager activity dispatch for buggy clients

---
+++ service/frontend/overrides_test.go
@@ -0,0 +1,42 @@
+package frontend
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.temporal.io/api/command/v1"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/common/headers"
+)
+
+func TestDisableEagerActivityDispatchForBuggyClients(t *testing.T) {
+	overrides := NewOverrides()
+
+	type Case struct {
+		sdkVersion   string
+		sdkName      string
+		eagerAllowed bool
+	}
+
+	cases := []Case{
+		{sdkName: headers.ClientNameGoSDK, sdkVersion: "1.18.1", eagerAllowed: true},
+		{sdkName: headers.ClientNameTypeScriptSDK, sdkVersion: "1.4.1", eagerAllowed: false},
+		{sdkName: headers.ClientNameTypeScriptSDK, sdkVersion: "1.4.4", eagerAllowed: true},
+		{sdkName: headers.ClientNamePythonSDK, sdkVersion: "0.1b2", eagerAllowed: false},
+		{sdkName: headers.ClientNamePythonSDK, sdkVersion: "0.1b1", eagerAllowed: true},
+		{sdkName: headers.ClientNamePythonSDK, sdkVersion: "0.1b3", eagerAllowed: true},
+	}
+	for _, testCase := range cases {
+		ctx := headers.SetVersionsForTests(context.Background(), testCase.sdkVersion, testCase.sdkName, headers.SupportedServerVersions, headers.AllFeatures)
+		req := &workflowservice.RespondWorkflowTaskCompletedRequest{
+			Commands: []*command.Command{
+				{Attributes: &command.Command_ScheduleActivityTaskCommandAttributes{ScheduleActivityTaskCommandAttributes: &command.ScheduleActivityTaskCommandAttributes{RequestEagerExecution: true}}},
+			},
+		}
+		overrides.DisableEagerActivityDispatchForBuggyClients(ctx, req)
+
+		assert.Equal(t, req.GetCommands()[0].GetScheduleActivityTaskCommandAttributes().GetRequestEagerExecution(), testCase.eagerAllowed)
+	}
+}
