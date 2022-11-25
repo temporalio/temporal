12903163bb19b5ecbdcdd6cad0ad2e2942193e4a
Yichao Yang <yichao@temporal.io>
2022-11-25 12:13:03 -0800

    fix sqlite schedule integration test

--- host/schedule_test.go
+++ host/schedule_test.go
@@ -90,11 +90,11 @@ func TestScheduleIntegrationSuite(t *testing.T) {
 }
 
 func (s *scheduleIntegrationSuite) SetupSuite() {
-	if TestFlags.PersistenceDriver != sqlite.PluginName {
+	if TestFlags.PersistenceDriver == sqlite.PluginName {
 		// sqlite tests are run without elasticsearch
-		s.setupSuite("testdata/schedule_integration_test_cluster_sqlite.yaml")
+		s.setupSuite("testdata/schedule_integration_test_cluster_std_vis.yaml")
 	} else {
-		s.setupSuite("testdata/schedule_integration_test_cluster.yaml")
+		s.setupSuite("testdata/schedule_integration_test_cluster_adv_vis.yaml")
 	}
 	s.hostPort = "127.0.0.1:7134"
 	if TestFlags.FrontendAddr != "" {
