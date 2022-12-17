7fdb8c40648298d5f33bc32a0cabe7a5508c1b40
Yichao Yang <yichao@temporal.io>
2022-12-16 16:30:53 -0800

    fix tests

--- service/history/ndc/workflow_resetter_test.go
+++ service/history/ndc/workflow_resetter_test.go
@@ -184,16 +184,6 @@ func (s *workflowResetterSuite) TestPersistToDB_CurrentTerminated() {
 		gomock.Any(),
 		workflow.TransactionPolicyActive,
 	).Return(resetSnapshot, resetEventsSeq, nil)
-	resetMutableState.EXPECT().GetNamespaceEntry().Return(namespace.FromPersistentState(&persistence.GetNamespaceResponse{
-		Namespace: &persistencespb.NamespaceDetail{
-			Info:   &persistencespb.NamespaceInfo{},
-			Config: &persistencespb.NamespaceConfig{},
-			ReplicationConfig: &persistencespb.NamespaceReplicationConfig{
-				ActiveClusterName: "active-cluster-name",
-			},
-		},
-		IsGlobalNamespace: false,
-	}))
 	resetContext.EXPECT().GetHistorySize().Return(resetEventsSize).AnyTimes()
 	resetContext.EXPECT().SetHistorySize(resetEventsSize + resetNewEventsSize)
 
