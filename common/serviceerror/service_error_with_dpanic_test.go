398945dd51d8b7232ae89cba8c150c3901c05c7b
Yujie Li <yujie.li@temporal.io>
2023-01-11 10:01:21 -0800

    address some comments

--- common/serviceerror/service_error_with_dpanic_test.go
+++ common/serviceerror/service_error_with_dpanic_test.go
@@ -25,61 +25,50 @@
 package serviceerror
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"go.temporal.io/api/serviceerror"
 	"go.temporal.io/server/common/log"
-	"go.temporal.io/server/tests/testhelper"
 )
 
-type ServiceErrorWithDPanicSuite struct {
+type serviceErrorWithDPanicSuite struct {
 	*require.Assertions
 	suite.Suite
 }
 
 func TestServiceErrorWithDPanicSuite(t *testing.T) {
-	suite.Run(t, new(ServiceErrorWithDPanicSuite))
+	suite.Run(t, new(serviceErrorWithDPanicSuite))
 }
 
-func (s *ServiceErrorWithDPanicSuite) SetupTest() {
+func (s *serviceErrorWithDPanicSuite) SetupTest() {
 	s.Assertions = require.New(s.T())
 }
 
-func (s *ServiceErrorWithDPanicSuite) TestNewDPanicInProd() {
-	dir := testhelper.MkdirTemp(s.T(), "", "config.testNewLogger")
-
+func (s *serviceErrorWithDPanicSuite) TestNewDPanicInProd() {
 	cfg := log.Config{
-		Level:      "info",
-		OutputFile: dir + "/test.logger",
+		Level: "info",
 	}
 
 	logger := log.NewZapLogger(log.BuildZapLogger(cfg))
 	s.NotNil(logger)
-	_, err := os.Stat(dir + "/test.logger")
-	s.Nil(err)
 
-	err = ServiceErrorWithDPanic(logger, "Must panic!")
+	err := ServiceErrorWithDPanic(logger, "Must not panic!")
 	s.NotNil(err)
 	_, ok := err.(*serviceerror.Internal)
 	s.True(ok)
 }
 
-func (s *ServiceErrorWithDPanicSuite) TestNewDPanicInDev() {
-	dir := testhelper.MkdirTemp(s.T(), "", "config.testNewLogger")
-
+func (s *serviceErrorWithDPanicSuite) TestNewDPanicInDev() {
 	cfg := log.Config{
 		Level:       "info",
-		OutputFile:  dir + "/test.logger",
 		Development: true,
 	}
 
 	logger := log.NewZapLogger(log.BuildZapLogger(cfg))
 	s.NotNil(logger)
-	_, err := os.Stat(dir + "/test.logger")
-	s.Nil(err)
+
 	s.Panics(nil, func() {
 		err := ServiceErrorWithDPanic(logger, "Must panic!")
 		s.Nil(err)
