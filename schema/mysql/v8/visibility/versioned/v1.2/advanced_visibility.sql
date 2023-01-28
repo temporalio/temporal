2294552ac0af7d348372becf2c8a22f022a915e6
rodrigozhou <rodrigo.zhou@temporal.io>
2023-01-28 00:45:09 -0800

    MySQL 8 schema changes to support advanced visibility

--- schema/mysql/v8/visibility/versioned/v1.2/advanced_visibility.sql
+++ schema/mysql/v8/visibility/versioned/v1.2/advanced_visibility.sql
@@ -1,45 +1,30 @@
-CREATE TABLE executions_visibility (
-  namespace_id        CHAR(64)      NOT NULL,
-  run_id              CHAR(64)      NOT NULL,
-  start_time          DATETIME(6)   NOT NULL,
-  execution_time      DATETIME(6)   NOT NULL,
-  workflow_id         VARCHAR(255)  NOT NULL,
-  workflow_type_name  VARCHAR(255)  NOT NULL,
-  status              INT           NOT NULL,  -- enum WorkflowExecutionStatus {RUNNING, COMPLETED, FAILED, CANCELED, TERMINATED, CONTINUED_AS_NEW, TIMED_OUT}
-  close_time          DATETIME(6)   NULL,
-  history_length      BIGINT        NULL,
-  memo                BLOB          NULL,
-  encoding            VARCHAR(64)   NOT NULL,
-  task_queue          VARCHAR(255)  NOT NULL DEFAULT '',
-  search_attributes   JSON          NULL,
-
-  -- Each search attribute has its own generated column.
-  -- For string types (keyword and text), we need to unquote the json string,
-  -- ie., use `->>` instead of `->` operator.
-  -- For text types, the generated column need to be STORED instead of VIRTUAL,
-  -- so we can create a full-text search index.
-  -- For datetime type, MySQL can't cast datetime string with timezone to
-  -- datetime type directly, so we need to call CONVERT_TZ to convert to UTC.
-  -- Check the `custom_search_attributes` table for complete set of examples.
-
-  -- Predefined search attributes
-  TemporalChangeVersion         JSON          GENERATED ALWAYS AS (search_attributes->"$.TemporalChangeVersion"),
-  BinaryChecksums               JSON          GENERATED ALWAYS AS (search_attributes->"$.BinaryChecksums"),
-  BatcherUser                   VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.BatcherUser"),
-  TemporalScheduledStartTime    DATETIME(6)   GENERATED ALWAYS AS (
+-- Add search attributes related columns
+ALTER TABLE executions_visibility
+  ADD COLUMN search_attributes          JSON NULL,
+  ADD COLUMN TemporalChangeVersion      JSON          GENERATED ALWAYS AS (search_attributes->"$.TemporalChangeVersion"),
+  ADD COLUMN BinaryChecksums            JSON          GENERATED ALWAYS AS (search_attributes->"$.BinaryChecksums"),
+  ADD COLUMN BatcherUser                VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.BatcherUser"),
+  ADD COLUMN TemporalScheduledStartTime DATETIME(6)   GENERATED ALWAYS AS (
     CONVERT_TZ(
       REGEXP_REPLACE(search_attributes->>"$.TemporalScheduledStartTime", 'Z|[+-][0-9]{2}:[0-9]{2}$', ''),
       SUBSTR(REPLACE(search_attributes->>"$.TemporalScheduledStartTime", 'Z', '+00:00'), -6, 6),
       '+00:00'
     )
   ),
-  TemporalScheduledById         VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.TemporalScheduledById"),
-  TemporalSchedulePaused        BOOLEAN       GENERATED ALWAYS AS (search_attributes->"$.TemporalSchedulePaused"),
-  TemporalNamespaceDivision     VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.TemporalNamespaceDivision"),
+  ADD COLUMN TemporalScheduledById      VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.TemporalScheduledById"),
+  ADD COLUMN TemporalSchedulePaused     BOOLEAN       GENERATED ALWAYS AS (search_attributes->"$.TemporalSchedulePaused"),
+  ADD COLUMN TemporalNamespaceDivision  VARCHAR(255)  GENERATED ALWAYS AS (search_attributes->>"$.TemporalNamespaceDivision");
 
-  PRIMARY KEY (namespace_id, run_id)
-);
+-- Drop existing indexes
+DROP INDEX by_type_start_time         ON executions_visibility;
+DROP INDEX by_workflow_id_start_time  ON executions_visibility;
+DROP INDEX by_status_by_start_time    ON executions_visibility;
+DROP INDEX by_type_close_time         ON executions_visibility;
+DROP INDEX by_workflow_id_close_time  ON executions_visibility;
+DROP INDEX by_status_by_close_time    ON executions_visibility;
+DROP INDEX by_close_time_by_status    ON executions_visibility;
 
+-- Create new indexes
 CREATE INDEX default_idx        ON executions_visibility (namespace_id, (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
 CREATE INDEX by_execution_time  ON executions_visibility (namespace_id, execution_time,     (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
 CREATE INDEX by_workflow_id     ON executions_visibility (namespace_id, workflow_id,        (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
@@ -58,6 +43,7 @@ CREATE INDEX by_temporal_schedule_paused      ON executions_visibility (namespac
 CREATE INDEX by_temporal_namespace_division   ON executions_visibility (namespace_id, TemporalNamespaceDivision,  (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
 
 
+-- Custom search attributes
 CREATE TABLE custom_search_attributes (
   namespace_id      CHAR(64)  NOT NULL,
   run_id            CHAR(64)  NOT NULL,
