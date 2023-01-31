38aae230f3c03b0914f98c65b4b9b7761bf30269
rodrigozhou <rodrigo.zhou@temporal.io>
2023-01-30 17:48:53 -0800

    Update search attributes operator commands to handle aliasing

--- service/frontend/operator_handler.go
+++ service/frontend/operator_handler.go
@@ -36,8 +36,10 @@ import (
 
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
+	namespacepb "go.temporal.io/api/namespace/v1"
 	"go.temporal.io/api/operatorservice/v1"
 	"go.temporal.io/api/serviceerror"
+	"go.temporal.io/api/workflowservice/v1"
 	sdkclient "go.temporal.io/sdk/client"
 
 	"go.temporal.io/server/api/adminservice/v1"
@@ -45,6 +47,7 @@ import (
 	persistencespb "go.temporal.io/server/api/persistence/v1"
 	svc "go.temporal.io/server/client"
 	"go.temporal.io/server/client/admin"
+	"go.temporal.io/server/client/frontend"
 	"go.temporal.io/server/common"
 	clustermetadata "go.temporal.io/server/common/cluster"
 	"go.temporal.io/server/common/log"
@@ -52,10 +55,12 @@ import (
 	"go.temporal.io/server/common/namespace"
 	"go.temporal.io/server/common/persistence"
 	"go.temporal.io/server/common/persistence/visibility/manager"
+	"go.temporal.io/server/common/persistence/visibility/store/elasticsearch"
 	esclient "go.temporal.io/server/common/persistence/visibility/store/elasticsearch/client"
 	"go.temporal.io/server/common/primitives"
 	"go.temporal.io/server/common/sdk"
 	"go.temporal.io/server/common/searchattribute"
+	"go.temporal.io/server/common/util"
 	"go.temporal.io/server/service/worker"
 	"go.temporal.io/server/service/worker/addsearchattributes"
 	"go.temporal.io/server/service/worker/deletenamespace"
@@ -151,7 +156,10 @@ func (h *OperatorHandlerImpl) Stop() {
 	}
 }
 
-func (h *OperatorHandlerImpl) AddSearchAttributes(ctx context.Context, request *operatorservice.AddSearchAttributesRequest) (_ *operatorservice.AddSearchAttributesResponse, retError error) {
+func (h *OperatorHandlerImpl) AddSearchAttributes(
+	ctx context.Context,
+	request *operatorservice.AddSearchAttributesRequest,
+) (_ *operatorservice.AddSearchAttributesResponse, retError error) {
 	defer log.CapturePanic(h.logger, &retError)
 
 	scope, startTime := h.startRequestProfile(metrics.OperatorAddSearchAttributesScope)
@@ -167,7 +175,6 @@ func (h *OperatorHandlerImpl) AddSearchAttributes(ctx context.Context, request *
 	}
 
 	indexName := h.visibilityMgr.GetIndexName()
-
 	currentSearchAttributes, err := h.saProvider.GetSearchAttributes(indexName, true)
 	if err != nil {
 		return nil, serviceerror.NewUnavailable(fmt.Sprintf(errUnableToGetSearchAttributesMessage, err))
@@ -185,6 +192,25 @@ func (h *OperatorHandlerImpl) AddSearchAttributes(ctx context.Context, request *
 		}
 	}
 
+	if h.visibilityMgr.GetName() == elasticsearch.PersistenceName {
+		err = h.addSearchAttributesElasticSearch(ctx, request, indexName)
+	} else {
+		err = h.addSearchAttributesSQL(ctx, request, currentSearchAttributes)
+	}
+
+	if err != nil {
+		scope.Counter(metrics.AddSearchAttributesWorkflowFailuresCount.GetMetricName()).Record(1)
+		return nil, err
+	}
+	scope.Counter(metrics.AddSearchAttributesWorkflowSuccessCount.GetMetricName()).Record(1)
+	return &operatorservice.AddSearchAttributesResponse{}, nil
+}
+
+func (h *OperatorHandlerImpl) addSearchAttributesElasticSearch(
+	ctx context.Context,
+	request *operatorservice.AddSearchAttributesRequest,
+	indexName string,
+) error {
 	// Execute workflow.
 	wfParams := addsearchattributes.WorkflowParams{
 		CustomAttributesToAdd: request.GetSearchAttributes(),
@@ -203,22 +229,89 @@ func (h *OperatorHandlerImpl) AddSearchAttributes(ctx context.Context, request *
 		wfParams,
 	)
 	if err != nil {
-		return nil, serviceerror.NewUnavailable(fmt.Sprintf(errUnableToStartWorkflowMessage, addsearchattributes.WorkflowName, err))
+		return serviceerror.NewUnavailable(
+			fmt.Sprintf(errUnableToStartWorkflowMessage, addsearchattributes.WorkflowName, err),
+		)
 	}
 
 	// Wait for workflow to complete.
 	err = run.Get(ctx, nil)
 	if err != nil {
-		scope.Counter(metrics.AddSearchAttributesWorkflowFailuresCount.GetMetricName()).Record(1)
-		execution := &commonpb.WorkflowExecution{WorkflowId: addsearchattributes.WorkflowName, RunId: run.GetRunID()}
-		return nil, serviceerror.NewSystemWorkflow(execution, err)
+		execution := &commonpb.WorkflowExecution{
+			WorkflowId: addsearchattributes.WorkflowName,
+			RunId:      run.GetRunID(),
+		}
+		return serviceerror.NewSystemWorkflow(execution, err)
 	}
-	scope.Counter(metrics.AddSearchAttributesWorkflowSuccessCount.GetMetricName()).Record(1)
-
-	return &operatorservice.AddSearchAttributesResponse{}, nil
+	return nil
 }
 
-func (h *OperatorHandlerImpl) RemoveSearchAttributes(ctx context.Context, request *operatorservice.RemoveSearchAttributesRequest) (_ *operatorservice.RemoveSearchAttributesResponse, retError error) {
+func (h *OperatorHandlerImpl) addSearchAttributesSQL(
+	ctx context.Context,
+	request *operatorservice.AddSearchAttributesRequest,
+	currentSearchAttributes searchattribute.NameTypeMap,
+) error {
+	client, err := h.clientFactory.NewLocalFrontendClientWithTimeout(
+		frontend.DefaultTimeout,
+		frontend.DefaultLongPollTimeout,
+	)
+	if err != nil {
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToCreateFrontendClientMessage, err))
+	}
+
+	nsName := request.GetNamespace()
+	if nsName == "" {
+		return errNamespaceNotSet
+	}
+	ns, err := h.namespaceRegistry.GetNamespace(namespace.Name(nsName))
+	if err != nil {
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToGetNamespaceInfoMessage, nsName))
+	}
+
+	customSearchAttributes := currentSearchAttributes.Custom()
+	mapper := ns.CustomSearchAttributesMapper()
+	fieldToAliasMap := util.CloneMapNonNil(mapper.FieldToAliasMap())
+	for saName, saType := range request.GetSearchAttributes() {
+		// check if alias is already in use
+		if _, err := mapper.GetFieldName(saName, nsName); err == nil {
+			return serviceerror.NewAlreadyExist(
+				fmt.Sprintf(errSearchAttributeAlreadyExistsMessage, saName),
+			)
+		}
+		// find the first available field for the given type
+		targetFieldName := ""
+		cntUsed := 0
+		for fieldName, fieldType := range customSearchAttributes {
+			if fieldType != saType {
+				continue
+			}
+			if _, ok := fieldToAliasMap[fieldName]; !ok {
+				targetFieldName = fieldName
+				break
+			}
+			cntUsed++
+		}
+		if targetFieldName == "" {
+			return serviceerror.NewInvalidArgument(
+				fmt.Sprintf(errTooManySearchAttributesMessage, cntUsed, saType.String()),
+			)
+		}
+		fieldToAliasMap[targetFieldName] = saName
+	}
+
+	_, err = client.UpdateNamespace(ctx, &workflowservice.UpdateNamespaceRequest{
+		Namespace: nsName,
+		Config: &namespacepb.NamespaceConfig{
+			CustomSearchAttributeAliases: fieldToAliasMap,
+		},
+	})
+	return err
+}
+
+func (h *OperatorHandlerImpl) RemoveSearchAttributes(
+	ctx context.Context,
+	request *operatorservice.RemoveSearchAttributesRequest,
+) (_ *operatorservice.RemoveSearchAttributesResponse, retError error) {
 	defer log.CapturePanic(h.logger, &retError)
 
 	// validate request
@@ -230,34 +323,94 @@ func (h *OperatorHandlerImpl) RemoveSearchAttributes(ctx context.Context, reques
 		return nil, errSearchAttributesNotSet
 	}
 
+	var err error
 	indexName := h.visibilityMgr.GetIndexName()
+	if h.visibilityMgr.GetName() == elasticsearch.PersistenceName {
+		err = h.removeSearchAttributesElasticsearch(ctx, request, indexName)
+	} else {
+		err = h.removeSearchAttributesSQL(ctx, request)
+	}
 
+	if err != nil {
+		return nil, err
+	}
+	return &operatorservice.RemoveSearchAttributesResponse{}, nil
+}
+
+func (h *OperatorHandlerImpl) removeSearchAttributesElasticsearch(
+	ctx context.Context,
+	request *operatorservice.RemoveSearchAttributesRequest,
+	indexName string,
+) error {
 	currentSearchAttributes, err := h.saProvider.GetSearchAttributes(indexName, true)
 	if err != nil {
-		return nil, serviceerror.NewUnavailable(fmt.Sprintf(errUnableToGetSearchAttributesMessage, err))
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToGetSearchAttributesMessage, err))
 	}
 
 	newCustomSearchAttributes := maps.Clone(currentSearchAttributes.Custom())
-
 	for _, saName := range request.GetSearchAttributes() {
 		if !currentSearchAttributes.IsDefined(saName) {
-			return nil, serviceerror.NewNotFound(fmt.Sprintf(errSearchAttributeDoesntExistMessage, saName))
+			return serviceerror.NewNotFound(fmt.Sprintf(errSearchAttributeDoesntExistMessage, saName))
 		}
 		if _, ok := newCustomSearchAttributes[saName]; !ok {
-			return nil, serviceerror.NewInvalidArgument(fmt.Sprintf(errUnableToRemoveNonCustomSearchAttributesMessage, saName))
+			return serviceerror.NewInvalidArgument(
+				fmt.Sprintf(errUnableToRemoveNonCustomSearchAttributesMessage, saName),
+			)
 		}
 		delete(newCustomSearchAttributes, saName)
 	}
 
 	err = h.saManager.SaveSearchAttributes(ctx, indexName, newCustomSearchAttributes)
 	if err != nil {
-		return nil, serviceerror.NewUnavailable(fmt.Sprintf(errUnableToSaveSearchAttributesMessage, err))
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToSaveSearchAttributesMessage, err))
 	}
-
-	return &operatorservice.RemoveSearchAttributesResponse{}, nil
+	return nil
 }
 
-func (h *OperatorHandlerImpl) ListSearchAttributes(ctx context.Context, request *operatorservice.ListSearchAttributesRequest) (_ *operatorservice.ListSearchAttributesResponse, retError error) {
+func (h *OperatorHandlerImpl) removeSearchAttributesSQL(
+	ctx context.Context,
+	request *operatorservice.RemoveSearchAttributesRequest,
+) error {
+	client, err := h.clientFactory.NewLocalFrontendClientWithTimeout(
+		frontend.DefaultTimeout,
+		frontend.DefaultLongPollTimeout,
+	)
+	if err != nil {
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToCreateFrontendClientMessage, err))
+	}
+
+	nsName := request.GetNamespace()
+	if nsName == "" {
+		return errNamespaceNotSet
+	}
+	ns, err := h.namespaceRegistry.GetNamespace(namespace.Name(nsName))
+	if err != nil {
+		return serviceerror.NewUnavailable(fmt.Sprintf(errUnableToGetNamespaceInfoMessage, nsName))
+	}
+
+	mapper := ns.CustomSearchAttributesMapper()
+	fieldToAliasMap := maps.Clone(mapper.FieldToAliasMap())
+	for _, saName := range request.GetSearchAttributes() {
+		fieldName, err := mapper.GetFieldName(saName, nsName)
+		if err != nil {
+			return serviceerror.NewNotFound(fmt.Sprintf(errSearchAttributeDoesntExistMessage, saName))
+		}
+		delete(fieldToAliasMap, fieldName)
+	}
+
+	_, err = client.UpdateNamespace(ctx, &workflowservice.UpdateNamespaceRequest{
+		Namespace: nsName,
+		Config: &namespacepb.NamespaceConfig{
+			CustomSearchAttributeAliases: fieldToAliasMap,
+		},
+	})
+	return err
+}
+
+func (h *OperatorHandlerImpl) ListSearchAttributes(
+	ctx context.Context,
+	request *operatorservice.ListSearchAttributesRequest,
+) (_ *operatorservice.ListSearchAttributesResponse, retError error) {
 	defer log.CapturePanic(h.logger, &retError)
 
 	if request == nil {
@@ -265,33 +418,56 @@ func (h *OperatorHandlerImpl) ListSearchAttributes(ctx context.Context, request 
 	}
 
 	indexName := h.visibilityMgr.GetIndexName()
+	searchAttributes, err := h.saProvider.GetSearchAttributes(indexName, true)
+	if err != nil {
+		return nil, serviceerror.NewUnavailable(
+			fmt.Sprintf("unable to read custom search attributes: %v", err),
+		)
+	}
 
-	var lastErr error
-	var esMapping map[string]string = nil
-	if h.esClient != nil {
-		esMapping, lastErr = h.esClient.GetMapping(ctx, indexName)
-		if lastErr != nil {
-			lastErr = serviceerror.NewUnavailable(fmt.Sprintf("unable to get mapping from Elasticsearch: %v", lastErr))
+	var customSearchAttributes map[string]enumspb.IndexedValueType
+	var storageSchema map[string]string
+	if h.visibilityMgr.GetName() == elasticsearch.PersistenceName {
+		customSearchAttributes = searchAttributes.Custom()
+		if h.esClient != nil {
+			storageSchema, err = h.esClient.GetMapping(ctx, indexName)
+			if err != nil {
+				return nil, serviceerror.NewUnavailable(
+					fmt.Sprintf("unable to get mapping from Elasticsearch: %v", err),
+				)
+			}
+		}
+	} else {
+		nsName := request.GetNamespace()
+		if nsName == "" {
+			return nil, errNamespaceNotSet
+		}
+		ns, err := h.namespaceRegistry.GetNamespace(namespace.Name(nsName))
+		if err != nil {
+			return nil, serviceerror.NewUnavailable(
+				fmt.Sprintf(errUnableToGetNamespaceInfoMessage, nsName),
+			)
+		}
+		mapper := ns.CustomSearchAttributesMapper()
+		customSearchAttributes = make(map[string]enumspb.IndexedValueType)
+		for field, tp := range searchAttributes.Custom() {
+			if alias, err := mapper.GetAlias(field, nsName); err == nil {
+				customSearchAttributes[alias] = tp
+			}
 		}
 	}
 
-	searchAttributes, err := h.saProvider.GetSearchAttributes(indexName, true)
-	if err != nil {
-		lastErr = serviceerror.NewUnavailable(fmt.Sprintf("unable to read custom search attributes: %v", err))
-	}
-
-	if lastErr != nil {
-		return nil, lastErr
-	}
-
 	return &operatorservice.ListSearchAttributesResponse{
-		CustomAttributes: searchAttributes.Custom(),
+		CustomAttributes: customSearchAttributes,
 		SystemAttributes: searchAttributes.System(),
-		StorageSchema:    esMapping,
+		StorageSchema:    storageSchema,
 	}, nil
 }
 
-func (h *OperatorHandlerImpl) DeleteNamespace(ctx context.Context, request *operatorservice.DeleteNamespaceRequest) (_ *operatorservice.DeleteNamespaceResponse, retError error) {
+func (h *OperatorHandlerImpl) DeleteNamespace(
+	ctx context.Context,
+	request *operatorservice.DeleteNamespaceRequest,
+) (_ *operatorservice.DeleteNamespaceResponse, retError error) {
 	defer log.CapturePanic(h.logger, &retError)
 
 	scope, startTime := h.startRequestProfile(metrics.OperatorDeleteNamespaceScope)
