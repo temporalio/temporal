1d9dcd4bcc932c70e2d0ee3f4192b66f294d631c
wenquan xing <wenquan.xing@gmail.com>
2023-02-08 10:05:55 -0800

    Reduce unnecessary log noise

--- service/frontend/redirection_interceptor.go
+++ service/frontend/redirection_interceptor.go
@@ -177,8 +177,9 @@ func (i *RedirectionInterceptor) Intercept(
 		return i.handleRedirectAPIInvocation(ctx, req, info, handler, methodName, respAllocFn, namespaceName)
 	}
 
-	// this should not happen, log warn and move on
-	i.logger.Warn(fmt.Sprintf("RedirectionInterceptor encountered unknown API: %v", methodName))
+	// this should not happen for frontend APIs but ok for admin and other future handlers
+	// log warn and move on
+	i.logger.Debug(fmt.Sprintf("RedirectionInterceptor encountered unknown API: %v", methodName))
 	return handler(ctx, req)
 }
 
