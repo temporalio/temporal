677890bc9a62470b4a3466902ee45caa1533aea4
Yu Xia <yuhsia89@gmail.com>
2023-02-13 15:01:02 -0800

    Separate metrics on read history and read raw history (#3925)

--- common/metrics/metric_defs.go
+++ common/metrics/metric_defs.go
@@ -490,6 +490,8 @@ const (
 	PersistenceReadHistoryBranchScope = "ReadHistoryBranch"
 	// PersistenceReadHistoryBranchReverseScope tracks ReadHistoryBranchReverse calls made by service to persistence layer
 	PersistenceReadHistoryBranchReverseScope = "ReadHistoryBranchReverse"
+	// PersistenceReadRawHistoryBranchScope tracks ReadRawHistoryBranch calls made by service to persistence layer
+	PersistenceReadRawHistoryBranchScope = "ReadRawHistoryBranch"
 	// PersistenceForkHistoryBranchScope tracks ForkHistoryBranch calls made by service to persistence layer
 	PersistenceForkHistoryBranchScope = "ForkHistoryBranch"
 	// PersistenceDeleteHistoryBranchScope tracks DeleteHistoryBranch calls made by service to persistence layer
