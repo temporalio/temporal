c589876e9235dff6aef5bf254567c6805a1620fd
wenquan xing <wenquan.xing@gmail.com>
2023-04-19 12:48:08 -0700

    fix

--- service/history/shard/context_mock.go
+++ service/history/shard/context_mock.go
@@ -35,7 +35,6 @@ import (
 
 	gomock "github.com/golang/mock/gomock"
 	v1 "go.temporal.io/api/common/v1"
-
 	v10 "go.temporal.io/server/api/adminservice/v1"
 	v11 "go.temporal.io/server/api/clock/v1"
 	v12 "go.temporal.io/server/api/historyservice/v1"
@@ -649,6 +648,20 @@ func (mr *MockContextMockRecorder) SetCurrentTime(cluster, currentTime interface
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetCurrentTime", reflect.TypeOf((*MockContext)(nil).SetCurrentTime), cluster, currentTime)
 }
 
+// SetQueueState mocks base method.
+func (m *MockContext) SetQueueState(category tasks.Category, state *v13.QueueState) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SetQueueState", category, state)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SetQueueState indicates an expected call of SetQueueState.
+func (mr *MockContextMockRecorder) SetQueueState(category, state interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetQueueState", reflect.TypeOf((*MockContext)(nil).SetQueueState), category, state)
+}
+
 // SetWorkflowExecution mocks base method.
 func (m *MockContext) SetWorkflowExecution(ctx context.Context, request *persistence.SetWorkflowExecutionRequest) (*persistence.SetWorkflowExecutionResponse, error) {
 	m.ctrl.T.Helper()
@@ -728,20 +741,6 @@ func (m *MockContext) UpdateQueueClusterAckLevel(category tasks.Category, cluste
 func (mr *MockContextMockRecorder) UpdateQueueClusterAckLevel(category, cluster, ackLevel interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateQueueClusterAckLevel", reflect.TypeOf((*MockContext)(nil).UpdateQueueClusterAckLevel), category, cluster, ackLevel)
-}
-
-// SetQueueState mocks base method.
-func (m *MockContext) SetQueueState(category tasks.Category, state *v13.QueueState) error {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "SetQueueState", category, state)
-	ret0, _ := ret[0].(error)
-	return ret0
-}
-
-// SetQueueState indicates an expected call of SetQueueState.
-func (mr *MockContextMockRecorder) SetQueueState(category, state interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetQueueState", reflect.TypeOf((*MockContext)(nil).SetQueueState), category, state)
 }
 
 // UpdateRemoteClusterInfo mocks base method.
