0953f9b6204fd1f1d640489ee88c6b00e8b3337f
Saman Barghi <samanbarghi@users.noreply.github.com>
2023-04-24 20:46:59 -0400

    Call List API from Scan API for SQL visibility (#4217)
    * Call List API from Scan API for SQL visibility
    
    * rename variable

--- common/persistence/visibility/store/sql/visibility_store.go
+++ common/persistence/visibility/store/sql/visibility_store.go
@@ -425,10 +425,10 @@ func (s *VisibilityStore) ListWorkflowExecutions(
 }
 
 func (s *VisibilityStore) ScanWorkflowExecutions(
-	_ context.Context,
-	_ *manager.ListWorkflowExecutionsRequestV2,
+	ctx context.Context,
+	request *manager.ListWorkflowExecutionsRequestV2,
 ) (*store.InternalListWorkflowExecutionsResponse, error) {
-	return nil, store.OperationNotSupportedErr
+	return s.ListWorkflowExecutions(ctx, request)
 }
 
 func (s *VisibilityStore) CountWorkflowExecutions(
