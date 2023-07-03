7814aebee89abcf1c39de731605d61a9c61058b4
Haifeng He <haifeng.he@temporal.io>
2023-07-03 16:05:55 -0700

    Add verification of replication tasks in force replication

--- service/worker/migration/activities.go
+++ service/worker/migration/activities.go
@@ -38,9 +38,11 @@ import (
 	"go.temporal.io/sdk/activity"
 
 	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/server/api/adminservice/v1"
 	enumsspb "go.temporal.io/server/api/enums/v1"
 	"go.temporal.io/server/api/historyservice/v1"
 	replicationspb "go.temporal.io/server/api/replication/v1"
+	"go.temporal.io/server/client/admin"
 	"go.temporal.io/server/common/definition"
 	"go.temporal.io/server/common/headers"
 	"go.temporal.io/server/common/log/tag"
@@ -50,6 +52,54 @@ import (
 	"go.temporal.io/server/common/quotas"
 	"go.temporal.io/server/common/util"
 )
+
+type (
+	VerifyStatus int
+	VerifyResult struct {
+		Status VerifyStatus
+		Reason string
+	}
+
+	replicationTasksHeartbeatDetails struct {
+		Results                       []VerifyResult
+		CheckPoint                    time.Time
+		LastNotFoundWorkflowExecution commonpb.WorkflowExecution
+	}
+
+	verifyReplicationTasksTimeoutErr struct {
+		Details replicationTasksHeartbeatDetails
+	}
+)
+
+const (
+	NOT_CREATED            VerifyStatus = 0
+	CREATED_TO_BE_VERIFIED VerifyStatus = 1
+	VERIFIED               VerifyStatus = 2
+	VERIFY_SKIPPED         VerifyStatus = 3
+
+	reasonZombieWorkflow   = "Zombie workflow"
+	reasonNotFoundWorkflow = "not found workflow"
+)
+
+func (r VerifyResult) isNotCreated() bool {
+	return r.Status == NOT_CREATED
+}
+
+func (r VerifyResult) isCreatedToBeVerified() bool {
+	return r.Status == CREATED_TO_BE_VERIFIED
+}
+
+func (r VerifyResult) isVerified() bool {
+	return r.Status == VERIFIED
+}
+
+func (r VerifyResult) isSkipped() bool {
+	return r.Status == VERIFY_SKIPPED
+}
+
+func (r VerifyResult) isCompleted() bool {
+	return r.isVerified() || r.isSkipped()
+}
 
 // TODO: CallerTypePreemptablee should be set in activity background context for all migration activities.
 // However, activity background context is per-worker, which means once set, all activities processed by the
@@ -275,20 +325,18 @@ func (a *activities) generateWorkflowReplicationTask(ctx context.Context, rateLi
 		},
 	})
 
-	switch err.(type) {
-	case nil:
-		stateTransitionCount := resp.StateTransitionCount
-		for stateTransitionCount > 0 {
-			token := util.Min(int(stateTransitionCount), rateLimiter.Burst())
-			stateTransitionCount -= int64(token)
-			_ = rateLimiter.ReserveN(time.Now(), token)
-		}
-		return nil
-	case *serviceerror.NotFound:
-		return nil
-	default:
+	if err != nil {
 		return err
 	}
+
+	stateTransitionCount := resp.StateTransitionCount
+	for stateTransitionCount > 0 {
+		token := util.Min(int(stateTransitionCount), rateLimiter.Burst())
+		stateTransitionCount -= int64(token)
+		_ = rateLimiter.ReserveN(time.Now(), token)
+	}
+
+	return nil
 }
 
 func (a *activities) UpdateNamespaceState(ctx context.Context, req updateStateRequest) error {
@@ -379,10 +427,11 @@ func (a *activities) GenerateReplicationTasks(ctx context.Context, request *gene
 
 	for i := startIndex; i < len(request.Executions); i++ {
 		we := request.Executions[i]
-		err := a.generateWorkflowReplicationTask(ctx, rateLimiter, definition.NewWorkflowKey(request.NamespaceID, we.WorkflowId, we.RunId))
-		if err != nil {
-			a.logger.Info("Force replicate failed", tag.WorkflowNamespaceID(request.NamespaceID), tag.WorkflowID(we.WorkflowId), tag.WorkflowRunID(we.RunId), tag.Error(err))
-			return err
+		if err := a.generateWorkflowReplicationTask(ctx, rateLimiter, definition.NewWorkflowKey(request.NamespaceID, we.WorkflowId, we.RunId)); err != nil {
+			if _, isNotFound := err.(*serviceerror.NotFound); !isNotFound {
+				a.logger.Error("force-replication failed to generate replication task", tag.WorkflowNamespaceID(request.NamespaceID), tag.WorkflowID(we.WorkflowId), tag.WorkflowRunID(we.RunId), tag.Error(err))
+				return err
+			}
 		}
 		activity.RecordHeartbeat(ctx, i)
 	}
@@ -394,7 +443,6 @@ func (a *activities) setCallerInfoForGenReplicationTask(
 	ctx context.Context,
 	namespaceID namespace.ID,
 ) context.Context {
-
 	nsName, err := a.namespaceRegistry.GetNamespaceName(namespaceID)
 	if err != nil {
 		a.logger.Error("Failed to get namespace name when generating replication task",
@@ -480,5 +528,182 @@ func (a *activities) SeedReplicationQueueWithUserDataEntries(ctx context.Context
 		heartbeatDetails.NextPageToken = response.NextPageToken
 		heartbeatDetails.IndexInPage = 0
 		activity.RecordHeartbeat(ctx, heartbeatDetails)
+	}
+}
+
+func (a *activities) createReplicationTasks(ctx context.Context, request *genearteAndVerifyReplicationTasksRequest, detail *replicationTasksHeartbeatDetails) error {
+	rateLimiter := quotas.NewRateLimiter(request.RPS, int(math.Ceil(request.RPS)))
+
+	for i := 0; i < len(request.Executions); i++ {
+		r := &detail.Results[i]
+		if r.isCompleted() {
+			continue
+		}
+
+		we := request.Executions[i]
+		tags := []tag.Tag{tag.WorkflowType(forceReplicationWorkflowName), tag.WorkflowNamespaceID(request.NamespaceID), tag.WorkflowID(we.WorkflowId), tag.WorkflowRunID(we.RunId)}
+
+		resp, err := a.historyClient.DescribeMutableState(ctx, &historyservice.DescribeMutableStateRequest{
+			NamespaceId: request.NamespaceID,
+			Execution:   &we,
+		})
+
+		switch err.(type) {
+		case nil:
+			if resp.GetCacheMutableState().GetExecutionState().GetState() == enumsspb.WORKFLOW_EXECUTION_STATE_ZOMBIE {
+				a.forceReplicationMetricsHandler.Counter(metrics.EncounterZombieWorkflowCount.GetMetricName()).Record(1)
+				a.logger.Info("createReplicationTasks skip Zombie workflow", tags...)
+
+				r.Status = VERIFY_SKIPPED
+				r.Reason = reasonZombieWorkflow
+				continue
+			}
+
+			if !r.isCreatedToBeVerified() {
+				err := a.generateWorkflowReplicationTask(ctx, rateLimiter, definition.NewWorkflowKey(request.NamespaceID, we.WorkflowId, we.RunId))
+
+				switch err.(type) {
+				case nil:
+					// replication task was created
+					r.Status = CREATED_TO_BE_VERIFIED
+				case *serviceerror.NotFound:
+					// rare case but in case if execution was deleted after above DescribeMutableState
+					r.Status = VERIFY_SKIPPED
+					r.Reason = reasonNotFoundWorkflow
+				default:
+					a.logger.Error(fmt.Sprintf("createReplicationTasks failed to generate replication task. Error: %v", err), tags...)
+					return err
+				}
+			}
+
+		case *serviceerror.NotFound:
+			r.Status = VERIFY_SKIPPED
+			r.Reason = reasonNotFoundWorkflow
+
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *activities) verifyReplicationTasks(
+	ctx context.Context,
+	request *genearteAndVerifyReplicationTasksRequest,
+	detail *replicationTasksHeartbeatDetails,
+	remoteClient adminservice.AdminServiceClient,
+) (bool, bool, error) {
+	progress := false
+	for i := 0; i < len(request.Executions); i++ {
+		r := &detail.Results[i]
+		we := request.Executions[i]
+		if r.isNotCreated() {
+			// invalid state
+			return false, progress, temporal.NewNonRetryableApplicationError(fmt.Sprintf("verifyReplicationTasks: replication task for %v was not created", we), "", nil)
+		}
+
+		if r.isCompleted() {
+			continue
+		}
+
+		_, err := remoteClient.DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
+			Namespace: request.Namespace,
+			Execution: &we,
+		})
+
+		switch err.(type) {
+		case nil:
+			a.forceReplicationMetricsHandler.Counter(metrics.VerifyReplicationTaskSuccess.GetMetricName()).Record(1)
+			r.Status = VERIFIED
+			progress = true
+
+		case *serviceerror.NotFound:
+			detail.LastNotFoundWorkflowExecution = we
+			return false, progress, nil
+
+		default:
+			return false, progress, err
+		}
+	}
+
+	return true, progress, nil
+}
+
+func (e verifyReplicationTasksTimeoutErr) Error() string {
+	return fmt.Sprintf("Failed to verify replication tasks. Details: %v", e.Details)
+}
+
+const (
+	defaultNoProgressRetryableTimeout    = 5 * time.Minute
+	defaultNoProgressNotRetryableTimeout = 15 * time.Minute
+)
+
+func (a *activities) GenerateAndVerifyReplicationTasks(ctx context.Context, request *genearteAndVerifyReplicationTasksRequest) error {
+	ctx = headers.SetCallerInfo(ctx, headers.NewPreemptableCallerInfo(request.Namespace))
+	remoteClient := a.clientFactory.NewRemoteAdminClientWithTimeout(
+		request.TargetClusterEndpoint,
+		admin.DefaultTimeout,
+		admin.DefaultLargeTimeout,
+	)
+
+	var details replicationTasksHeartbeatDetails
+	if activity.HasHeartbeatDetails(ctx) {
+		if err := activity.GetHeartbeatDetails(ctx, &details); err != nil {
+			return err
+		}
+	} else {
+		details.Results = make([]VerifyResult, len(request.Executions))
+		details.CheckPoint = time.Now()
+		activity.RecordHeartbeat(ctx, details)
+	}
+
+	start := time.Now()
+	if err := a.createReplicationTasks(ctx, request, &details); err != nil {
+		return err
+	}
+	a.forceReplicationMetricsHandler.Timer(metrics.CreateReplicationTasksLatency.GetMetricName()).Record(time.Since(start))
+
+	activity.RecordHeartbeat(ctx, details)
+
+	for {
+		var verified, progress bool
+		var err error
+
+		start := time.Now()
+		if verified, progress, err = a.verifyReplicationTasks(ctx, request, &details, remoteClient); err != nil {
+			return err
+		}
+		a.forceReplicationMetricsHandler.Timer(metrics.VerifyReplicationTasksLatency.GetMetricName()).Record(time.Since(start))
+
+		if progress {
+			details.CheckPoint = time.Now()
+		}
+
+		activity.RecordHeartbeat(ctx, details)
+
+		if verified == true {
+			return nil
+		}
+
+		diff := time.Now().Sub(details.CheckPoint)
+		if diff > defaultNoProgressRetryableTimeout {
+			if diff > defaultNoProgressNotRetryableTimeout {
+				// Potentially encountered a missing execution, return non-retryable error
+				return temporal.NewNonRetryableApplicationError(
+					fmt.Sprintf("verifyReplicationTasks was not able to make progress for more than %v minutes (not retryable). Not found WorkflowExecution: %v, Checkpoint: %v",
+						diff.Minutes(),
+						details.LastNotFoundWorkflowExecution, details.CheckPoint),
+					"", nil)
+			}
+
+			// return error to trigger activity retry
+			return fmt.Errorf("verifyReplicationTasks was not able to make progress for more than %v minutes (retryable). Not found WorkflowExecution: %v,",
+				diff.Minutes(),
+				details.LastNotFoundWorkflowExecution,
+			)
+		}
+
+		time.Sleep(request.VerifyInterval)
 	}
 }
---
+++ service/worker/migration/activities_test.go
@@ -0,0 +1,255 @@
+package migration
+
+import (
+	"testing"
+	"time"
+
+	commonpb "go.temporal.io/api/common/v1"
+	"go.temporal.io/api/serviceerror"
+	"go.temporal.io/api/workflowservicemock/v1"
+	"go.temporal.io/sdk/interceptor"
+	"go.temporal.io/sdk/testsuite"
+	"go.temporal.io/sdk/worker"
+	"go.temporal.io/server/api/adminservice/v1"
+	"go.temporal.io/server/api/adminservicemock/v1"
+	enumsspb "go.temporal.io/server/api/enums/v1"
+	"go.temporal.io/server/api/historyservice/v1"
+	"go.temporal.io/server/api/historyservicemock/v1"
+	persistencepb "go.temporal.io/server/api/persistence/v1"
+	"go.temporal.io/server/client"
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/metrics"
+	"go.temporal.io/server/common/namespace"
+	"go.temporal.io/server/common/persistence"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/suite"
+)
+
+type activitiesSuite struct {
+	suite.Suite
+	testsuite.WorkflowTestSuite
+
+	controller                    *gomock.Controller
+	mockTaskManager               *persistence.MockTaskManager
+	mockNamespaceReplicationQueue *persistence.MockNamespaceReplicationQueue
+	mockNamespaceRegistry         *namespace.MockRegistry
+	mockClientFactory             *client.MockFactory
+
+	mockFrontendClient    *workflowservicemock.MockWorkflowServiceClient
+	mockHistoryClient     *historyservicemock.MockHistoryServiceClient
+	mockRemoteAdminClient *adminservicemock.MockAdminServiceClient
+
+	logger             log.Logger
+	mockMetricsHandler *metrics.MockHandler
+
+	a *activities
+}
+
+const (
+	mockedNamespace   = "test_namespace"
+	mockedNamespaceID = "test_namespace_id"
+	remoteRpcAddress  = "remote"
+)
+
+var (
+	execution1 = commonpb.WorkflowExecution{
+		WorkflowId: "workflow1",
+		RunId:      "run1",
+	}
+
+	execution2 = commonpb.WorkflowExecution{
+		WorkflowId: "workflow2",
+		RunId:      "run2",
+	}
+
+	zombieExecution = commonpb.WorkflowExecution{
+		WorkflowId: "zombie",
+		RunId:      "z1",
+	}
+)
+
+func TestActivitiesSuite(t *testing.T) {
+	suite.Run(t, new(activitiesSuite))
+}
+
+func (s *activitiesSuite) SetupTest() {
+	s.controller = gomock.NewController(s.T())
+	s.mockTaskManager = persistence.NewMockTaskManager(s.controller)
+	s.mockNamespaceReplicationQueue = persistence.NewMockNamespaceReplicationQueue(s.controller)
+	s.mockNamespaceRegistry = namespace.NewMockRegistry(s.controller)
+	s.mockClientFactory = client.NewMockFactory(s.controller)
+
+	s.mockFrontendClient = workflowservicemock.NewMockWorkflowServiceClient(s.controller)
+	s.mockHistoryClient = historyservicemock.NewMockHistoryServiceClient(s.controller)
+	s.mockRemoteAdminClient = adminservicemock.NewMockAdminServiceClient(s.controller)
+
+	s.logger = log.NewNoopLogger()
+	s.mockMetricsHandler = metrics.NewMockHandler(s.controller)
+	s.mockMetricsHandler.EXPECT().Timer(gomock.Any()).Return(metrics.NoopTimerMetricFunc).AnyTimes()
+	s.mockMetricsHandler.EXPECT().Counter(gomock.Any()).Return(metrics.NoopCounterMetricFunc).AnyTimes()
+	s.mockClientFactory.EXPECT().NewRemoteAdminClientWithTimeout(remoteRpcAddress, gomock.Any(), gomock.Any()).
+		Return(s.mockRemoteAdminClient).AnyTimes()
+
+	s.a = &activities{
+		namespaceRegistry:              s.mockNamespaceRegistry,
+		namespaceReplicationQueue:      s.mockNamespaceReplicationQueue,
+		clientFactory:                  s.mockClientFactory,
+		taskManager:                    s.mockTaskManager,
+		frontendClient:                 s.mockFrontendClient,
+		historyClient:                  s.mockHistoryClient,
+		logger:                         log.NewCLILogger(),
+		metricsHandler:                 s.mockMetricsHandler,
+		forceReplicationMetricsHandler: s.mockMetricsHandler,
+	}
+}
+
+func (s *activitiesSuite) TearDownTest() {
+	s.controller.Finish()
+}
+
+func (s *activitiesSuite) initEnv() (*testsuite.TestActivityEnvironment, *heartbeatRecordingInterceptor) {
+	env := s.NewTestActivityEnvironment()
+	env.RegisterActivity(s.a)
+	iceptor := heartbeatRecordingInterceptor{T: s.T()}
+	env.SetWorkerOptions(worker.Options{Interceptors: []interceptor.WorkerInterceptor{&iceptor}})
+
+	return env, &iceptor
+}
+
+func (s *activitiesSuite) TestGenerateAndVerifyReplicationTasks_Success() {
+	env, iceptor := s.initEnv()
+
+	request := genearteAndVerifyReplicationTasksRequest{
+		Namespace:             mockedNamespace,
+		NamespaceID:           mockedNamespaceID,
+		RPS:                   10,
+		TargetClusterEndpoint: remoteRpcAddress,
+		Executions:            []commonpb.WorkflowExecution{execution1, execution2},
+	}
+
+	// Setup create replication tasks
+	for i := 0; i < len(request.Executions); i++ {
+		we := request.Executions[i]
+		s.mockHistoryClient.EXPECT().DescribeMutableState(gomock.Any(), &historyservice.DescribeMutableStateRequest{
+			NamespaceId: mockedNamespaceID,
+			Execution:   &we,
+		}).Return(&historyservice.DescribeMutableStateResponse{
+			CacheMutableState: &persistencepb.WorkflowMutableState{
+				ExecutionState: &persistencepb.WorkflowExecutionState{
+					State: enumsspb.WORKFLOW_EXECUTION_STATE_COMPLETED,
+				},
+			},
+		}, nil).Times(1)
+
+		s.mockHistoryClient.EXPECT().GenerateLastHistoryReplicationTasks(gomock.Any(), &historyservice.GenerateLastHistoryReplicationTasksRequest{
+			NamespaceId: mockedNamespaceID,
+			Execution:   &we,
+		}).Return(&historyservice.GenerateLastHistoryReplicationTasksResponse{}, nil).Times(1)
+	}
+
+	// Immediately replicated
+	s.mockRemoteAdminClient.EXPECT().DescribeMutableState(gomock.Any(), &adminservice.DescribeMutableStateRequest{
+		Namespace: mockedNamespace,
+		Execution: &execution1,
+	}).Return(&adminservice.DescribeMutableStateResponse{}, nil).Times(1)
+
+	// Slowly replicated
+	replicationSlowReponses := []struct {
+		resp *adminservice.DescribeMutableStateResponse
+		err  error
+	}{
+		{nil, serviceerror.NewNotFound("")},
+		{nil, serviceerror.NewNotFound("")},
+		{&adminservice.DescribeMutableStateResponse{}, nil},
+	}
+
+	for _, r := range replicationSlowReponses {
+		s.mockRemoteAdminClient.EXPECT().DescribeMutableState(gomock.Any(), &adminservice.DescribeMutableStateRequest{
+			Namespace: mockedNamespace,
+			Execution: &execution2,
+		}).Return(r.resp, r.err).Times(1)
+	}
+
+	_, err := env.ExecuteActivity(s.a.GenerateAndVerifyReplicationTasks, &request)
+	s.NoError(err)
+
+	s.Greater(len(iceptor.replicationRecordedHeartbeats), 0)
+	lastHeartBeat := iceptor.replicationRecordedHeartbeats[len(iceptor.replicationRecordedHeartbeats)-1]
+	s.Equal(len(request.Executions), len(lastHeartBeat.Results))
+	for _, r := range lastHeartBeat.Results {
+		s.True(r.isVerified())
+	}
+}
+
+func (s *activitiesSuite) TestGenerateAndVerifyReplicationTasks_Skipped() {
+	mockErr := serviceerror.NewInternal("mock error")
+	var testcases = []struct {
+		resp           *historyservice.DescribeMutableStateResponse
+		err            error
+		expectedStatus VerifyStatus
+		expectedReason string
+		expectedErr    error
+	}{
+		{
+			&historyservice.DescribeMutableStateResponse{
+				CacheMutableState: &persistencepb.WorkflowMutableState{
+					ExecutionState: &persistencepb.WorkflowExecutionState{
+						State: enumsspb.WORKFLOW_EXECUTION_STATE_ZOMBIE,
+					},
+				},
+			},
+			nil,
+			VERIFY_SKIPPED,
+			reasonZombieWorkflow,
+			nil,
+		},
+		{
+			nil, serviceerror.NewNotFound(""),
+			VERIFY_SKIPPED,
+			reasonNotFoundWorkflow,
+			nil,
+		},
+		{
+			nil, mockErr,
+			NOT_CREATED,
+			"",
+			mockErr,
+		},
+	}
+
+	request := genearteAndVerifyReplicationTasksRequest{
+		Namespace:             mockedNamespace,
+		NamespaceID:           mockedNamespaceID,
+		RPS:                   10,
+		TargetClusterEndpoint: remoteRpcAddress,
+		Executions:            []commonpb.WorkflowExecution{execution1},
+	}
+
+	start := time.Now()
+	for _, t := range testcases {
+		env, iceptor := s.initEnv()
+
+		s.mockHistoryClient.EXPECT().DescribeMutableState(gomock.Any(), &historyservice.DescribeMutableStateRequest{
+			NamespaceId: mockedNamespaceID,
+			Execution:   &execution1,
+		}).Return(t.resp, t.err)
+
+		_, err := env.ExecuteActivity(s.a.GenerateAndVerifyReplicationTasks, &request)
+		if t.expectedErr == nil {
+			s.NoError(err)
+		} else {
+			s.ErrorContains(err, "mock error")
+		}
+
+		s.Greater(len(iceptor.replicationRecordedHeartbeats), 0)
+		lastHeartBeat := iceptor.replicationRecordedHeartbeats[len(iceptor.replicationRecordedHeartbeats)-1]
+		s.Equal(len(request.Executions), len(lastHeartBeat.Results))
+		for _, r := range lastHeartBeat.Results {
+			s.Equal(t.expectedStatus, r.Status)
+			s.Equal(t.expectedReason, r.Reason)
+		}
+
+		s.True(lastHeartBeat.CheckPoint.After(start))
+	}
+}
