6e811dd84e8a93062713bb343202429e2642e5cb
Yu Xia <yuhsia89@gmail.com>
2023-07-12 20:15:46 -0700

    Handle replicate workflow state with exist workflow data (#4617)
    <!-- Describe what has changed in this PR -->
    **What changed?**
    Handle replicate workflow state with exist workflow data.
    
    <!-- Tell your future self why have you made these changes -->
    **Why?**
    During migration interruption, some workflow data might be partially
    migrated. This case is not handle in replicate workflow state.
    
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    **How did you test it?**
    Update current unit test. 
    
    A manual test will be perform: 
    1. Connect two cluster.
    2. Start a long running workflow.
    3. Disconnect two cluster.
    4. Terminate the old workflow and start a new long running workflow.
    5. Force replication open workflow.
    6. Force replication closed workflow.
    
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    **Potential risks**
    
    
    <!-- Is this PR a hotfix candidate or require that a notification be
    sent to the broader community? (Yes/No) -->
    **Is hotfix candidate?**
    Yes.

--- service/history/ndc/history_replicator.go
+++ service/history/ndc/history_replicator.go
@@ -303,6 +303,46 @@ func (r *HistoryReplicatorImpl) ApplyWorkflowState(
 		}
 	}()
 
+	// Handle existing workflows
+	ms, err := wfCtx.LoadMutableState(ctx)
+	switch err.(type) {
+	case *serviceerror.NotFound:
+		// no-op, continue to replicate workflow state
+	case nil:
+		// workflow exists, do resend if version histories are not match.
+		localVersionHistory, err := versionhistory.GetCurrentVersionHistory(ms.GetExecutionInfo().GetVersionHistories())
+		if err != nil {
+			return err
+		}
+		localHistoryLastItem, err := versionhistory.GetLastVersionHistoryItem(localVersionHistory)
+		if err != nil {
+			return err
+		}
+		incomingVersionHistory, err := versionhistory.GetCurrentVersionHistory(request.GetWorkflowState().GetExecutionInfo().GetVersionHistories())
+		if err != nil {
+			return err
+		}
+		incomingHistoryLastItem, err := versionhistory.GetLastVersionHistoryItem(incomingVersionHistory)
+		if err != nil {
+			return err
+		}
+		if !versionhistory.IsEqualVersionHistoryItem(localHistoryLastItem, incomingHistoryLastItem) {
+			return serviceerrors.NewRetryReplication(
+				"Failed to sync workflow state due to version history mismatch",
+				namespaceID.String(),
+				wid,
+				rid,
+				localHistoryLastItem.GetEventId(),
+				localHistoryLastItem.GetVersion(),
+				common.EmptyEventID,
+				common.EmptyVersion,
+			)
+		}
+		return nil
+	default:
+		return err
+	}
+
 	currentVersionHistory, err := versionhistory.GetCurrentVersionHistory(executionInfo.VersionHistories)
 	if err != nil {
 		return err
