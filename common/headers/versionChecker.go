458109931e68e6c56bafaad1f8cae88933510587
Mind.R <3825716+mindaugasrukas@users.noreply.github.com>
2023-07-13 13:31:22 -0700

    Bump server version for 1.20.4 patch release

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -46,7 +46,7 @@ const (
 	ClientNameCLI           = "temporal-cli"
 	ClientNameUI            = "temporal-ui"
 
-	ServerVersion = "1.20.3"
+	ServerVersion = "1.20.4"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
