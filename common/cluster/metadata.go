a01a309af6999c42ed7b0e383903764b0a91a90a
Yu Xia <yuhsia89@gmail.com>
2023-07-14 09:51:54 -0700

    Support cluster metadata customized tags (#4622)
    <!-- Describe what has changed in this PR -->
    **What changed?**
    Support cluster metadata customized tags
    
    <!-- Tell your future self why have you made these changes -->
    **Why?**
    Allow static tagging cluster 
    
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    **How did you test it?**
    Local testing
    
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    **Potential risks**
    
    
    <!-- Is this PR a hotfix candidate or require that a notification be
    sent to the broader community? (Yes/No) -->
    **Is hotfix candidate?**
    No

--- common/cluster/metadata.go
+++ common/cluster/metadata.go
@@ -35,6 +35,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"golang.org/x/exp/maps"
+
 	"go.temporal.io/server/common"
 	"go.temporal.io/server/common/collection"
 	"go.temporal.io/server/common/dynamicconfig"
@@ -98,6 +100,8 @@ type (
 		CurrentClusterName string `yaml:"currentClusterName"`
 		// ClusterInformation contains all cluster names to corresponding information about that cluster
 		ClusterInformation map[string]ClusterInformation `yaml:"clusterInformation"`
+		// Tag contains customized tag about the current cluster
+		Tags map[string]string `yaml:"tags"`
 	}
 
 	// ClusterInformation contains the information about each cluster which participated in cross DC
@@ -107,8 +111,9 @@ type (
 		// Address indicate the remote service address(Host:Port). Host can be DNS name.
 		RPCAddress string `yaml:"rpcAddress"`
 		// Cluster ID allows to explicitly set the ID of the cluster. Optional.
-		ClusterID  string `yaml:"-"`
-		ShardCount int32  `yaml:"-"` // Ignore this field when loading config.
+		ClusterID  string            `yaml:"-"`
+		ShardCount int32             `yaml:"-"` // Ignore this field when loading config.
+		Tags       map[string]string `yaml:"-"` // Ignore this field. Use cluster.Config.Tags for customized tags.
 		// private field to track cluster information updates
 		version int64
 	}
@@ -463,12 +468,14 @@ func (m *metadataImpl) refreshClusterMetadata(ctx context.Context) error {
 				InitialFailoverVersion: newClusterInfo.InitialFailoverVersion,
 				RPCAddress:             newClusterInfo.RPCAddress,
 				ShardCount:             newClusterInfo.ShardCount,
+				Tags:                   newClusterInfo.Tags,
 				version:                newClusterInfo.version,
 			}
 		} else if newClusterInfo.version > oldClusterInfo.version {
 			if newClusterInfo.Enabled == oldClusterInfo.Enabled &&
 				newClusterInfo.RPCAddress == oldClusterInfo.RPCAddress &&
-				newClusterInfo.InitialFailoverVersion == oldClusterInfo.InitialFailoverVersion {
+				newClusterInfo.InitialFailoverVersion == oldClusterInfo.InitialFailoverVersion &&
+				maps.Equal(newClusterInfo.Tags, oldClusterInfo.Tags) {
 				// key cluster info does not change
 				continue
 			}
@@ -478,6 +485,7 @@ func (m *metadataImpl) refreshClusterMetadata(ctx context.Context) error {
 				InitialFailoverVersion: oldClusterInfo.InitialFailoverVersion,
 				RPCAddress:             oldClusterInfo.RPCAddress,
 				ShardCount:             oldClusterInfo.ShardCount,
+				Tags:                   oldClusterInfo.Tags,
 				version:                oldClusterInfo.version,
 			}
 			newEntries[clusterName] = &ClusterInformation{
@@ -485,6 +493,7 @@ func (m *metadataImpl) refreshClusterMetadata(ctx context.Context) error {
 				InitialFailoverVersion: newClusterInfo.InitialFailoverVersion,
 				RPCAddress:             newClusterInfo.RPCAddress,
 				ShardCount:             newClusterInfo.ShardCount,
+				Tags:                   newClusterInfo.Tags,
 				version:                newClusterInfo.version,
 			}
 		}
@@ -589,6 +598,7 @@ func (m *metadataImpl) listAllClusterMetadataFromDB(
 			InitialFailoverVersion: getClusterResp.GetInitialFailoverVersion(),
 			RPCAddress:             getClusterResp.GetClusterAddress(),
 			ShardCount:             getClusterResp.GetHistoryShardCount(),
+			Tags:                   getClusterResp.GetTags(),
 			version:                getClusterResp.Version,
 		}
 	}
