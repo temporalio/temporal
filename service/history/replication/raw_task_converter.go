0bbf26fd0f496a69261b8ff0e66d4ddec2482e86
yux0 <yuhsia89@gmail.com>
2023-07-17 13:17:39 -0700

    regenerate mock files

--- service/history/replication/raw_task_converter.go
+++ service/history/replication/raw_task_converter.go
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+//go:generate mockgen -copyright_file ../../../LICENSE -package $GOPACKAGE -source $GOFILE -destination raw_task_converter_mock.go
+
 package replication
 
 import (
