42cebcf6b3ed775b679f2a28e40840f038847d66
Michael Snowden <michaelosnowden@gmail.com>
2023-07-17 15:11:52 -0700

    Add namespace_count_limit_test

---
+++ common/rpc/interceptor/namespace_count_limit_test.go
@@ -0,0 +1,201 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package interceptor
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/namespace"
+	"google.golang.org/grpc"
+)
+
+type testCase struct {
+	// name of the test case
+	name string
+	// request to be intercepted
+	request interface{}
+	// numBlockedRequests is the number of pending requests that will be blocked before the final request is sent.
+	numBlockedRequests int
+	// perInstanceLimit is the limit on the number of pending requests per instance. It is ignored if globalCountLimit
+	// is used.
+	perInstanceLimit int
+	methodName       string
+	// tokens is a map of method names to the number of tokens that will be consumed by that method.
+	tokens map[string]int
+	// expectRateLimit is true if the interceptor should respond with a rate limit error.
+	expectRateLimit bool
+}
+
+// TestNamespaceCountLimitInterceptor_Intercept verifies that the NamespaceCountLimitInterceptor responds with a rate
+// limit error when requests would exceed the concurrent poller limit for a namespace.
+func TestNamespaceCountLimitInterceptor_Intercept(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []testCase{
+		{
+			name:               "no limit hit",
+			request:            nil,
+			numBlockedRequests: 2,
+			perInstanceLimit:   3,
+			methodName:         "/temporal.api.workflowservice.v1.WorkflowService/DescribeNamespace",
+			tokens: map[string]int{
+				"DescribeNamespace": 1,
+			},
+			expectRateLimit: false,
+		},
+		{
+			name:               "per-instance limit hit",
+			request:            nil,
+			numBlockedRequests: 2,
+			perInstanceLimit:   2,
+			methodName:         "/temporal.api.workflowservice.v1.WorkflowService/DescribeNamespace",
+			tokens: map[string]int{
+				"DescribeNamespace": 1,
+			},
+			expectRateLimit: true,
+		},
+		{
+			name:               "method name does not consume token",
+			request:            nil,
+			numBlockedRequests: 2,
+			perInstanceLimit:   2,
+			methodName:         "/temporal.api.workflowservice.v1.WorkflowService/DescribeNamespace",
+			tokens:             map[string]int{},
+			expectRateLimit:    false,
+		},
+		{
+			name:               "long poll request",
+			request:            &workflowservice.GetWorkflowExecutionHistoryRequest{WaitNewEvent: true},
+			numBlockedRequests: 2,
+			perInstanceLimit:   2,
+			methodName:         "/temporal.api.workflowservice.v1.WorkflowService/GetWorkflowExecutionHistory",
+			tokens: map[string]int{
+				"GetWorkflowExecutionHistory": 1,
+			},
+			expectRateLimit: true,
+		},
+		{
+			name:               "non-long poll request",
+			request:            &workflowservice.GetWorkflowExecutionHistoryRequest{WaitNewEvent: false},
+			numBlockedRequests: 2,
+			perInstanceLimit:   2,
+			methodName:         "/temporal.api.workflowservice.v1.WorkflowService/GetWorkflowExecutionHistory",
+			tokens: map[string]int{
+				"GetWorkflowExecutionHistory": 1,
+			},
+			expectRateLimit: false,
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			tc.run(t)
+		})
+	}
+}
+
+// run the test case by simulating a bunch of blocked pollers, sending a final request, and verifying that it is either
+// rate limited or not.
+func (tc *testCase) run(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	interceptor := tc.createInterceptor(ctrl)
+	handler, errs := tc.createBlockedPollRequests(interceptor)
+
+	_, err := interceptor.Intercept(context.Background(), tc.request, &grpc.UnaryServerInfo{
+		FullMethod: tc.methodName,
+	}, noOpHandler)
+
+	if tc.expectRateLimit {
+		assert.ErrorContains(t, err, "namespace concurrent poller limit exceeded")
+	} else {
+		assert.NoError(t, err)
+	}
+	handler.Unblock()
+	for i := 0; i < tc.numBlockedRequests; i++ {
+		assert.NoError(t, <-errs)
+	}
+}
+
+func (tc *testCase) createBlockedPollRequests(interceptor *NamespaceCountLimitInterceptor) (testRequestHandler, chan error) {
+	handler := testRequestHandler{
+		started: make(chan struct{}),
+		respond: make(chan struct{}),
+	}
+	errs := make(chan error, tc.numBlockedRequests)
+	for i := 0; i < tc.numBlockedRequests; i++ {
+		go func() {
+			_, err := interceptor.Intercept(context.Background(), tc.request, &grpc.UnaryServerInfo{
+				FullMethod: tc.methodName,
+			}, handler.Handle)
+			errs <- err
+		}()
+	}
+	for i := 0; i < tc.numBlockedRequests; i++ {
+		<-handler.started
+	}
+	return handler, errs
+}
+
+func (tc *testCase) createInterceptor(ctrl *gomock.Controller) *NamespaceCountLimitInterceptor {
+	registry := namespace.NewMockRegistry(ctrl)
+	registry.EXPECT().GetNamespace(gomock.Any()).Return(&namespace.Namespace{}, nil).AnyTimes()
+	logger := log.NewNoopLogger()
+	perInstanceCountLimit := func(ns string) int {
+		return tc.perInstanceLimit
+	}
+	interceptor := NewNamespaceCountLimitInterceptor(
+		registry,
+		logger,
+		perInstanceCountLimit,
+		tc.tokens,
+	)
+	return interceptor
+}
+
+// noOpHandler is a grpc.UnaryHandler which does nothing.
+func noOpHandler(context.Context, interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+// testRequestHandler provides a grpc.UnaryHandler which signals when it starts and does not respond until signaled.
+type testRequestHandler struct {
+	started chan struct{}
+	respond chan struct{}
+}
+
+func (h testRequestHandler) Unblock() {
+	close(h.respond)
+}
+
+// Handle signals that the request has started and then blocks until signaled to respond.
+func (h testRequestHandler) Handle(context.Context, interface{}) (interface{}, error) {
+	h.started <- struct{}{}
+	<-h.respond
+	return nil, nil
+}
