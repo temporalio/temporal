ac773d35f91c6364122c99a19d70a5aa571ac639
Jacob LeGrone <git@jacob.work>
2023-07-26 16:33:36 -0400

    Update freeport.go to be unique implementation

--- internal/temporalite/freeport.go
+++ internal/temporalite/freeport.go
@@ -4,8 +4,6 @@
 //
 // Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
 //
-// Copyright (c) 2020 Uber Technologies, Inc.
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -31,8 +29,6 @@ import (
 	"net"
 )
 
-// Modified from https://github.com/phayes/freeport/blob/95f893ade6f232a5f1511d61735d89b1ae2df543/freeport.go
-
 func newPortProvider() *portProvider {
 	return &portProvider{}
 }
@@ -41,7 +37,7 @@ type portProvider struct {
 	listeners []*net.TCPListener
 }
 
-// GetFreePort asks the kernel for a free open port that is ready to use.
+// GetFreePort finds an open port on the system which is ready to use.
 func (p *portProvider) GetFreePort() (int, error) {
 	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
 	if err != nil {
@@ -60,6 +56,7 @@ func (p *portProvider) GetFreePort() (int, error) {
 	return l.Addr().(*net.TCPAddr).Port, nil
 }
 
+// MustGetFreePort calls GetFreePort, panicking on error.
 func (p *portProvider) MustGetFreePort() int {
 	port, err := p.GetFreePort()
 	if err != nil {
