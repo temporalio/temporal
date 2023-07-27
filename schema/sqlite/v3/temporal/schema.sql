44449d4801a7bad7aae50829ced0a397e3aba372
Saman Barghi <saman.barghi@temporal.io>
2023-07-27 15:51:19 -0400

    Add SQL/CQL to schema.{c,s}ql

--- schema/sqlite/v3/temporal/schema.sql
+++ schema/sqlite/v3/temporal/schema.sql
@@ -313,3 +313,27 @@ CREATE TABLE cluster_membership
 --	INDEX (record_expiry),
 	PRIMARY KEY (membership_partition, host_id)
 );
+
+CREATE TABLE queue_metadata_v2 (
+    queue_type INT NOT NULL,
+    queue_name VARCHAR(255) NOT NULL,
+    metadata_payload MEDIUMBLOB NOT NULL,
+    metadata_encoding VARCHAR(16) NOT NULL,
+    version BIGINT NOT NULL,
+    PRIMARY KEY (queue_type, queue_name)
+);
+
+CREATE TABLE queue_v2 (
+    queue_type INT NOT NULL,
+    queue_name VARCHAR(255) NOT NULL,
+    message_id BIGINT NOT NULL,
+    queue_partition BIGINT NOT NULL,
+    message_payload MEDIUMBLOB NOT NULL,
+    message_encoding VARCHAR(16) NOT NULL,
+    PRIMARY KEY (
+        queue_type,
+        queue_name,
+        queue_partition,
+        message_id
+    )
+);
