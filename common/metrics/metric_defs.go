4928b73c479fd74068569226c3090c0b6b5ea0a1
Haifeng He <haifeng.he@temporal.io>
2023-08-02 16:38:52 -0700

    Reduce the heartbeat size of VerifyReplicationTasks activity (#4705)
    <!-- Describe what has changed in this PR -->
    **What changed?**
    
    Reducing heart beat size (current payload size is 26K for 1000
    executions).
    
    <!-- Tell your future self why have you made these changes -->
    **Why?**
    
    
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    **How did you test it?**
    unit tests + cluster tests 
    
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    **Potential risks**
    
    
    <!-- Is this PR a hotfix candidate or require that a notification be
    sent to the broader community? (Yes/No) -->
    **Is hotfix candidate?**

--- common/metrics/metric_defs.go
+++ common/metrics/metric_defs.go
@@ -1574,6 +1574,7 @@ var (
 
 	// Force replication
 	EncounterZombieWorkflowCount      = NewCounterDef("encounter_zombie_workflow_count")
+	EncounterNotFoundWorkflowCount    = NewCounterDef("encounter_not_found_workflow_count")
 	GenerateReplicationTasksLatency   = NewTimerDef("generate_replication_tasks_latency")
 	VerifyReplicationTaskSuccess      = NewCounterDef("verify_replication_task_success")
 	VerifyReplicationTaskNotFound     = NewCounterDef("verify_replication_task_not_found")
