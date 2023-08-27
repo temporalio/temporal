temporalio/temporal

commit 8740a205ffbe6a69aba4ce667962af5982807595

Support signals that do not immediately generate a workflow task (#4091)
* wip

* tidy

* wip

* do not copy out buffered events

* tests

* feedback

* mocks

* feedback

* feedback

* flush buffered events

* test

* remove dupe reapply logic

* test

service/history/api/reapplyevents/api.go
@@ -177,15 +177,7 @@ func Invoke(
 				}, nil
 			}
 
-			postActions := &api.UpdateWorkflowAction{
-				Noop:               false,
-				CreateWorkflowTask: true,
-			}
-			if mutableState.IsWorkflowPendingOnWorkflowTaskBackoff() {
-				// Do not create workflow task when the workflow has first workflow task backoff and execution is not started yet
-				postActions.CreateWorkflowTask = false
-			}
-			reappliedEvents, err := eventsReapplier.ReapplyEvents(
+			_, err = eventsReapplier.ReapplyEvents(
 				ctx,
 				mutableState,
 				toReapplyEvents,
@@ -195,13 +187,10 @@ func Invoke(
 				shard.GetLogger().Error("failed to re-apply stale events", tag.Error(err))
 				return nil, err
 			}
-			if len(reappliedEvents) == 0 {
-				return &api.UpdateWorkflowAction{
-					Noop:               true,
-					CreateWorkflowTask: false,
-				}, nil
-			}
-			return postActions, nil
+			return &api.UpdateWorkflowAction{
+				Noop:               true,
+				CreateWorkflowTask: false,
+			}, nil
 		},
 		nil,
 		shard,

service/history/api/signalworkflow/api.go
@@ -74,11 +74,9 @@ func Invoke(
 			}
 
 			executionInfo := mutableState.GetExecutionInfo()
-			createWorkflowTask := true
-			if mutableState.IsWorkflowPendingOnWorkflowTaskBackoff() {
-				// Do not create workflow task when the workflow has first workflow task backoff and execution is not started yet
-				createWorkflowTask = false
-			}
+
+			// Do not create workflow task when the workflow has first workflow task backoff and execution is not started yet
+			createWorkflowTask := !mutableState.IsWorkflowPendingOnWorkflowTaskBackoff() && !request.GetSkipGenerateWorkflowTask()
 
 			if err := api.ValidateSignal(
 				ctx,
@@ -106,7 +104,8 @@ func Invoke(
 				request.GetSignalName(),
 				request.GetInput(),
 				request.GetIdentity(),
-				request.GetHeader()); err != nil {
+				request.GetHeader(),
+				request.GetSkipGenerateWorkflowTask()); err != nil {
 				return nil, err
 			}
 

service/history/api/create_workflow_util.go
@@ -95,6 +95,7 @@ func NewWorkflowWithSignal(
 			signalWithStartRequest.GetSignalInput(),
 			signalWithStartRequest.GetIdentity(),
 			signalWithStartRequest.GetHeader(),
+			signalWithStartRequest.GetSkipGenerateWorkflowTask(),
 		); err != nil {
 			return nil, err
 		}

service/history/api/signalwithstartworkflow/signal_with_start_workflow.go
@@ -279,12 +279,13 @@ func signalWorkflow(
 		request.GetSignalInput(),
 		request.GetIdentity(),
 		request.GetHeader(),
+		request.GetSkipGenerateWorkflowTask(),
 	); err != nil {
 		return err
 	}
 
 	// Create a transfer task to schedule a workflow task
-	if !mutableState.HasPendingWorkflowTask() {
+	if !mutableState.HasPendingWorkflowTask() && !request.GetSkipGenerateWorkflowTask() {
 		_, err := mutableState.AddWorkflowTaskScheduledEvent(false, enumsspb.WORKFLOW_TASK_TYPE_NORMAL)
 		if err != nil {
 			return err

service/history/api/signalwithstartworkflow/signal_with_start_workflow_test.go
@@ -137,6 +137,7 @@ func (s *signalWithStartWorkflowSuite) TestSignalWorkflow_NewWorkflowTask() {
 		s.currentMutableState,
 	)
 	request := s.randomRequest()
+	request.SkipGenerateWorkflowTask = false
 
 	s.currentMutableState.EXPECT().IsSignalRequested(request.GetRequestId()).Return(false)
 	s.currentMutableState.EXPECT().AddSignalRequested(request.GetRequestId())
@@ -145,6 +146,7 @@ func (s *signalWithStartWorkflowSuite) TestSignalWorkflow_NewWorkflowTask() {
 		request.GetSignalInput(),
 		request.GetIdentity(),
 		request.GetHeader(),
+		request.GetSkipGenerateWorkflowTask(),
 	).Return(&history.HistoryEvent{}, nil)
 	s.currentMutableState.EXPECT().HasPendingWorkflowTask().Return(false)
 	s.currentMutableState.EXPECT().AddWorkflowTaskScheduledEvent(false, enumsspb.WORKFLOW_TASK_TYPE_NORMAL).Return(&workflow.WorkflowTaskInfo{}, nil)
@@ -175,6 +177,7 @@ func (s *signalWithStartWorkflowSuite) TestSignalWorkflow_NoNewWorkflowTask() {
 		request.GetSignalInput(),
 		request.GetIdentity(),
 		request.GetHeader(),
+		request.GetSkipGenerateWorkflowTask(),
 	).Return(&history.HistoryEvent{}, nil)
 	s.currentMutableState.EXPECT().HasPendingWorkflowTask().Return(true)
 	s.currentContext.EXPECT().UpdateWorkflowExecutionAsActive(ctx).Return(nil)

service/history/historyEngine_test.go
@@ -1666,6 +1666,78 @@ func (s *engineSuite) TestRespondWorkflowTaskCompletedSingleActivityScheduledWor
 	s.Equal(5*time.Second, timestamp.DurationValue(activity1Attributes.HeartbeatTimeout))
 }
 
+func (s *engineSuite) TestRespondWorkflowTaskCompleted_SignalTaskGeneration() {
+	resp := s.testRespondWorkflowTaskCompletedSignalGeneration(false)
+	s.NotNil(resp.GetStartedResponse())
+}
+
+func (s *engineSuite) TestRespondWorkflowTaskCompleted_SkipSignalTaskGeneration() {
+	resp := s.testRespondWorkflowTaskCompletedSignalGeneration(true)
+	s.Nil(resp.GetStartedResponse())
+}
+
+func (s *engineSuite) testRespondWorkflowTaskCompletedSignalGeneration(skipGenerateTask bool) *historyservice.RespondWorkflowTaskCompletedResponse {
+	we := commonpb.WorkflowExecution{
+		WorkflowId: tests.WorkflowID,
+		RunId:      tests.RunID,
+	}
+	tl := "testTaskQueue"
+	tt := &tokenspb.Task{
+		Attempt:          1,
+		NamespaceId:      tests.NamespaceID.String(),
+		WorkflowId:       tests.WorkflowID,
+		RunId:            we.GetRunId(),
+		ScheduledEventId: 2,
+	}
+	taskToken, _ := tt.Marshal()
+	identity := "testIdentity"
+
+	signal := workflowservice.SignalWorkflowExecutionRequest{
+		Namespace:                tests.NamespaceID.String(),
+		WorkflowExecution:        &we,
+		Identity:                 identity,
+		SignalName:               "test signal name",
+		Input:                    payloads.EncodeString("test input"),
+		SkipGenerateWorkflowTask: skipGenerateTask,
+		RequestId:                uuid.New(),
+	}
+	signalRequest := &historyservice.SignalWorkflowExecutionRequest{
+		NamespaceId:   tests.NamespaceID.String(),
+		SignalRequest: &signal,
+	}
+
+	ms := workflow.TestLocalMutableState(s.mockHistoryEngine.shard, s.eventsCache,
+		tests.LocalNamespaceEntry, log.NewTestLogger(), we.GetRunId())
+	addWorkflowExecutionStartedEvent(ms, we, "wType", tl, payloads.EncodeString("input"), 100*time.Second, 90*time.Second, 200*time.Second, identity)
+	wt := addWorkflowTaskScheduledEvent(ms)
+	addWorkflowTaskStartedEvent(ms, wt.ScheduledEventID, tl, identity)
+
+	wfMs := workflow.TestCloneToProto(ms)
+	gwmsResponse := &persistence.GetWorkflowExecutionResponse{State: wfMs}
+	s.mockExecutionMgr.EXPECT().GetWorkflowExecution(gomock.Any(), gomock.Any()).Return(gwmsResponse, nil)
+	s.mockExecutionMgr.EXPECT().UpdateWorkflowExecution(gomock.Any(), gomock.Any()).Return(tests.UpdateWorkflowExecutionResponse, nil).AnyTimes()
+
+	_, err := s.mockHistoryEngine.SignalWorkflowExecution(context.Background(), signalRequest)
+	s.NoError(err)
+
+	var commands []*commandpb.Command
+	resp, err := s.mockHistoryEngine.RespondWorkflowTaskCompleted(context.Background(), &historyservice.RespondWorkflowTaskCompletedRequest{
+		NamespaceId: tests.NamespaceID.String(),
+		CompleteRequest: &workflowservice.RespondWorkflowTaskCompletedRequest{
+			TaskToken:             taskToken,
+			Commands:              commands,
+			Identity:              identity,
+			ReturnNewWorkflowTask: true,
+		},
+	})
+	s.NoError(err)
+	s.NotNil(resp)
+
+	s.Equal(enumsspb.WORKFLOW_EXECUTION_STATE_RUNNING, ms.GetExecutionState().State)
+
+	return resp
+}
+
 func (s *engineSuite) TestRespondWorkflowTaskCompleted_ActivityEagerExecution_NotCancelled() {
 	namespaceID := tests.NamespaceID
 	we := commonpb.WorkflowExecution{

service/history/ndc/events_reapplier.go
@@ -96,13 +96,16 @@ func (r *EventsReapplierImpl) ReapplyEvents(
 		return nil, serviceerror.NewInternal("unable to reapply events to closed workflow.")
 	}
 
+	shouldScheduleWorkflowTask := false
 	for _, event := range reappliedEvents {
 		signal := event.GetWorkflowExecutionSignaledEventAttributes()
+		shouldScheduleWorkflowTask = shouldScheduleWorkflowTask || !signal.GetSkipGenerateWorkflowTask()
 		if _, err := ms.AddWorkflowExecutionSignaled(
 			signal.GetSignalName(),
 			signal.GetInput(),
 			signal.GetIdentity(),
 			signal.GetHeader(),
+			signal.GetSkipGenerateWorkflowTask(),
 		); err != nil {
 			return nil, err
 		}
@@ -115,6 +118,11 @@ func (r *EventsReapplierImpl) ReapplyEvents(
 		// Do not create workflow task when the workflow has first workflow task backoff and execution is not started yet
 		return reappliedEvents, nil
 	}
+	if !shouldScheduleWorkflowTask {
+		// Do not create workflow task when all reapplied signals had SkipGenerateWorkflowTask=true flag set
+		return reappliedEvents, nil
+	}
+
 	if !ms.HasPendingWorkflowTask() {
 		if _, err := ms.AddWorkflowTaskScheduledEvent(
 			false,

service/history/ndc/events_reapplier_test.go
@@ -104,6 +104,7 @@ func (s *nDCEventReapplicationSuite) TestReapplyEvents_AppliedEvent() {
 		attr.GetInput(),
 		attr.GetIdentity(),
 		attr.GetHeader(),
+		attr.GetSkipGenerateWorkflowTask(),
 	).Return(event, nil)
 	msCurrent.EXPECT().IsWorkflowPendingOnWorkflowTaskBackoff().Return(true)
 	dedupResource := definition.NewEventReappliedID(runID, event.GetEventId(), event.GetVersion())
@@ -178,6 +179,7 @@ func (s *nDCEventReapplicationSuite) TestReapplyEvents_PartialAppliedEvent() {
 		attr1.GetInput(),
 		attr1.GetIdentity(),
 		attr1.GetHeader(),
+		attr1.GetSkipGenerateWorkflowTask(),
 	).Return(event1, nil)
 	msCurrent.EXPECT().IsWorkflowPendingOnWorkflowTaskBackoff().Return(true)
 	dedupResource1 := definition.NewEventReappliedID(runID, event1.GetEventId(), event1.GetVersion())
@@ -221,6 +223,7 @@ func (s *nDCEventReapplicationSuite) TestReapplyEvents_Error() {
 		attr.GetInput(),
 		attr.GetIdentity(),
 		attr.GetHeader(),
+		attr.GetSkipGenerateWorkflowTask(),
 	).Return(nil, fmt.Errorf("test"))
 	dedupResource := definition.NewEventReappliedID(runID, event.GetEventId(), event.GetVersion())
 	msCurrent.EXPECT().IsResourceDuplicated(dedupResource).Return(false)

commit 7200c4dd3ac7689588b17bf20dfc0a0e42e9b332

Fix mutable state change not persisted bug (#4813)
<!-- Describe what has changed in this PR -->
**What changed?**
* Persist mutable state changes if any event is reapplied

<!-- Tell your future self why have you made these changes -->
**Why?**
Fix issue introduced in #4091,
[ref](https://github.com/temporalio/temporal/pull/4091/files#r1306573612)

<!-- How have you verified this change? Tested locally? Added a unit
test? Checked in staging env? -->
**How did you test it?**
N/A

<!-- Assuming the worst case, what can be broken when deploying this
change to production? -->
**Potential risks**
N/A

<!-- Is this PR a hotfix candidate or require that a notification be
sent to the broader community? (Yes/No) -->
**Is hotfix candidate?**
N/A

service/history/api/reapplyevents/api.go
@@ -175,7 +175,7 @@ func Invoke(
 				}, nil
 			}
 
-			_, err = eventsReapplier.ReapplyEvents(
+			reappliedEvents, err := eventsReapplier.ReapplyEvents(
 				ctx,
 				mutableState,
 				toReapplyEvents,
@@ -185,8 +185,14 @@ func Invoke(
 				shard.GetLogger().Error("failed to re-apply stale events", tag.Error(err))
 				return nil, err
 			}
+			if len(reappliedEvents) == 0 {
+				return &api.UpdateWorkflowAction{
+					Noop:               true,
+					CreateWorkflowTask: false,
+				}, nil
+			}
 			return &api.UpdateWorkflowAction{
-				Noop:               true,
+				Noop:               false,
 				CreateWorkflowTask: false,
 			}, nil
 		},