3e21218ac46d4587fa34a4c326117a22443da734
yux0 <yuhsia89@gmail.com>
2023-09-14 20:32:52 -0700

    fotmat

--- service/history/replication/sequential_queue.go
+++ service/history/replication/sequential_queue.go
@@ -31,7 +31,6 @@ import (
 
 	enumsspb "go.temporal.io/server/api/enums/v1"
 	replicationspb "go.temporal.io/server/api/replication/v1"
-
 	"go.temporal.io/server/common/collection"
 	"go.temporal.io/server/common/definition"
 	ctasks "go.temporal.io/server/common/tasks"
