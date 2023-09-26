d526eec3a3b416d826b6e2279f356607a65bb6dd
David Reiss <david@temporal.io>
2023-09-26 17:44:13 +0100

    actually use WithNoDumpCommands

--- tests/transient_task_test.go
+++ tests/transient_task_test.go
@@ -260,7 +260,7 @@ func (s *integrationSuite) TestTransientWorkflowTaskHistorySize() {
 	}
 
 	// stage 1
-	_, err := poller.PollAndProcessWorkflowTask()
+	_, err := poller.PollAndProcessWorkflowTask(WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
@@ -268,7 +268,7 @@ func (s *integrationSuite) TestTransientWorkflowTaskHistorySize() {
 	s.NoError(err, "failed to send signal to execution")
 
 	// stage 2
-	_, err = poller.PollAndProcessWorkflowTask()
+	_, err = poller.PollAndProcessWorkflowTask(WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
@@ -276,7 +276,7 @@ func (s *integrationSuite) TestTransientWorkflowTaskHistorySize() {
 	s.NoError(err, "failed to send signal to execution")
 
 	// stage 3: this one fails with a panic
-	_, err = poller.PollAndProcessWorkflowTask()
+	_, err = poller.PollAndProcessWorkflowTask(WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
@@ -285,7 +285,7 @@ func (s *integrationSuite) TestTransientWorkflowTaskHistorySize() {
 	s.testCluster.host.dcClient.OverrideValue(dynamicconfig.HistorySizeSuggestContinueAsNew, 8*1024*1024)
 
 	// stage 4
-	_, err = poller.PollAndProcessWorkflowTask()
+	_, err = poller.PollAndProcessWorkflowTask(WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
@@ -293,12 +293,12 @@ func (s *integrationSuite) TestTransientWorkflowTaskHistorySize() {
 	s.NoError(err, "failed to send signal to execution")
 
 	// drop workflow task to cause a workflow task timeout
-	_, err = poller.PollAndProcessWorkflowTask(WithDumpHistory, WithDropTask)
+	_, err = poller.PollAndProcessWorkflowTask(WithDropTask, WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
 	// stage 5
-	_, err = poller.PollAndProcessWorkflowTask()
+	_, err = poller.PollAndProcessWorkflowTask(WithNoDumpCommands)
 	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
 	s.NoError(err)
 
