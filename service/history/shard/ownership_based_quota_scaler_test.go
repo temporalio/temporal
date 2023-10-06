d54f4d8c1547222c4cae48fdf59c67b5776c3c2e
Yichao Yang <yichao@temporal.io>
2023-10-06 11:03:01 -0700

    Shard count based rate limiting (#4923)

--- service/history/shard/ownership_based_quota_scaler_test.go
+++ service/history/shard/ownership_based_quota_scaler_test.go
@@ -51,24 +51,6 @@ func (s *shardCounter) Unsubscribe() {
 	s.closed = true
 }
 
-// constantRateBurst is a quotas.RateBurst implementation that returns the same rate and burst every time.
-type constantRateBurst struct {
-	rate  float64
-	burst int
-}
-
-func (rb constantRateBurst) Rate() float64 {
-	return rb.rate
-}
-
-func (rb constantRateBurst) Burst() int {
-	return rb.burst
-}
-
-func newRateBurst(rate float64, burst int) constantRateBurst {
-	return constantRateBurst{rate: rate, burst: burst}
-}
-
 func TestOwnershipBasedQuotaScaler_NonPositiveTotalNumShards(t *testing.T) {
 	t.Parallel()
 
@@ -84,7 +66,6 @@ func TestOwnershipBasedQuotaScaler_NonPositiveTotalNumShards(t *testing.T) {
 func TestOwnershipBasedQuotaScaler(t *testing.T) {
 	t.Parallel()
 
-	rb := newRateBurst(2, 4)
 	sc := &shardCounter{
 		ch:     make(chan int),
 		closed: false,
@@ -93,22 +74,20 @@ func TestOwnershipBasedQuotaScaler(t *testing.T) {
 	updateAppliedCallback := make(chan struct{})
 	scaler, err := shard.NewOwnershipBasedQuotaScaler(sc, totalNumShards, updateAppliedCallback)
 	require.NoError(t, err)
-	srb := scaler.ScaleRateBurst(rb)
-	assert.Equal(t, 2.0, srb.Rate(), "Rate should be equal to the base rate before any shard count updates")
-	assert.Equal(t, 4, srb.Burst(), "Burst should be equal to the base burst before any shard count updates")
-	sc.ch <- 3
+	_, ok := scaler.ScaleFactor()
+	assert.False(t, ok, "ScaleFactor should return false before any shard count updates")
 
+	sc.ch <- 3
 	// Wait for the update to be applied. Even though the send above is blocking, we still need to wait for the
 	// rate/burst scaler's goroutine to use it to adjust the scale factor.
 	<-updateAppliedCallback
 
-	// After the update is applied, the scale factor is calculated as 3/10 = 0.3, so the rate and burst should be
-	// multiplied by 0.3. Since the initial rate and burst are 2 and 4, respectively, the final rate and burst should be
-	// 0.6 and 1.2, respectively. However, since the burst is rounded up to the nearest integer, the final burst should
-	// be 2.
-	assert.Equal(t, 0.6, srb.Rate())
-	assert.Equal(t, 2, srb.Burst())
+	// After the update is applied, the scale factor is calculated as 3/10 = 0.3
+	factor, ok := scaler.ScaleFactor()
+	assert.True(t, ok)
+	assert.Equal(t, 0.3, factor)
+
 	assert.False(t, sc.closed, "The shard counter should not be closed until the srb scaler is stopped")
-	srb.StopScaling()
+	scaler.Close()
 	assert.True(t, sc.closed, "The shard counter should be closed after the srb scaler is stopped")
 }
