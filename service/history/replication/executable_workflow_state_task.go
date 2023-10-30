6a2b7122e12e4a44cbb642533098df016482045b
Will Duan <xinw.duan@gmail.com>
2023-10-30 16:23:54 -0700

    Add log for skipped replication task (#5042)
    <!-- Describe what has changed in this PR -->
    **What changed?**
    Change Skip replication task metrics tag to use namespaceId
    
    <!-- Tell your future self why have you made these changes -->
    **Why?**
    We are seeing a case where target cluster cannot find the namespace info
    by ID, so we want to know which namespaceID it is, so we can do further
    investigation
    
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    **How did you test it?**
    N/a
    
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    **Potential risks**
    n/a
    
    <!-- Is this PR a hotfix candidate or require that a notification be
    sent to the broader community? (Yes/No) -->
    **Is hotfix candidate?**
    n/a

--- service/history/replication/executable_workflow_state_task.go
+++ service/history/replication/executable_workflow_state_task.go
@@ -104,6 +104,12 @@ func (e *ExecutableWorkflowStateTask) Execute() error {
 	if err != nil {
 		return err
 	} else if !apply {
+		e.Logger.Warn("Skipping the replication task",
+			tag.WorkflowNamespaceID(e.NamespaceID),
+			tag.WorkflowID(e.WorkflowID),
+			tag.WorkflowRunID(e.RunID),
+			tag.TaskID(e.ExecutableTask.TaskID()),
+		)
 		e.MetricsHandler.Counter(metrics.ReplicationTasksSkipped.GetMetricName()).Record(
 			1,
 			metrics.OperationTag(metrics.SyncWorkflowStateTaskScope),
