459864aee40cee9f7d638dd10f9824ecf40b89e4
PJ Doerner <pj.doerner@temporal.io>
2024-01-12 10:34:37 -0800

    sqlite impl

--- common/persistence/sql/sqlplugin/sqlite/nexus_incoming_services.go
+++ common/persistence/sql/sqlplugin/sqlite/nexus_incoming_services.go
@@ -25,51 +25,127 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 
-	"go.temporal.io/api/serviceerror"
-
+	"go.temporal.io/server/common/persistence"
 	"go.temporal.io/server/common/persistence/sql/sqlplugin"
 )
 
+const (
+	createIncomingServicesTableVersionQry    = `INSERT INTO nexus_services_table_versions(service_type, version) VALUES(0, 1)`
+	incrementIncomingServicesTableVersionQry = `UPDATE nexus_services_table_versions SET version = ? WHERE service_type = 0 AND version = ?`
+	getIncomingServicesTableVersionQry       = `SELECT version FROM nexus_services_table_versions WHERE service_type = 0 LIMIT 1`
+
+	createIncomingServiceQry = `INSERT INTO nexus_incoming_services(service_id, data, data_encoding, version) VALUES (?, ?, ?, 1)`
+	updateIncomingServiceQry = `UPDATE nexus_incoming_services SET data = ?, data_encoding = ?, version = ? WHERE service_id = ? AND version = ?`
+	deleteIncomingServiceQry = `DELETE FROM nexus_incoming_services WHERE service_id = ?`
+	getIncomingServicesQry   = `SELECT service_id, data, data_encoding, version FROM nexus_incoming_services WHERE service_id > ? LIMIT ?`
+)
+
 func (mdb *db) InitializeNexusIncomingServicesTableVersion(ctx context.Context) error {
-	return serviceerror.NewUnimplemented("InitializeNexusIncomingServicesTableVersion is not implemented for SQLite plugin")
+	result, err := mdb.conn.ExecContext(ctx, createIncomingServicesTableVersionQry)
+	if err != nil {
+		return err
+	}
+	nRows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if nRows != 1 {
+		return persistence.ErrNexusTableVersionConflict
+	}
+	return nil
 }
 
 func (mdb *db) IncrementNexusIncomingServicesTableVersion(
 	ctx context.Context,
 	lastKnownTableVersion int64,
 ) error {
-	return serviceerror.NewUnimplemented("IncrementNexusIncomingServicesTableVersion is not implemented for SQLite plugin")
+	result, err := mdb.conn.ExecContext(ctx, incrementIncomingServicesTableVersionQry, lastKnownTableVersion+1, lastKnownTableVersion)
+	if err != nil {
+		return err
+	}
+	nRows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if nRows != 1 {
+		return persistence.ErrNexusTableVersionConflict
+	}
+	return nil
 }
 
 func (mdb *db) GetNexusIncomingServicesTableVersion(ctx context.Context) (int64, error) {
-	return 0, serviceerror.NewUnimplemented("GetNexusIncomingServicesTableVersion is not implemented for SQLite plugin")
+	version := int64(-1)
+	err := mdb.conn.GetContext(ctx, &version, getIncomingServicesTableVersionQry)
+	if errors.Is(err, sql.ErrNoRows) {
+		return 0, nil
+	}
+	return version, err
 }
 
 func (mdb *db) InsertIntoNexusIncomingServices(
 	ctx context.Context,
 	row *sqlplugin.NexusIncomingServicesRow,
 ) error {
-	return serviceerror.NewUnimplemented("InsertIntoNexusIncomingServices is not implemented for SQLite plugin")
+	result, err := mdb.conn.ExecContext(
+		ctx,
+		createIncomingServiceQry,
+		row.ServiceID,
+		row.Data,
+		row.DataEncoding)
+	if err != nil {
+		return err
+	}
+	nRows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if nRows != 1 {
+		return persistence.ErrNexusTableVersionConflict
+	}
+	return err
 }
 
 func (mdb *db) UpdateNexusIncomingService(
 	ctx context.Context,
 	row *sqlplugin.NexusIncomingServicesRow,
 ) error {
-	return serviceerror.NewUnimplemented("UpdateNexusIncomingService is not implemented for SQLite plugin")
-}
+	result, err := mdb.conn.ExecContext(
+		ctx,
+		updateIncomingServiceQry,
+		row.Data,
+		row.DataEncoding,
+		row.Version+1,
+		row.ServiceID,
+		row.Version)
+	if err != nil {
+		return err
+	}
 
-func (mdb *db) ListNexusIncomingServices(
-	ctx context.Context,
-	request *sqlplugin.ListNexusIncomingServicesRequest,
-) ([]sqlplugin.NexusIncomingServicesRow, error) {
-	return nil, serviceerror.NewUnimplemented("ListNexusIncomingServices is not implemented for SQLite plugin")
+	nRows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if nRows != 1 {
+		return persistence.ErrNexusIncomingServiceVersionConflict
+	}
+
+	return nil
 }
 
 func (mdb *db) DeleteFromNexusIncomingServices(
 	ctx context.Context,
 	serviceID []byte,
 ) (sql.Result, error) {
-	return nil, serviceerror.NewUnimplemented("DeleteFromNexusIncomingServices is not implemented for SQLite plugin")
+	return mdb.conn.ExecContext(ctx, deleteIncomingServiceQry, serviceID)
+}
+
+func (mdb *db) ListNexusIncomingServices(
+	ctx context.Context,
+	request *sqlplugin.ListNexusIncomingServicesRequest,
+) ([]sqlplugin.NexusIncomingServicesRow, error) {
+	var rows []sqlplugin.NexusIncomingServicesRow
+	err := mdb.conn.SelectContext(ctx, &rows, getIncomingServicesQry, request.LastServiceID, request.Limit)
+	return rows, err
 }
