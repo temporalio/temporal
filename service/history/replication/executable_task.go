079805bcb2f028230f2131d89b57db994d1036aa
xwduan <xinw.duan@gmail.com>
2024-01-17 09:23:07 -0800

    Change replication latency metrics definition

--- service/history/replication/executable_task.go
+++ service/history/replication/executable_task.go
@@ -272,6 +272,11 @@ func (e *ExecutableTaskImpl) emitFinishMetrics(
 		nsTag,
 	)
 	e.MetricsHandler.Timer(metrics.ReplicationLatency.Name()).Record(
+		now.Sub(e.taskCreationTime),
+		metrics.OperationTag(e.metricsTag),
+		nsTag,
+	)
+	e.MetricsHandler.Timer(metrics.ReplicationTaskReceiveLatency.Name()).Record(
 		e.taskReceivedTime.Sub(e.taskCreationTime),
 		metrics.OperationTag(e.metricsTag),
 		nsTag,
