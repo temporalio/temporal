0de4a396bc4b3efe25a86008d9ad6deea7a9679c
Alex Shtin <alex@shtin.com>
2024-02-13 16:29:59 -0800

    Minor fixes

--- common/testing/testvars/rand.go
+++ common/testing/testvars/rand.go
@@ -25,11 +25,22 @@
 package testvars
 
 import (
-	"hash/fnv"
+	"math"
+	"math/rand"
 )
 
-func hash(s string) uint32 {
-	h := fnv.New32a()
-	_, _ = h.Write([]byte(s))
-	return h.Sum32()
+func randInt(testHash uint32, hashLen, padLen, randomLen int) int {
+	testID := int(testHash) % int(math.Pow10(hashLen))
+	pad := int(math.Pow10(padLen + randomLen))
+	random := rand.Int() % int(math.Pow10(randomLen))
+	return testID*pad + random
+}
+
+func randString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
