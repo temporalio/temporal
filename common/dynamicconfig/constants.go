6b210331c4f9ebf00a2b64de96ffb5e6c31ce8ea
David Reiss <david@temporal.io>
2024-02-14 23:29:45 -0800

    Unload matching task queues on membership change (#5345)
    ## What changed?
    Matching pays attention to membership changes and unload task queue
    partitions that it no longer owns. It waits a few seconds before
    unloading to avoid situations where a task queue would get immediately
    reloaded because the membership change hadn't propagated to some
    frontend or history. The delay of 3s was chosen because membership
    changes seem to propagate in <1s generally.
    
    ## Why?
    Fixes #5198
    
    ## How did you test it?
    unit test, lots of simulated restarts and monitoring metrics
    
    ## Potential risks
    If membership changes are very slow to propagate, this could lead to
    more loading/unloading, although that could happen anyway (if two
    frontend/history disagree on the owner of a partition and they fight for
    it). We could address that by checking membership at load time also, but
    that has other risks. It could come in a separate PR.

--- common/dynamicconfig/constants.go
+++ common/dynamicconfig/constants.go
@@ -461,6 +461,9 @@ const (
 	MatchingMaxWaitForPollerBeforeFwd = "matching.maxWaitForPollerBeforeFwd"
 	// QueryPollerUnavailableWindow WF Queries are rejected after a while if no poller has been seen within the window
 	QueryPollerUnavailableWindow = "matching.queryPollerUnavailableWindow"
+	// MatchingMembershipUnloadDelay is how long to wait to re-confirm loss of ownership before unloading a task queue.
+	// Set to zero to disable proactive unload.
+	MatchingMembershipUnloadDelay = "matching.membershipUnloadDelay"
 
 	// for matching testing only:
 
