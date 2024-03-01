ace6a6a42bc723b7c1f3a48bcd73666459f490cf
Tim Deeb-Swihart <tim.deebswihart@temporal.io>
2024-03-01 13:49:20 -0800

    Specify content type
    See https://github.com/OAI/OpenAPI-Specification/issues/110#issuecomment-364498200

--- service/frontend/openapi_http_handler.go
+++ service/frontend/openapi_http_handler.go
@@ -60,7 +60,7 @@ func (h *OpenAPIHTTPHandler) RegisterRoutes(r *mux.Router) {
 		return
 	}
 
-	serve := func(version int, apiName string, spec []byte) func(http.ResponseWriter, *http.Request) {
+	serve := func(version int, apiName string, contentType string, spec []byte) func(http.ResponseWriter, *http.Request) {
 		return func(w http.ResponseWriter, r *http.Request) {
 			if err := h.rateLimitInterceptor.Allow(apiName, r.Header); err != nil {
 				w.WriteHeader(http.StatusTooManyRequests)
@@ -82,6 +82,16 @@ func (h *OpenAPIHTTPHandler) RegisterRoutes(r *mux.Router) {
 		}
 	}
 
-	r.PathPrefix("/api/v1/swagger.json").Methods("GET").HandlerFunc(serve(2, configs.OpenAPIV2APIName, openapi.OpenAPIV2JSONSpec))
-	r.PathPrefix("/api/v1/openapi.yaml").Methods("GET").HandlerFunc(serve(3, configs.OpenAPIV3APIName, openapi.OpenAPIV3YAMLSpec))
+	r.PathPrefix("/api/v1/swagger.json").Methods("GET").HandlerFunc(serve(
+		2,
+		configs.OpenAPIV2APIName,
+		"application/vnd.oai.openapi+json;version=2.0",
+		openapi.OpenAPIV2JSONSpec,
+	))
+	r.PathPrefix("/api/v1/openapi.yaml").Methods("GET").HandlerFunc(serve(
+		3,
+		configs.OpenAPIV3APIName,
+		"application/vnd.oai.openapi;version=3.0",
+		openapi.OpenAPIV3YAMLSpec,
+	))
 }
