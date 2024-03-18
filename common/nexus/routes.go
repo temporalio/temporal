3bcdaceca03b5ceb9aeb862bfa83151b7464d965
Michael Snowden <MichaelSnowden@users.noreply.github.com>
2024-03-18 15:45:24 -0700

    Create common/routing package for HTTP routes (#5539)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    I added a `common/routing` package for defining HTTP routes which
    clients can use to construct URLs, and servers can use to parse
    requests, all in a type-safe manner.
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    I was starting to write some functional tests for Nexus, and I realized
    that I didn't know what the routes were for it, and it felt flimsy to
    copy/paste what I found in other places. Instead, I felt like having a
    common set of "routes" similar to what you find in common web frameworks
    would be a good idea. It's a small package, and the only use case is
    Nexus, but I think it makes it a bit more ergonomic / safer.
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    I added unit tests for the new package.
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

---
+++ common/nexus/routes.go
@@ -0,0 +1,56 @@
+// The MIT License
+//
+// Copyright (c) 2024 Temporal Technologies Inc.  All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package nexus
+
+import "go.temporal.io/server/common/routing"
+
+// Routes returns a RouteSet for the Nexus HTTP API. These routes can be used by both the server and clients for
+// type-safe URL construction and parsing. It's a function instead of a variable so that the underlying
+// set cannot be modified by the caller.
+func Routes() RouteSet {
+	return routes
+}
+
+type RouteSet struct {
+	DispatchNexusTaskByNamespaceAndTaskQueue routing.Route[NamespaceAndTaskQueue]
+	DispatchNexusTaskByService               routing.Route[string]
+}
+
+type NamespaceAndTaskQueue struct {
+	Namespace string
+	TaskQueue string
+}
+
+var routes = RouteSet{
+	DispatchNexusTaskByNamespaceAndTaskQueue: routing.NewBuilder[NamespaceAndTaskQueue]().
+		Constant("api", "v1", "namespaces").
+		Variable("namespace", func(params *NamespaceAndTaskQueue) *string { return &params.Namespace }).
+		Constant("task-queues").
+		Variable("task_queue", func(params *NamespaceAndTaskQueue) *string { return &params.TaskQueue }).
+		Constant("dispatch-nexus-task").
+		Build(),
+	DispatchNexusTaskByService: routing.NewBuilder[string]().
+		Constant("api", "v1", "services").
+		Variable("service", func(service *string) *string { return service }).
+		Build(),
+}
