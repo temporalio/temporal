465acbbef3969b86eae80af2412276a39ceda68d
Michael Snowden <michaelosnowden@gmail.com>
2024-03-27 16:59:26 -0700

    Add missing go:generate command for scheduler

--- common/tasks/scheduler.go
+++ common/tasks/scheduler.go
@@ -24,6 +24,7 @@
 
 package tasks
 
+//go:generate go run ../../cmd/tools/mockgen/main.go -copyright_file ../../LICENSE -package $GOPACKAGE -source $GOFILE -destination scheduler_mock.go
 type (
 	// Scheduler is the generic interface for scheduling & processing tasks
 	Scheduler[T Task] interface {
