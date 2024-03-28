1c396c1020fc102d563e4709945a4664294bc038
Temporal Data <commander-data@temporal.io>
2024-03-28 19:02:06 +0000

    Bump Server version to 1.22.7

--- common/headers/version_checker.go
+++ common/headers/version_checker.go
@@ -47,7 +47,7 @@ const (
 	ClientNameCLI           = "temporal-cli"
 	ClientNameUI            = "temporal-ui"
 
-	ServerVersion = "1.22.6"
+	ServerVersion = "1.22.7"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
