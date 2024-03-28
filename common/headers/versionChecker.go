10bf7ba152194b7c9405ca175b0b1f9734b52366
Tim Deeb-Swihart <tim.deebswihart@temporal.io>
2024-03-28 12:33:38 -0700

    Update server version

--- common/headers/versionChecker.go
+++ common/headers/versionChecker.go
@@ -46,7 +46,7 @@ const (
 	ClientNameCLI           = "temporal-cli"
 	ClientNameUI            = "temporal-ui"
 
-	ServerVersion = "1.20.4"
+	ServerVersion = "1.20.5"
 
 	// SupportedServerVersions is used by CLI and inter role communication.
 	SupportedServerVersions = ">=1.0.0 <2.0.0"
