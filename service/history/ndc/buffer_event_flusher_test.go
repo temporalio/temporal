eafe088ee2e7dac2071f52f0eef53d0d0b8a1fde
Yichao Yang <yichao@temporal.io>
2024-05-20 18:56:45 -0700

    fix tests

--- service/history/ndc/buffer_event_flusher_test.go
+++ service/history/ndc/buffer_event_flusher_test.go
@@ -143,7 +143,7 @@ func (s *bufferEventFlusherSuite) TestClearTransientWorkflowTask() {
 
 func (s *bufferEventFlusherSuite) TestFlushBufferedEvents() {
 
-	lastEventVersion := int64(300)
+	lastWriteVersion := int64(300)
 	versionHistory := versionhistory.NewVersionHistory([]byte("some random base branch token"), []*historyspb.VersionHistoryItem{
 		versionhistory.NewVersionHistoryItem(10, 0),
 		versionhistory.NewVersionHistoryItem(50, 100),
@@ -159,10 +159,10 @@ func (s *bufferEventFlusherSuite) TestFlushBufferedEvents() {
 	)
 	s.NoError(err)
 
-	s.mockMutableState.EXPECT().GetLastEventVersion().Return(lastEventVersion, nil).AnyTimes()
+	s.mockMutableState.EXPECT().GetLastWriteVersion().Return(lastWriteVersion, nil).AnyTimes()
 	s.mockMutableState.EXPECT().HasBufferedEvents().Return(true).AnyTimes()
 	s.mockMutableState.EXPECT().IsWorkflowExecutionRunning().Return(true).AnyTimes()
-	s.mockMutableState.EXPECT().UpdateCurrentVersion(lastEventVersion, true).Return(nil)
+	s.mockMutableState.EXPECT().UpdateCurrentVersion(lastWriteVersion, true).Return(nil)
 	workflowTask := &workflow.WorkflowTaskInfo{
 		ScheduledEventID: 1234,
 		StartedEventID:   2345,
@@ -187,7 +187,7 @@ func (s *bufferEventFlusherSuite) TestFlushBufferedEvents() {
 		enumsspb.WORKFLOW_TASK_TYPE_NORMAL,
 	).Return(&workflow.WorkflowTaskInfo{}, nil)
 	s.mockMutableState.EXPECT().FlushBufferedEvents()
-	s.mockClusterMetadata.EXPECT().ClusterNameForFailoverVersion(true, lastEventVersion).Return(cluster.TestCurrentClusterName).AnyTimes()
+	s.mockClusterMetadata.EXPECT().ClusterNameForFailoverVersion(true, lastWriteVersion).Return(cluster.TestCurrentClusterName).AnyTimes()
 	s.mockClusterMetadata.EXPECT().GetCurrentClusterName().Return(cluster.TestCurrentClusterName).AnyTimes()
 
 	s.mockContext.EXPECT().UpdateWorkflowExecutionAsActive(gomock.Any(), s.mockShard).Return(nil)
