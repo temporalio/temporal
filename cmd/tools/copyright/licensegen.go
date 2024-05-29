e078f7f764b15a72bef749e49babd1992697f112
Brendan Gerrity <brendan.gerrity@datadoghq.com>
2024-05-29 12:44:21 -0400

    fix license spelling

--- cmd/tools/copyright/licensegen.go
+++ cmd/tools/copyright/licensegen.go
@@ -59,7 +59,7 @@ var (
 	// directories to be excluded
 	dirBlocklist = []string{".gen/", ".git/", ".vscode/", ".idea/"}
 	// default perms for the newly created files
-	defaultFilePerms = os.FileMode(0644)
+	defaultFilePerms = os.FileMode(0o644)
 )
 
 // command line utility that adds license header
@@ -121,7 +121,7 @@ func (task *addLicenseHeaderTask) handleFile(path string, fileInfo os.FileInfo, 
 		return nil
 	}
 
-	// Used as part of the cli to write licence headers on files, does not use user supplied input so marked as nosec
+	// Used as part of the cli to write license headers on files, does not use user supplied input so marked as nosec
 	// #nosec
 	f, err := os.Open(path)
 	if err != nil {
@@ -153,7 +153,7 @@ func (task *addLicenseHeaderTask) handleFile(path string, fileInfo os.FileInfo, 
 		}
 	}
 
-	// Used as part of the cli to write licence headers on files, does not use user supplied input so marked as nosec
+	// Used as part of the cli to write license headers on files, does not use user supplied input so marked as nosec
 	// #nosec
 	data, err := os.ReadFile(path)
 	if err != nil {
