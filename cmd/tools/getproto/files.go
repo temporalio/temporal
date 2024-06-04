103db00a91029894eaba0881b540f0bb37b5c2cb
David Reiss <david@temporal.io>
2024-06-04 14:55:29 +0100

    update files.go with new dep

--- cmd/tools/getproto/files.go
+++ cmd/tools/getproto/files.go
@@ -88,6 +88,7 @@ func init() {
 	importMap["temporal/api/replication/v1/message.proto"] = replication.File_temporal_api_replication_v1_message_proto
 	importMap["temporal/api/schedule/v1/message.proto"] = schedule.File_temporal_api_schedule_v1_message_proto
 	importMap["temporal/api/sdk/v1/task_complete_metadata.proto"] = sdk.File_temporal_api_sdk_v1_task_complete_metadata_proto
+	importMap["temporal/api/sdk/v1/user_metadata.proto"] = sdk.File_temporal_api_sdk_v1_user_metadata_proto
 	importMap["temporal/api/taskqueue/v1/message.proto"] = taskqueue.File_temporal_api_taskqueue_v1_message_proto
 	importMap["temporal/api/update/v1/message.proto"] = update.File_temporal_api_update_v1_message_proto
 	importMap["temporal/api/version/v1/message.proto"] = version.File_temporal_api_version_v1_message_proto
