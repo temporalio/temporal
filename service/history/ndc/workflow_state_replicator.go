4d8ca319ba57306606e6aa79cd636ef0a128e2a3
Yu Xia <yuhsia89@gmail.com>
2024-06-08 00:39:25 -0700

    Acquire workflow lock during backfill history (#6088)
    ## What changed?
    Acquire workflow lock during backfill history
    
    ## Why?
    Stream replication processes WorkflowRun concurrently. This could lead
    to concurrent append history node on same node id and write dirty
    history node into the table.
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- service/history/ndc/workflow_state_replicator.go
+++ service/history/ndc/workflow_state_replicator.go
@@ -265,7 +265,26 @@ func (r *WorkflowStateReplicatorImpl) backfillHistory(
 	lastEventID int64,
 	lastEventVersion int64,
 	branchToken []byte,
-) (int64, error) {
+) (taskID int64, retError error) {
+
+	// Get the current run lock to make sure no concurrent backfill history across multiple runs.
+	currentRunReleaseFn, err := r.workflowCache.GetOrCreateCurrentWorkflowExecution(
+		ctx,
+		r.shardContext,
+		namespaceID,
+		workflowID,
+		workflow.LockPriorityLow,
+	)
+	if err != nil {
+		return common.EmptyEventTaskID, err
+	}
+	defer func() {
+		if rec := recover(); rec != nil {
+			currentRunReleaseFn(errPanic)
+			panic(rec)
+		}
+		currentRunReleaseFn(retError)
+	}()
 
 	// Get the last batch node id to check if the history data is already in DB.
 	localHistoryIterator := collection.NewPagingIterator(r.getHistoryFromLocalPaginationFn(
