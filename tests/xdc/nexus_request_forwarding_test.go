4e226b96e98183f9a06c89beee85eb2b0f8c9bae
PJ Doerner <pj.doerner@temporal.io>
2024-06-12 10:55:56 -0700

    CompleteNexusOperation request forwarding

--- tests/xdc/nexus_request_forwarding_test.go
+++ tests/xdc/nexus_request_forwarding_test.go
@@ -27,7 +27,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
+	"slices"
 	"testing"
 	"time"
 
@@ -36,18 +38,31 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	commandpb "go.temporal.io/api/command/v1"
+	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
+	historypb "go.temporal.io/api/history/v1"
 	nexuspb "go.temporal.io/api/nexus/v1"
+	"go.temporal.io/api/operatorservice/v1"
+	replicationpb "go.temporal.io/api/replication/v1"
 	taskqueuepb "go.temporal.io/api/taskqueue/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/converter"
 	"google.golang.org/protobuf/types/known/durationpb"
 
 	"go.temporal.io/server/common/dynamicconfig"
+	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/metrics/metricstest"
 	"go.temporal.io/server/common/namespace"
 	cnexus "go.temporal.io/server/common/nexus"
+	"go.temporal.io/server/common/nexus/nexustest"
+	"go.temporal.io/server/components/nexusoperations"
 	"go.temporal.io/server/tests"
+
+	"go.temporal.io/api/taskqueue/v1"
+	sdkclient "go.temporal.io/sdk/client"
 )
 
 var op = nexus.NewOperationReference[string, string]("my-operation")
@@ -73,6 +88,14 @@ func (s *NexusRequestForwardingSuite) SetupSuite() {
 
 func (s *NexusRequestForwardingSuite) SetupTest() {
 	s.setupTest()
+	s.cluster1.GetHost().OverrideDCValue(
+		s.T(),
+		nexusoperations.CallbackURLTemplate,
+		"http://"+s.cluster1.GetHost().FrontendHTTPAddress()+"/namespaces/{{.NamespaceName}}/nexus/callback")
+	s.cluster2.GetHost().OverrideDCValue(
+		s.T(),
+		nexusoperations.CallbackURLTemplate,
+		"http://"+s.cluster2.GetHost().FrontendHTTPAddress()+"/namespaces/{{.NamespaceName}}/nexus/callback")
 }
 
 func (s *NexusRequestForwardingSuite) TearDownSuite() {
@@ -314,6 +337,201 @@ func (s *NexusRequestForwardingSuite) TestCancelOperationForwardedFromStandbyToA
 	}
 }
 
+func (s *NexusRequestForwardingSuite) TestCompleteOperationForwardedFromStandbyToActive() {
+	// TODO: enable test once Nexus replication work is complete
+	s.T().SkipNow()
+
+	ctx := tests.NewContext()
+	ns := s.createNexusRequestForwardingNamespace()
+	taskQueue := fmt.Sprintf("%v-%v", "test-task-queue", uuid.New())
+	endpointName := fmt.Sprintf("%v-%v", "test-endpoint", uuid.New())
+
+	var callbackToken, publicCallbackUrl string
+
+	h := nexustest.Handler{
+		OnStartOperation: func(ctx context.Context, service, operation string, input *nexus.LazyValue, options nexus.StartOperationOptions) (nexus.HandlerStartOperationResult[any], error) {
+			callbackToken = options.CallbackHeader.Get(cnexus.CallbackTokenHeader)
+			publicCallbackUrl = options.CallbackURL
+			return &nexus.HandlerStartOperationResultAsync{OperationID: "test"}, nil
+		},
+	}
+	listenAddr := nexustest.AllocListenAddress(s.T())
+	nexustest.NewNexusServer(s.T(), listenAddr, h)
+
+	createEndpointReq := &operatorservice.CreateNexusEndpointRequest{
+		Spec: &nexuspb.EndpointSpec{
+			Name: endpointName,
+			Target: &nexuspb.EndpointTarget{
+				Variant: &nexuspb.EndpointTarget_External_{
+					External: &nexuspb.EndpointTarget_External{
+						Url: "http://" + listenAddr,
+					},
+				},
+			},
+		},
+	}
+
+	_, err := s.cluster1.GetOperatorClient().CreateNexusEndpoint(ctx, createEndpointReq)
+	s.NoError(err)
+
+	_, err = s.cluster2.GetOperatorClient().CreateNexusEndpoint(ctx, createEndpointReq)
+	s.NoError(err)
+
+	activeSDKClient, err := sdkclient.Dial(sdkclient.Options{
+		HostPort:  s.cluster1.GetHost().FrontendGRPCAddress(),
+		Namespace: ns,
+		Logger:    log.NewSdkLogger(s.logger),
+	})
+	s.NoError(err)
+
+	run, err := activeSDKClient.ExecuteWorkflow(ctx, client.StartWorkflowOptions{
+		TaskQueue: taskQueue,
+	}, "workflow")
+	s.NoError(err)
+
+	feClient1 := s.cluster1.GetFrontendClient()
+	feClient2 := s.cluster2.GetFrontendClient()
+
+	pollResp, err := feClient1.PollWorkflowTaskQueue(ctx, &workflowservice.PollWorkflowTaskQueueRequest{
+		Namespace: ns,
+		TaskQueue: &taskqueue.TaskQueue{
+			Name: taskQueue,
+			Kind: enumspb.TASK_QUEUE_KIND_NORMAL,
+		},
+		Identity: "test",
+	})
+	s.NoError(err)
+	_, err = feClient1.RespondWorkflowTaskCompleted(ctx, &workflowservice.RespondWorkflowTaskCompletedRequest{
+		Identity:  "test",
+		TaskToken: pollResp.TaskToken,
+		Commands: []*commandpb.Command{
+			{
+				CommandType: enumspb.COMMAND_TYPE_SCHEDULE_NEXUS_OPERATION,
+				Attributes: &commandpb.Command_ScheduleNexusOperationCommandAttributes{
+					ScheduleNexusOperationCommandAttributes: &commandpb.ScheduleNexusOperationCommandAttributes{
+						Endpoint:  endpointName,
+						Service:   "service",
+						Operation: "operation",
+						Input:     s.mustToPayload("input"),
+					},
+				},
+			},
+		},
+	})
+	s.NoError(err)
+
+	// Poll and verify that the "started" event was recorded.
+	pollResp, err = feClient1.PollWorkflowTaskQueue(ctx, &workflowservice.PollWorkflowTaskQueueRequest{
+		Namespace: ns,
+		TaskQueue: &taskqueue.TaskQueue{
+			Name: taskQueue,
+			Kind: enumspb.TASK_QUEUE_KIND_NORMAL,
+		},
+		Identity: "test",
+	})
+	s.NoError(err)
+	_, err = feClient1.RespondWorkflowTaskCompleted(ctx, &workflowservice.RespondWorkflowTaskCompletedRequest{
+		Identity:  "test",
+		TaskToken: pollResp.TaskToken,
+	})
+	s.NoError(err)
+
+	startedEventIdx := slices.IndexFunc(pollResp.History.Events, func(e *historypb.HistoryEvent) bool {
+		return e.GetNexusOperationStartedEventAttributes() != nil
+	})
+	s.Greater(startedEventIdx, 0)
+
+	// Wait for Nexus operation to be replicated
+	s.Eventually(func() bool {
+		resp, err := feClient2.DescribeWorkflowExecution(ctx, &workflowservice.DescribeWorkflowExecutionRequest{
+			Namespace: ns,
+			Execution: &commonpb.WorkflowExecution{
+				WorkflowId: run.GetID(),
+				RunId:      run.GetRunID(),
+			},
+		})
+		return err == nil && len(resp.PendingNexusOperations) > 0
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// Fail over to switch active and passive clusters.
+	s.failoverNamespace(ctx, ns)
+
+	// TODO: remove once replication changes are merged
+	// Manipulate the token to fix namespace failover version.
+	gen := &cnexus.CallbackTokenGenerator{}
+	decodedToken, err := cnexus.DecodeCallbackToken(callbackToken)
+	s.NoError(err)
+	completionToken, err := gen.DecodeCompletion(decodedToken)
+	s.NoError(err)
+	completionToken.Ref.MutableStateNamespaceFailoverVersion++
+	callbackToken, err = gen.Tokenize(completionToken)
+	s.NoError(err)
+
+	// Send a valid - successful completion request to now standby cluster.
+	completion, err := nexus.NewOperationCompletionSuccessful(s.mustToPayload("result"), nexus.OperationCompletionSuccesfulOptions{
+		Serializer: cnexus.PayloadSerializer,
+	})
+	s.NoError(err)
+	res, snap := s.sendNexusCompletionRequest(ctx, s.T(), s.cluster1, publicCallbackUrl, completion, callbackToken)
+	s.Equal(http.StatusOK, res.StatusCode)
+	s.Equal(1, len(snap["nexus_completion_requests"]))
+	s.Subset(snap["nexus_completion_requests"][0].Tags, map[string]string{"namespace": ns, "outcome": "request_forwarded"})
+
+	// Ensure that CompleteOperation request is tracked as part of normal service telemetry metrics
+	s.Condition(func() bool {
+		for _, m := range snap["service_requests"] {
+			if opTag, ok := m.Tags["operation"]; ok && opTag == "CompleteNexusOperation" {
+				return true
+			}
+		}
+		return false
+	})
+
+	// Resend the request and verify we get a not found error since the operation has already completed.
+	res, snap = s.sendNexusCompletionRequest(ctx, s.T(), s.cluster1, publicCallbackUrl, completion, callbackToken)
+	s.Equal(http.StatusNotFound, res.StatusCode)
+	s.Equal(1, len(snap["nexus_completion_requests"]))
+	s.Subset(snap["nexus_completion_requests"][0].Tags, map[string]string{"namespace": ns, "outcome": "error_not_found"})
+
+	// Poll now active cluster and verify the completion is recorded and triggers workflow progress.
+	pollResp, err = feClient2.PollWorkflowTaskQueue(ctx, &workflowservice.PollWorkflowTaskQueueRequest{
+		Namespace: ns,
+		TaskQueue: &taskqueue.TaskQueue{
+			Name: taskQueue,
+			Kind: enumspb.TASK_QUEUE_KIND_NORMAL,
+		},
+		Identity: "test",
+	})
+	s.NoError(err)
+	completedEventIdx := slices.IndexFunc(pollResp.History.Events, func(e *historypb.HistoryEvent) bool {
+		return e.GetNexusOperationCompletedEventAttributes() != nil
+	})
+	s.Greater(completedEventIdx, 0)
+
+	_, err = feClient2.RespondWorkflowTaskCompleted(ctx, &workflowservice.RespondWorkflowTaskCompletedRequest{
+		Identity:  "test",
+		TaskToken: pollResp.TaskToken,
+		Commands: []*commandpb.Command{
+			{
+				CommandType: enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION,
+				Attributes: &commandpb.Command_CompleteWorkflowExecutionCommandAttributes{
+					CompleteWorkflowExecutionCommandAttributes: &commandpb.CompleteWorkflowExecutionCommandAttributes{
+						Result: &commonpb.Payloads{
+							Payloads: []*commonpb.Payload{
+								pollResp.History.Events[completedEventIdx].GetNexusOperationCompletedEventAttributes().Result,
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	s.NoError(err)
+	var result string
+	s.NoError(run.Get(ctx, &result))
+	s.Equal("result", result)
+}
+
 func (s *NexusRequestForwardingSuite) nexusTaskPoller(ctx context.Context, frontendClient tests.FrontendClient, ns string, taskQueue string, handler func(*workflowservice.PollNexusTaskQueueResponse) (*nexuspb.Response, *nexuspb.HandlerError)) {
 	res, err := frontendClient.PollNexusTaskQueue(ctx, &workflowservice.PollNexusTaskQueueRequest{
 		Namespace: ns,
@@ -373,6 +591,33 @@ func (s *NexusRequestForwardingSuite) createNexusRequestForwardingNamespace() st
 	return ns
 }
 
+func (s *NexusRequestForwardingSuite) sendNexusCompletionRequest(
+	ctx context.Context,
+	t *testing.T,
+	testCluster *tests.TestCluster,
+	url string,
+	completion nexus.OperationCompletion,
+	callbackToken string,
+) (*http.Response, map[string][]*metricstest.CapturedRecording) {
+	metricsHandler, ok := testCluster.GetHost().GetMetricsHandler().(*metricstest.CaptureHandler)
+	s.True(ok)
+	capture := metricsHandler.StartCapture()
+	defer metricsHandler.StopCapture(capture)
+
+	req, err := nexus.NewCompletionHTTPRequest(ctx, url, completion)
+	require.NoError(t, err)
+	if callbackToken != "" {
+		req.Header.Add(cnexus.CallbackTokenHeader, callbackToken)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	_, err = io.ReadAll(res.Body)
+	require.NoError(t, err)
+	defer res.Body.Close()
+	return res, capture.Snapshot()
+}
+
 func requireExpectedMetricsCaptured(t *testing.T, snap map[string][]*metricstest.CapturedRecording, ns string, method string, expectedOutcome string) {
 	require.Equal(t, 1, len(snap["nexus_requests"]))
 	require.Subset(t, snap["nexus_requests"][0].Tags, map[string]string{"namespace": ns, "method": method, "outcome": expectedOutcome})
@@ -381,4 +626,27 @@ func requireExpectedMetricsCaptured(t *testing.T, snap map[string][]*metricstest
 	require.Equal(t, 1, len(snap["nexus_latency"]))
 	require.Subset(t, snap["nexus_latency"][0].Tags, map[string]string{"namespace": ns, "method": method, "outcome": expectedOutcome})
 	require.Equal(t, metrics.MetricUnit(metrics.Milliseconds), snap["nexus_latency"][0].Unit)
+}
+
+func (s *NexusRequestForwardingSuite) mustToPayload(v any) *commonpb.Payload {
+	conv := converter.GetDefaultDataConverter()
+	payload, err := conv.ToPayload(v)
+	s.NoError(err)
+	return payload
+}
+
+func (s *NexusRequestForwardingSuite) failoverNamespace(ctx context.Context, ns string) {
+	_, err := s.cluster1.GetFrontendClient().UpdateNamespace(ctx, &workflowservice.UpdateNamespaceRequest{
+		Namespace: ns,
+		ReplicationConfig: &replicationpb.NamespaceReplicationConfig{
+			ActiveClusterName: s.clusterNames[1],
+		},
+	})
+	s.NoError(err)
+
+	// Wait until failover is complete
+	s.Eventually(func() bool {
+		cached, err := s.cluster2.GetHost().GetFrontendNamespaceRegistry().GetNamespace(namespace.Name(ns))
+		return err == nil && cached.ActiveInCluster(s.clusterNames[1])
+	}, 10*time.Second, 1*time.Second)
 }
