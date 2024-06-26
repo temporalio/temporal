8aec9b258884b3d8ba88dd8e1a874a23771f110b
PJ Doerner <pj.doerner@temporal.io>
2024-06-26 11:32:30 -0700

    update API and SDK versions

--- service/frontend/workflow_handler.go
+++ service/frontend/workflow_handler.go
@@ -2822,7 +2822,7 @@ func (wh *WorkflowHandler) GetSystemInfo(ctx context.Context, request *workflows
 			SdkMetadata:                     true,
 			BuildIdBasedVersioning:          true,
 			CountGroupByExecutionStatus:     true,
-			Nexus:                           true,
+			Nexus:                           wh.config.EnableNexusAPIs(),
 		},
 	}, nil
 }
