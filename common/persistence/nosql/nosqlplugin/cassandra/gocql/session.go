temporalio/temporal

commit 7244681f31f282d0a74fe55196b6c3cbb92884b9

Reconnect on a few more gocql errors (#4132)

common/persistence/nosql/nosqlplugin/cassandra/gocql/session.go
@@ -28,6 +28,7 @@ import (
 	"context"
 	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -181,7 +182,10 @@ func (s *session) handleError(
 	err error,
 ) {
 	switch err {
-	case gocql.ErrNoConnections:
+	case gocql.ErrNoConnections,
+		gocql.ErrSessionClosed,
+		gocql.ErrConnectionClosed,
+		syscall.ECONNRESET:
 		s.refresh()
 	default:
 		// noop

commit 20c8869d4628655b35361b101cc601ad57afc48b

Avoid recreating gocql session on transient connection errors (#6207)
## What changed?
Modifies gocql error handling introduced in
https://github.com/temporalio/temporal/pull/4132 to avoid recreating the
gocql session on transient connection errors.

## Why?
Recreating the session adds latency and isn't necessary when connections
are closed or reset.

## How did you test it?
Ran a prod-like workload and periodically restarted cassandra nodes.
With the patch applied, we observed a reduction in p50 and p99 latency
across persistence operations. 

## Potential risks
No

## Is hotfix candidate?
No

common/persistence/nosql/nosqlplugin/cassandra/gocql/session.go
@@ -28,7 +28,6 @@ import (
 	"context"
 	"sync"
 	"sync/atomic"
-	"syscall"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -203,9 +202,7 @@ func (s *session) handleError(
 ) {
 	switch err {
 	case gocql.ErrNoConnections,
-		gocql.ErrSessionClosed,
-		gocql.ErrConnectionClosed,
-		syscall.ECONNRESET:
+		gocql.ErrSessionClosed:
 		s.refresh()
 	default:
 		// noop