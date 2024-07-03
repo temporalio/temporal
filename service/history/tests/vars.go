e2eaae8155e714ae57edc34d722b713d2352e038
Yichao Yang <yichao@temporal.io>
2024-07-03 16:15:50 -0700

    Add feature flag for workflowIDReuse start time validation (#6231)
    <!-- Describe what has changed in this PR -->
    - Add a feature flag for workflowIDReuse start time validation, default
    to **_disabled_**
    
    <!-- Tell your future self why have you made these changes -->
    - The start time validation introduces one more db read for start
    workflow operation. This can be avoided by adding the start time to
    workflow execution state.
    - Also we should only get workflow start time when TerminateIfRunning
    policy is used.
    
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    - Existing functional test `TestStartWorkflowExecution_Terminate`
    
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->
    - Yes

--- service/history/tests/vars.go
+++ service/history/tests/vars.go
@@ -189,5 +189,6 @@ func NewDynamicConfig() *configs.Config {
 	config.EnableNexus = dynamicconfig.GetBoolPropertyFn(true)
 	config.ReplicationEnableUpdateWithNewTaskMerge = dynamicconfig.GetBoolPropertyFn(true)
 	config.EnableWorkflowExecutionTimeoutTimer = dynamicconfig.GetBoolPropertyFn(true)
+	config.EnableWorkflowIdReuseStartTimeValidation = dynamicconfig.GetBoolPropertyFnFilteredByNamespace(true)
 	return config
 }
