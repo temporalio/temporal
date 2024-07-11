e56c652c0cf18f491ac93461e9417fbd61e22fa6
Prathyush PV <prathyush.pv@temporal.io>
2024-07-11 16:50:54 -0700

    Fix import

--- service/history/fx.go
+++ service/history/fx.go
@@ -30,6 +30,7 @@ import (
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
+
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
 	"go.temporal.io/server/common"
