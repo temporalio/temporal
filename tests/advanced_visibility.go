c30b011a4bc0cf8183e286e9c69896b047f0bdf0
justinp <174377431+justinp-tt@users.noreply.github.com>
2024-08-06 12:59:14 -0500

    List schedule query should support "ScheduleId" fake search attribute
    If the user defines a custom search attribute with the name ScheduleId, a rewrite should not take place. Instead, we should query what the user supplied.
    Otherwise, we should prepend the const value of temporal-sys-scheduler: and replace ScheduleId with WorkflowId.

--- tests/advanced_visibility.go
+++ tests/advanced_visibility.go
@@ -1802,17 +1802,17 @@ func (s *AdvancedVisibilitySuite) TestUpsertWorkflowExecution_InvalidKey() {
 	if s.isElasticsearchEnabled {
 		s.ErrorContains(err, "BadSearchAttributes: search attribute INVALIDKEY is not defined")
 		s.EqualHistoryEvents(`
- 1 WorkflowExecutionStarted
- 2 WorkflowTaskScheduled
- 3 WorkflowTaskStarted
- 4 WorkflowTaskFailed {"Cause":23,"Failure":{"Message":"BadSearchAttributes: search attribute INVALIDKEY is not defined"}}`, historyEvents)
+  1 WorkflowExecutionStarted
+  2 WorkflowTaskScheduled
+  3 WorkflowTaskStarted
+  4 WorkflowTaskFailed {"Cause":23,"Failure":{"Message":"BadSearchAttributes: search attribute INVALIDKEY is not defined"}}`, historyEvents)
 	} else {
 		s.ErrorContains(err, fmt.Sprintf("BadSearchAttributes: Namespace %s has no mapping defined for search attribute INVALIDKEY", s.namespace))
 		s.EqualHistoryEvents(fmt.Sprintf(`
- 1 WorkflowExecutionStarted
- 2 WorkflowTaskScheduled
- 3 WorkflowTaskStarted
- 4 WorkflowTaskFailed {"Cause":23,"Failure":{"Message":"BadSearchAttributes: Namespace %s has no mapping defined for search attribute INVALIDKEY"}}`, s.namespace), historyEvents)
+  1 WorkflowExecutionStarted
+  2 WorkflowTaskScheduled
+  3 WorkflowTaskStarted
+  4 WorkflowTaskFailed {"Cause":23,"Failure":{"Message":"BadSearchAttributes: Namespace %s has no mapping defined for search attribute INVALIDKEY"}}`, s.namespace), historyEvents)
 	}
 }
 
