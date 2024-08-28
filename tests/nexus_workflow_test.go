20e519fcaaf0635c37fff53796ebb713c10ea2e4
Rodrigo Zhou <2068124+rodrigozhou@users.noreply.github.com>
2024-08-28 16:41:43 -0500

    Update nexus link converter (#6460)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    Update nexus link converter
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    Sync with code in SDK.
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- tests/nexus_workflow_test.go
+++ tests/nexus_workflow_test.go
@@ -410,8 +410,7 @@ func (s *ClientFunctionalSuite) TestNexusOperationAsyncCompletion() {
 			},
 		},
 	}
-	handlerNexusLink, err := nexusoperations.ConvertLinkWorkflowEventToNexusLink(handlerLink)
-	s.NoError(err)
+	handlerNexusLink := nexusoperations.ConvertLinkWorkflowEventToNexusLink(handlerLink)
 
 	h := nexustest.Handler{
 		OnStartOperation: func(
