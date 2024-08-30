e0c72f1620ffac9c6c6308436705438051883bdf
Chetan Gowda <gow@users.noreply.github.com>
2024-08-30 11:45:49 -0700

    [UserMetadata] Pass the user metadata to new executions from schedule actions (#6462)
    When user metadata is provided in the
    [ScheduleAction](https://github.com/temporalio/api/blob/4a1ab98a84b94b59f82aab4b35ea505cff968804/temporal/api/schedule/v1/message.proto#L252-L259),
    we were not passing it through to the new executions that were started
    by the scheduler. This fixes it
    (https://github.com/temporalio/temporal/issues/6413).
    
    To be feature complete. Every schedule action that contains user
    metadata should pass it on to the newly created executions.
    
    Unit tests.
    
    N/A
    
    N/A
    
    No
    
    ---------
    
    Co-authored-by: David Reiss <david@temporal.io>

--- service/worker/scheduler/workflow.go
+++ service/worker/scheduler/workflow.go
@@ -1339,6 +1339,7 @@ func (s *scheduler) startWorkflow(
 			Header:                   newWorkflow.Header,
 			LastCompletionResult:     lastCompletionResult,
 			ContinuedFailure:         continuedFailure,
+			UserMetadata:             newWorkflow.UserMetadata,
 		},
 	}
 	for {
