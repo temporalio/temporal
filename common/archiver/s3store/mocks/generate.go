85df1bbcca69708ef46479ae5773d87d060f351a
Yichao Yang <yichao@temporal.io>
2024-09-06 14:31:18 -0700

    fix s3 mock

--- common/archiver/s3store/mocks/generate.go
+++ common/archiver/s3store/mocks/generate.go
@@ -22,6 +22,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//go:generate ./generate.sh
+//go:generate mockgen -copyright_file ../../../../LICENSE -package "$GOPACKAGE" -destination s3api.go github.com/aws/aws-sdk-go/service/s3/s3iface S3API
 
 package mocks
