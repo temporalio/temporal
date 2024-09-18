b5043116c2783e3a42fd32799363041d4e8fdb69
yux0 <yuhsia89@gmail.com>
2024-09-18 10:21:07 -0700

    Treat connect to history pod failure as unhealthy

--- service/frontend/admin_handler.go
+++ service/frontend/admin_handler.go
@@ -192,7 +192,7 @@ func NewAdminHandler(
 		func(ctx context.Context, hostAddress string) (enumsspb.HealthState, error) {
 			resp, err := args.HistoryClient.DeepHealthCheck(ctx, &historyservice.DeepHealthCheckRequest{HostAddress: hostAddress})
 			if err != nil {
-				return enumsspb.HEALTH_STATE_UNSPECIFIED, err
+				return enumsspb.HEALTH_STATE_NOT_SERVING, err
 			}
 			return resp.GetState(), nil
 		},
