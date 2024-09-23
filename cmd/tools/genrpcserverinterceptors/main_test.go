ffde8550c3ec22aebe98c0c0682a001a57ac52d2
Alex Shtin <alex@shtin.com>
2024-09-23 12:16:25 -0700

    More tests

--- cmd/tools/genrpcserverinterceptors/main_test.go
+++ cmd/tools/genrpcserverinterceptors/main_test.go
@@ -29,13 +29,77 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/api/historyservice/v1"
 	"go.temporal.io/server/api/matchingservice/v1"
 )
 
 func TestWorkflowTagGetters(t *testing.T) {
-	requestT := reflect.TypeOf(&matchingservice.QueryWorkflowRequest{})
-	md := workflowTagGetters(requestT, 0)
-	assert.Equal(t, md.WorkflowIdGetter, "GetQueryRequest().GetExecution().GetWorkflowId()")
-	assert.Equal(t, md.RunIdGetter, "GetQueryRequest().GetExecution().GetRunId()")
-	assert.Equal(t, md.TaskTokenGetter, "")
+	testCases := []struct {
+		name             string
+		reqT             reflect.Type
+		workflowIDGetter string
+		runIDGetter      string
+		taskTokenGetter  string
+	}{
+		{
+			name:             "Request with only workflowID",
+			reqT:             reflect.TypeOf(&workflowservice.StartWorkflowExecutionRequest{}),
+			workflowIDGetter: "GetWorkflowId()",
+		},
+		{
+			name:             "Request with workflowID and runID",
+			reqT:             reflect.TypeOf(&workflowservice.RecordActivityTaskHeartbeatByIdRequest{}),
+			workflowIDGetter: "GetWorkflowId()",
+			runIDGetter:      "GetRunId()",
+		},
+		{
+			name:             "Request with execution",
+			reqT:             reflect.TypeOf(&workflowservice.GetWorkflowExecutionHistoryRequest{}),
+			workflowIDGetter: "GetExecution().GetWorkflowId()",
+			runIDGetter:      "GetExecution().GetRunId()",
+		},
+		{
+			name:             "Request with workflow_execution",
+			reqT:             reflect.TypeOf(&workflowservice.RequestCancelWorkflowExecutionRequest{}),
+			workflowIDGetter: "GetWorkflowExecution().GetWorkflowId()",
+			runIDGetter:      "GetWorkflowExecution().GetRunId()",
+		},
+		{
+			name:            "Request with task_token",
+			reqT:            reflect.TypeOf(&workflowservice.RespondActivityTaskCompletedRequest{}),
+			taskTokenGetter: "GetTaskToken()",
+		},
+		{
+			name: "Special handling for RespondQueryTaskCompletedRequest",
+			reqT: reflect.TypeOf(&workflowservice.RespondQueryTaskCompletedRequest{}),
+		},
+		{
+			name:             "Matching request",
+			reqT:             reflect.TypeOf(&matchingservice.QueryWorkflowRequest{}),
+			workflowIDGetter: "GetQueryRequest().GetExecution().GetWorkflowId()",
+			runIDGetter:      "GetQueryRequest().GetExecution().GetRunId()",
+		},
+		{
+			name:             "History request",
+			reqT:             reflect.TypeOf(&historyservice.SignalWorkflowExecutionRequest{}),
+			workflowIDGetter: "GetSignalRequest().GetWorkflowExecution().GetWorkflowId()",
+			runIDGetter:      "GetSignalRequest().GetWorkflowExecution().GetRunId()",
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			rd := workflowTagGetters(tt.reqT, 0)
+			if tt.workflowIDGetter != "" {
+				assert.Equal(t, rd.WorkflowIdGetter, tt.workflowIDGetter)
+			}
+			if tt.runIDGetter != "" {
+				assert.Equal(t, rd.RunIdGetter, tt.runIDGetter)
+			}
+			if tt.taskTokenGetter != "" {
+				assert.Equal(t, rd.TaskTokenGetter, tt.taskTokenGetter)
+			}
+		})
+	}
 }
