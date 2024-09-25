499e8992c609f97795144849e0ef34ef54755bdb
David Reiss <david@temporal.io>
2024-09-25 00:04:27 -0700

    dynamicconfig.MemoryClient supports subscriptions

--- common/dynamicconfig/memory_client.go
+++ common/dynamicconfig/memory_client.go
@@ -27,12 +27,17 @@ package dynamicconfig
 import (
 	"sync"
 	"sync/atomic"
+
+	expmaps "golang.org/x/exp/maps"
 )
 
 type (
 	MemoryClient struct {
 		lock      sync.RWMutex
 		overrides []kvpair
+
+		subscriptionIdx int
+		subscriptions   map[int]ClientUpdateFunc
 	}
 
 	kvpair struct {
@@ -44,13 +49,16 @@ type (
 
 // NewMemoryClient - returns a memory based dynamic config client
 func NewMemoryClient() *MemoryClient {
-	return &MemoryClient{}
+	return &MemoryClient{subscriptions: make(map[int]ClientUpdateFunc)}
 }
 
 func (d *MemoryClient) GetValue(key Key) []ConstrainedValue {
 	d.lock.RLock()
 	defer d.lock.RUnlock()
+	return d.getValueLocked(key)
+}
 
+func (d *MemoryClient) getValueLocked(key Key) []ConstrainedValue {
 	for i := len(d.overrides) - 1; i >= 0; i-- {
 		if d.overrides[i].valid && d.overrides[i].key == key {
 			v := d.overrides[i].value
@@ -63,18 +71,42 @@ func (d *MemoryClient) GetValue(key Key) []ConstrainedValue {
 	return nil
 }
 
+func (d *MemoryClient) Subscribe(f ClientUpdateFunc) (cancel func()) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	d.subscriptionIdx++
+	id := d.subscriptionIdx
+	d.subscriptions[id] = f
+
+	return func() {
+		d.lock.Lock()
+		defer d.lock.Unlock()
+		delete(d.subscriptions, id)
+	}
+}
+
 func (d *MemoryClient) OverrideSetting(setting GenericSetting, value any) (cleanup func()) {
 	return d.OverrideValue(setting.Key(), value)
 }
 
 func (d *MemoryClient) OverrideValue(key Key, value any) (cleanup func()) {
 	d.lock.Lock()
-	defer d.lock.Unlock()
 
 	var idx atomic.Int64
 	idx.Store(int64(len(d.overrides)))
 
 	d.overrides = append(d.overrides, kvpair{valid: true, key: key, value: value})
+
+	subscriptions := expmaps.Values(d.subscriptions)
+	newValue := d.getValueLocked(key)
+
+	d.lock.Unlock()
+
+	changed := map[Key][]ConstrainedValue{key: newValue}
+	for _, update := range subscriptions {
+		update(changed)
+	}
 
 	return func() {
 		// only do this once
@@ -86,13 +118,23 @@ func (d *MemoryClient) OverrideValue(key Key, value any) (cleanup func()) {
 
 func (d *MemoryClient) remove(idx int) {
 	d.lock.Lock()
-	defer d.lock.Unlock()
 
+	key := d.overrides[idx].key
 	// mark this pair deleted
 	d.overrides[idx] = kvpair{}
 
 	// pop all deleted pairs
 	for l := len(d.overrides); l > 0 && !d.overrides[l-1].valid; l = len(d.overrides) {
 		d.overrides = d.overrides[:l-1]
+	}
+
+	subscriptions := expmaps.Values(d.subscriptions)
+	newValue := d.getValueLocked(key)
+
+	d.lock.Unlock()
+
+	changed := map[Key][]ConstrainedValue{key: newValue}
+	for _, update := range subscriptions {
+		update(changed)
 	}
 }
--- common/dynamicconfig/memory_client_test.go
+++ common/dynamicconfig/memory_client_test.go
@@ -65,3 +65,43 @@ func TestMemoryClient(t *testing.T) {
 	remove2() // no-op
 	assert.Nil(t, c.GetValue(k))
 }
+
+func TestMemoryClientSubscriptions(t *testing.T) {
+	c := dynamicconfig.NewMemoryClient()
+	k := dynamicconfig.Key("key")
+
+	calls := 0
+
+	c.Subscribe(func(changed map[dynamicconfig.Key][]dynamicconfig.ConstrainedValue) {
+		calls++
+		assert.Contains(t, changed, k)
+		switch calls {
+		case 1:
+			assert.Equal(t, []dynamicconfig.ConstrainedValue{{Value: 123}}, changed[k])
+		case 2:
+			assert.Nil(t, changed[k])
+		case 3:
+			assert.Equal(t, []dynamicconfig.ConstrainedValue{{Value: 456}}, changed[k])
+		case 4:
+			assert.Equal(t, []dynamicconfig.ConstrainedValue{{Value: 789}}, changed[k])
+		case 5:
+			assert.Equal(t, []dynamicconfig.ConstrainedValue{{Value: 456}}, changed[k])
+		case 6:
+			assert.Nil(t, changed[k])
+		}
+	})
+
+	remove := c.OverrideValue(k, 123)
+	assert.Equal(t, 1, calls)
+	remove()
+	assert.Equal(t, 2, calls)
+
+	remove1 := c.OverrideValue(k, 456)
+	assert.Equal(t, 3, calls)
+	remove2 := c.OverrideValue(k, 789)
+	assert.Equal(t, 4, calls)
+	remove2()
+	assert.Equal(t, 5, calls)
+	remove1()
+	assert.Equal(t, 6, calls)
+}
