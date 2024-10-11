4ba312f523295f6bd06fa21e3cac0745f48e107d
Yuri <ychebotarev@gmail.com>
2024-10-11 10:24:40 -0700

    proto

--- api/persistence/v1/executions.pb.go
+++ api/persistence/v1/executions.pb.go
@@ -1978,7 +1978,7 @@ type ActivityInfo struct {
 	// The version stamp of the worker to whom this activity was most-recently dispatched
 	LastWorkerVersionStamp        *v12.WorkerVersionStamp `protobuf:"bytes,37,opt,name=last_worker_version_stamp,json=lastWorkerVersionStamp,proto3" json:"last_worker_version_stamp,omitempty"`
 	LastUpdateVersionedTransition *VersionedTransition    `protobuf:"bytes,38,opt,name=last_update_versioned_transition,json=lastUpdateVersionedTransition,proto3" json:"last_update_versioned_transition,omitempty"`
-	// Fist time the activity was scheduled.
+	// First time the activity was scheduled.
 	FirstScheduledTime *timestamppb.Timestamp `protobuf:"bytes,39,opt,name=first_scheduled_time,json=firstScheduledTime,proto3" json:"first_scheduled_time,omitempty"`
 	// Last time an activity failure was recorded by the server.
 	LastAttemptCompleteTime *timestamppb.Timestamp `protobuf:"bytes,40,opt,name=last_attempt_complete_time,json=lastAttemptCompleteTime,proto3" json:"last_attempt_complete_time,omitempty"`
