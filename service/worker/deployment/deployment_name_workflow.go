ad2428607454bde6c1b280d34028361af90f0ecc
Shivam Saraf <shivam.saraf@temporal.io>
2024-11-04 18:05:08 -0500

    work on creating a deployment entity workflow

---
+++ service/worker/deployment/deployment_name_workflow.go
@@ -0,0 +1,81 @@
+// The MIT License
+//
+// Copyright (c) 2024 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2024 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package deployment
+
+import (
+	sdkclient "go.temporal.io/sdk/client"
+	sdklog "go.temporal.io/sdk/log"
+	"go.temporal.io/sdk/workflow"
+)
+
+type (
+	DeploymentNameWorkflowArgs struct {
+		NamespaceName  string
+		NamespaceID    string
+		DefaultBuildID string
+	}
+
+	// DeploymentWorkflowRunner holds the local state while running a deployment name workflow
+	DeploymentNameWorkflowRunner struct {
+		ctx     workflow.Context
+		a       *DeploymentNameActivities
+		logger  sdklog.Logger
+		metrics sdkclient.MetricsHandler
+		// local state denoting the current "default" build-ID of a deploymentName (can be nil)
+		defaultBuildID string
+	}
+)
+
+const (
+	UpdateDeploymentNameBuildIDSignalName = "update-deployment-name-buildID"
+
+	DeploymentNameWorkflowIDPrefix = "temporal-sys-deployment-name:"
+)
+
+// TODO Shivam - Define workflow for DeploymentName
+func DeploymentNameWorkflow(ctx workflow.Context, deploymentNameArgs DeploymentNameWorkflowArgs) error {
+	deploymentWorkflowNameRunner := &DeploymentNameWorkflowRunner{
+		ctx:            ctx,
+		logger:         sdklog.With(workflow.GetLogger(ctx), "wf-namespace", deploymentNameArgs.NamespaceName),
+		metrics:        workflow.GetMetricsHandler(ctx).WithTags(map[string]string{"namespace": deploymentNameArgs.NamespaceName}),
+		defaultBuildID: "", // TODO Shivam - extract buildID from the workflowID
+	}
+	return deploymentWorkflowNameRunner.run()
+}
+
+func (d *DeploymentNameWorkflowRunner) run() error {
+	/* TODO Shivam:
+
+	Implement this workflow to be an infinitely long running workflow
+
+	Query handlers to return current default buildID of the deployment name
+
+	Signal handler(s) to:
+	signal DeploymentWorkflow and update the buildID of all task-queues in the deployment name.
+	update default buildID of the deployment name.
+
+	*/
+	return nil
+}
---
+++ service/worker/deployment/deployment_workflow.go
@@ -0,0 +1,150 @@
+// The MIT License
+//
+// Copyright (c) 2024 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2024 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package deployment
+
+import (
+	"time"
+
+	enumspb "go.temporal.io/api/enums/v1"
+	sdkclient "go.temporal.io/sdk/client"
+	sdklog "go.temporal.io/sdk/log"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/workflow"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type (
+	TaskQueue struct {
+		Name          string
+		TaskQueueType enumspb.TaskQueueType
+	}
+	// DeploymentTaskQueue holds relevant information for a task-queue present in a Deployment
+	DeploymentTaskQueue struct {
+		FirstPollerTimeStamp *timestamppb.Timestamp
+		TaskQueue            TaskQueue
+	}
+
+	// DeploymentWorkflowArgs represents the arguments passed for a DeploymentWorkflow
+	DeploymentWorkflowArgs struct {
+		NamespaceName string
+		NamespaceID   string
+		TaskQueues    []*DeploymentTaskQueue // required for CAN
+	}
+
+	DeploymentLocalState struct {
+		NamespaceName  string
+		NamespaceID    string
+		DeploymentName string
+		BuildID        string
+		TaskQueues     []*DeploymentTaskQueue // All the task queues associated with this buildID/deployment
+	}
+
+	// DeploymentWorkflowRunner holds the local state for a deployment workflow
+	DeploymentWorkflowRunner struct {
+		ctx                  workflow.Context
+		a                    *DeploymentActivities
+		logger               sdklog.Logger
+		metrics              sdkclient.MetricsHandler
+		deploymentlocalState *DeploymentLocalState
+	}
+)
+
+var (
+	defaultActivityOptions = workflow.ActivityOptions{
+		ScheduleToCloseTimeout: 1 * time.Hour,
+		StartToCloseTimeout:    30 * time.Second,
+		RetryPolicy: &temporal.RetryPolicy{
+			InitialInterval: 1 * time.Second,
+			MaximumInterval: 60 * time.Second,
+		},
+	}
+)
+
+const (
+	UpdateDeploymentSignalName        = "update_deployment"
+	UpdateDeploymentBuildIDSignalName = "update_deployment_build_id"
+
+	DeploymentWorkflowIDPrefix = "temporal-sys-deployment:"
+)
+
+func DeploymentWorkflow(ctx workflow.Context, deploymentWorkflowArgs DeploymentWorkflowArgs) error {
+	deploymentWorkflowRunner := &DeploymentWorkflowRunner{
+		deploymentlocalState: &DeploymentLocalState{
+			NamespaceName:  deploymentWorkflowArgs.NamespaceName,
+			NamespaceID:    deploymentWorkflowArgs.NamespaceID,
+			DeploymentName: "", // TODO Shivam - extract the Deployment
+			BuildID:        "", // TODO Shivam - extract BuildID from the workflowID
+			TaskQueues:     deploymentWorkflowArgs.TaskQueues,
+		},
+		ctx:     ctx,
+		logger:  sdklog.With(workflow.GetLogger(ctx), "wf-namespace", deploymentWorkflowArgs.NamespaceName),
+		metrics: workflow.GetMetricsHandler(ctx).WithTags(map[string]string{"namespace": deploymentWorkflowArgs.NamespaceName}),
+	}
+	return deploymentWorkflowRunner.run()
+}
+
+func (d *DeploymentWorkflowRunner) run() error {
+
+	// Set up Query Handlers here:
+
+	// Fetch signal channels
+	updateDeploymentSignalChannel := workflow.GetSignalChannel(d.ctx, UpdateDeploymentSignalName)
+	updateBuildIDSignalChannel := workflow.GetSignalChannel(d.ctx, UpdateDeploymentBuildIDSignalName)
+
+	selector := workflow.NewSelector(d.ctx)
+	selector.AddReceive(updateDeploymentSignalChannel, func(c workflow.ReceiveChannel, more bool) {
+		// Process Signal
+
+	})
+	selector.AddReceive(updateBuildIDSignalChannel, func(c workflow.ReceiveChannel, more bool) {
+		// Process Signal
+	})
+
+	// async draining before CAN
+	for !workflow.GetInfo(d.ctx).GetContinueAsNewSuggested() || selector.HasPending() {
+		selector.Select(d.ctx)
+	}
+
+	/*
+
+		 	Posting this as a reminder to limit the number of signals coming through since we use CAN:
+
+			Workflows cannot have infinitely-sized history and when the event count grows too large, `ContinueAsNew` can be returned
+			to start a new one atomically. However, in order not to lose any data, signals must be drained and any other futures
+			that need to be reacted to must be completed first. This means there must be a period where there are no signals to
+			drain and no futures to wait on. If signals come in faster than processed or futures wait so long there is no idle
+			period, `ContinueAsNew` will not happen in a timely manner and history will grow.
+
+	*/
+
+	d.logger.Debug("Deployment doing continue-as-new")
+	workflowArgs := DeploymentWorkflowArgs{
+		NamespaceName: d.deploymentlocalState.NamespaceName,
+		NamespaceID:   d.deploymentlocalState.NamespaceID,
+		TaskQueues:    d.deploymentlocalState.TaskQueues,
+	}
+	return workflow.NewContinueAsNewError(d.ctx, DeploymentWorkflow, workflowArgs)
+
+}
