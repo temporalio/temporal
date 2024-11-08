9c90f78df6fbbde3ce49d3dd71d48b5fe006c05f
Yuri <ychebotarev@gmail.com>
2024-11-07 18:26:15 -0800

    comments

--- schema/elasticsearch/visibility/versioned/v8/upgrade.sh
+++ schema/elasticsearch/visibility/versioned/v8/upgrade.sh
@@ -31,7 +31,7 @@ echo "=== Step 1. Add new builtin search attributes ==="
 new_mapping='
 {
   "properties": {
-    "TemporalPausedInfo": {
+    "TemporalPauseInfo": {
       "type": "keyword"
     }
   }
