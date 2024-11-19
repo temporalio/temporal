f3c8e7ed654e13eac8a07836d640505686d5f682
Carly de Frondeville <carly.defrondeville@temporal.io>
2024-11-18 17:15:28 -0800

    test MergeOptions

--- common/rpc/fieldmask/fieldmask.go
+++ common/rpc/fieldmask/fieldmask.go
@@ -2,34 +2,40 @@ package fieldmask
 
 import (
 	"fmt"
-	fieldmask_utils "github.com/mennanov/fieldmask-utils"
+	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/serviceerror"
 	workflowpb "go.temporal.io/api/workflow/v1"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
-
-// A function that maps field mask field names to the names used in Go structs.
-// This can be replaced by a generic CamelCase function at some point.
-// Using this for now to be simple.
-func naming(s string) string {
-	if s == "versioning_behavior_override" {
-		return "VersioningBehaviorOverride"
-	}
-	return s
-}
 
 // MergeOptions copies `src` struct to `dst` struct using the given FieldMask, and returns the mutated `dst`.
 // Only the fields present in FieldMask will be copied to `dst`. `src` and `dst` must be coherent in terms of
 // the field names, but it is not required for them to be of the same type. Unexported fields are copied only
 // if the corresponding struct filter is empty and `dst` is assignable to `src`.
 func MergeOptions(maskpb *fieldmaskpb.FieldMask, src, dst *workflowpb.WorkflowExecutionOptions) (*workflowpb.WorkflowExecutionOptions, error) {
-	mask, err := fieldmask_utils.MaskFromPaths(maskpb.GetPaths(), naming)
+	if maskpb == nil {
+		return nil, serviceerror.NewInvalidArgument("UpdateMask must be non-nil")
+	}
+
+	// This will error if any of the given fields are not present in WorkflowExecutionOptions
+	_, err := fieldmaskpb.New(src, maskpb.GetPaths()...)
 	if err != nil {
 		return nil, serviceerror.NewInvalidArgument(fmt.Sprintf("error parsing UpdateMask: %s", err.Error()))
 	}
-	err = fieldmask_utils.StructToStruct(mask, src, dst)
-	if err != nil {
-		return nil, serviceerror.NewInvalidArgument(fmt.Sprintf("error applying UpdateMask: %s", err.Error()))
+
+	// Apply masked fields
+	for _, p := range maskpb.GetPaths() {
+		switch p {
+		case "versioning_behavior_override":
+			dst.VersioningBehaviorOverride = src.GetVersioningBehaviorOverride()
+		default:
+			return nil, serviceerror.NewInvalidArgument(fmt.Sprintf("error parsing UpdateMask: path %s not recognized", p))
+		}
+	}
+
+	// Confirm resulting versioning behavior is valid
+	if dst.GetVersioningBehaviorOverride().GetBehavior() == enumspb.VERSIONING_BEHAVIOR_PINNED && src.GetVersioningBehaviorOverride().GetWorkerDeployment() == nil {
+		return nil, serviceerror.NewInvalidArgument("Deployment must be set if behavior override is PINNED")
 	}
 	return dst, nil
 }
