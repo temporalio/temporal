808973c58de1f089d285c8c24d173342d632be1c
justinp-tt <174377431+justinp-tt@users.noreply.github.com>
2024-11-20 17:02:04 -0600

    Update service/history/hsm/tree.go
    Co-authored-by: Roey Berman <roey@temporal.io>

--- service/history/hsm/tree.go
+++ service/history/hsm/tree.go
@@ -40,7 +40,7 @@ import (
 // ErrStateMachineNotFound is returned when looking up a non-existing state machine in a [Node] or a [Collection].
 var ErrStateMachineNotFound = errors.New("state machine not found")
 
-// ErrStateMachineInvalidState is returned a [Node] state transition is attempted to a state that is invalid.
+// ErrStateMachineInvalidState is returned when modifying a [Node] in an invalid state (e.g. deleted).
 var ErrStateMachineInvalidState = errors.New("invalid state machine state")
 
 // ErrStateMachineAlreadyExists is returned when trying to add a state machine with an ID that already exists in a [Collection].
