f4daa156a5ac6b335c08bed62f9f241ab11159c5
Shivam <57200924+Shivs11@users.noreply.github.com>
2024-11-22 20:22:44 -0500

    Deployment Read API's - Tests (#6864)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    - Added basic happy path functional tests for deployment read API's
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    - versioning-3
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    - ran them
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- service/worker/deployment/deployment_workflow.go
+++ service/worker/deployment/deployment_workflow.go
@@ -102,7 +102,7 @@ func (d *DeploymentWorkflowRunner) run() error {
 		func(ctx workflow.Context, updateInput *deployspb.RegisterWorkerInDeploymentArgs) error {
 			err := d.lock.Lock(d.ctx)
 			if err != nil {
-				d.logger.Error("Could not acquire deploymnet workflow lock")
+				d.logger.Error("Could not acquire deployment workflow lock")
 				return err
 			}
 			pendingUpdates++
@@ -115,7 +115,10 @@ func (d *DeploymentWorkflowRunner) run() error {
 			if d.DeploymentLocalState.TaskQueueFamilies == nil {
 				d.DeploymentLocalState.TaskQueueFamilies = make(map[string]*deployspb.DeploymentLocalState_TaskQueueFamilyInfo)
 			}
-			// if no TaskQueues have been registered for the TaskQueueName
+
+			if _, ok := d.DeploymentLocalState.TaskQueueFamilies[updateInput.TaskQueueName]; !ok {
+				d.DeploymentLocalState.TaskQueueFamilies[updateInput.TaskQueueName] = &deployspb.DeploymentLocalState_TaskQueueFamilyInfo{}
+			}
 			if d.DeploymentLocalState.TaskQueueFamilies[updateInput.TaskQueueName].TaskQueues == nil {
 				d.DeploymentLocalState.TaskQueueFamilies[updateInput.TaskQueueName].TaskQueues = make(map[int32]*deployspb.DeploymentLocalState_TaskQueueFamilyInfo_TaskQueueInfo)
 			}
