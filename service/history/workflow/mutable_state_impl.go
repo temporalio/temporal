0e1a8f304d03c0e2211a14bf936f257fc86d0f4c
Carly de Frondeville <cdefrondeville@berkeley.edu>
2024-11-22 19:21:12 -0500

    Add line breaks to comment (#6881)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- service/history/workflow/mutable_state_impl.go
+++ service/history/workflow/mutable_state_impl.go
@@ -4285,7 +4285,11 @@ func (ms *MutableStateImpl) ApplyWorkflowExecutionOptionsUpdatedEvent(event *his
 		// behavior and effective deployment. In that case, we would not hit the code path to remove the transition or
 		// reschedule the WFT. For this to happen, the existing behavior and the override would both have to be unpinned.
 		// If we don't remove the transition or reschedule pending tasks, the outstanding WFT on the transition's
-		// target queue will be started on the transition's target deployment. Most likely this matches user's intention because they add the unpinned override, they want to workflow to do the transition. Even if we removed the transition, the rescheduled task will be redirected by Matching to the old transition's deployment again and it will start the same transition in the workflow. So removing the transition does not make a difference and just adds some extra work for the server.
+		// target queue will be started on the transition's target deployment. Most likely this matches user's intention
+		// because they add the unpinned override, they want to workflow to do the transition. Even if we removed the
+		// transition, the rescheduled task will be redirected by Matching to the old transition's deployment again,
+		// and it will start the same transition in the workflow. So removing the transition would not make a difference
+		// and would in fact add some extra work for the server.
 		ms.executionInfo.GetVersioningInfo().DeploymentTransition = nil
 		// TODO (carly) part 2: if safe mode, do replay test on new deployment if deployment changed, if fail, revert changes and abort
 		ms.ClearStickyTaskQueue()
