1ea10d7bed5d7d436ce27d69b0ad50fbc96b9dc6
Carly de Frondeville <carly.defrondeville@temporal.io>
2024-11-29 17:53:18 -0800

    Revert "Make ChildWorkflowExecution inherit versioning override of parent (#6911)"
    This reverts commit 74864793db7fb9476e860242a7a19698600aace9.

--- service/history/transfer_queue_active_task_executor.go
+++ service/history/transfer_queue_active_task_executor.go
@@ -854,7 +854,6 @@ func (t *transferQueueActiveTaskExecutor) processStartChildExecution(
 		rootExecutionInfo,
 		inheritedBuildId,
 		initiatedEvent.GetUserMetadata(),
-		mutableState.GetExecutionInfo().GetVersioningInfo().GetVersioningOverride(),
 	)
 	if err != nil {
 		t.logger.Debug("Failed to start child workflow execution", tag.Error(err))
@@ -1345,7 +1344,6 @@ func (t *transferQueueActiveTaskExecutor) startWorkflow(
 	rootExecutionInfo *workflowspb.RootExecutionInfo,
 	inheritedBuildId string,
 	userMetadata *sdkpb.UserMetadata,
-	inheritedOverride *workflowpb.VersioningOverride,
 ) (string, *clockspb.VectorClock, error) {
 	request := common.CreateHistoryStartWorkflowRequest(
 		task.TargetNamespaceID,
@@ -1368,7 +1366,6 @@ func (t *transferQueueActiveTaskExecutor) startWorkflow(
 			Memo:                  attributes.Memo,
 			SearchAttributes:      attributes.SearchAttributes,
 			UserMetadata:          userMetadata,
-			VersioningOverride:    inheritedOverride,
 		},
 		&workflowspb.ParentExecutionInfo{
 			NamespaceId: task.NamespaceID,
