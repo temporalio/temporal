28b49059d99e5545a0a9e97fe7007339ed9ca35c
ShahabT <shahab.tajik@temporal.io>
2024-12-08 21:25:45 -0800

    fix flaky test

--- tests/deployment_test.go
+++ tests/deployment_test.go
@@ -233,7 +233,7 @@ func (s *DeploymentSuite) TestDescribeDeployment_RegisterTaskQueue_ConcurrentPol
 
 		a.True(d.Equal(resp.GetDeploymentInfo().GetDeployment()))
 
-		if !a.Equal(1, len(resp.GetDeploymentInfo().GetTaskQueueInfos())) {
+		if !a.Equal(2, len(resp.GetDeploymentInfo().GetTaskQueueInfos())) {
 			return
 		}
 		a.Equal(tv.TaskQueue().GetName(), resp.GetDeploymentInfo().GetTaskQueueInfos()[0].Name)
