b65e92866023ea726015355ccf22726d484e7bb5
Lina Jodoin <lina.jodoin@temporal.io>
2025-01-06 14:05:40 -0800

    top-level scheduler feedback pt 2

--- components/scheduler2/scheduler_test.go
+++ components/scheduler2/scheduler_test.go
@@ -1,4 +1,4 @@
-package scheduler2_test
+package scheduler2
 
 import (
 	"testing"
@@ -7,7 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 	apischedule "go.temporal.io/api/schedule/v1"
 	srvschedule "go.temporal.io/server/api/schedule/v1"
-	"go.temporal.io/server/components/scheduler2"
 	v1scheduler "go.temporal.io/server/service/worker/scheduler"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
@@ -18,7 +17,7 @@ func TestUseScheduledActionPeek(t *testing.T) {
 	decrement := false
 
 	// Starts paused, with unlimited actions.
-	scheduler := scheduler2.Scheduler{
+	scheduler := Scheduler{
 		SchedulerInternal: &srvschedule.SchedulerInternal{
 			ConflictToken: initialConflictToken,
 			Schedule: &apischedule.Schedule{
@@ -32,33 +31,33 @@ func TestUseScheduledActionPeek(t *testing.T) {
 	}
 
 	// Paused schedules should deny and never mutate.
-	require.False(t, scheduler.UseScheduledAction(decrement))
+	require.False(t, scheduler.useScheduledAction(decrement))
 	require.Equal(t, initialRemainingActions, scheduler.Schedule.State.RemainingActions)
 	require.Equal(t, initialConflictToken, scheduler.ConflictToken)
 
 	// Running schedules with unlimited actions should allow and never mutate.
 	scheduler.Schedule.State.Paused = false
-	require.True(t, scheduler.UseScheduledAction(decrement))
+	require.True(t, scheduler.useScheduledAction(decrement))
 	require.Equal(t, initialRemainingActions, scheduler.Schedule.State.RemainingActions)
 	require.Equal(t, initialConflictToken, scheduler.ConflictToken)
 
 	// Limit the schedule's actions, and check that we can peek the value.
 	scheduler.Schedule.State.LimitedActions = true
 	decrement = false
-	require.True(t, scheduler.UseScheduledAction(decrement))
+	require.True(t, scheduler.useScheduledAction(decrement))
 	require.Equal(t, initialRemainingActions, scheduler.Schedule.State.RemainingActions)
 	require.Equal(t, initialConflictToken, scheduler.ConflictToken)
 
 	// When not peeking, we should mutate RemainingActions and ConflictToken.
 	decrement = true
-	require.True(t, scheduler.UseScheduledAction(decrement))
+	require.True(t, scheduler.useScheduledAction(decrement))
 	require.Equal(t, initialRemainingActions-1, scheduler.Schedule.State.RemainingActions)
 	require.NotEqual(t, initialConflictToken, scheduler.ConflictToken)
 
 	// False when out of remaining actions.
 	scheduler.Schedule.State.RemainingActions = 0
 	oldConflictToken := scheduler.ConflictToken
-	require.False(t, scheduler.UseScheduledAction(decrement))
+	require.False(t, scheduler.useScheduledAction(decrement))
 	require.Equal(t, oldConflictToken, scheduler.ConflictToken)
 }
 
@@ -69,7 +68,7 @@ func TestCompiledSpec(t *testing.T) {
 		Phase:    durationpb.New(0),
 	}
 
-	scheduler := scheduler2.Scheduler{
+	scheduler := Scheduler{
 		SchedulerInternal: &srvschedule.SchedulerInternal{
 			ConflictToken: initialConflictToken,
 			Schedule: &apischedule.Schedule{
@@ -82,16 +81,16 @@ func TestCompiledSpec(t *testing.T) {
 	specBuilder := v1scheduler.NewSpecBuilder()
 
 	// Test that the same compiled spec is returned between invocations.
-	spec, err := scheduler.CompiledSpec(specBuilder)
+	spec, err := scheduler.getCompiledSpec(specBuilder)
 	require.NoError(t, err)
 
-	spec2, err := scheduler.CompiledSpec(specBuilder)
+	spec2, err := scheduler.getCompiledSpec(specBuilder)
 	require.NoError(t, err)
 	require.Equal(t, spec, spec2)
 
 	// Test that an update to conflict token will recompile the spec.
-	scheduler.UpdateConflictToken()
-	spec3, err := scheduler.CompiledSpec(specBuilder)
+	scheduler.updateConflictToken()
+	spec3, err := scheduler.getCompiledSpec(specBuilder)
 	require.NoError(t, err)
 	require.NotSame(t, spec, spec3)
 }
