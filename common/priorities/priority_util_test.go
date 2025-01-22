ad09472ec423dde841a7d08c324cd99a5c5ec6d1
Stephan Behnke <stephan.behnke@temporal.io>
2025-01-22 11:34:27 -0800

    use 2025

--- common/priorities/priority_util_test.go
+++ common/priorities/priority_util_test.go
@@ -2,7 +2,7 @@
 //
 // Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
 //
-// Copyright (c) 2020 Uber Technologies, Inc.
+// Copyright (c) 2025 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
