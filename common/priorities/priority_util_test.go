f86b93e86bfd365d3565fc164af015eafd55094a
Stephan Behnke <stephan.behnke@temporal.io>
2025-01-22 20:07:24 -0800

    fix header

--- common/priorities/priority_util_test.go
+++ common/priorities/priority_util_test.go
@@ -1,8 +1,6 @@
 // The MIT License
 //
-// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
-//
-// Copyright (c) 2025 Uber Technologies, Inc.
+// Copyright (c) 2025 Temporal Technologies Inc.  All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
