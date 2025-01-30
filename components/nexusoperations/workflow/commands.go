446c0c1309d6e8fa4f45435a10d0f0e7c428262b
justinp <174377431+justinp-tt@users.noreply.github.com>
2025-01-30 14:09:05 -0600

    Revert "Fix syntax error"
    This reverts commit f5d21370920bf8f5cb468adc26e6d5d759289312.

--- components/nexusoperations/workflow/commands.go
+++ components/nexusoperations/workflow/commands.go
@@ -225,31 +225,31 @@ func (ch *commandHandler) HandleCancelCommand(
 		} else {
 			return err
 		}
-	} else if node == nil && !ms.HasAnyBufferedEvent(makeNexusOperationTerminalEventFilter(attrs.ScheduledEventId)) {
+	} else if node == nil &&  !ms.HasAnyBufferedEvent(makeNexusOperationTerminalEventFilter(attrs.ScheduledEventId)) {
 		return workflow.FailWorkflowTaskError{
 			Cause:   enumspb.WORKFLOW_TASK_FAILED_CAUSE_BAD_REQUEST_CANCEL_NEXUS_OPERATION_ATTRIBUTES,
 			Message: fmt.Sprintf("requested cancelation for a non-existing or already completed operation with scheduled event ID of %d", attrs.ScheduledEventId),
 		}
 		// Fallthrough and apply the event, there's special logic that will handle state machine not found below.
 	}
-
-	// TODO(bergundy): Remove this when operation auto-deletes itself on terminal state.
-	// Operation may already be in a terminal state because it doesn't yet delete itself. We don't want to accept
-	// cancelation in this case.
-	op, err := hsm.MachineData[nexusoperations.Operation](node)
-	if err != nil {
-		return err
-	}
-	// The operation is already in a terminal state and the terminal NexusOperation event has not just been buffered.
-	// We allow the workflow to request canceling an operation that has just completed while a workflow task is in
-	// flight since it cannot know about the state of the operation.
-	if !nexusoperations.TransitionCanceled.Possible(op) && !ms.HasAnyBufferedEvent(makeNexusOperationTerminalEventFilter(attrs.ScheduledEventId)) {
-		return workflow.FailWorkflowTaskError{
-			Cause:   enumspb.WORKFLOW_TASK_FAILED_CAUSE_BAD_REQUEST_CANCEL_NEXUS_OPERATION_ATTRIBUTES,
-			Message: fmt.Sprintf("requested cancelation for an already complete operation with scheduled event ID of %d", attrs.ScheduledEventId),
+		// TODO(bergundy): Remove this when operation auto-deletes itself on terminal state.
+		// Operation may already be in a terminal state because it doesn't yet delete itself. We don't want to accept
+		// cancelation in this case.
+		op, err := hsm.MachineData[nexusoperations.Operation](node)
+		if err != nil {
+			return err
 		}
+		// The operation is already in a terminal state and the terminal NexusOperation event has not just been buffered.
+		// We allow the workflow to request canceling an operation that has just completed while a workflow task is in
+		// flight since it cannot know about the state of the operation.
+		if !nexusoperations.TransitionCanceled.Possible(op) && !ms.HasAnyBufferedEvent(makeNexusOperationTerminalEventFilter(attrs.ScheduledEventId)) {
+			return workflow.FailWorkflowTaskError{
+				Cause:   enumspb.WORKFLOW_TASK_FAILED_CAUSE_BAD_REQUEST_CANCEL_NEXUS_OPERATION_ATTRIBUTES,
+				Message: fmt.Sprintf("requested cancelation for an already complete operation with scheduled event ID of %d", attrs.ScheduledEventId),
+			}
+		}
+		// END TODO
 	}
-	// END TODO
 
 	// Always create the event even if there's a buffered completion to avoid breaking replay in the SDK.
 	// The event will be applied before the completion since buffered events are reordered and put at the end of the
