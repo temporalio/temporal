8c10645c564bf5f7ec069f4af459799e39a2b943
justinp <174377431+justinp-tt@users.noreply.github.com>
2025-01-31 12:21:22 -0600

    yichao code review follow-up

--- components/nexusoperations/workflow/commands.go
+++ components/nexusoperations/workflow/commands.go
@@ -228,15 +228,7 @@ func (ch *commandHandler) HandleCancelCommand(
 		}
 	}
 
-	if node == nil {
-		if !hasBufferedEvent {
-			return workflow.FailWorkflowTaskError{
-				Cause:   enumspb.WORKFLOW_TASK_FAILED_CAUSE_BAD_REQUEST_CANCEL_NEXUS_OPERATION_ATTRIBUTES,
-				Message: fmt.Sprintf("requested cancelation for a non-existing or already completed operation with scheduled event ID of %d", attrs.ScheduledEventId),
-			}
-		}
-		// Fallthrough and apply the event, there's special logic that will handle state machine not found below.
-	} else {
+	if node != nil {
 		// TODO(bergundy): Remove this when operation auto-deletes itself on terminal state.
 		// Operation may already be in a terminal state because it doesn't yet delete itself. We don't want to accept
 		// cancelation in this case.
