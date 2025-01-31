54249ad69fd7f35247cc1a361e9f4be69add91f4
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-01-31 15:15:00 -0800

    remove DeleteDeployment implementation from deployment workflow

--- service/worker/workerdeployment/workflow.go
+++ service/worker/workerdeployment/workflow.go
@@ -48,7 +48,6 @@ type (
 		metrics        sdkclient.MetricsHandler
 		lock           workflow.Mutex
 		pendingUpdates int
-		done           bool
 	}
 )
 
@@ -118,17 +117,6 @@ func (d *WorkflowRunner) run(ctx workflow.Context) error {
 
 	if err := workflow.SetUpdateHandlerWithOptions(
 		ctx,
-		DeleteDeployment,
-		d.handleDeleteDeployment,
-		workflow.UpdateHandlerOptions{
-			Validator: d.validateDeleteDeployment,
-		},
-	); err != nil {
-		return err
-	}
-
-	if err := workflow.SetUpdateHandlerWithOptions(
-		ctx,
 		DeleteVersion,
 		d.handleDeleteVersion,
 		workflow.UpdateHandlerOptions{
@@ -139,14 +127,9 @@ func (d *WorkflowRunner) run(ctx workflow.Context) error {
 	}
 
 	// Wait until we can continue as new or are cancelled.
-	err = workflow.Await(ctx, func() bool {
-		return (workflow.GetInfo(ctx).GetContinueAsNewSuggested() && pendingUpdates == 0) || d.done
-	})
+	err = workflow.Await(ctx, func() bool { return workflow.GetInfo(ctx).GetContinueAsNewSuggested() && pendingUpdates == 0 })
 	if err != nil {
 		return err
-	}
-	if d.done {
-		return nil
 	}
 
 	// Continue as new when there are no pending updates and history size is greater than requestsBeforeContinueAsNew.
@@ -254,18 +237,6 @@ func (d *WorkflowRunner) handleSetWorkerDeploymentRampingVersion(ctx workflow.Co
 		PreviousPercentage: prevRampingVersionPercentage,
 	}, nil
 
-}
-
-func (d *WorkflowRunner) validateDeleteDeployment() error {
-	if len(d.State.Versions) > 0 {
-		return serviceerror.NewFailedPrecondition("deployment can only be deleted when it has no versions")
-	}
-	return nil
-}
-
-func (d *WorkflowRunner) handleDeleteDeployment() error {
-	d.done = true
-	return nil
 }
 
 func (d *WorkflowRunner) validateDeleteVersion(args *deploymentspb.DeleteVersionArgs) error {
