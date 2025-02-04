816cab63a2e8fe7615ec55c35d8c6ebb920ef338
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-02-04 14:29:39 -0800

    fix import diff

--- tests/activity_api_pause_test.go
+++ tests/activity_api_pause_test.go
@@ -2,8 +2,6 @@
 //
 // Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
 //
-// Copyright (c) 2020 Uber Technologies, Inc.
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
