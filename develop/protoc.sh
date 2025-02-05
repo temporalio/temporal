5df69c7273b3e6870113af1978e42a0ff3c4fa5c
Prathyush PV <prathyush.pv@temporal.io>
2025-02-05 15:17:47 -0800

    Edit protoc.sh command

--- develop/protoc.sh
+++ develop/protoc.sh
@@ -38,12 +38,11 @@ color "Update license headers for proto files..."
 go run ./cmd/tools/copyright/licensegen.go --scanDir "$new"
 
 color "Modify history service server interface..."
-sed -i.bak -e \
-    's/GetWorkflowExecutionHistory(context\.Context, \*GetWorkflowExecutionHistoryRequest) (\*GetWorkflowExecutionHistoryResponse, error)/GetWorkflowExecutionHistory(context.Context, *GetWorkflowExecutionHistoryRequest) (*GetWorkflowExecutionHistoryResponseWithRaw, error)/g' \
-    "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go && rm "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go.bak
-sed -i.bak -e \
-    's/RecordWorkflowTaskStarted(context\.Context, \*RecordWorkflowTaskStartedRequest) (\*RecordWorkflowTaskStartedResponse, error)/RecordWorkflowTaskStarted(context.Context, *RecordWorkflowTaskStartedRequest) (*RecordWorkflowTaskStartedResponseWithRawHistory, error)/g' \
-    "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go && rm "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go.bak
+sed -i.bak \
+    -e 's/GetWorkflowExecutionHistory(context\.Context, \*GetWorkflowExecutionHistoryRequest) (\*GetWorkflowExecutionHistoryResponse, error)/GetWorkflowExecutionHistory(context.Context, *GetWorkflowExecutionHistoryRequest) (*GetWorkflowExecutionHistoryResponseWithRaw, error)/g' \
+    -e 's/RecordWorkflowTaskStarted(context\.Context, \*RecordWorkflowTaskStartedRequest) (\*RecordWorkflowTaskStartedResponse, error)/RecordWorkflowTaskStarted(context.Context, *RecordWorkflowTaskStartedRequest) (*RecordWorkflowTaskStartedResponseWithRawHistory, error)/g' \
+    "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go
+rm "$new"/temporal/server/api/historyservice/v1/service_grpc.pb.go.bak
 
 color "Moving proto files into place..."
 old=$api.old
