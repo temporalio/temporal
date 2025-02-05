74e715a3a7d66f5746e3d888686f457a9388bb46
Stephan Behnke <stephanos@users.noreply.github.com>
2025-02-05 10:32:03 -0800

    Dynamic config subscriber in Update Registry (#7224)
    ## What changed?
    <!-- Describe what has changed in this PR -->
    
    Using the new dynamic config subscribers in the Update registry.
    
    ## Why?
    <!-- Tell your future self why have you made these changes -->
    
    To keep the Update registry's settings up-to-date.
    
    ## How did you test it?
    <!-- How have you verified this change? Tested locally? Added a unit
    test? Checked in staging env? -->
    
    Updated existing tests.
    
    ## Potential risks
    <!-- Assuming the worst case, what can be broken when deploying this
    change to production? -->
    
    ## Documentation
    <!-- Have you made sure this change doesn't falsify anything currently
    stated in `docs/`? If significant
    new behavior is added, have you described that in `docs/`? -->
    
    ## Is hotfix candidate?
    <!-- Is this PR a hotfix candidate or does it require a notification to
    be sent to the broader community? (Yes/No) -->

--- service/history/configs/config.go
+++ service/history/configs/config.go
@@ -367,9 +367,9 @@ type Config struct {
 	ArchivalBackendMaxRPS                               dynamicconfig.FloatPropertyFn
 	ArchivalQueueMaxReaderCount                         dynamicconfig.IntPropertyFn
 
-	WorkflowExecutionMaxInFlightUpdates        dynamicconfig.IntPropertyFnWithNamespaceFilter
-	WorkflowExecutionMaxInFlightUpdatePayloads dynamicconfig.IntPropertyFnWithNamespaceFilter
-	WorkflowExecutionMaxTotalUpdates           dynamicconfig.IntPropertyFnWithNamespaceFilter
+	WorkflowExecutionMaxInFlightUpdates        dynamicconfig.TypedSubscribableWithNamespaceFilter[int]
+	WorkflowExecutionMaxInFlightUpdatePayloads dynamicconfig.TypedSubscribableWithNamespaceFilter[int]
+	WorkflowExecutionMaxTotalUpdates           dynamicconfig.TypedSubscribableWithNamespaceFilter[int]
 
 	SendRawWorkflowHistory dynamicconfig.BoolPropertyFnWithNamespaceFilter
 
@@ -683,9 +683,9 @@ func NewConfig(
 		ArchivalQueueMaxReaderCount:                         dynamicconfig.ArchivalQueueMaxReaderCount.Get(dc),
 
 		// workflow update related
-		WorkflowExecutionMaxInFlightUpdates:        dynamicconfig.WorkflowExecutionMaxInFlightUpdates.Get(dc),
-		WorkflowExecutionMaxInFlightUpdatePayloads: dynamicconfig.WorkflowExecutionMaxInFlightUpdatePayloads.Get(dc),
-		WorkflowExecutionMaxTotalUpdates:           dynamicconfig.WorkflowExecutionMaxTotalUpdates.Get(dc),
+		WorkflowExecutionMaxInFlightUpdates:        dynamicconfig.WorkflowExecutionMaxInFlightUpdates.Subscribe(dc),
+		WorkflowExecutionMaxInFlightUpdatePayloads: dynamicconfig.WorkflowExecutionMaxInFlightUpdatePayloads.Subscribe(dc),
+		WorkflowExecutionMaxTotalUpdates:           dynamicconfig.WorkflowExecutionMaxTotalUpdates.Subscribe(dc),
 
 		SendRawWorkflowHistory:                   dynamicconfig.SendRawWorkflowHistory.Get(dc),
 		WorkflowIdReuseMinimalInterval:           dynamicconfig.WorkflowIdReuseMinimalInterval.Get(dc),
