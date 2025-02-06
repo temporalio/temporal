77555906cb94d9e2dce435ce0c8ca5d43fd105ce
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-02-05 19:01:20 -0800

    fix copy paste mistake

--- schema/postgresql/v12/visibility/versioned/v1.7/add_pause_info_search_attribute.sql
+++ schema/postgresql/v12/visibility/versioned/v1.7/add_pause_info_search_attribute.sql
@@ -1,7 +1,2 @@
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeploymentVersion    JSONB GENERATED ALWAYS AS (search_attributes->'TemporalWorkerDeploymentVersion') STORED;
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkflowVersioningBehavior JSONB GENERATED ALWAYS AS (search_attributes->'TemporalWorkflowVersioningBehavior') STORED;
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeployment           JSONB GENERATED ALWAYS AS (search_attributes->'TemporalWorkerDeployment') STORED;
-
-CREATE INDEX by_temporal_worker_deployment_version ON executions_visibility USING GIN (namespace_id, TemporalWorkerDeploymentVersion jsonb_path_ops);
-CREATE INDEX by_temporal_workflow_versioning_behavior ON executions_visibility USING GIN (namespace_id, TemporalWorkflowVersioningBehavior jsonb_path_ops);
-CREATE INDEX by_temporal_worker_deployment ON executions_visibility USING GIN (namespace_id, TemporalWorkerDeployment jsonb_path_ops);
+ALTER TABLE executions_visibility ADD COLUMN TemporalPauseInfo JSONB GENERATED ALWAYS AS (search_attributes->'TemporalPauseInfo') STORED;
+CREATE INDEX by_temporal_pause_info ON executions_visibility USING GIN (namespace_id, TemporalPauseInfo jsonb_path_ops);
