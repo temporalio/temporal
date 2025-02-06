04abb8dd24827366541b45674a2630753c292d05
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-02-06 12:13:46 -0800

    Update schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
    Co-authored-by: Rodrigo Zhou <2068124+rodrigozhou@users.noreply.github.com>

--- schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
+++ schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
@@ -2,6 +2,6 @@ ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeploymentVersion    
 ALTER TABLE executions_visibility ADD COLUMN TemporalWorkflowVersioningBehavior VARCHAR(255) GENERATED ALWAYS AS (search_attributes->>'TemporalWorkflowVersioningBehavior') STORED;
 ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeployment           VARCHAR(255) GENERATED ALWAYS AS (search_attributes->>'TemporalWorkerDeployment') STORED;
 
-CREATE INDEX by_temporal_worker_deployment_version ON executions_visibility USING GIN (namespace_id, TemporalWorkerDeploymentVersion jsonb_path_ops);
-CREATE INDEX by_temporal_workflow_versioning_behavior ON executions_visibility USING GIN (namespace_id, TemporalWorkflowVersioningBehavior jsonb_path_ops);
-CREATE INDEX by_temporal_worker_deployment ON executions_visibility USING GIN (namespace_id, TemporalWorkerDeployment jsonb_path_ops);
+CREATE INDEX by_temporal_worker_deployment_version ON executions_visibility (namespace_id, TemporalWorkerDeploymentVersion, (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
+CREATE INDEX by_temporal_workflow_versioning_behavior ON executions_visibility (namespace_id, TemporalWorkflowVersioningBehavior, (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
+CREATE INDEX by_temporal_worker_deployment ON executions_visibility (namespace_id, TemporalWorkerDeployment, (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
