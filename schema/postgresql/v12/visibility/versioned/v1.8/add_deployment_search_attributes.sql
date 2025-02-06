c73c5bd7709b37cf3b4c19a1be8006b552c25409
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-02-06 12:11:45 -0800

    Update schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
    Co-authored-by: Rodrigo Zhou <2068124+rodrigozhou@users.noreply.github.com>

--- schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
+++ schema/postgresql/v12/visibility/versioned/v1.8/add_deployment_search_attributes.sql
@@ -1,6 +1,6 @@
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeploymentVersion    JSONB GENERATED ALWAYS AS (search_attributes->>'TemporalWorkerDeploymentVersion') STORED;
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkflowVersioningBehavior JSONB GENERATED ALWAYS AS (search_attributes->>'TemporalWorkflowVersioningBehavior') STORED;
-ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeployment           JSONB GENERATED ALWAYS AS (search_attributes->>'TemporalWorkerDeployment') STORED;
+ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeploymentVersion    VARCHAR(255) GENERATED ALWAYS AS (search_attributes->>'TemporalWorkerDeploymentVersion') STORED;
+ALTER TABLE executions_visibility ADD COLUMN TemporalWorkflowVersioningBehavior VARCHAR(255) GENERATED ALWAYS AS (search_attributes->>'TemporalWorkflowVersioningBehavior') STORED;
+ALTER TABLE executions_visibility ADD COLUMN TemporalWorkerDeployment           VARCHAR(255) GENERATED ALWAYS AS (search_attributes->>'TemporalWorkerDeployment') STORED;
 
 CREATE INDEX by_temporal_worker_deployment_version ON executions_visibility USING GIN (namespace_id, TemporalWorkerDeploymentVersion jsonb_path_ops);
 CREATE INDEX by_temporal_workflow_versioning_behavior ON executions_visibility USING GIN (namespace_id, TemporalWorkflowVersioningBehavior jsonb_path_ops);
