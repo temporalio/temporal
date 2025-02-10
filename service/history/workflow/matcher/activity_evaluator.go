2d893d355dde4a699bc420190ce6dff53de606c0
Yuri <ychebotarev@gmail.com>
2025-02-10 10:19:43 -0800

    add activity query evaluator

--- service/history/workflow/matcher/activity_evaluator.go
+++ service/history/workflow/matcher/activity_evaluator.go
@@ -28,28 +28,36 @@ import (
 	"time"
 
 	"github.com/temporalio/sqlparser"
-	"go.temporal.io/server/common/searchattribute"
+	enumspb "go.temporal.io/api/enums/v1"
+	persistencespb "go.temporal.io/server/api/persistence/v1"
 	"go.temporal.io/server/common/sqlquery"
 	"go.temporal.io/server/service/history/workflow"
 )
 
 // Supported Fields
 const (
-	workflowIDColName              = searchattribute.WorkflowID
-	workflowTypeNameColName        = searchattribute.WorkflowType
-	workflowStartTimeColName       = searchattribute.StartTime
-	workflowExecutionStatusColName = searchattribute.ExecutionStatus
+	activityIDColName              = "ActivityId"
+	activityTypeNameColName        = "ActivityType"
+	activityStatusColName          = "ActivityStatus"
+	activityAttemptsColName        = "Attempts"
+	activityBackoffIntervalColName = "BackoffInterval"
+	activityLastFailureColName     = "LastFailure"
+	activityTaskQueueColName       = "TaskQueue"
+	activityStartedTime            = "StartedTime"
 )
 
-type MutableStateMatchEvaluator struct {
-	ms workflow.MutableState
+type ActivityMatchEvaluator struct {
+	ai *persistencespb.ActivityInfo
 }
 
-func newMutableStateMatchEvaluator(ms workflow.MutableState) *MutableStateMatchEvaluator {
-	return &MutableStateMatchEvaluator{ms: ms}
+func newActivityMatchEvaluator(ai *persistencespb.ActivityInfo) *ActivityMatchEvaluator {
+	return &ActivityMatchEvaluator{ai: ai}
 }
 
-func (m *MutableStateMatchEvaluator) Evaluate(query string) (bool, error) {
+func (m *ActivityMatchEvaluator) Evaluate(query string) (bool, error) {
+	if m.ai == nil {
+		return false, NewMatcherError("activity info is nil")
+	}
 	query, err := prepareQuery(query)
 	if err != nil {
 		return false, err
@@ -63,7 +71,7 @@ func (m *MutableStateMatchEvaluator) Evaluate(query string) (bool, error) {
 	return m.evaluateExpression(whereCause)
 }
 
-func (m *MutableStateMatchEvaluator) evaluateExpression(expr sqlparser.Expr) (bool, error) {
+func (m *ActivityMatchEvaluator) evaluateExpression(expr sqlparser.Expr) (bool, error) {
 
 	if expr == nil {
 		return false, NewMatcherError("input expression cannot be nil")
@@ -76,10 +84,10 @@ func (m *MutableStateMatchEvaluator) evaluateExpression(expr sqlparser.Expr) (bo
 		return m.evaluateOr(e)
 	case *sqlparser.ComparisonExpr:
 		return m.evaluateComparison(e)
-	case *sqlparser.RangeCond:
-		return m.evaluateRange(e)
 	case *sqlparser.ParenExpr:
 		return m.evaluateExpression(e.Expr)
+	case *sqlparser.RangeCond:
+		return m.evaluateRange(e)
 	case *sqlparser.IsExpr:
 		return false, NewMatcherError("%s: 'is' expression", notSupportedErrMessage)
 	case *sqlparser.NotExpr:
@@ -92,7 +100,8 @@ func (m *MutableStateMatchEvaluator) evaluateExpression(expr sqlparser.Expr) (bo
 		return false, NewMatcherError("%s: expression of type %T", notSupportedErrMessage, expr)
 	}
 }
-func (m *MutableStateMatchEvaluator) evaluateAnd(expr *sqlparser.AndExpr) (bool, error) {
+
+func (m *ActivityMatchEvaluator) evaluateAnd(expr *sqlparser.AndExpr) (bool, error) {
 	if expr == nil {
 		return false, NewMatcherError("And expression input expression cannot be nil")
 	}
@@ -104,7 +113,7 @@ func (m *MutableStateMatchEvaluator) evaluateAnd(expr *sqlparser.AndExpr) (bool,
 	return m.evaluateExpression(expr.Right)
 }
 
-func (m *MutableStateMatchEvaluator) evaluateOr(expr *sqlparser.OrExpr) (bool, error) {
+func (m *ActivityMatchEvaluator) evaluateOr(expr *sqlparser.OrExpr) (bool, error) {
 	if expr == nil {
 		return false, NewMatcherError("Or expression input expression cannot be nil")
 	}
@@ -115,7 +124,7 @@ func (m *MutableStateMatchEvaluator) evaluateOr(expr *sqlparser.OrExpr) (bool, e
 	return m.evaluateExpression(expr.Right)
 }
 
-func (m *MutableStateMatchEvaluator) evaluateComparison(expr *sqlparser.ComparisonExpr) (bool, error) {
+func (m *ActivityMatchEvaluator) evaluateComparison(expr *sqlparser.ComparisonExpr) (bool, error) {
 	if expr == nil {
 		return false, NewMatcherError("ComparisonExpr input expression cannot be nil")
 	}
@@ -132,33 +141,56 @@ func (m *MutableStateMatchEvaluator) evaluateComparison(expr *sqlparser.Comparis
 	valStr := sqlparser.String(valExpr)
 
 	switch colNameStr {
-	case workflowTypeNameColName:
+	case activityTypeNameColName:
 		val, err := sqlquery.ExtractStringValue(valStr)
 		if err != nil {
 			return false, err
 		}
-		return m.compareWorkflowType(val, expr.Operator)
-	case workflowIDColName:
+		return m.compareActivityType(val, expr.Operator)
+	case activityIDColName:
 		val, err := sqlquery.ExtractStringValue(valStr)
 		if err != nil {
 			return false, err
 		}
-		return m.compareWorkflowID(val, expr.Operator)
-	case workflowExecutionStatusColName:
+		return m.compareActivityId(val, expr.Operator)
+	case activityStatusColName:
 		val, err := sqlquery.ExtractStringValue(valStr)
 		if err != nil {
 			return false, err
 		}
-		return m.compareWorkflowStatus(val, expr.Operator)
-	case workflowStartTimeColName:
+		return m.compareActivityStatus(val, expr.Operator)
+	case activityAttemptsColName:
+		val, err := sqlquery.ExtractIntValue(valStr)
+		if err != nil {
+			return false, err
+		}
+		return m.compareActivityAttempts(val, expr.Operator)
+	case activityBackoffIntervalColName:
+		val, err := sqlquery.ExtractIntValue(valStr)
+		if err != nil {
+			return false, err
+		}
+		return m.compareBackoffInterval(val, expr.Operator)
+	case activityLastFailureColName:
+		val, err := sqlquery.ExtractStringValue(valStr)
+		if err != nil {
+			return false, err
+		}
+		return m.compareLastFailure(val, expr.Operator)
+	case activityTaskQueueColName:
+		val, err := sqlquery.ExtractStringValue(valStr)
+		if err != nil {
+			return false, err
+		}
+		return m.compareActivityTaskQueue(val, expr.Operator)
+	case activityStartedTime:
 		return m.compareStartTime(valStr, expr.Operator)
-
 	default:
 		return false, fmt.Errorf("unknown or unsupported search attribute name: %s", colNameStr)
 	}
 }
 
-func (m *MutableStateMatchEvaluator) evaluateRange(expr *sqlparser.RangeCond) (bool, error) {
+func (m *ActivityMatchEvaluator) evaluateRange(expr *sqlparser.RangeCond) (bool, error) {
 	if expr == nil {
 		return false, NewMatcherError("RangeCond input expression cannot be nil")
 	}
@@ -169,7 +201,7 @@ func (m *MutableStateMatchEvaluator) evaluateRange(expr *sqlparser.RangeCond) (b
 	colNameStr := sqlparser.String(colName)
 
 	switch colNameStr {
-	case workflowStartTimeColName:
+	case activityStartedTime:
 		fromValue, err := sqlquery.ConvertToTime(sqlparser.String(expr.From))
 		if err != nil {
 			return false, err
@@ -196,55 +228,67 @@ func (m *MutableStateMatchEvaluator) evaluateRange(expr *sqlparser.RangeCond) (b
 	}
 }
 
-func (m *MutableStateMatchEvaluator) compareWorkflowType(workflowType string, operation string) (bool, error) {
-	existingWorkflowType := m.ms.GetExecutionInfo().WorkflowTypeName
-	return m.compareString(existingWorkflowType, workflowType, operation, workflowTypeNameColName)
+func (m *ActivityMatchEvaluator) compareActivityType(activityType string, operation string) (bool, error) {
+	existingActivityType := m.ai.GetActivityType().GetName()
+	return compareQueryString(activityType, existingActivityType, operation, activityTypeNameColName)
 }
 
-func (m *MutableStateMatchEvaluator) compareWorkflowID(workflowID string, operation string) (bool, error) {
-	existingWorkflowId := m.ms.GetExecutionInfo().WorkflowId
-	return m.compareString(workflowID, existingWorkflowId, operation, workflowIDColName)
+func (m *ActivityMatchEvaluator) compareActivityId(activityId string, operation string) (bool, error) {
+	existingActivityId := m.ai.GetActivityId()
+	return compareQueryString(activityId, existingActivityId, operation, workflowIDColName)
 }
 
-func (m *MutableStateMatchEvaluator) compareString(inStr string, expectedStr string, operation string, fieldName string) (bool, error) {
-	if len(inStr) == 0 {
-		return false, NewMatcherError("%s cannot be empty", fieldName)
+func (m *ActivityMatchEvaluator) compareActivityStatus(status string, operator string) (bool, error) {
+	if m.ai.Paused {
+		return compareQueryString(status, "Paused", operator, activityStatusColName)
 	}
-	switch operation {
-	case sqlparser.EqualStr:
-		return expectedStr == inStr, nil
-	case sqlparser.NotEqualStr:
-		return expectedStr != inStr, nil
-	case sqlparser.StartsWithStr:
-		return strings.HasPrefix(expectedStr, inStr), nil
-	case sqlparser.NotStartsWithStr:
-		return !strings.HasPrefix(expectedStr, inStr), nil
-	default:
-		return false, NewMatcherError("%s: operation %s is not supported for %s filter", invalidExpressionErrMessage, operation, fieldName)
+	switch workflow.GetActivityState(m.ai) {
+	case enumspb.PENDING_ACTIVITY_STATE_CANCEL_REQUESTED:
+		return compareQueryString(status, "Cancelled", operator, activityStatusColName)
+	case enumspb.PENDING_ACTIVITY_STATE_STARTED:
+		return compareQueryString(status, "Running", operator, activityStatusColName)
+	case enumspb.PENDING_ACTIVITY_STATE_SCHEDULED:
+		return compareQueryString(status, "Scheduled", operator, activityStatusColName)
 	}
+	return false, NewMatcherError("unknown activity status: %s", status)
 }
 
-func (m *MutableStateMatchEvaluator) compareWorkflowStatus(status string, operation string) (bool, error) {
-	if len(status) == 0 {
-		return false, NewMatcherError("workflow status cannot be empty")
-	}
-	msStatus := m.ms.GetExecutionState().Status.String()
-	switch operation {
-	case sqlparser.EqualStr:
-		return msStatus == status, nil
-	case sqlparser.NotEqualStr:
-		return msStatus != status, nil
-	default:
-		return false, NewMatcherError("%s: operation %s is not supported for execution status filter", invalidExpressionErrMessage, operation)
-	}
+func (m *ActivityMatchEvaluator) compareActivityTaskQueue(taskQueue string, operator string) (bool, error) {
+	existingTaskQueue := m.ai.TaskQueue
+	return compareQueryString(taskQueue, existingTaskQueue, operator, activityTaskQueueColName)
 }
 
-func (m *MutableStateMatchEvaluator) compareStartTime(val string, operation string) (bool, error) {
+func (m *ActivityMatchEvaluator) compareActivityAttempts(attempts int, operator string) (bool, error) {
+	existingAttempts := m.ai.Attempt
+	return compareQueryInt(attempts, int(existingAttempts), operator, activityAttemptsColName)
+}
+
+func (m *ActivityMatchEvaluator) compareBackoffInterval(intervalInSec int, operator string) (bool, error) {
+	if m.ai.Attempt < 2 {
+		// no backoff interval for first attempt
+		return false, nil
+	}
+
+	backoffIntervalSec := int(m.ai.ScheduledTime.AsTime().Sub(m.ai.LastAttemptCompleteTime.AsTime()).Seconds())
+	return compareQueryInt(intervalInSec, backoffIntervalSec, operator, activityBackoffIntervalColName)
+}
+
+func (m *ActivityMatchEvaluator) compareLastFailure(val string, operator string) (bool, error) {
+	if m.ai.RetryLastFailure == nil || m.ai.RetryLastFailure.Message == "" {
+		return false, nil
+	}
+	if operator != "contains" {
+		return false, NewMatcherError("unsupported operator for LastFailure column: %s", operator)
+	}
+	return strings.Contains(m.ai.RetryLastFailure.Message, val), nil
+}
+
+func (m *ActivityMatchEvaluator) compareStartTime(val string, operation string) (bool, error) {
 	expectedTime, err := sqlquery.ConvertToTime(val)
 	if err != nil {
 		return false, err
 	}
-	startTime := m.ms.GetExecutionState().StartTime.AsTime()
+	startTime := m.ai.GetStartedTime().AsTime()
 	switch operation {
 	case sqlparser.GreaterEqualStr:
 		return startTime.Compare(expectedTime) >= 0, nil
@@ -259,52 +303,14 @@ func (m *MutableStateMatchEvaluator) compareStartTime(val string, operation stri
 	case sqlparser.NotEqualStr:
 		return startTime != expectedTime, nil
 	default:
-		return false, NewMatcherError("%s: operation %s is not supported for StartTime", invalidExpressionErrMessage, operation)
+		return false, NewMatcherError("%s: operation %s is not supported for activity StartTime", invalidExpressionErrMessage, operation)
 	}
 }
 
-func (m *MutableStateMatchEvaluator) compareStartTimeBetween(fromTime time.Time, toTime time.Time) (bool, error) {
-	startTime := m.ms.GetExecutionState().StartTime.AsTime()
-	lc := startTime.Compare(fromTime)
-	rc := startTime.Compare(toTime)
-	return lc >= 0 && rc <= 0, nil
-}
-
-func getWhereCause(query string) (sqlparser.Expr, error) {
-	stmt, err := sqlparser.Parse(query)
-	if err != nil {
-		return nil, NewMatcherError("%s: %v", malformedSqlQueryErrMessage, err)
+func (m *ActivityMatchEvaluator) compareStartTimeBetween(fromTime time.Time, toTime time.Time) (bool, error) {
+	if m.ai.GetStartedTime() == nil {
+		return false, nil
 	}
-
-	selectStmt, isSelect := stmt.(*sqlparser.Select)
-	if !isSelect {
-		return nil, NewMatcherError("%s: statement must be 'select' not %T", notSupportedErrMessage, stmt)
-	}
-
-	if selectStmt.Limit != nil {
-		return nil, NewMatcherError("%s: 'limit' clause", notSupportedErrMessage)
-	}
-
-	if selectStmt.Where == nil {
-		return nil, NewMatcherError("%s: 'where' clause is missing", notSupportedErrMessage)
-	}
-
-	return selectStmt.Where.Expr, nil
-}
-
-func prepareQuery(query string) (string, error) {
-	query = strings.TrimSpace(query)
-	if query == "" {
-		return "", nil
-	}
-
-	if strings.HasPrefix(strings.ToLower(query), "where ") ||
-		strings.HasPrefix(strings.ToLower(query), "select ") {
-		return "", fmt.Errorf("invalid filter: %s", query)
-	}
-
-	// sqlparser can't parse just WHERE clause but instead accepts only valid SQL statement.
-	query = fmt.Sprintf("select * from table1 where %s", query)
-
-	return query, nil
+	startTime := m.ai.GetStartedTime().AsTime()
+	return startTime.Compare(fromTime) >= 0 && startTime.Compare(toTime) <= 0, nil
 }
