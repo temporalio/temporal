b54b21bd15af825625523b31740232b11fa16ecd
Lina Jodoin <lina.jodoin@temporal.io>
2025-02-11 14:40:59 -0800

    fix a  lint

--- components/scheduler/invoker_executors_test.go
+++ components/scheduler/invoker_executors_test.go
@@ -32,7 +32,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
-
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/serviceerror"
