deb1647aea9710cdb3e0bf14a62cb14695d71941
David Reiss <david@temporal.io>
2025-02-18 19:49:53 +0000

    Remove code to parse task queue persistence keys (#7348)
    ## What changed?
    Remove ParsePhysicalTaskQueueKey.
    
    ## Why?
    We shouldn't ever need to parse these. This was only used in tests, but
    the tests don't actually need to do it.

--- service/matching/physical_task_queue_key.go
+++ service/matching/physical_task_queue_key.go
@@ -27,9 +27,7 @@ package matching
 import (
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"strconv"
-	"strings"
 
 	deploymentpb "go.temporal.io/api/deployment/v1"
 	enumspb "go.temporal.io/api/enums/v1"
@@ -165,88 +163,6 @@ func (q *PhysicalTaskQueueKey) PersistenceName() string {
 	default:
 		panic("unsupported partition kind: " + p.Kind().String())
 	}
-}
-
-// ParsePhysicalTaskQueueKey takes the persistence name of a DB task queue and returns a PhysicalTaskQueueKey. Returns an error if the
-// given name is not a valid persistence name.
-func ParsePhysicalTaskQueueKey(persistenceName string, namespaceId string, taskType enumspb.TaskQueueType) (*PhysicalTaskQueueKey, error) {
-	baseName := persistenceName
-	partitionId := 0
-	versionSet := ""
-	buildId := ""
-	deploymentName := ""
-
-	if strings.HasPrefix(persistenceName, nonRootPartitionPrefix) {
-		suffixOff := strings.LastIndex(persistenceName, partitionDelimiter)
-		if suffixOff <= len(nonRootPartitionPrefix) {
-			return nil, fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-		}
-		baseName = persistenceName[len(nonRootPartitionPrefix):suffixOff]
-		suffix := persistenceName[suffixOff+1:]
-		var err error
-		partitionId, versionSet, buildId, deploymentName, err = parseSuffix(persistenceName, suffix)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	f, err := tqid.NewTaskQueueFamily(namespaceId, baseName)
-	if err != nil {
-		return nil, err
-	}
-	return &PhysicalTaskQueueKey{
-		partition: f.TaskQueue(taskType).NormalPartition(partitionId),
-		version: PhysicalTaskQueueVersion{
-			versionSet:           versionSet,
-			buildId:              buildId,
-			deploymentSeriesName: deploymentName,
-		},
-	}, nil
-}
-
-//nolint:revive // cognitive complexity will be simpler once versioning 1-2 is cleaned up
-func parseSuffix(persistenceName string, suffix string) (partition int, versionSet string, buildId string, deploymentName string, err error) {
-	if buildIdOff := strings.LastIndex(suffix, deploymentNameDelimiter); buildIdOff == 0 {
-		return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-	} else if buildIdOff > 0 {
-		deploymentNameBytes, err := base64.RawURLEncoding.DecodeString(suffix[:buildIdOff])
-		if err != nil {
-			return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-		}
-		deploymentName = string(deploymentNameBytes)
-
-		suffix = suffix[buildIdOff+1:]
-		if partitionOff := strings.LastIndex(suffix, buildIdDelimiter); partitionOff == 0 {
-			return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-		} else if partitionOff > 0 {
-			buildIdBytes, err := base64.RawURLEncoding.DecodeString(suffix[:partitionOff])
-			if err != nil {
-				return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-			}
-			buildId = string(buildIdBytes)
-			suffix = suffix[partitionOff+1:]
-		}
-	} else if partitionOff := strings.LastIndex(suffix, buildIdDelimiter); partitionOff == 0 {
-		return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-	} else if partitionOff > 0 {
-		buildIdBytes, err := base64.URLEncoding.DecodeString(suffix[:partitionOff])
-		if err != nil {
-			return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-		}
-		buildId = string(buildIdBytes)
-		suffix = suffix[partitionOff+1:]
-	} else if partitionOff := strings.LastIndex(suffix, versionSetDelimiter); partitionOff == 0 {
-		return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-	} else if partitionOff > 0 {
-		// pull out version set
-		versionSet, suffix = suffix[:partitionOff], suffix[partitionOff+1:]
-	}
-
-	partition, err = strconv.Atoi(suffix)
-	if err != nil || partition < 0 || (partition == 0 && len(versionSet) == 0 && len(buildId) == 0) {
-		return 0, "", "", "", fmt.Errorf("%w: %s", ErrInvalidPersistenceName, persistenceName)
-	}
-	return partition, versionSet, buildId, deploymentName, err
 }
 
 func (q *PhysicalTaskQueueKey) IsVersioned() bool {
