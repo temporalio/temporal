00c10cf50ef0d358bd770b4138273a26df3d1081
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-02-20 12:33:46 -0800

    put drainage workflow in TemporalWorkerDeployment namespace division to hide it from default wf list

--- service/worker/workerdeployment/version_workflow.go
+++ service/worker/workerdeployment/version_workflow.go
@@ -26,8 +26,6 @@ package workerdeployment
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/pborman/uuid"
 	commonpb "go.temporal.io/api/common/v1"
 	deploymentpb "go.temporal.io/api/deployment/v1"
@@ -39,8 +37,10 @@ import (
 	"go.temporal.io/sdk/workflow"
 	deploymentspb "go.temporal.io/server/api/deployment/v1"
 	"go.temporal.io/server/common"
+	"go.temporal.io/server/common/searchattribute"
 	"go.temporal.io/server/common/worker_versioning"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
 )
 
 type (
@@ -234,13 +234,20 @@ func (d *VersionWorkflowRunner) handleUpdateVersionMetadata(ctx workflow.Context
 
 func (d *VersionWorkflowRunner) startDrainage(ctx workflow.Context, isCan bool) {
 	childCtx := workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{
-		ParentClosePolicy: enumspb.PARENT_CLOSE_POLICY_TERMINATE,
+		ParentClosePolicy:     enumspb.PARENT_CLOSE_POLICY_TERMINATE,
+		TypedSearchAttributes: d.buildSearchAttributes(),
 	})
 	fut := workflow.ExecuteChildWorkflow(childCtx, WorkerDeploymentDrainageWorkflowType, &deploymentspb.DrainageWorkflowArgs{
 		Version: d.VersionState.Version,
 		IsCan:   isCan,
 	})
 	d.drainageWorkflowFuture = &fut
+}
+
+func (d *VersionWorkflowRunner) buildSearchAttributes() temporal.SearchAttributes {
+	return temporal.NewSearchAttributes(
+		temporal.NewSearchAttributeKeyString(searchattribute.TemporalNamespaceDivision).ValueSet(WorkerDeploymentNamespaceDivision),
+	)
 }
 
 func (d *VersionWorkflowRunner) stopDrainage(ctx workflow.Context) error {
