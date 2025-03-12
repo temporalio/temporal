bc307c9aa12ebc6334ff1bdacf7a44461416c5c5
yux0 <yuhsia89@gmail.com>
2025-03-12 16:54:25 -0700

    add metric on page size

--- common/metrics/metric_defs.go
+++ common/metrics/metric_defs.go
@@ -928,6 +928,7 @@ var (
 	ReplicationTaskSendError              = NewCounterDef("replication_task_send_error")
 	ReplicationTaskGenerationLatency      = NewTimerDef("replication_task_generation_latency")
 	ReplicationTaskLoadLatency            = NewTimerDef("replication_task_load_latency")
+	ReplicationTaskLoadSize               = NewDimensionlessHistogramDef("replication_task_load_size")
 	ReplicationTaskSendLatency            = NewTimerDef("replication_task_send_latency")
 	ReplicationTasksRecv                  = NewCounterDef("replication_tasks_recv")
 	ReplicationTasksRecvBacklog           = NewDimensionlessHistogramDef("replication_tasks_recv_backlog")
