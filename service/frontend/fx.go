81f133fcf0c313c34ff9a5f20ff5a59c3d6b0a06
Nikolay Tretyak <ndtretyak@gmail.com>
2025-03-25 08:54:57 +0100

    Use OperatorHandler interface in fx graph

--- service/frontend/fx.go
+++ service/frontend/fx.go
@@ -143,7 +143,7 @@ func NewServiceProvider(
 	httpAPIServer *HTTPAPIServer,
 	handler Handler,
 	adminHandler *AdminHandler,
-	operatorHandler *OperatorHandlerImpl,
+	operatorHandler OperatorHandler,
 	versionChecker *VersionChecker,
 	visibilityMgr manager.VisibilityManager,
 	logger log.SnTaggedLogger,
@@ -684,7 +684,7 @@ func OperatorHandlerProvider(
 	clientFactory client.Factory,
 	namespaceRegistry namespace.Registry,
 	nexusEndpointClient *NexusEndpointClient,
-) *OperatorHandlerImpl {
+) OperatorHandler {
 	args := NewOperatorHandlerImplArgs{
 		configuration,
 		esClient,
@@ -843,7 +843,7 @@ func HTTPAPIServerProvider(
 	grpcListener net.Listener,
 	tlsConfigProvider encryption.TLSConfigProvider,
 	handler Handler,
-	operatorHandler *OperatorHandlerImpl,
+	operatorHandler OperatorHandler,
 	grpcServerOptions GrpcServerOptions,
 	metricsHandler metrics.Handler,
 	namespaceRegistry namespace.Registry,
