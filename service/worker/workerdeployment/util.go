b69a83d979eeceb9fcc146f6a5aa98705c518d03
Shivam Saraf <shivam.saraf@temporal.io>
2025-04-15 13:19:13 -0400

    re-word

--- service/worker/workerdeployment/util.go
+++ service/worker/workerdeployment/util.go
@@ -95,8 +95,8 @@ const (
 	errConflictTokenMismatchType  = "errConflictTokenMismatch"
 	errFailedPrecondition         = "FailedPrecondition"
 
-	ErrRampingVersionDoesNotHaveAllTaskQueues = "New ramping version does not have all the task queues from the previous current version and some missing task queues are active and would become unversioned after setting the new version as the ramping version"
-	ErrCurrentVersionDoesNotHaveAllTaskQueues = "New current version does not have all the task queues from the previous current version and some missing task queues are active and would become unversioned after setting the new version as the current version"
+	ErrRampingVersionDoesNotHaveAllTaskQueues = "New ramping version is missing active task queues from the current version; these would become unversioned after setting the new version as the ramping version"
+	ErrCurrentVersionDoesNotHaveAllTaskQueues = "New current version is missing active task queues from the previous current version; these would become unversioned after setting the new version as the current version"
 
 	syncBatchSize = 25
 )
