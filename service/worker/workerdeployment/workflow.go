95c22aa6c6819d5794489790e6df1fb6aa156fcb
Shivam <57200924+Shivs11@users.noreply.github.com>
2025-05-24 10:57:43 -0400

    fixing all occurrences of cryptic errors in the versioning entity wfs (#7779)
    ## What changed?
    - WISOTT
    - Replace individual error types that fall under `FailedPreCondition`
    and just check for these in the deployment client. This makes reading
    code in the deployment client simpler.
    - Reduce `Error` log spamming by the deployment client when it's
    actually a `PreconditionFailure`. The deployment client should only log
    `error` when something actually goes wrong in our internal entity
    workflows.
    
    ## Why?
    - So that users don't see any cryptic error messages while using this
    feature.
    
    ## How did you test it?
    - [x] built
    - [x] run locally and tested manually
    - [x] covered by existing tests
    - [ ] added new unit test(s)
    - [x] added new functional test(s)

--- service/worker/workerdeployment/workflow.go
+++ service/worker/workerdeployment/workflow.go
@@ -257,7 +257,7 @@ func (d *WorkflowRunner) addVersionToWorkerDeployment(ctx workflow.Context, args
 	if len(d.State.Versions) >= maxVersions {
 		err := d.tryDeleteVersion(ctx)
 		if err != nil {
-			return temporal.NewApplicationError(fmt.Sprintf("cannot add version, already at max versions %d", maxVersions), errTooManyVersions)
+			return temporal.NewApplicationError(fmt.Sprintf("cannot add version %s since maximum number of versions (%d) have been registered in the deployment", args.Version, maxVersions), errTooManyVersions)
 		}
 	}
 
@@ -301,7 +301,7 @@ func (d *WorkflowRunner) handleRegisterWorker(ctx workflow.Context, args *deploy
 		if errors.As(err, &appError) {
 			if appError.Type() == errMaxTaskQueuesInVersionType {
 				return temporal.NewApplicationError(
-					fmt.Sprintf("maximum number of task queues (%d) have been registered in deployment", args.MaxTaskQueues),
+					fmt.Sprintf("cannot add task queue %v since maximum number of task queues (%d) have been registered in deployment", args.TaskQueueName, args.MaxTaskQueues),
 					errMaxTaskQueuesInVersionType,
 				)
 			}
@@ -334,16 +334,16 @@ func (d *WorkflowRunner) validateStateBeforeAcceptingRampingUpdate(args *deploym
 	}
 
 	if args.ConflictToken != nil && !bytes.Equal(args.ConflictToken, d.State.ConflictToken) {
-		return temporal.NewApplicationError("conflict token mismatch", errConflictTokenMismatchType)
+		return temporal.NewApplicationError("conflict token mismatch", errFailedPrecondition)
 	}
 	if args.Version == d.State.RoutingConfig.CurrentVersion {
 		d.logger.Info("version can't be set to ramping since it is already current")
-		return temporal.NewApplicationError("version can't be set to ramping since it is already current", errVersionAlreadyCurrentType)
+		return temporal.NewApplicationError(fmt.Sprintf("requested ramping version %s is already current", args.Version), errFailedPrecondition)
 	}
 
 	if _, ok := d.State.Versions[args.Version]; !ok && args.Version != "" && args.Version != worker_versioning.UnversionedVersionId {
 		d.logger.Info("version not found in deployment")
-		return temporal.NewApplicationError("version not found in deployment", errVersionNotFound)
+		return temporal.NewApplicationError(fmt.Sprintf("requested ramping version %s not found in deployment", args.Version), errFailedPrecondition)
 	}
 
 	return nil
@@ -569,11 +569,11 @@ func (d *WorkflowRunner) validateStateBeforeAcceptingSetCurrent(args *deployment
 		return temporal.NewApplicationError("no change", errNoChangeType, d.State.ConflictToken)
 	}
 	if args.ConflictToken != nil && !bytes.Equal(args.ConflictToken, d.State.ConflictToken) {
-		return temporal.NewApplicationError("conflict token mismatch", errConflictTokenMismatchType)
+		return temporal.NewApplicationError("conflict token mismatch", errFailedPrecondition)
 	}
 	if _, ok := d.State.Versions[args.Version]; !ok && args.Version != worker_versioning.UnversionedVersionId {
 		d.logger.Info("version not found in deployment")
-		return temporal.NewApplicationError("version not found in deployment", errVersionNotFound)
+		return temporal.NewApplicationError(fmt.Sprintf("version %s not found in deployment", args.Version), errFailedPrecondition)
 	}
 	return nil
 }
