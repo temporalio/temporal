bccb1850be112d4f78de2cff9bf7729e32327e92
Alex Shtin <alex@shtin.com>
2025-05-29 22:12:05 -0700

    Fix unit tests

--- service/matching/matching_engine_test.go
+++ service/matching/matching_engine_test.go
@@ -535,7 +535,6 @@ func (s *matchingEngineSuite) TestPollWorkflowTaskQueues() {
 		BacklogCountHint:       0,
 		StickyExecutionEnabled: true,
 		Query:                  nil,
-		TransientWorkflowTask:  nil,
 		WorkflowExecutionTaskQueue: &taskqueuepb.TaskQueue{
 			Name: tl,
 			Kind: enumspb.TASK_QUEUE_KIND_NORMAL,
