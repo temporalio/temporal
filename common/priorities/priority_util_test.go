44acb3579860b7648fad2e5678619186c6c16f3b
David Reiss <david@temporal.io>
2025-08-04 10:55:43 -0700

    Handle fairness in priorities.Merge (#8143)
    ## What changed?
    Handle fairness fields in priorities.Merge
    
    ## How did you test it?
    - [x] added new unit test(s)

--- common/priorities/priority_util_test.go
+++ common/priorities/priority_util_test.go
@@ -46,6 +46,30 @@ func TestMerge(t *testing.T) {
 			override: defaultPriority,
 			expected: &commonpb.Priority{PriorityKey: 1},
 		},
+		{
+			name:     "fairness key is overriden",
+			base:     defaultPriority,
+			override: &commonpb.Priority{FairnessKey: "one"},
+			expected: &commonpb.Priority{FairnessKey: "one"},
+		},
+		{
+			name:     "fairness key is not overriden by default value",
+			base:     &commonpb.Priority{FairnessKey: "two"},
+			override: defaultPriority,
+			expected: &commonpb.Priority{FairnessKey: "two"},
+		},
+		{
+			name:     "fairness weight is overriden",
+			base:     defaultPriority,
+			override: &commonpb.Priority{FairnessWeight: 3.0},
+			expected: &commonpb.Priority{FairnessWeight: 3.0},
+		},
+		{
+			name:     "fairness weight is not overriden by default value",
+			base:     &commonpb.Priority{FairnessWeight: 3.0},
+			override: defaultPriority,
+			expected: &commonpb.Priority{FairnessWeight: 3.0},
+		},
 	}
 
 	for _, tc := range testcases {
