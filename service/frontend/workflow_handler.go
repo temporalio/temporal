temporalio/temporal

commit 356eecafdd3fcc57c8c68019c53a27ada803d31b

improvement: bypass middle-man-struct for batch operations (#8081)
## What changed?
Remove `BatchParams` to safely serialize proto -> json -> proto and add
a `BatchOperation` proto definition

## Why?
Remove custom serialization of proto for json safety.

## How did you test it?
- [ ] built
- [ ] run locally and tested manually
- [X] covered by existing tests
- [ ] added new unit test(s)
- [ ] added new functional test(s)

## Potential risks
Could break batching backwards compatibility but tests should catch this

service/frontend/workflow_handler.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/pborman/uuid"
+	"github.com/temporalio/sqlparser"
 	batchpb "go.temporal.io/api/batch/v1"
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
@@ -26,6 +27,7 @@ import (
 	workerpb "go.temporal.io/api/worker/v1"
 	workflowpb "go.temporal.io/api/workflow/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	batchspb "go.temporal.io/server/api/batch/v1"
 	deploymentspb "go.temporal.io/server/api/deployment/v1"
 	"go.temporal.io/server/api/historyservice/v1"
 	"go.temporal.io/server/api/matchingservice/v1"
@@ -4462,26 +4464,8 @@ func (wh *WorkflowHandler) StartBatchOperation(
 		return nil, errRequestNotSet
 	}
 
-	if len(request.GetJobId()) == 0 {
-		return nil, errBatchJobIDNotSet
-	}
-	if len(request.Namespace) == 0 {
-		return nil, errNamespaceNotSet
-	}
-	if len(request.VisibilityQuery) == 0 && len(request.Executions) == 0 {
-		return nil, errBatchOpsWorkflowFilterNotSet
-	}
-	if len(request.VisibilityQuery) != 0 && len(request.Executions) != 0 {
-		return nil, errBatchOpsWorkflowFiltersNotAllowed
-	}
-	if len(request.Executions) > wh.config.MaxExecutionCountBatchOperation(request.Namespace) {
-		return nil, errBatchOpsMaxWorkflowExecutionCount
-	}
-	if len(request.Reason) == 0 {
-		return nil, errReasonNotSet
-	}
-	if request.Operation == nil {
-		return nil, errBatchOperationNotSet
+	if err := batcher.ValidateBatchOperation(request); err != nil {
+		return nil, err
 	}
 
 	if !wh.config.EnableBatcher(request.Namespace) {
@@ -4513,216 +4497,67 @@ func (wh *WorkflowHandler) StartBatchOperation(
 	if err != nil {
 		return nil, err
 	}
+
+	input := &batchspb.BatchOperationInput{
+		Request:     request,
+		NamespaceId: namespaceID.String(),
+	}
+
 	var identity string
-	var operationType string
-	var signalParams batcher.SignalParams
-	var resetParams batcher.ResetParams
-	var updateOptionsParams batcher.UpdateOptionsParams
-	var unpauseActivitiesParams batcher.UnpauseActivitiesParams
-	var resetActivitiesParams batcher.ResetActivitiesParams
-	var updateActivitiesOptionsParams batcher.UpdateActivitiesOptionsParams
 	switch op := request.Operation.(type) {
 	case *workflowservice.StartBatchOperationRequest_TerminationOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_TERMINATE
 		identity = op.TerminationOperation.GetIdentity()
-		operationType = batcher.BatchTypeTerminate
 	case *workflowservice.StartBatchOperationRequest_SignalOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_SIGNAL
 		identity = op.SignalOperation.GetIdentity()
-		operationType = batcher.BatchTypeSignal
-		signalParams.SignalName = op.SignalOperation.GetSignal()
-		signalParams.Input = op.SignalOperation.GetInput()
 	case *workflowservice.StartBatchOperationRequest_CancellationOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_CANCEL
 		identity = op.CancellationOperation.GetIdentity()
-		operationType = batcher.BatchTypeCancel
 	case *workflowservice.StartBatchOperationRequest_DeletionOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_DELETE
 		identity = op.DeletionOperation.GetIdentity()
-		operationType = batcher.BatchTypeDelete
 	case *workflowservice.StartBatchOperationRequest_ResetOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_RESET
 		identity = op.ResetOperation.GetIdentity()
-		operationType = batcher.BatchTypeReset
-		if op.ResetOperation.Options != nil {
-			if op.ResetOperation.Options.Target == nil {
-				return nil, serviceerror.NewInvalidArgument("batch reset missing target")
-			}
-			encodedResetOptions, err := op.ResetOperation.Options.Marshal()
-			if err != nil {
-				return nil, err
-			}
-			resetParams.ResetOptions = encodedResetOptions
-			resetParams.PostResetOperations = make([][]byte, len(op.ResetOperation.PostResetOperations))
-			for i, postResetOperation := range op.ResetOperation.PostResetOperations {
-				encodedPostResetOperations, err := postResetOperation.Marshal()
-				if err != nil {
-					return nil, err
-				}
-				resetParams.PostResetOperations[i] = encodedPostResetOperations
-			}
-		} else {
-			// TODO: remove support for old fields later
-			resetType := op.ResetOperation.GetResetType()
-			if _, ok := enumspb.ResetType_name[int32(resetType)]; !ok || resetType == enumspb.RESET_TYPE_UNSPECIFIED {
-				return nil, serviceerror.NewInvalidArgumentf("unknown batch reset type %v", resetType)
-			}
-			resetParams.ResetType = resetType
-			resetParams.ResetReapplyType = op.ResetOperation.GetResetReapplyType()
-		}
 	case *workflowservice.StartBatchOperationRequest_UpdateWorkflowOptionsOperation:
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_UPDATE_EXECUTION_OPTIONS
 		identity = op.UpdateWorkflowOptionsOperation.GetIdentity()
-		operationType = batcher.BatchTypeUpdateOptions
-		updateOptionsParams.WorkflowExecutionOptions = op.UpdateWorkflowOptionsOperation.GetWorkflowExecutionOptions()
-		if updateMask := op.UpdateWorkflowOptionsOperation.GetUpdateMask(); updateMask != nil {
-			updateOptionsParams.UpdateMask = &batcher.FieldMask{Paths: updateMask.Paths}
-		}
-		// TODO(carlydf): remove hacky usage of deprecated fields later, after adding support for oneof in BatchParams encoder
-		if o := updateOptionsParams.WorkflowExecutionOptions.VersioningOverride; o.GetOverride() != nil {
-			deprecatedOverride := &workflowpb.VersioningOverride{}
-			if o.GetAutoUpgrade() {
-				deprecatedOverride.Behavior = enumspb.VERSIONING_BEHAVIOR_AUTO_UPGRADE //nolint:staticcheck // SA1019: worker versioning v0.31
-			} else if o.GetPinned().GetBehavior() == workflowpb.VersioningOverride_PINNED_OVERRIDE_BEHAVIOR_PINNED {
-				deprecatedOverride.Behavior = enumspb.VERSIONING_BEHAVIOR_PINNED                                                            //nolint:staticcheck // SA1019: worker versioning v0.31
-				deprecatedOverride.PinnedVersion = worker_versioning.ExternalWorkerDeploymentVersionToStringV31(o.GetPinned().GetVersion()) //nolint:staticcheck // SA1019: worker versioning v0.31
-			}
-			updateOptionsParams.WorkflowExecutionOptions.VersioningOverride = deprecatedOverride
-		}
 	case *workflowservice.StartBatchOperationRequest_UnpauseActivitiesOperation:
-		operationType = batcher.BatchTypeUnpauseActivities
-		if op.UnpauseActivitiesOperation == nil {
-			return nil, serviceerror.NewInvalidArgument("unpause activities operation is not set")
-		}
-		if op.UnpauseActivitiesOperation.GetActivity() == nil {
-			return nil, serviceerror.NewInvalidArgument("activity filter must be set")
-		}
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_UNPAUSE_ACTIVITY
+		identity = op.UnpauseActivitiesOperation.GetIdentity()
 
 		switch a := op.UnpauseActivitiesOperation.GetActivity().(type) {
 		case *batchpb.BatchOperationUnpauseActivities_Type:
-			if len(a.Type) == 0 {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
-			unpauseCause := fmt.Sprintf("%s = 'property:activityType=%s'", searchattribute.TemporalPauseInfo, a.Type)
-			visibilityQuery = fmt.Sprintf("(%s) AND (%s)", visibilityQuery, unpauseCause)
-			unpauseActivitiesParams.ActivityType = a.Type
+			searchValue := fmt.Sprintf("property:activityType=%s", a.Type)
+			escapedSearchValue := sqlparser.String(sqlparser.NewStrVal([]byte(searchValue)))
+			input.Request.VisibilityQuery = fmt.Sprintf("%s = %s", searchattribute.TemporalPauseInfo, escapedSearchValue)
 		case *batchpb.BatchOperationUnpauseActivities_MatchAll:
-			if !a.MatchAll {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
 			wildCardUnpause := fmt.Sprintf("%s STARTS_WITH 'property:activityType='", searchattribute.TemporalPauseInfo)
-			visibilityQuery = fmt.Sprintf("(%s) AND (%s)", visibilityQuery, wildCardUnpause)
-			unpauseActivitiesParams.MatchAll = true
+			input.Request.VisibilityQuery = fmt.Sprintf("(%s) AND (%s)", visibilityQuery, wildCardUnpause)
 		}
-
-		unpauseActivitiesParams.ResetAttempts = op.UnpauseActivitiesOperation.ResetAttempts
-		unpauseActivitiesParams.ResetHeartbeat = op.UnpauseActivitiesOperation.ResetHeartbeat
-		unpauseActivitiesParams.Jitter = op.UnpauseActivitiesOperation.Jitter.AsDuration()
-		unpauseActivitiesParams.Identity = op.UnpauseActivitiesOperation.GetIdentity()
 	case *workflowservice.StartBatchOperationRequest_ResetActivitiesOperation:
-		operationType = batcher.BatchTypeResetActivities
-		if op.ResetActivitiesOperation == nil {
-			return nil, serviceerror.NewInvalidArgument("reset activities operation is not set")
-		}
-		if op.ResetActivitiesOperation.GetActivity() == nil {
-			return nil, serviceerror.NewInvalidArgument("activity filter must be set")
-		}
-
-		switch a := op.ResetActivitiesOperation.GetActivity().(type) {
-		case *batchpb.BatchOperationResetActivities_Type:
-			if len(a.Type) == 0 {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
-			resetActivitiesParams.ActivityType = a.Type
-		case *batchpb.BatchOperationResetActivities_MatchAll:
-			if !a.MatchAll {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
-			resetActivitiesParams.MatchAll = true
-		}
-
-		resetActivitiesParams.ResetAttempts = op.ResetActivitiesOperation.ResetAttempts
-		resetActivitiesParams.ResetHeartbeat = op.ResetActivitiesOperation.ResetHeartbeat
-		resetActivitiesParams.Jitter = op.ResetActivitiesOperation.Jitter.AsDuration()
-		resetActivitiesParams.KeepPaused = op.ResetActivitiesOperation.KeepPaused
-		resetActivitiesParams.RestoreOriginalOptions = op.ResetActivitiesOperation.RestoreOriginalOptions
-		resetActivitiesParams.Identity = op.ResetActivitiesOperation.GetIdentity()
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_RESET_ACTIVITY
+		identity = op.ResetActivitiesOperation.GetIdentity()
 	case *workflowservice.StartBatchOperationRequest_UpdateActivityOptionsOperation:
-		operationType = batcher.BatchTypeUpdateActivitiesOptions
-		if op.UpdateActivityOptionsOperation == nil {
-			return nil, serviceerror.NewInvalidArgument("update activity options operation is not set")
-		}
-		if op.UpdateActivityOptionsOperation.GetActivityOptions() != nil && op.UpdateActivityOptionsOperation.GetRestoreOriginal() {
-			return nil, serviceerror.NewInvalidArgument("cannot set both activity options and restore original")
-		}
-		if op.UpdateActivityOptionsOperation.GetActivityOptions() == nil && !op.UpdateActivityOptionsOperation.GetRestoreOriginal() {
-			return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or restore original should be set to true")
-		}
-
-		switch a := op.UpdateActivityOptionsOperation.GetActivity().(type) {
-		case *batchpb.BatchOperationUpdateActivityOptions_Type:
-			if len(a.Type) == 0 {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
-			updateActivitiesOptionsParams.ActivityType = a.Type
-		case *batchpb.BatchOperationUpdateActivityOptions_MatchAll:
-			if !a.MatchAll {
-				return nil, serviceerror.NewInvalidArgument("Either activity type must be set, or match all should be set to true")
-			}
-			updateActivitiesOptionsParams.MatchAll = true
-		}
-
-		updateActivitiesOptionsParams.RestoreOriginal = op.UpdateActivityOptionsOperation.GetRestoreOriginal()
-		updateActivitiesOptionsParams.Identity = op.UpdateActivityOptionsOperation.GetIdentity()
-		if updateMask := op.UpdateActivityOptionsOperation.GetUpdateMask(); updateMask != nil {
-			updateActivitiesOptionsParams.UpdateMask = &batcher.FieldMask{Paths: updateMask.Paths}
-		}
-		if ao := op.UpdateActivityOptionsOperation.GetActivityOptions(); ao != nil {
-			updateActivitiesOptionsParams.ActivityOptions = &batcher.ActivityOptions{
-				ScheduleToStartTimeout: ao.ScheduleToStartTimeout.AsDuration(),
-				ScheduleToCloseTime:    ao.ScheduleToCloseTimeout.AsDuration(),
-				StartToCloseTimeout:    ao.StartToCloseTimeout.AsDuration(),
-				HeartbeatTimeout:       ao.HeartbeatTimeout.AsDuration(),
-			}
-			if rp := ao.RetryPolicy; rp != nil {
-				updateActivitiesOptionsParams.ActivityOptions.RetryPolicy = &batcher.RetryPolicy{
-					InitialInterval:        rp.InitialInterval.AsDuration(),
-					MaximumInterval:        rp.MaximumInterval.AsDuration(),
-					BackoffCoefficient:     rp.BackoffCoefficient,
-					NonRetryableErrorTypes: rp.NonRetryableErrorTypes,
-					MaximumAttempts:        rp.MaximumAttempts,
-				}
-			}
-			if tq := ao.TaskQueue; tq != nil {
-				updateActivitiesOptionsParams.ActivityOptions.TaskQueue = &batcher.TaskQueue{
-					Name: tq.Name,
-					Kind: int32(tq.Kind),
-				}
-			}
-		}
+		input.BatchType = enumspb.BATCH_OPERATION_TYPE_UPDATE_ACTIVITY_OPTIONS
+		identity = op.UpdateActivityOptionsOperation.GetIdentity()
 	default:
 		return nil, serviceerror.NewInvalidArgumentf("The operation type %T is not supported", op)
 	}
 
-	input := &batcher.BatchParams{
-		Namespace:                     request.GetNamespace(),
-		Query:                         visibilityQuery,
-		Executions:                    request.GetExecutions(),
-		Reason:                        request.GetReason(),
-		BatchType:                     operationType,
-		RPS:                           float64(request.GetMaxOperationsPerSecond()),
-		TerminateParams:               batcher.TerminateParams{},
-		CancelParams:                  batcher.CancelParams{},
-		SignalParams:                  signalParams,
-		DeleteParams:                  batcher.DeleteParams{},
-		ResetParams:                   resetParams,
-		UpdateOptionsParams:           updateOptionsParams,
-		UnpauseActivitiesParams:       unpauseActivitiesParams,
-		ResetActivitiesParams:         resetActivitiesParams,
-		UpdateActivitiesOptionsParams: updateActivitiesOptionsParams,
+	if err := batcher.ValidateBatchOperation(request); err != nil {
+		return nil, err
 	}
-	inputPayload, err := sdk.PreferProtoDataConverter.ToPayloads(input)
+
+	inputPayload, err := payloads.Encode(input)
 	if err != nil {
 		return nil, err
 	}
 
 	memo := &commonpb.Memo{
 		Fields: map[string]*commonpb.Payload{
-			batcher.BatchOperationTypeMemo: payload.EncodeString(operationType),
+			batcher.BatchOperationTypeMemo: payload.EncodeString(snakeCaseBatchType(input.BatchType)),
 			batcher.BatchReasonMemo:        payload.EncodeString(request.GetReason()),
 		},
 	}
@@ -4735,7 +4570,7 @@ func (wh *WorkflowHandler) StartBatchOperation(
 	startReq := &workflowservice.StartWorkflowExecutionRequest{
 		Namespace:                request.Namespace,
 		WorkflowId:               request.GetJobId(),
-		WorkflowType:             &commonpb.WorkflowType{Name: batcher.BatchWFTypeName},
+		WorkflowType:             &commonpb.WorkflowType{Name: batcher.BatchWFTypeProtobufName},
 		TaskQueue:                &taskqueuepb.TaskQueue{Name: primitives.PerNSWorkerTaskQueue},
 		Input:                    inputPayload,
 		Identity:                 identity,
@@ -4761,6 +4596,23 @@ func (wh *WorkflowHandler) StartBatchOperation(
 		return nil, err
 	}
 	return &workflowservice.StartBatchOperationResponse{}, nil
+}
+
+func snakeCaseBatchType(batchType enumspb.BatchOperationType) string {
+	switch batchType {
+	case enumspb.BATCH_OPERATION_TYPE_TERMINATE, enumspb.BATCH_OPERATION_TYPE_CANCEL, enumspb.BATCH_OPERATION_TYPE_SIGNAL, enumspb.BATCH_OPERATION_TYPE_DELETE, enumspb.BATCH_OPERATION_TYPE_RESET:
+		return strings.ToLower(batchType.String())
+	case enumspb.BATCH_OPERATION_TYPE_UPDATE_EXECUTION_OPTIONS:
+		return "update_options"
+	case enumspb.BATCH_OPERATION_TYPE_UNPAUSE_ACTIVITY:
+		return "unpause_activities"
+	case enumspb.BATCH_OPERATION_TYPE_UPDATE_ACTIVITY_OPTIONS:
+		return "update_activity_options"
+	case enumspb.BATCH_OPERATION_TYPE_RESET_ACTIVITY:
+		return "reset_activities"
+	default:
+		return ""
+	}
 }
 
 func (wh *WorkflowHandler) StopBatchOperation(
@@ -4965,7 +4817,7 @@ func (wh *WorkflowHandler) ListBatchOperations(
 		NextPageToken: request.GetNextPageToken(),
 		Query: fmt.Sprintf("%s = '%s' and %s = '%s'",
 			searchattribute.WorkflowType,
-			batcher.BatchWFTypeName,
+			batcher.BatchWFTypeProtobufName,
 			searchattribute.TemporalNamespaceDivision,
 			batcher.NamespaceDivision,
 		),

api/batch/v1/request_response.go-helpers.pb.go
@@ -1,0 +1,43 @@
+// Code generated by protoc-gen-go-helpers. DO NOT EDIT.
+package batch
+
+import (
+	"google.golang.org/protobuf/proto"
+)
+
+// Marshal an object of type BatchOperationInput to the protobuf v3 wire format
+func (val *BatchOperationInput) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type BatchOperationInput from the protobuf v3 wire format
+func (val *BatchOperationInput) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *BatchOperationInput) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two BatchOperationInput values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *BatchOperationInput) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *BatchOperationInput
+	switch t := that.(type) {
+	case *BatchOperationInput:
+		that1 = t
+	case BatchOperationInput:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}

api/batch/v1/request_response.pb.go
@@ -1,0 +1,188 @@
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// plugins:
+// 	protoc-gen-go
+// 	protoc
+// source: temporal/server/api/batch/v1/request_response.proto
+
+package batch
+
+import (
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
+
+	v1 "go.temporal.io/api/enums/v1"
+	v11 "go.temporal.io/api/workflowservice/v1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	durationpb "google.golang.org/protobuf/types/known/durationpb"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+type BatchOperationInput struct {
+	state                    protoimpl.MessageState `protogen:"open.v1"`
+	NamespaceId              string                 `protobuf:"bytes,1,opt,name=namespace_id,json=namespaceId,proto3" json:"namespace_id,omitempty"`
+	Concurrency              int64                  `protobuf:"varint,2,opt,name=concurrency,proto3" json:"concurrency,omitempty"`
+	AttemptsOnRetryableError int64                  `protobuf:"varint,3,opt,name=attempts_on_retryable_error,json=attemptsOnRetryableError,proto3" json:"attempts_on_retryable_error,omitempty"`
+	ActivityHeartbeatTimeout *durationpb.Duration   `protobuf:"bytes,4,opt,name=activity_heartbeat_timeout,json=activityHeartbeatTimeout,proto3" json:"activity_heartbeat_timeout,omitempty"`
+	NonRetryableErrors       []string               `protobuf:"bytes,5,rep,name=non_retryable_errors,json=nonRetryableErrors,proto3" json:"non_retryable_errors,omitempty"`
+	BatchType                v1.BatchOperationType  `protobuf:"varint,6,opt,name=batch_type,json=batchType,proto3,enum=temporal.api.enums.v1.BatchOperationType" json:"batch_type,omitempty"`
+	// The request to start the batch operation.
+	Request       *v11.StartBatchOperationRequest `protobuf:"bytes,7,opt,name=request,proto3" json:"request,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *BatchOperationInput) Reset() {
+	*x = BatchOperationInput{}
+	mi := &file_temporal_server_api_batch_v1_request_response_proto_msgTypes[0]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *BatchOperationInput) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*BatchOperationInput) ProtoMessage() {}
+
+func (x *BatchOperationInput) ProtoReflect() protoreflect.Message {
+	mi := &file_temporal_server_api_batch_v1_request_response_proto_msgTypes[0]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use BatchOperationInput.ProtoReflect.Descriptor instead.
+func (*BatchOperationInput) Descriptor() ([]byte, []int) {
+	return file_temporal_server_api_batch_v1_request_response_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *BatchOperationInput) GetNamespaceId() string {
+	if x != nil {
+		return x.NamespaceId
+	}
+	return ""
+}
+
+func (x *BatchOperationInput) GetConcurrency() int64 {
+	if x != nil {
+		return x.Concurrency
+	}
+	return 0
+}
+
+func (x *BatchOperationInput) GetAttemptsOnRetryableError() int64 {
+	if x != nil {
+		return x.AttemptsOnRetryableError
+	}
+	return 0
+}
+
+func (x *BatchOperationInput) GetActivityHeartbeatTimeout() *durationpb.Duration {
+	if x != nil {
+		return x.ActivityHeartbeatTimeout
+	}
+	return nil
+}
+
+func (x *BatchOperationInput) GetNonRetryableErrors() []string {
+	if x != nil {
+		return x.NonRetryableErrors
+	}
+	return nil
+}
+
+func (x *BatchOperationInput) GetBatchType() v1.BatchOperationType {
+	if x != nil {
+		return x.BatchType
+	}
+	return v1.BatchOperationType(0)
+}
+
+func (x *BatchOperationInput) GetRequest() *v11.StartBatchOperationRequest {
+	if x != nil {
+		return x.Request
+	}
+	return nil
+}
+
+var File_temporal_server_api_batch_v1_request_response_proto protoreflect.FileDescriptor
+
+const file_temporal_server_api_batch_v1_request_response_proto_rawDesc = "" +
+	"\n" +
+	"3temporal/server/api/batch/v1/request_response.proto\x12\x1ctemporal.server.api.batch.v1\x1a6temporal/api/workflowservice/v1/request_response.proto\x1a+temporal/api/enums/v1/batch_operation.proto\x1a\x1egoogle/protobuf/duration.proto\"\xc5\x03\n" +
+	"\x13BatchOperationInput\x12!\n" +
+	"\fnamespace_id\x18\x01 \x01(\tR\vnamespaceId\x12 \n" +
+	"\vconcurrency\x18\x02 \x01(\x03R\vconcurrency\x12=\n" +
+	"\x1battempts_on_retryable_error\x18\x03 \x01(\x03R\x18attemptsOnRetryableError\x12W\n" +
+	"\x1aactivity_heartbeat_timeout\x18\x04 \x01(\v2\x19.google.protobuf.DurationR\x18activityHeartbeatTimeout\x120\n" +
+	"\x14non_retryable_errors\x18\x05 \x03(\tR\x12nonRetryableErrors\x12H\n" +
+	"\n" +
+	"batch_type\x18\x06 \x01(\x0e2).temporal.api.enums.v1.BatchOperationTypeR\tbatchType\x12U\n" +
+	"\arequest\x18\a \x01(\v2;.temporal.api.workflowservice.v1.StartBatchOperationRequestR\arequestB*Z(go.temporal.io/server/api/batch/v1;batchb\x06proto3"
+
+var (
+	file_temporal_server_api_batch_v1_request_response_proto_rawDescOnce sync.Once
+	file_temporal_server_api_batch_v1_request_response_proto_rawDescData []byte
+)
+
+func file_temporal_server_api_batch_v1_request_response_proto_rawDescGZIP() []byte {
+	file_temporal_server_api_batch_v1_request_response_proto_rawDescOnce.Do(func() {
+		file_temporal_server_api_batch_v1_request_response_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_temporal_server_api_batch_v1_request_response_proto_rawDesc), len(file_temporal_server_api_batch_v1_request_response_proto_rawDesc)))
+	})
+	return file_temporal_server_api_batch_v1_request_response_proto_rawDescData
+}
+
+var file_temporal_server_api_batch_v1_request_response_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_temporal_server_api_batch_v1_request_response_proto_goTypes = []any{
+	(*BatchOperationInput)(nil),            // 0: temporal.server.api.batch.v1.BatchOperationInput
+	(*durationpb.Duration)(nil),            // 1: google.protobuf.Duration
+	(v1.BatchOperationType)(0),             // 2: temporal.api.enums.v1.BatchOperationType
+	(*v11.StartBatchOperationRequest)(nil), // 3: temporal.api.workflowservice.v1.StartBatchOperationRequest
+}
+var file_temporal_server_api_batch_v1_request_response_proto_depIdxs = []int32{
+	1, // 0: temporal.server.api.batch.v1.BatchOperationInput.activity_heartbeat_timeout:type_name -> google.protobuf.Duration
+	2, // 1: temporal.server.api.batch.v1.BatchOperationInput.batch_type:type_name -> temporal.api.enums.v1.BatchOperationType
+	3, // 2: temporal.server.api.batch.v1.BatchOperationInput.request:type_name -> temporal.api.workflowservice.v1.StartBatchOperationRequest
+	3, // [3:3] is the sub-list for method output_type
+	3, // [3:3] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
+}
+
+func init() { file_temporal_server_api_batch_v1_request_response_proto_init() }
+func file_temporal_server_api_batch_v1_request_response_proto_init() {
+	if File_temporal_server_api_batch_v1_request_response_proto != nil {
+		return
+	}
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_temporal_server_api_batch_v1_request_response_proto_rawDesc), len(file_temporal_server_api_batch_v1_request_response_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   1,
+			NumExtensions: 0,
+			NumServices:   0,
+		},
+		GoTypes:           file_temporal_server_api_batch_v1_request_response_proto_goTypes,
+		DependencyIndexes: file_temporal_server_api_batch_v1_request_response_proto_depIdxs,
+		MessageInfos:      file_temporal_server_api_batch_v1_request_response_proto_msgTypes,
+	}.Build()
+	File_temporal_server_api_batch_v1_request_response_proto = out.File
+	file_temporal_server_api_batch_v1_request_response_proto_goTypes = nil
+	file_temporal_server_api_batch_v1_request_response_proto_depIdxs = nil
+}

proto/internal/temporal/server/api/batch/v1/request_response.proto
@@ -1,0 +1,25 @@
+syntax = "proto3";
+
+package temporal.server.api.batch.v1;
+option go_package = "go.temporal.io/server/api/batch/v1;batch";
+
+import "temporal/api/workflowservice/v1/request_response.proto";
+import "temporal/api/enums/v1/batch_operation.proto";
+import "google/protobuf/duration.proto";
+
+message BatchOperationInput { 
+  string namespace_id = 1; 
+  
+  int64 concurrency = 2;
+
+  int64 attempts_on_retryable_error = 3;
+
+  google.protobuf.Duration activity_heartbeat_timeout = 4;
+
+  repeated string non_retryable_errors = 5;
+
+  temporal.api.enums.v1.BatchOperationType batch_type = 6;
+
+  // The request to start the batch operation.
+  temporal.api.workflowservice.v1.StartBatchOperationRequest request = 7;
+}

commit 1298b8060452c009ffc2443f3595f8b625d5e803

Fix ListBatchOperations list filter (#8160)
## What changed?

Change the backing query to avoid filtering on a specific workflow type
since we now have two types for the transition period.

## Why?

Fix for a bug introduced in #8081

service/frontend/workflow_handler.go
@@ -4824,9 +4824,7 @@ func (wh *WorkflowHandler) ListBatchOperations(
 		Namespace:     request.GetNamespace(),
 		PageSize:      request.PageSize,
 		NextPageToken: request.GetNextPageToken(),
-		Query: fmt.Sprintf("%s = '%s' and %s = '%s'",
-			searchattribute.WorkflowType,
-			batcher.BatchWFTypeProtobufName,
+		Query: fmt.Sprintf("%s = '%s'",
 			searchattribute.TemporalNamespaceDivision,
 			batcher.NamespaceDivision,
 		),