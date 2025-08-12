d3ba3a3985d9ba1c5fe8cf702f91a6adf45c8715
Chetan Gowda <chetan.gowda@temporal.io>
2025-08-11 19:10:17 -0700

    Add functional tests

--- tests/activity_api_pause_test.go
+++ tests/activity_api_pause_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -10,12 +11,15 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"github.com/temporalio/sqlparser"
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/sdk/activity"
 	sdkclient "go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
+	"go.temporal.io/server/common/searchattribute"
 	"go.temporal.io/server/common/testing/testvars"
 	"go.temporal.io/server/common/util"
 	"go.temporal.io/server/tests/testcore"
@@ -63,6 +67,34 @@ func (s *ActivityApiPauseClientTestSuite) makeWorkflowFunc(activityFunction Acti
 			RetryPolicy:            s.activityRetryPolicy,
 		}), activityFunction).Get(ctx, &ret)
 		return err
+	}
+}
+
+func (s *ActivityApiPauseClientTestSuite) makeTestWorkflowFuncMultiActivities(activityFunctions ...ActivityFunctions) WorkflowFunction {
+	return func(ctx workflow.Context) error {
+		var activityExecutions []workflow.Future
+		for i, activityFunction := range activityFunctions {
+			activityExecutions = append(activityExecutions, workflow.ExecuteActivity(workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+				ActivityID:             fmt.Sprintf("activity-id-%d", i),
+				DisableEagerExecution:  true,
+				StartToCloseTimeout:    s.startToCloseTimeout,
+				ScheduleToCloseTimeout: s.scheduleToCloseTimeout,
+				RetryPolicy:            s.activityRetryPolicy,
+			}), activityFunction))
+
+			if i < len(activityFunctions)-1 {
+				// wait for the signal from the test before proceeding to start another activity
+				workflow.GetSignalChannel(ctx, "UNBLOCK-WORKFLOW").Receive(ctx, nil)
+			}
+		}
+
+		for _, activityExecution := range activityExecutions {
+			err := activityExecution.Get(ctx, nil)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
 	}
 }
 
@@ -577,6 +609,138 @@ func (s *ActivityApiPauseClientTestSuite) TestActivityPauseApi_WithReset() {
 	activityCompleteCn <- struct{}{}
 
 	// wait for workflow to finish
+	var out string
+	err = workflowRun.Get(ctx, &out)
+
+	s.NoError(err)
+}
+
+func (s *ActivityApiPauseClientTestSuite) TestActivityPauseApi_ByType() {
+	// This tests the following scenario:
+	// 1. Start a workflow with 2 activities of the same type
+	// 2. First activity is started immediately and the second one is started after the workflow receives a signal to unblock it (simulating future activity)
+	// 3. Invoke pause activity API by type
+	// 4. Verify that the currently running activity is paused.
+	// 5. Unblock the workflow so that the second activity can start.
+	// 6. Verify that both activities are paused and the search attribute is updated.
+	// 7. Unpause the activity by type. Now all pending activities should be unpaused and the workflow should complete.
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	activityTypeName := "test-activity-type"
+	activityPausedCn := make(chan struct{})
+	var startedActivityCount atomic.Int32
+	activityErr := errors.New("test-activity-error")
+
+	activityFunction := func() (string, error) {
+		startedActivityCount.Add(1)
+		if startedActivityCount.Load() == 1 {
+			s.WaitForChannel(ctx, activityPausedCn)
+			return "", activityErr
+		}
+		return "done!", nil
+	}
+
+	// Create a workflow that starts 2 activities of the same type.
+	// The first one will be started immediately, and the second one will be started after the workflow receives a signal to unblock it.
+	workflowFn := s.makeTestWorkflowFuncMultiActivities(activityFunction, activityFunction)
+
+	s.Worker().RegisterWorkflow(workflowFn)
+	s.Worker().RegisterActivityWithOptions(activityFunction, activity.RegisterOptions{Name: activityTypeName})
+
+	workflowOptions := sdkclient.StartWorkflowOptions{
+		ID:        testcore.RandomizeStr("wf_id-" + s.T().Name()),
+		TaskQueue: s.TaskQueue(),
+	}
+
+	workflowRun, err := s.SdkClient().ExecuteWorkflow(ctx, workflowOptions, workflowFn)
+	s.NoError(err)
+
+	// wait for activity to start
+	s.EventuallyWithT(func(t *assert.CollectT) {
+		description, err := s.SdkClient().DescribeWorkflowExecution(ctx, workflowRun.GetID(), workflowRun.GetRunID())
+		require.NoError(t, err)
+		require.Len(t, description.PendingActivities, 1)
+		require.Equal(t, int32(1), startedActivityCount.Load())
+	}, 5*time.Second, 100*time.Millisecond)
+
+	// pause activity by type
+	// Note: this will pause all activities (current + future)of the same type in the workflow.
+	testIdentity := "test-identity"
+	testReason := "test-reason"
+	pauseRequest := &workflowservice.PauseActivityRequest{
+		Namespace: s.Namespace().String(),
+		Execution: &commonpb.WorkflowExecution{
+			WorkflowId: workflowRun.GetID(),
+		},
+		Activity: &workflowservice.PauseActivityRequest_Type{Type: activityTypeName},
+		Identity: testIdentity,
+		Reason:   testReason,
+	}
+	resp, err := s.FrontendClient().PauseActivity(ctx, pauseRequest)
+	s.NoError(err)
+	s.NotNil(resp)
+
+	// make sure activity is pause-requested on server while running on worker
+	s.EventuallyWithT(func(t *assert.CollectT) {
+		description, err := s.SdkClient().DescribeWorkflowExecution(ctx, workflowRun.GetID(), workflowRun.GetRunID())
+		require.NoError(t, err)
+		require.Len(t, description.PendingActivities, 1)
+		require.Equal(t, enumspb.PENDING_ACTIVITY_STATE_PAUSE_REQUESTED, description.PendingActivities[0].State)
+		require.Equal(t, int32(1), startedActivityCount.Load())
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// unblock the activity so that it returns an error giving a chance to actually pause on the server.
+	activityPausedCn <- struct{}{}
+
+	// make sure activity is paused on server now.
+	s.EventuallyWithT(func(t *assert.CollectT) {
+		description, err := s.SdkClient().DescribeWorkflowExecution(ctx, workflowRun.GetID(), workflowRun.GetRunID())
+		require.NoError(t, err)
+		require.Len(t, description.PendingActivities, 1)
+		require.Equal(t, enumspb.PENDING_ACTIVITY_STATE_PAUSED, description.PendingActivities[0].State)
+		require.Equal(t, int32(1), startedActivityCount.Load())
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// unblock the workflow so that it can start the second activity
+	err = s.SdkClient().SignalWorkflow(ctx, workflowRun.GetID(), workflowRun.GetRunID(), "UNBLOCK-WORKFLOW", nil)
+	s.NoError(err)
+
+	// make sure both activities are paused and the search attribute is updated
+	s.EventuallyWithT(func(t *assert.CollectT) {
+		description, err := s.SdkClient().DescribeWorkflowExecution(ctx, workflowRun.GetID(), workflowRun.GetRunID())
+		require.NoError(t, err)
+		require.Equal(t, 2, len(description.PendingActivities))
+		require.True(t, description.PendingActivities[0].Paused)
+		require.True(t, description.PendingActivities[1].Paused)
+
+		searchValue := fmt.Sprintf("property:activityType=%s", activityTypeName)
+		escapedSearchValue := sqlparser.String(sqlparser.NewStrVal([]byte(searchValue)))
+		pauseAttribute := fmt.Sprintf("%s = %s", searchattribute.TemporalPauseInfo, escapedSearchValue)
+		query := fmt.Sprintf("(WorkflowId='%s' AND %s)", workflowRun.GetID(), pauseAttribute)
+		visibilityResponse, err := s.FrontendClient().ListWorkflowExecutions(ctx, &workflowservice.ListWorkflowExecutionsRequest{
+			Namespace: s.Namespace().String(),
+			Query:     query,
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(visibilityResponse.Executions))
+	}, 5*time.Second, 500*time.Millisecond)
+
+	// unpause the activity by type. Now all pending activities should be unpaused and the workflow should complete.
+	unpauseRequest := &workflowservice.UnpauseActivityRequest{
+		Namespace: s.Namespace().String(),
+		Execution: &commonpb.WorkflowExecution{
+			WorkflowId: workflowRun.GetID(),
+		},
+		Activity: &workflowservice.UnpauseActivityRequest_Type{Type: activityTypeName},
+	}
+	unpauseResp, err := s.FrontendClient().UnpauseActivity(ctx, unpauseRequest)
+	s.NoError(err)
+	s.NotNil(unpauseResp)
+
+	// TODO: Verify that the pause info in the describe response is updated correctly.
+
 	var out string
 	err = workflowRun.Get(ctx, &out)
 
