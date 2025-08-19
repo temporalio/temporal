4d32a397c0d899416b0135c80fb724c1c3cb2442
Lina Jodoin <lina.jodoin@temporal.io>
2025-08-18 18:17:11 -0700

    chasm-port-hsm-components feedback 2

--- chasm/lib/scheduler/gen/schedulerpb/v1/tasks.go-helpers.pb.go
+++ chasm/lib/scheduler/gen/schedulerpb/v1/tasks.go-helpers.pb.go
@@ -5,6 +5,43 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// Marshal an object of type SchedulerIdleTask to the protobuf v3 wire format
+func (val *SchedulerIdleTask) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type SchedulerIdleTask from the protobuf v3 wire format
+func (val *SchedulerIdleTask) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *SchedulerIdleTask) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two SchedulerIdleTask values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *SchedulerIdleTask) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *SchedulerIdleTask
+	switch t := that.(type) {
+	case *SchedulerIdleTask:
+		that1 = t
+	case SchedulerIdleTask:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type GeneratorTask to the protobuf v3 wire format
 func (val *GeneratorTask) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
