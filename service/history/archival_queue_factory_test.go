678cf2f6ca406aab56fb8d036403f9d7418e4a1e
Prathyush PV <prathyush.pv@temporal.io>
2025-08-27 14:08:36 -0700

    Test

--- service/history/archival_queue_factory_test.go
+++ service/history/archival_queue_factory_test.go
@@ -1,8 +1,6 @@
 package history
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,12 +9,9 @@ import (
 	persistencespb "go.temporal.io/server/api/persistence/v1"
 	"go.temporal.io/server/common/clock"
 	"go.temporal.io/server/common/cluster"
-	"go.temporal.io/server/common/dynamicconfig"
 	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/primitives/timestamp"
-	"go.temporal.io/server/common/quotas"
-	"go.temporal.io/server/service/history/queues"
 	"go.temporal.io/server/service/history/shard"
 	"go.temporal.io/server/service/history/tasks"
 	"go.temporal.io/server/service/history/tests"
@@ -71,74 +66,4 @@ func TestArchivalQueueFactory(t *testing.T) {
 
 	require.NotNil(t, queue)
 	assert.Equal(t, tasks.CategoryArchival, queue.Category())
-}
-
-func TestArchivalQueueFactory_RateLimiter(t *testing.T) {
-	for _, enabled := range []bool{false, true} {
-		t.Run(fmt.Sprintf("enabled=%v", enabled), func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			cfg := tests.NewDynamicConfig()
-			if enabled {
-				cfg.TaskSchedulerEnableRateLimiter = dynamicconfig.GetBoolPropertyFn(true)
-			}
-
-			shardCtx := shard.NewTestContext(
-				ctrl,
-				&persistencespb.ShardInfo{
-					ShardId: 0,
-					RangeId: 1,
-					QueueStates: map[int32]*persistencespb.QueueState{
-						int32(tasks.CategoryIDArchival): {
-							ReaderStates: nil,
-							ExclusiveReaderHighWatermark: &persistencespb.TaskKey{
-								FireTime: timestamp.TimeNowPtrUtc(),
-							},
-						},
-					},
-				},
-				cfg,
-			)
-			shardCtx.Resource.ClusterMetadata.EXPECT().GetCurrentClusterName().Return(cluster.TestCurrentClusterName).AnyTimes()
-
-			rl := quotas.NewRequestRateLimiterAdapter(quotas.NewRateLimiter(1, 1))
-
-			factory := NewArchivalQueueFactory(ArchivalQueueFactoryParams{
-				QueueFactoryBaseParams: QueueFactoryBaseParams{
-					NamespaceRegistry:    shardCtx.Resource.GetNamespaceRegistry(),
-					ClusterMetadata:      shardCtx.Resource.GetClusterMetadata(),
-					Config:               cfg,
-					TimeSource:           clock.NewEventTimeSource(),
-					MetricsHandler:       metrics.NoopMetricsHandler,
-					Logger:               log.NewNoopLogger(),
-					TracerProvider:       noop.NewTracerProvider(),
-					SchedulerRateLimiter: rl,
-				},
-			})
-
-			q := factory.CreateQueue(shardCtx)
-			require.NotNil(t, q)
-
-			rateLimiter, shadow := getSchedulerInfo(q)
-			if enabled {
-				assert.NotEqual(t, quotas.NoopRequestRateLimiter, rateLimiter)
-				assert.Equal(t, cfg.TaskSchedulerEnableRateLimiterShadowMode(), shadow)
-			} else {
-				assert.Equal(t, quotas.NoopRequestRateLimiter, rateLimiter)
-				assert.False(t, shadow)
-			}
-		})
-	}
-}
-
-func getSchedulerInfo(q queues.Queue) (quotas.RequestRateLimiter, bool) {
-	v := reflect.ValueOf(q).Elem()
-	qb := v.FieldByName("queueBase")
-	schedulerVal := qb.Elem().FieldByName("scheduler")
-	rlScheduler := reflect.ValueOf(schedulerVal.Interface()).Elem()
-	tasksScheduler := reflect.ValueOf(rlScheduler.Field(0).Interface()).Elem()
-	rateLimiter := tasksScheduler.FieldByName("rateLimiter").Interface().(quotas.RequestRateLimiter)
-	enableShadow := tasksScheduler.FieldByName("options").FieldByName("EnableShadowMode").Bool()
-	return rateLimiter, enableShadow
 }
