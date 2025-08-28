a013e46540b3c3d350dbcad5e8d2425a757b33ce
Lina Jodoin <lina.jodoin@temporal.io>
2025-08-27 17:02:42 -0700

    chasm-generate-task feedback 3

--- chasm/lib/scheduler/scheduler_tasks.go
+++ chasm/lib/scheduler/scheduler_tasks.go
@@ -1,21 +1,32 @@
 package scheduler
 
 import (
+	"time"
+
 	"go.temporal.io/server/chasm"
-	schedulespb "go.temporal.io/server/chasm/lib/scheduler/gen/schedulerpb/v1"
+	"go.temporal.io/server/chasm/lib/scheduler/gen/schedulerpb/v1"
+	"go.uber.org/fx"
 )
 
-type SchedulerIdleTaskExecutor struct{}
+type SchedulerIdleTaskExecutorOptions struct {
+	fx.In
 
-func NewSchedulerIdleTaskExecutor() *SchedulerIdleTaskExecutor {
-	return &SchedulerIdleTaskExecutor{}
+	Config *Config
+}
+
+type SchedulerIdleTaskExecutor struct {
+	SchedulerIdleTaskExecutorOptions
+}
+
+func NewSchedulerIdleTaskExecutor(opts SchedulerIdleTaskExecutorOptions) *SchedulerIdleTaskExecutor {
+	return &SchedulerIdleTaskExecutor{opts}
 }
 
 func (r *SchedulerIdleTaskExecutor) Execute(
 	ctx chasm.MutableContext,
 	scheduler *Scheduler,
 	_ chasm.TaskAttributes,
-	_ *schedulespb.SchedulerIdleTask,
+	_ *schedulerpb.SchedulerIdleTask,
 ) error {
 	scheduler.Closed = true
 	return nil
@@ -24,8 +35,17 @@ func (r *SchedulerIdleTaskExecutor) Execute(
 func (r *SchedulerIdleTaskExecutor) Validate(
 	ctx chasm.Context,
 	scheduler *Scheduler,
-	_ chasm.TaskAttributes,
-	_ *schedulespb.SchedulerIdleTask,
+	task chasm.TaskAttributes,
+	_ *schedulerpb.SchedulerIdleTask,
 ) (bool, error) {
+	idleTimeTotal := r.Config.Tweakables(scheduler.Namespace).IdleTime
+	idleExpiration, isIdle := scheduler.getIdleExpiration(ctx, idleTimeTotal, time.Time{})
+
+	// If the scheduler has since woken up, or its idle expiration time changed, this
+	// task must be obsolete.
+	if !isIdle || idleExpiration.Compare(task.ScheduledTime) != 0 {
+		return false, nil
+	}
+
 	return !scheduler.Closed, nil
 }
