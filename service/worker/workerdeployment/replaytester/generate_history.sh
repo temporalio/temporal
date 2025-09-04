d3ebb09b4a30c958104e38860a94bd693076f0cb
Carly de Frondeville <carly.defrondeville@temporal.io>
2025-09-04 16:47:34 -0700

    allow nil/unversioned ramping version if current version is already nil/unversioned and ramp percentage is 0 (#8172)
    ## What changed?
    allow nil/unversioned ramping version if current version is already
    nil/unversioned and ramp percentage is 0
    
    ## Why?
    So that people can revert a task queue back to unversioned state without
    needing to set a 0% ramped non-nil Ramping Version.
    
    ## How did you test it?
    - [ ] built
    - [ ] run locally and tested manually
    - [ ] covered by existing tests
    - [ ] added new unit test(s)
    - [ ] added new functional test(s)

--- service/worker/workerdeployment/replaytester/generate_history.sh
+++ service/worker/workerdeployment/replaytester/generate_history.sh
@@ -33,7 +33,13 @@ temporal operator namespace create default
 
 # Run the worker which shall start the deployment entity workflows....
 echo "Running the Go program..."
-go run "$(dirname "$0")/worker/worker.go"
+
+if ! go run "$(dirname "$0")/worker/worker.go"; then
+  echo "Go program exited with an error. Exiting bash script." >&2
+  exit 1
+fi
+
+echo "Go program completed successfully."
 
 echo "Waiting 5 seconds for all workflows to show up in visibility..."
 sleep 5
