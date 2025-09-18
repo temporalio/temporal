a2400dec65f4474e834418c622f470f23936098b
Lina Jodoin <lina.jodoin@temporal.io>
2025-09-17 17:00:55 -0700

    Also record input payload, and record on schedule creation

--- service/worker/scheduler/workflow.go
+++ service/worker/scheduler/workflow.go
@@ -253,6 +253,8 @@ func (s *scheduler) run() error {
 		s.State.LastProcessedTime = timestamppb.New(s.now())
 		s.State.ConflictToken = InitialConflictToken
 		s.Info.CreateTime = s.State.LastProcessedTime
+
+		s.recordActionPayloadMetrics()
 	}
 
 	// A schedule may be created with an initial Patch, e.g. start one immediately. Put that in
@@ -877,6 +879,31 @@ func (s *scheduler) processWatcherResult(id string, f workflow.Future, long bool
 	s.logger.Debug("started workflow finished", "workflow", id, "status", res.Status, "pause-after-failure", pauseOnFailure, "long", long)
 }
 
+// recordPayloadMetrics should be called after the customer's action is updated
+// (or when the schedule is created) to record the size of their payloads.
+//
+// Payloads for workflow completions and failures are recorded as part of the
+// same metric.
+func (s *scheduler) recordActionPayloadMetrics() {
+	startWf := s.Schedule.Action.GetStartWorkflow()
+	if startWf == nil {
+		return
+	}
+
+	inputPayload, err := startWf.Input.Marshal()
+	if err != nil {
+		return
+	}
+
+	memoPayload, err := startWf.Memo.Marshal()
+	if err != nil {
+		return
+	}
+
+	payloadSize := int64(len(memoPayload) + len(inputPayload))
+	s.metrics.Counter(metrics.SchedulePayloadSize.Name()).Inc(payloadSize)
+}
+
 func (s *scheduler) processUpdate(req *schedulespb.FullUpdateRequest) {
 	if err := s.checkConflict(req.ConflictToken); err != nil {
 		s.logger.Warn("Update conflicted with concurrent change")
@@ -897,12 +924,7 @@ func (s *scheduler) processUpdate(req *schedulespb.FullUpdateRequest) {
 	s.updateCustomSearchAttributes(req.SearchAttributes)
 
 	// Record customer start workflow memo payload size on each update.
-	startWf := s.Schedule.Action.GetStartWorkflow()
-	if startWf != nil {
-		if memoPayload, err := startWf.Memo.Marshal(); err == nil {
-			s.metrics.Counter(metrics.SchedulePayloadSize.Name()).Inc(int64(len(memoPayload)))
-		}
-	}
+	s.recordActionPayloadMetrics()
 
 	if s.hasMinVersion(UpdateFromPrevious) && !s.hasMinVersion(UseLastAction) {
 		// We need to start re-processing from the last event, so that we catch actions whose
