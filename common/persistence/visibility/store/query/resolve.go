f3cbd45319322bf9cac0632bdabd64632e02e413
Sean Kane <sean.kane@temporal.io>
2025-09-29 13:27:18 -0600

    making code more readable

--- common/persistence/visibility/store/query/resolve.go
+++ common/persistence/visibility/store/query/resolve.go
@@ -17,58 +17,85 @@ func ResolveSearchAttributeAlias(
 	mapper searchattribute.Mapper,
 	saTypeMap searchattribute.NameTypeMap,
 ) (string, enumspb.IndexedValueType, error) {
-	// 1. Skip mapping to custom search attribute if the name has "Temporal" prefix
 	if strings.HasPrefix(name, "Temporal") {
-		// Check if it's a system/predefined search attribute with Temporal prefix
-		if saType, err := saTypeMap.GetType(name); err == nil {
-			return name, saType, nil
+		saType, err := saTypeMap.GetType(name)
+		if err != nil {
+			return "", enumspb.INDEXED_VALUE_TYPE_UNSPECIFIED, NewConverterError("invalid search attribute: %s", name)
 		}
-		return "", enumspb.INDEXED_VALUE_TYPE_UNSPECIFIED, NewConverterError("invalid search attribute: %s", name)
-	}
-
-	// 2. If the search attribute exists in the visibility mapper, pass it through
-	if searchattribute.IsMappable(name) {
-		if mapper != nil {
-			fieldName, err := mapper.GetFieldName(name, ns.String())
-			if err == nil {
-				fieldType, err := saTypeMap.GetType(fieldName)
-				if err == nil {
-					return fieldName, fieldType, nil
-				}
-			} else {
-				var invalidArgument *serviceerror.InvalidArgument
-				if errors.As(err, &invalidArgument) {
-					if name == searchattribute.ScheduleID {
-						saType, err := saTypeMap.GetType(searchattribute.WorkflowID)
-						if err == nil {
-							return searchattribute.WorkflowID, saType, nil
-						}
-					}
-
-					return "", enumspb.INDEXED_VALUE_TYPE_UNSPECIFIED, err
-				}
-			}
-		}
-	}
-
-	// 3. If it exists as a system or pre-defined attribute, map it
-	if saType, err := saTypeMap.GetType(name); err == nil {
 		return name, saType, nil
 	}
-	prefixedName := fmt.Sprintf("Temporal%s", name)
-	if saType, err := saTypeMap.GetType(prefixedName); err == nil {
-		return prefixedName, saType, nil
-	}
 
-	// 4. Handle special cases (maintain ScheduleID -> WorkflowID mapping)
-	if name == searchattribute.ScheduleID {
-		saType, err := saTypeMap.GetType(searchattribute.WorkflowID)
-		if err == nil {
-			return searchattribute.WorkflowID, saType, nil
+	if searchattribute.IsMappable(name) {
+		if result, found := tryVisibilityMapper(name, ns, mapper, saTypeMap); found {
+			return result.fieldName, result.fieldType, result.err
 		}
 	}
 
-	// 5. In the future we will need to lookup in the CHASM archetype search attribute mapping.
-	// For now, return error if not found
+	if result, found := tryDirectAndPrefixedLookup(name, saTypeMap); found {
+		return result.fieldName, result.fieldType, result.err
+	}
+
+	if name == searchattribute.ScheduleID {
+		saType, err := saTypeMap.GetType(searchattribute.WorkflowID)
+		if err != nil {
+			return "", enumspb.INDEXED_VALUE_TYPE_UNSPECIFIED, NewConverterError("invalid search attribute: %s", searchattribute.ScheduleID)
+		}
+		return searchattribute.WorkflowID, saType, nil
+	}
+
 	return "", enumspb.INDEXED_VALUE_TYPE_UNSPECIFIED, NewConverterError("invalid search attribute: %s", name)
+}
+
+type resolveResult struct {
+	fieldName string
+	fieldType enumspb.IndexedValueType
+	err       error
+}
+
+func tryVisibilityMapper(name string, ns namespace.Name, mapper searchattribute.Mapper, saTypeMap searchattribute.NameTypeMap) (resolveResult, bool) {
+	if mapper == nil {
+		return resolveResult{}, false
+	}
+
+	fieldName, err := mapper.GetFieldName(name, ns.String())
+	if err == nil {
+		fieldType, err := saTypeMap.GetType(fieldName)
+		if err == nil {
+			return resolveResult{fieldName: fieldName, fieldType: fieldType}, true
+		}
+		return resolveResult{}, false
+	}
+
+	// Check if this is a mapper error that should be returned vs ignored
+	// Based on the TestMapper comments, "mapper error" should be returned,
+	// but "unmapped alias" and "invalid alias" should be ignored
+	var internalErr serviceerror.Internal
+	if errors.Is(err, &internalErr) {
+		return resolveResult{err: err}, true
+	}
+
+	// For other mapper errors (like "unmapped alias", "invalid alias"), ignore and continue
+	// Special case: ScheduleID mapping
+	var invalidArgument *serviceerror.InvalidArgument
+	if errors.As(err, &invalidArgument) && name == searchattribute.ScheduleID {
+		saType, err := saTypeMap.GetType(searchattribute.WorkflowID)
+		if err == nil {
+			return resolveResult{fieldName: searchattribute.WorkflowID, fieldType: saType}, true
+		}
+	}
+
+	return resolveResult{}, false
+}
+
+func tryDirectAndPrefixedLookup(name string, saTypeMap searchattribute.NameTypeMap) (resolveResult, bool) {
+	if saType, err := saTypeMap.GetType(name); err == nil {
+		return resolveResult{fieldName: name, fieldType: saType}, true
+	}
+
+	prefixedName := fmt.Sprintf("Temporal%s", name)
+	if saType, err := saTypeMap.GetType(prefixedName); err == nil {
+		return resolveResult{fieldName: prefixedName, fieldType: saType}, true
+	}
+
+	return resolveResult{}, false
 }
