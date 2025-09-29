1a83dd1aa674adb91572c65427afdaf2e774d581
Roey Berman <roey@temporal.io>
2025-09-29 13:30:10 -0700

    Update components/callbacks/request.go

--- components/callbacks/request.go
+++ components/callbacks/request.go
@@ -18,14 +18,14 @@ func routeRequest(
 	r *http.Request,
 	clusterMetadata cluster.Metadata,
 	httpClientCache *cluster.FrontendHTTPClientCache,
-	defaultCilent *http.Client,
+	defaultClient *http.Client,
 	localClient *common.FrontendHTTPClient,
 	logger log.Logger,
 ) (*http.Response, error) {
 	// This source header is populated in nexusoperations/executors (via the ClientProvider) for worker targets
 	// if this header is not populated then we assume it's and external target.
 	if r.Header == nil || r.Header.Get(callbackSourceHeader) == "" {
-		return defaultCilent.Do(r)
+		return defaultClient.Do(r)
 	}
 	// If we got here, we assume that the endpoint in the original call was a worker target, and we should route
 	// internally, either to a local frontend, or one of the other connected clusters' frontends.
@@ -33,17 +33,21 @@ func routeRequest(
 	callbackSource := r.Header.Get(callbackSourceHeader)
 	for clusterName, clusterInfo := range clusterMetadata.GetAllClusterInfo() {
 		if callbackSource == clusterInfo.ClusterID {
-			fec, err := httpClientCache.Get(clusterName)
-			if err != nil {
-				logger.Warn(
-					"HTTPCallerProvider unable to get FrontendHTTPClient for callback target cluster. Using local HTTP Client.",
-					tag.SourceCluster(clusterMetadata.GetCurrentClusterName()),
-					tag.TargetCluster(clusterName),
-					tag.Error(err),
-				)
+			if clusterMetadata.GetCurrentClusterName() == clusterName {
 				frontendClient = localClient
 			} else {
-				frontendClient = fec
+				fec, err := httpClientCache.Get(clusterName)
+				if err != nil {
+					logger.Warn(
+						"HTTPCallerProvider unable to get FrontendHTTPClient for callback target cluster. Using local HTTP Client.",
+						tag.SourceCluster(clusterMetadata.GetCurrentClusterName()),
+						tag.TargetCluster(clusterName),
+						tag.Error(err),
+					)
+					frontendClient = localClient
+				} else {
+					frontendClient = fec
+				}
 			}
 			break
 		}
