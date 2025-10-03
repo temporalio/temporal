8ee51cc42e496936c02451cb9c8f001e918a58a3
Fred Tzeng <fred.tzeng@temporal.io>
2025-10-02 20:36:07 -0700

    Remove outdated comment.

--- chasm/interceptors.go
+++ chasm/interceptors.go
@@ -27,11 +27,6 @@ func (i *ChasmRequestInterceptor) Intercept(
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler,
 ) (resp interface{}, retError error) {
-	// TODO There is work to have existing RPC calls utilize this interceptor so we can remove this check.
-	// There's also additional boiler plate code we can reduce, but needs thinking:
-	// 1. Need to await the engine ready, which ultimately should await handler startWG.
-	// 2. How to best handle other actions like getting namespace, shard ctx, etc. since req is of any type. See if
-	// need a base message proto and cast it first to extract boiler plate.
 	if strings.HasPrefix(info.FullMethod, chasmRequestPrefix) {
 		defer metrics.CapturePanic(i.logger, i.metricsHandler, &retError)
 
