e9151b1e11444f3c870e164d9ec56a6b80487668
Rob Holland <rob@temporal.io>
2025-10-03 10:53:31 +0100

    Remove out of band failover history update.

--- common/namespace/nsreplication/replication_task_executor.go
+++ common/namespace/nsreplication/replication_task_executor.go
@@ -296,9 +296,6 @@ func (h *taskExecutorImpl) handleNamespaceUpdateReplicationTask(
 		request.Namespace.FailoverVersion = task.GetFailoverVersion()
 		request.Namespace.FailoverNotificationVersion = notificationVersion
 		request.Namespace.ReplicationConfig.FailoverHistory = ConvertFailoverHistoryToPersistenceProto(task.GetFailoverHistory())
-	} else if len(resp.Namespace.ReplicationConfig.FailoverHistory) == 0 && len(task.GetFailoverHistory()) > 0 {
-		recordUpdated = true
-		request.Namespace.ReplicationConfig.FailoverHistory = ConvertFailoverHistoryToPersistenceProto(task.GetFailoverHistory())
 	}
 
 	if !recordUpdated {
