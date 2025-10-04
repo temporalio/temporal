65cf450b4e72fc511dd05c19b9d2285b1e9df84f
Stephan Behnke <stephan.behnke@temporal.io>
2025-10-03 17:34:20 -0700

    use fx serialization.NewSerializer here too

--- service/matching/matching_engine.go
+++ service/matching/matching_engine.go
@@ -215,6 +215,7 @@ func NewEngine(
 	saProvider searchattribute.Provider,
 	saMapperProvider searchattribute.MapperProvider,
 	rateLimiter TaskDispatchRateLimiter,
+	historySerializer serialization.Serializer,
 ) Engine {
 	scopedMetricsHandler := metricsHandler.WithTags(metrics.OperationTag(metrics.MatchingEngineScope))
 	e := &matchingEngineImpl{
@@ -226,7 +227,7 @@ func NewEngine(
 		deploymentStoreClient:         deploymentStoreClient,
 		tokenSerializer:               tasktoken.NewSerializer(),
 		workerDeploymentClient:        workerDeploymentClient,
-		historySerializer:             serialization.NewSerializer(),
+		historySerializer:             historySerializer,
 		logger:                        log.With(logger, tag.ComponentMatchingEngine),
 		throttledLogger:               log.With(throttledLogger, tag.ComponentMatchingEngine),
 		namespaceRegistry:             namespaceRegistry,
