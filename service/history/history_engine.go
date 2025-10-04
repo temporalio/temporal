b12ad58495ec128a058349b3f9944968dafcc0c2
Stephan Behnke <stephan.behnke@temporal.io>
2025-10-03 17:46:36 -0700

    only allocate task serializer once

--- service/history/history_engine.go
+++ service/history/history_engine.go
@@ -129,6 +129,7 @@ type (
 		searchAttributesValidator  *searchattribute.Validator
 		workflowDeleteManager      deletemanager.DeleteManager
 		eventSerializer            serialization.Serializer
+		taskSerializer             serialization.TaskSerializer
 		workflowConsistencyChecker api.WorkflowConsistencyChecker
 		versionChecker             headers.VersionChecker
 		tracer                     trace.Tracer
@@ -154,6 +155,7 @@ func NewEngineWithShardContext(
 	workflowCache wcache.Cache,
 	replicationProgressCache replication.ProgressCache,
 	eventSerializer serialization.Serializer,
+	taskSerializer serialization.TaskSerializer,
 	queueProcessorFactories []QueueFactory,
 	replicationTaskFetcherFactory replication.TaskFetcherFactory,
 	replicationTaskExecutorProvider replication.TaskExecutorProvider,
@@ -206,6 +208,7 @@ func NewEngineWithShardContext(
 		persistenceVisibilityMgr:   persistenceVisibilityMgr,
 		workflowDeleteManager:      workflowDeleteManager,
 		eventSerializer:            eventSerializer,
+		taskSerializer:             taskSerializer,
 		workflowConsistencyChecker: workflowConsistencyChecker,
 		versionChecker:             headers.NewDefaultVersionChecker(),
 		tracer:                     tracerProvider.Tracer(consts.LibraryName),
@@ -1024,11 +1027,10 @@ func (e *historyEngineImpl) AddTasks(
 	ctx context.Context,
 	request *historyservice.AddTasksRequest,
 ) (_ *historyservice.AddTasksResponse, retError error) {
-	taskSerializer := serialization.NewTaskSerializer(e.eventSerializer)
 	return addtasks.Invoke(
 		ctx,
 		e.shardContext,
-		taskSerializer,
+		e.taskSerializer,
 		int(e.config.NumberOfShards),
 		request,
 		e.taskCategoryRegistry,
