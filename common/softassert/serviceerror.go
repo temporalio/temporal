2bcaba1aafd423c407ada3bcba3ff5fa2af25292
Stephan Behnke <stephanos@users.noreply.github.com>
2025-10-07 11:30:39 -0700

    softassert serviceerror.Internal helper (#8388)
    ## What changed?
    
    Added a new helper `UnexpectedInternalErr` to combine logging and error
    construction in one go.
    
    ## Why?
    
    Observed use case in CHASM code and this collapses two separate
    statements (softassert and serviceerror.NewInternal) into one - which
    reduces friction.
    
    ## How did you test it?
    - [ ] built
    - [ ] run locally and tested manually
    - [x] covered by existing tests
    - [ ] added new unit test(s)
    - [ ] added new functional test(s)

--- common/softassert/serviceerror.go
+++ common/softassert/serviceerror.go
@@ -13,6 +13,27 @@ const (
 	errorDetailsTagName = "softassert-error-details"
 )
 
+// UnexpectedInternalErr creates a serviceerror.Internal from a static message and error details.
+// The error message follows the format "<staticMessage>: <optionalErr>", if optionalErr is not nil.
+//
+// It also logs `staticMessage` using softassert.Fail.
+// `optionalErr` is added as a tag with key "softassert-error-details".
+func UnexpectedInternalErr(logger log.Logger, staticMessage string, optionalErr error, tags ...tag.Tag) error {
+	// (1) trigger softassert
+	combinedTags := tags
+	if optionalErr != nil {
+		combinedTags = append([]tag.Tag{tag.NewErrorTag(errorDetailsTagName, optionalErr)}, tags...)
+	}
+	Fail(logger, staticMessage, combinedTags...)
+
+	// (2) return serviceerror
+	message := staticMessage
+	if optionalErr != nil {
+		message = fmt.Sprintf("%s: %s", staticMessage, optionalErr)
+	}
+	return serviceerror.NewInternal(message)
+}
+
 // UnexpectedDataLoss creates a serviceerror.DataLoss from a static message and optional error.
 // The error message follows the format "<staticMessage>: <optionalErr>", if optionalErr is not nil.
 //
