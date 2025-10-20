d6b651f256710c3e990224795199a0e826fd2df5
Sean Kane <sean.kane@temporal.io>
2025-10-20 17:12:05 -0400

    batch: register proto and non-proto workflow workflows

--- service/worker/batcher/fx.go
+++ service/worker/batcher/fx.go
@@ -67,7 +67,7 @@ func (s *workerComponent) DedicatedWorkerOptions(ns *namespace.Namespace) *worke
 }
 
 func (s *workerComponent) Register(registry sdkworker.Registry, ns *namespace.Namespace, _ workercommon.RegistrationDetails) func() {
-	registry.RegisterWorkflowWithOptions(BatchWorkflow, workflow.RegisterOptions{Name: BatchWFTypeName})
+	registry.RegisterWorkflowWithOptions(BatchWorkflowProtobuf, workflow.RegisterOptions{Name: BatchWFTypeName})
 	// Newer version of the batch workflow which was rewritten to accept a proto struct as input.
 	registry.RegisterWorkflowWithOptions(BatchWorkflowProtobuf, workflow.RegisterOptions{Name: BatchWFTypeProtobufName})
 	registry.RegisterActivity(s.activities(ns.Name(), ns.ID()))
