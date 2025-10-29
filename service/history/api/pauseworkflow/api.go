aac81808e619e518d48678f939520d10485a02d7
Chetan Gowda <chetan.gowda@temporal.io>
2025-10-28 18:49:53 -0700

    Added pauseworkflow history api package

---
+++ service/history/api/pauseworkflow/api.go
@@ -0,0 +1,90 @@
+package pauseworkflow
+
+import (
+	"context"
+
+	"go.temporal.io/api/serviceerror"
+	"go.temporal.io/server/api/historyservice/v1"
+	"go.temporal.io/server/common/definition"
+	"go.temporal.io/server/common/namespace"
+	"go.temporal.io/server/service/history/api"
+	"go.temporal.io/server/service/history/consts"
+	historyi "go.temporal.io/server/service/history/interfaces"
+)
+
+func Invoke(
+	ctx context.Context,
+	req *historyservice.PauseWorkflowExecutionRequest,
+	shard historyi.ShardContext,
+	workflowConsistencyChecker api.WorkflowConsistencyChecker,
+) (resp *historyservice.PauseWorkflowExecutionResponse, retError error) {
+	namespaceEntry, err := api.GetActiveNamespace(shard, namespace.ID(req.GetNamespaceId()))
+	if err != nil {
+		return nil, err
+	}
+	namespaceID := namespaceEntry.ID()
+
+	pauseRequest := req.GetPauseRequest()
+	if pauseRequest == nil {
+		return nil, serviceerror.NewInvalidArgument("pause request is nil.")
+	}
+
+	if !shard.GetConfig().WorkflowPauseEnabled(namespaceID.String()) {
+		return nil, serviceerror.NewUnimplementedf("workflow pause is not enabled for namespace: %s", namespaceID.String())
+	}
+
+	err = api.GetAndUpdateWorkflowWithNew(
+		ctx,
+		nil,
+		definition.NewWorkflowKey(
+			namespaceID.String(),
+			pauseRequest.GetWorkflowExecution().GetWorkflowId(),
+			pauseRequest.GetWorkflowExecution().GetRunId(),
+		),
+		func(workflowLease api.WorkflowLease) (*api.UpdateWorkflowAction, error) {
+			mutableState := workflowLease.GetMutableState()
+
+			releaseFn := workflowLease.GetReleaseFn()
+			// Make sure the workflow is not closed.
+			if !mutableState.IsWorkflowExecutionRunning() {
+				// in-memory mutable state is still clean, release the lock with nil error to prevent
+				// clearing and reloading mutable state
+				releaseFn(nil)
+				return nil, consts.ErrWorkflowCompleted
+			}
+
+			// Check if workflow is already paused
+			if mutableState.IsWorkflowExecutionPaused() {
+				// Already paused, nothing to do
+				return &api.UpdateWorkflowAction{
+					Noop:               true,
+					CreateWorkflowTask: false,
+				}, nil
+			}
+
+			// Add the workflow execution paused event
+			err := mutableState.PauseWorkflowExecution(
+				pauseRequest.GetIdentity(),
+				pauseRequest.GetReason(),
+				pauseRequest.GetRequestId(),
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			return &api.UpdateWorkflowAction{
+				Noop:               false,
+				CreateWorkflowTask: false, // Don't create workflow task when pausing
+			}, nil
+		},
+		nil,
+		shard,
+		workflowConsistencyChecker,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return response
+	return &historyservice.PauseWorkflowExecutionResponse{}, nil
+}
