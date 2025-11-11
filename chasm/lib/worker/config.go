0feeec84652ee2ae35643cec4e8bc5dda5900493
Kannan Rajah <kannan.rajah@temporal.io>
2025-11-11 14:01:38 -0800

    Pass full request proto to recordHeartbeat.

--- chasm/lib/worker/config.go
+++ chasm/lib/worker/config.go
@@ -4,6 +4,13 @@ import (
 	"go.temporal.io/server/common/dynamicconfig"
 )
 
+// InactiveWorkerCleanupDelay is the time to wait before cleaning up inactive workers after their lease expires.
+var InactiveWorkerCleanupDelay = dynamicconfig.NewNamespaceDurationSetting(
+	"worker.inactiveWorkerCleanupDelay",
+	60*time.Minute,
+	`InactiveWorkerCleanupDelay is the time to wait before cleaning up inactive workers after their lease expires.`,
+)
+
 type Config struct {
 	// Future configs can be added here.
 }
--- chasm/lib/worker/worker_test.go
+++ chasm/lib/worker/worker_test.go
@@ -2,10 +2,10 @@ package worker
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 	workerpb "go.temporal.io/api/worker/v1"
+	workflowservice "go.temporal.io/api/workflowservice/v1"
 	"go.temporal.io/server/chasm"
 	workerstatepb "go.temporal.io/server/chasm/lib/worker/gen/workerpb/v1"
 )
@@ -40,18 +40,26 @@ func TestWorkerRecordHeartbeat(t *testing.T) {
 	worker := NewWorker()
 	ctx := &chasm.MockMutableContext{}
 
-	heartbeat := &workerpb.WorkerHeartbeat{
-		WorkerInstanceKey: "test-worker-3",
+	req := &workerstatepb.RecordHeartbeatRequest{
+		NamespaceId: "test-namespace-id",
+		FrontendRequest: &workflowservice.RecordWorkerHeartbeatRequest{
+			Namespace: "test-namespace",
+			Identity:  "test-identity",
+			WorkerHeartbeat: []*workerpb.WorkerHeartbeat{
+				{
+					WorkerInstanceKey: "test-worker-3",
+				},
+			},
+			// LeaseDuration: durationpb.New(30 * time.Second), // Will be available after proto regeneration
+		},
 	}
-	leaseDuration := 30 * time.Second
 
-	// Test recording heartbeat on new worker (no token)
-	newToken, err := worker.recordHeartbeat(ctx, heartbeat, nil, leaseDuration)
+	// Test recording heartbeat on new worker
+	err := worker.recordHeartbeat(ctx, req)
 	require.NoError(t, err)
 	require.NotEmpty(t, newToken)
 
 	// Verify heartbeat data was set
-	require.Equal(t, heartbeat, worker.WorkerHeartbeat)
 	require.Equal(t, "test-worker-3", worker.workerID())
 
 	// Verify lease expiration time was set
