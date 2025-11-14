e9e0f8f405adb3419cc6d4259d739a1b499fae25
Chetan Gowda <chetan.gowda@temporal.io>
2025-11-13 22:01:12 -0800

    Fix ValidateUpdateWorkflowStateStatus() added more tests

--- service/history/workflow/workflow_task_state_machine.go
+++ service/history/workflow/workflow_task_state_machine.go
@@ -1031,7 +1031,6 @@ func (m *workflowTaskStateMachine) UpdateWorkflowTask(
 
 	// NOTE:
 	// - do not update executionInfo.TaskQueue!
-	// - do not update executionInfo.WorkflowTaskStamp (only changed when rescheduling workflow task)
 
 	m.ms.logger.Debug("Workflow task updated",
 		tag.WorkflowScheduledEventID(workflowTask.ScheduledEventID),
