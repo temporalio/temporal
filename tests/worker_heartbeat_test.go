1c92af4fce2c221b6c2437915695826e0c541b5e
Kannan Rajah <kannan.rajah@temporal.io>
2025-11-14 12:24:26 -0800

    Add verification for functional test

--- tests/worker_heartbeat_test.go
+++ tests/worker_heartbeat_test.go
@@ -9,6 +9,9 @@ import (
 	"github.com/stretchr/testify/suite"
 	workerpb "go.temporal.io/api/worker/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/chasm"
+	chasmworker "go.temporal.io/server/chasm/lib/worker"
+	workerstatepb "go.temporal.io/server/chasm/lib/worker/gen/workerpb/v1"
 	"go.temporal.io/server/common/dynamicconfig"
 	"go.temporal.io/server/common/testing/testvars"
 	"go.temporal.io/server/tests/testcore"
@@ -16,6 +19,8 @@ import (
 
 type workerHeartbeatTestSuite struct {
 	testcore.FunctionalTestBase
+
+	chasmEngine chasm.Engine
 }
 
 func TestWorkerHeartbeatTestSuite(t *testing.T) {
@@ -23,19 +28,24 @@ func TestWorkerHeartbeatTestSuite(t *testing.T) {
 	suite.Run(t, new(workerHeartbeatTestSuite))
 }
 
+func (s *workerHeartbeatTestSuite) SetupSuite() {
+	s.FunctionalTestBase.SetupSuiteWithCluster(
+		testcore.WithDynamicConfigOverrides(map[dynamicconfig.Key]any{
+			dynamicconfig.EnableChasm.Key():               true,
+			dynamicconfig.EnableWorkerStateTracking.Key(): true,
+		}),
+	)
+
+	var err error
+	s.chasmEngine, err = s.FunctionalTestBase.GetTestCluster().Host().ChasmEngine()
+	s.Require().NoError(err)
+	s.Require().NotNil(s.chasmEngine)
+}
+
 func (s *workerHeartbeatTestSuite) TestRecordHeartbeat() {
 	t := s.T()
 	ctx, cancel := context.WithTimeout(t.Context(), 10*time.Second)
 	defer cancel()
-
-	s.OverrideDynamicConfig(
-		dynamicconfig.EnableChasm,
-		true,
-	)
-	s.OverrideDynamicConfig(
-		dynamicconfig.EnableWorkerStateTracking,
-		true,
-	)
 
 	tv := testvars.New(t)
 	workerInstanceKey := tv.Any().String()
@@ -53,24 +63,33 @@ func (s *workerHeartbeatTestSuite) TestRecordHeartbeat() {
 	require.NoError(t, err)
 	require.NotNil(t, resp)
 
-	// TODO: Add verification that the worker entity was created in CHASM tree
-	// Once the frontend handler is hooked up to the CHASM handler, this will
-	// actually create/update worker entities. For now, this verifies the API works.
+	// Verify the worker entity was created in CHASM
+	engineCtx := chasm.NewEngineContext(ctx, s.chasmEngine)
+	worker, err := chasm.ReadComponent(
+		engineCtx,
+		chasm.NewComponentRef[*chasmworker.Worker](
+			chasm.EntityKey{
+				NamespaceID: s.NamespaceID().String(),
+				BusinessID:  workerInstanceKey,
+			},
+		),
+		func(w *chasmworker.Worker, _ chasm.Context, _ any) (*chasmworker.Worker, error) {
+			return w, nil
+		},
+		nil,
+	)
+
+	require.NoError(t, err)
+	require.NotNil(t, worker)
+	require.Equal(t, workerstatepb.WORKER_STATUS_ACTIVE, worker.Status)
+	require.Equal(t, workerInstanceKey, worker.GetWorkerHeartbeat().GetWorkerInstanceKey())
+	require.NotNil(t, worker.GetLeaseExpirationTime())
 }
 
 func (s *workerHeartbeatTestSuite) TestRecordMultipleHeartbeats() {
 	t := s.T()
 	ctx, cancel := context.WithTimeout(t.Context(), 10*time.Second)
 	defer cancel()
-
-	s.OverrideDynamicConfig(
-		dynamicconfig.EnableChasm,
-		true,
-	)
-	s.OverrideDynamicConfig(
-		dynamicconfig.EnableWorkerStateTracking,
-		true,
-	)
 
 	tv := testvars.New(t)
 	workerInstanceKey := tv.Any().String()
@@ -90,11 +109,54 @@ func (s *workerHeartbeatTestSuite) TestRecordMultipleHeartbeats() {
 	require.NoError(t, err)
 	require.NotNil(t, resp1)
 
+	// Read worker state after first heartbeat
+	engineCtx := chasm.NewEngineContext(ctx, s.chasmEngine)
+	worker1, err := chasm.ReadComponent(
+		engineCtx,
+		chasm.NewComponentRef[*chasmworker.Worker](
+			chasm.EntityKey{
+				NamespaceID: s.NamespaceID().String(),
+				BusinessID:  workerInstanceKey,
+			},
+		),
+		func(w *chasmworker.Worker, _ chasm.Context, _ any) (*chasmworker.Worker, error) {
+			return w, nil
+		},
+		nil,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, worker1)
+	firstLeaseExpiration := worker1.GetLeaseExpirationTime().AsTime()
+
+	// Wait a bit to ensure lease expiration time will be different
+	time.Sleep(100 * time.Millisecond)
+
 	// Send second heartbeat for the same worker
 	resp2, err := s.FrontendClient().RecordWorkerHeartbeat(ctx, req)
 	require.NoError(t, err)
 	require.NotNil(t, resp2)
 
-	// TODO: Verify that the second heartbeat updated the existing worker entity
-	// rather than creating a new one. This would require querying the CHASM tree.
+	// Verify that the second heartbeat updated the existing worker entity
+	worker2, err := chasm.ReadComponent(
+		engineCtx,
+		chasm.NewComponentRef[*chasmworker.Worker](
+			chasm.EntityKey{
+				NamespaceID: s.NamespaceID().String(),
+				BusinessID:  workerInstanceKey,
+			},
+		),
+		func(w *chasmworker.Worker, _ chasm.Context, _ any) (*chasmworker.Worker, error) {
+			return w, nil
+		},
+		nil,
+	)
+	require.NoError(t, err)
+	require.NotNil(t, worker2)
+	require.Equal(t, workerstatepb.WORKER_STATUS_ACTIVE, worker2.Status)
+	require.Equal(t, workerInstanceKey, worker2.GetWorkerHeartbeat().GetWorkerInstanceKey())
+
+	// Verify lease expiration time was updated (extended)
+	secondLeaseExpiration := worker2.GetLeaseExpirationTime().AsTime()
+	require.True(t, secondLeaseExpiration.After(firstLeaseExpiration),
+		"Second heartbeat should extend the lease expiration time")
 }
