d8d8a6f102767a809e873e78f9eae06360a4a7b3
Carly de Frondeville <cdefrondeville@berkeley.edu>
2025-11-19 18:13:40 -0800

    add comment about state-based replication

--- service/history/ndc/workflow_resetter.go
+++ service/history/ndc/workflow_resetter.go
@@ -1018,7 +1018,7 @@ func reapplyEvents(
 // This function is intended to pick up all the events for a child that was already initialized before the reset point.
 // Re-applying these events is needed to support reconnecting of the child with parent.
 func reapplyChildEvents(mutableState historyi.MutableState, event *historypb.HistoryEvent) error { // nolint:revive
-	switch event.GetEventType() { // nolint:exhaustive
+	switch event.GetEventType() {                                                                  // nolint:exhaustive
 	case enumspb.EVENT_TYPE_START_CHILD_WORKFLOW_EXECUTION_FAILED:
 		childEventAttributes := event.GetStartChildWorkflowExecutionFailedEventAttributes()
 		_, childExists := mutableState.GetChildExecutionInfo(childEventAttributes.GetInitiatedEventId())
@@ -1163,6 +1163,7 @@ func (r *workflowResetterImpl) performPostResetOperations(ctx context.Context, r
 	for _, operation := range postResetOperations {
 		switch op := operation.GetVariant().(type) {
 		case *workflowpb.PostResetOperation_UpdateWorkflowOptions_:
+			// TODO(carlydf): Put the reset requester in the event so that with state-based replication this code will run on the passive side.
 			_, _, err := updateworkflowoptions.MergeAndApply(resetMS, op.UpdateWorkflowOptions.GetWorkflowExecutionOptions(), op.UpdateWorkflowOptions.GetUpdateMask(), "")
 			if err != nil {
 				return err
