09d2deea73baa0afb7f4e71bd6081defcf8bd266
Dan Davison <dan.davison@temporal.io>
2025-11-26 13:01:58 -0500

    errors.New

--- chasm/ref.go
+++ chasm/ref.go
@@ -1,6 +1,7 @@
 package chasm
 
 import (
+	"errors"
 	"reflect"
 
 	"go.temporal.io/api/serviceerror"
@@ -132,7 +133,7 @@ func (r *ComponentRef) Serialize(
 // Provides caller the access to information including EntityKey, Archetype, and ShardingKey.
 func DeserializeComponentRef(data []byte) (ComponentRef, error) {
 	if len(data) == 0 {
-		return ComponentRef{}, serviceerror.NewInternal("empty chasm component ref")
+		return ComponentRef{}, errors.New("empty chasm component ref")
 	}
 	var pRef persistencespb.ChasmComponentRef
 	if err := pRef.Unmarshal(data); err != nil {
