81df7937dbb265ea1f5302019fc6d9015ba9ac21
Stephan Behnke <stephanos@users.noreply.github.com>
2025-12-01 11:42:47 -0800

    softassert for test hooks

--- common/testing/testhooks/noop_impl.go
+++ common/testing/testhooks/noop_impl.go
@@ -3,24 +3,28 @@
 package testhooks
 
 import (
-	"context"
-
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/softassert"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
-	fx.Provide(func() (_ TestHooks) { return }),
+	fx.Provide(NewTestHooks),
 )
 
-type (
-	// TestHooks (in production mode) is an empty struct just so the build works.
-	// See TestHooks in test_impl.go.
-	//
-	// TestHooks are an inherently unclean way of writing tests. They require mixing test-only
-	// concerns into production code. In general you should prefer other ways of writing tests
-	// wherever possible, and only use TestHooks sparingly, as a last resort.
-	TestHooks struct{}
-)
+// TestHooks (in production mode) is an empty struct just so the build works.
+// See TestHooks in test_impl.go.
+//
+// TestHooks are an inherently unclean way of writing tests. They require mixing test-only
+// concerns into production code. In general you should prefer other ways of writing tests
+// wherever possible, and only use TestHooks sparingly, as a last resort.
+type TestHooks struct {
+	logger log.Logger
+}
+
+func NewTestHooks(logger log.Logger) TestHooks {
+	return TestHooks{logger: logger}
+}
 
 // Get gets the value of a test hook. In production mode it always returns the zero value and
 // false, which hopefully the compiler will inline and remove the hook as dead code.
@@ -31,23 +35,14 @@ func Get[T any](_ TestHooks, key Key) (T, bool) {
 	return zero, false
 }
 
-// GetCtx gets the value of a test hook from the registry embedded in the
-// context chain.
-//
-// TestHooks should be used sparingly, see comment on TestHooks.
-func GetCtx[T any](ctx context.Context, key Key) (T, bool) {
-	var zero T
-	return zero, false
-}
-
 // Call calls a func() hook if present.
 //
 // TestHooks should be used very sparingly, see comment on TestHooks.
 func Call(_ TestHooks, key Key) {
 }
 
-// CallCtx calls a func(context.Context) hook if present.
-//
-// TestHooks should be used very sparingly, see comment on TestHooks.
-func CallCtx(_ context.Context, key Key) {
+// Set is only to be used by test code together with the test_dep build tag.
+func Set[T any](th TestHooks, key Key, val T) func() {
+	softassert.Fail(th.logger, "testhooks.Set called without test_dep build tag")
+	return func() {}
 }
--- common/testing/testhooks/test_impl.go
+++ common/testing/testhooks/test_impl.go
@@ -3,14 +3,14 @@
 package testhooks
 
 import (
-	"context"
 	"sync"
 
+	"go.temporal.io/server/common/log"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
-	fx.Provide(NewTestHooksImpl),
+	fx.Provide(NewTestHooks),
 )
 
 type (
@@ -31,9 +31,11 @@ type (
 	testHooksImpl struct {
 		m sync.Map
 	}
-
-	contextKey struct{}
 )
+
+func NewTestHooks(_ log.Logger) TestHooks {
+	return &testHooksImpl{}
+}
 
 // Get gets the value of a test hook from the registry.
 //
@@ -50,19 +52,6 @@ func Get[T any](th TestHooks, key Key) (T, bool) {
 	return zero, false
 }
 
-// GetCtx gets the value of a test hook from the registry embedded in the
-// context chain.
-//
-// TestHooks should be used sparingly, see comment on TestHooks.
-func GetCtx[T any](ctx context.Context, key Key) (T, bool) {
-	hooks := ctx.Value(contextKey{})
-	if hooks, ok := hooks.(TestHooks); ok {
-		return Get[T](hooks, key)
-	}
-	var zero T
-	return zero, false
-}
-
 // Call calls a func() hook if present.
 //
 // TestHooks should be used sparingly, see comment on TestHooks.
@@ -72,47 +61,11 @@ func Call(th TestHooks, key Key) {
 	}
 }
 
-// CallCtx calls a func() hook if present and a TestHooks implementation
-// exists in the context chain.
-//
-// TestHooks should be used sparingly, see comment on TestHooks.
-func CallCtx(ctx context.Context, key Key) {
-	hooks := ctx.Value(contextKey{})
-	if hooks, ok := hooks.(TestHooks); ok {
-		Call(hooks, key)
-	}
-}
-
 // Set sets a test hook to a value and returns a cleanup function to unset it.
 // Calls to Set and the cleanup function should form a stack.
 func Set[T any](th TestHooks, key Key, val T) func() {
 	th.set(key, val)
 	return func() { th.del(key) }
-}
-
-// SetCtx sets a test hook to a value on the provided context and returns a
-// cleanup function to unset it. Calls to SetCtx and the cleanup function
-// should form a stack.
-func SetCtx[T any](ctx context.Context, key Key, val T) func() {
-	hooks := ctx.Value(contextKey{})
-	if hooks, ok := hooks.(TestHooks); ok {
-		return Set(hooks, key, val)
-	}
-	return func() {}
-}
-
-// NewTestHooksImpl returns a new instance of a test hook registry. This is provided and used
-// in the main "resource" module as a default, but in functional tests, it's overridden by an
-// explicitly constructed instance.
-func NewTestHooksImpl() TestHooks {
-	return &testHooksImpl{}
-}
-
-// NewInjectedTestHooksImpl returns a new instance of a test hook registry and a context with the
-// registry injected.
-func NewInjectedTestHooksImpl(parent context.Context) (context.Context, TestHooks) {
-	hooks := NewTestHooksImpl()
-	return context.WithValue(parent, contextKey{}, hooks), hooks
 }
 
 func (th *testHooksImpl) get(key Key) (any, bool) {
