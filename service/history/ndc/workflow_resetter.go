10f2b21d80a5ba2bc8fbfbb87b7d4953b43a82ac
Vladyslav Simonenko <vlad.simonenko@temporal.io>
2025-12-08 12:46:18 -0800

    Payload size tracking

--- service/history/ndc/workflow_resetter.go
+++ service/history/ndc/workflow_resetter.go
@@ -457,7 +457,7 @@ func (r *workflowResetterImpl) replayResetWorkflow(
 		r.shardContext.GetMetricsHandler(),
 	)
 
-	resetMutableState, resetHistorySize, err := r.stateRebuilder.Rebuild(
+	resetMutableState, resetStats, err := r.stateRebuilder.Rebuild(
 		ctx,
 		r.shardContext.GetTimeSource().Now(),
 		definition.NewWorkflowKey(
@@ -485,7 +485,9 @@ func (r *workflowResetterImpl) replayResetWorkflow(
 		baseRebuildLastEventID,
 		baseRebuildLastEventVersion,
 	)
-	resetMutableState.AddHistorySize(resetHistorySize)
+	resetMutableState.AddHistorySize(resetStats.HistorySize)
+	resetMutableState.AddExternalPayloadSize(resetStats.ExternalPayloadSize)
+	resetMutableState.AddExternalPayloadCount(resetStats.ExternalPayloadCount)
 	return NewWorkflow(
 		r.clusterMetadata,
 		resetContext,
