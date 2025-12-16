fd5d1b69393dcb5ca01a0dfdce6883a984fcce65
Prathyush PV <prathyush.pv@temporal.io>
2025-12-15 19:05:22 -0800

    Fix unit test failures for raw history feature
    - Keep SendRawHistoryBetweenInternalServices default as false
    - TestQueryNoCurrentPollersButRecentPollers, TestQueryNoRecentPoller: Use emptyClientPollWorkflowTaskQueueResponse for mock client tests since client interface uses PollWorkflowTaskQueueResponse (not the WithRawHistory variant)

--- common/dynamicconfig/constants.go
+++ common/dynamicconfig/constants.go
@@ -2623,7 +2623,7 @@ that task will be sent to DLQ.`,
 	)
 	SendRawHistoryBetweenInternalServices = NewGlobalBoolSetting(
 		"history.sendRawHistoryBetweenInternalServices",
-		true,
+		false,
 		`SendRawHistoryBetweenInternalServices is whether to send raw history events between internal temporal services`,
 	)
 
