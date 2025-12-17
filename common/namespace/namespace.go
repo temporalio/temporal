12256978f64ef2b91da7c3ca64ac78fa6921a063
xwduan <xinw.duan@gmail.com>
2025-12-16 17:01:53 -0800

    Update replication resolver api

--- common/namespace/namespace.go
+++ common/namespace/namespace.go
@@ -186,13 +186,13 @@ func (ns *Namespace) ReplicationState() enumspb.ReplicationState {
 // ActiveClusterName observes the name of the cluster that is currently active
 // for this namspace.
 func (ns *Namespace) ActiveClusterName(businessID string) string {
-	return ns.replicationResolver.ActiveClusterName()
+	return ns.replicationResolver.ActiveClusterName(businessID)
 }
 
 // ClusterNames observes the names of the clusters to which this namespace is
 // replicated.
 func (ns *Namespace) ClusterNames() []string {
-	return ns.replicationResolver.ClusterNames()
+	return ns.replicationResolver.ClusterNames(EmptyBusinessID)
 }
 
 // IsOnCluster returns true is namespace is registered on cluster otherwise false.
@@ -212,7 +212,7 @@ func (ns *Namespace) ConfigVersion() int64 {
 
 // FailoverVersion return the namespace failover version
 func (ns *Namespace) FailoverVersion() int64 {
-	return ns.replicationResolver.FailoverVersion()
+	return ns.replicationResolver.FailoverVersion(EmptyBusinessID)
 }
 
 // IsGlobalNamespace returns whether the namespace is a global namespace.
