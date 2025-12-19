1ac80b326cef0040c6d617063b9cfb5219d5c935
Alex Stanfield <13949480+chaptersix@users.noreply.github.com>
2025-12-19 12:14:03 -0600

    Correct Docker Build on main (#8877)
    ## What changed?
    * add a different build step when the arch is not specified
    
    ## Why?
    * overwriting the GOARCH variable to be blank is invalid
    
    ## How did you test it?
    I tried to trigger the build manually on a non-main branch and it
    doesn't seem to pick up this change. So we need to merge it to test.

--- .github/actions/build-docker-images/scripts/main.go
+++ .github/actions/build-docker-images/scripts/main.go
@@ -212,6 +212,7 @@ func organizeBinaries() error {
 				}
 				fmt.Printf("Copied %s -> %s\n", distPath, buildPath)
 			} else {
+				printDirectoryContents("dist")
 				return fmt.Errorf("binary not found: %s for architecture %s (expected at %s)", binary, arch, distPath)
 			}
 		}
@@ -558,4 +559,22 @@ func downloadFile(url, fpath string) error {
 
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+func printDirectoryContents(dir string) {
+	fmt.Fprintf(os.Stderr, "\nContents of %s directory:\n", dir)
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil // skip errors
+		}
+		if info.IsDir() {
+			fmt.Fprintf(os.Stderr, "  %s/\n", path)
+		} else {
+			fmt.Fprintf(os.Stderr, "  %s\n", path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "  (failed to list %s directory: %v)\n", dir, err)
+	}
 }
