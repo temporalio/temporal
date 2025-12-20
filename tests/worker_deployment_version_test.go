cb1fa926e4cd71e0ebd3320b6f9e2e9c1e322db8
Carly de Frondeville <cdefrondeville@berkeley.edu>
2025-12-19 18:44:17 -0800

    lint

--- tests/worker_deployment_version_test.go
+++ tests/worker_deployment_version_test.go
@@ -59,15 +59,15 @@ var (
 
 func TestDeploymentVersionSuite(t *testing.T) {
 	t.Parallel()
-	//t.Run("sync_workflows", func(t *testing.T) {
-	//	suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.InitialVersion})
-	//})
-	//t.Run("async_workflows", func(t *testing.T) {
-	//	suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.AsyncSetCurrentAndRamping})
-	//})
-	//t.Run("v2", func(t *testing.T) {
-	suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.VersionDataRevisionNumber})
-	//})
+	t.Run("sync_workflows", func(t *testing.T) {
+		suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.InitialVersion})
+	})
+	t.Run("async_workflows", func(t *testing.T) {
+		suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.AsyncSetCurrentAndRamping})
+	})
+	t.Run("v2", func(t *testing.T) {
+		suite.Run(t, &DeploymentVersionSuite{workflowVersion: workerdeployment.VersionDataRevisionNumber})
+	})
 }
 
 func (s *DeploymentVersionSuite) SetupSuite() {
@@ -1109,7 +1109,7 @@ func (s *DeploymentVersionSuite) TestUpdateVersionMetadata() {
 
 	// validating the metadata
 	entries := resp.GetWorkerDeploymentVersionInfo().GetMetadata().GetEntries()
-	s.Equal(2, len(entries))
+	s.Len(entries, 2)
 	s.Equal(testRandomMetadataValue, entries["key1"].Data)
 	s.Equal(testRandomMetadataValue, entries["key2"].Data)
 
