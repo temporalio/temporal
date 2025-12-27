5b9bf676c20a982c95d0d93e7d202735ca43ddec
Kannan Rajah <kannan.rajah@temporal.io>
2025-12-27 15:18:11 -0800

    Review comments and cleanup

--- common/log/tag/tags.go
+++ common/log/tag/tags.go
@@ -306,11 +306,6 @@ func WorkerBuildId(buildId string) ZapTag {
 	return NewStringTag("worker-build-id", buildId)
 }
 
-// WorkerID returns tag for a worker
-func WorkerID(workerID string) ZapTag {
-	return NewStringTag("worker-id", workerID)
-}
-
 // ReachabilityExitPointTag returns tag for reachabilityExitPoint
 func ReachabilityExitPointTag(reachabilityExitPoint string) ZapTag {
 	return NewStringTag("reachability-exit-point", reachabilityExitPoint)
