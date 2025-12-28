5b13f49614fcd855d79d4d8636e059da87226423
Kannan Rajah <kannan.rajah@temporal.io>
2025-12-28 13:01:27 -0800

    Add request_response.proto with WorkerService and RecordHeartbeat RPC

--- chasm/lib/worker/handler.go
+++ chasm/lib/worker/handler.go
@@ -2,10 +2,18 @@ package worker
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
+	"go.temporal.io/api/serviceerror"
 	"go.temporal.io/server/chasm"
 	workerstatepb "go.temporal.io/server/chasm/lib/worker/gen/workerpb/v1"
+)
+
+const (
+	// Default lease duration for worker heartbeats.
+	defaultLeaseDuration = 1 * time.Minute
 )
 
 type handler struct {
@@ -20,7 +28,7 @@ func (h *handler) RecordHeartbeat(ctx context.Context, req *workerstatepb.Record
 	// Validate that exactly one worker heartbeat is present
 	frontendReq := req.GetFrontendRequest()
 	if frontendReq == nil || len(frontendReq.GetWorkerHeartbeat()) != 1 {
-		return nil, fmt.Errorf("exactly one worker heartbeat must be present in the request")
+		return nil, errors.New("exactly one worker heartbeat must be present in the request")
 	}
 
 	workerHeartbeat := frontendReq.GetWorkerHeartbeat()[0]
@@ -30,24 +38,36 @@ func (h *handler) RecordHeartbeat(ctx context.Context, req *workerstatepb.Record
 		BusinessID:  workerHeartbeat.WorkerInstanceKey,
 	}
 
+	// TODO: Get lease duration from request once the proto supports it.
+	leaseDuration := defaultLeaseDuration
+
 	// Try to update existing worker, or create new one if not found
-	resp, _, _, _, err := chasm.UpdateWithNewExecution(
+	_, _, _, _, err := chasm.UpdateWithNewExecution(
 		ctx,
 		executionKey,
 		// newFn: called if worker doesn't exist
-		func(ctx chasm.MutableContext, req *workerstatepb.RecordHeartbeatRequest) (*Worker, *workerstatepb.RecordHeartbeatResponse, error) {
+		func(ctx chasm.MutableContext, _ *workerstatepb.RecordHeartbeatRequest) (*Worker, []byte, error) {
 			w := NewWorker()
-			resp, err := w.recordHeartbeat(ctx, req)
-			return w, resp, err
+			token, err := w.recordHeartbeat(ctx, workerHeartbeat, nil, leaseDuration)
+			return w, token, err
 		},
 		// updateFn: called if worker exists
-		(*Worker).recordHeartbeat,
+		func(w *Worker, ctx chasm.MutableContext, _ *workerstatepb.RecordHeartbeatRequest) ([]byte, error) {
+			// TODO: Extract token from request once the proto supports it.
+			return w.recordHeartbeat(ctx, workerHeartbeat, nil, leaseDuration)
+		},
 		req,
 	)
 
 	if err != nil {
+		if _, ok := err.(*WorkerInactiveError); ok {
+			return nil, serviceerror.NewFailedPrecondition(err.Error())
+		}
+		if tokenErr, ok := err.(*TokenMismatchError); ok {
+			return nil, serviceerror.NewFailedPrecondition(fmt.Sprintf("%s: current token %x", err.Error(), tokenErr.CurrentToken))
+		}
 		return nil, err
 	}
 
-	return resp, nil
+	return &workerstatepb.RecordHeartbeatResponse{}, nil
 }
