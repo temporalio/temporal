fadysalib/temporal-workflow

Add rpc endpoint to worker chasm component to record worker heartbeat.

chasm/lib/worker/gen/workerpb/v1/service.pb.go
@@ -1,0 +1,69 @@
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// plugins:
+// 	protoc-gen-go
+// 	protoc
+// source: temporal/server/chasm/lib/worker/proto/v1/service.proto
+
+package workerpb
+
+import (
+	reflect "reflect"
+	unsafe "unsafe"
+
+	_ "go.temporal.io/server/api/routing/v1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+var File_temporal_server_chasm_lib_worker_proto_v1_service_proto protoreflect.FileDescriptor
+
+const file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc = "" +
+	"\n" +
+	"7temporal/server/chasm/lib/worker/proto/v1/service.proto\x12)temporal.server.chasm.lib.worker.proto.v1\x1a@temporal/server/chasm/lib/worker/proto/v1/request_response.proto\x1a.temporal/server/api/routing/v1/extension.proto2\xd6\x01\n" +
+	"\rWorkerService\x12\xc4\x01\n" +
+	"\x0fRecordHeartbeat\x12A.temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest\x1aB.temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse\"*\x92\xc4\x03&\x1a$worker_heartbeat.worker_instance_keyB>Z<go.temporal.io/server/chasm/lib/worker/gen/workerpb;workerpbb\x06proto3"
+
+var file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes = []any{
+	(*RecordHeartbeatRequest)(nil),  // 0: temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest
+	(*RecordHeartbeatResponse)(nil), // 1: temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse
+}
+var file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs = []int32{
+	0, // 0: temporal.server.chasm.lib.worker.proto.v1.WorkerService.RecordHeartbeat:input_type -> temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest
+	1, // 1: temporal.server.chasm.lib.worker.proto.v1.WorkerService.RecordHeartbeat:output_type -> temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse
+	1, // [1:2] is the sub-list for method output_type
+	0, // [0:1] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_temporal_server_chasm_lib_worker_proto_v1_service_proto_init() }
+func file_temporal_server_chasm_lib_worker_proto_v1_service_proto_init() {
+	if File_temporal_server_chasm_lib_worker_proto_v1_service_proto != nil {
+		return
+	}
+	file_temporal_server_chasm_lib_worker_proto_v1_request_response_proto_init()
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc), len(file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   0,
+			NumExtensions: 0,
+			NumServices:   1,
+		},
+		GoTypes:           file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes,
+		DependencyIndexes: file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs,
+	}.Build()
+	File_temporal_server_chasm_lib_worker_proto_v1_service_proto = out.File
+	file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes = nil
+	file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs = nil
+}

temporalio/temporal

chasm/lib/worker/gen/workerpb/v1/service.pb.go
@@ -1,0 +1,69 @@
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// plugins:
+// 	protoc-gen-go
+// 	protoc
+// source: temporal/server/chasm/lib/worker/proto/v1/service.proto
+
+package workerpb
+
+import (
+	reflect "reflect"
+	unsafe "unsafe"
+
+	_ "go.temporal.io/server/api/routing/v1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+var File_temporal_server_chasm_lib_worker_proto_v1_service_proto protoreflect.FileDescriptor
+
+const file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc = "" +
+	"\n" +
+	"7temporal/server/chasm/lib/worker/proto/v1/service.proto\x12)temporal.server.chasm.lib.worker.proto.v1\x1a@temporal/server/chasm/lib/worker/proto/v1/request_response.proto\x1a.temporal/server/api/routing/v1/extension.proto2\xc5\x01\n" +
+	"\rWorkerService\x12\xb3\x01\n" +
+	"\x0fRecordHeartbeat\x12A.temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest\x1aB.temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse\"\x19\x92\xc4\x03\x15\x1a\x13worker_instance_keyB>Z<go.temporal.io/server/chasm/lib/worker/gen/workerpb;workerpbb\x06proto3"
+
+var file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes = []any{
+	(*RecordHeartbeatRequest)(nil),  // 0: temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest
+	(*RecordHeartbeatResponse)(nil), // 1: temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse
+}
+var file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs = []int32{
+	0, // 0: temporal.server.chasm.lib.worker.proto.v1.WorkerService.RecordHeartbeat:input_type -> temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatRequest
+	1, // 1: temporal.server.chasm.lib.worker.proto.v1.WorkerService.RecordHeartbeat:output_type -> temporal.server.chasm.lib.worker.proto.v1.RecordHeartbeatResponse
+	1, // [1:2] is the sub-list for method output_type
+	0, // [0:1] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_temporal_server_chasm_lib_worker_proto_v1_service_proto_init() }
+func file_temporal_server_chasm_lib_worker_proto_v1_service_proto_init() {
+	if File_temporal_server_chasm_lib_worker_proto_v1_service_proto != nil {
+		return
+	}
+	file_temporal_server_chasm_lib_worker_proto_v1_request_response_proto_init()
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc), len(file_temporal_server_chasm_lib_worker_proto_v1_service_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   0,
+			NumExtensions: 0,
+			NumServices:   1,
+		},
+		GoTypes:           file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes,
+		DependencyIndexes: file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs,
+	}.Build()
+	File_temporal_server_chasm_lib_worker_proto_v1_service_proto = out.File
+	file_temporal_server_chasm_lib_worker_proto_v1_service_proto_goTypes = nil
+	file_temporal_server_chasm_lib_worker_proto_v1_service_proto_depIdxs = nil
+}