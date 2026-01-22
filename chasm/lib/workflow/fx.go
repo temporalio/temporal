c68c2a497d4a6e4d374ad55c03b1cb9dd4821a37
Stephan Behnke <stephanos@users.noreply.github.com>
2026-01-22 13:21:07 -0800

    fix fx

--- chasm/lib/workflow/fx.go
+++ chasm/lib/workflow/fx.go
@@ -7,17 +7,30 @@ import (
 	"go.uber.org/fx"
 )
 
+type nexusCommandHandlerDeps struct {
+	fx.In
+
+	Library          *Library
+	EndpointRegistry commonnexus.EndpointRegistry `optional:"true"`
+	Config           *nexusoperation.Config       `optional:"true"`
+}
+
 var Module = fx.Module(
 	"chasm.lib.workflow",
 	fx.Provide(NewLibrary),
 	fx.Invoke(func(registry *chasm.Registry, library *Library) error {
 		return registry.Register(library)
 	}),
-	fx.Invoke(func(
-		l *Library,
-		endpointRegistry commonnexus.EndpointRegistry,
-		config *nexusoperation.Config,
-	) error {
-		return nexusoperation.RegisterCommandHandlers(l.commandHandlers, endpointRegistry, config)
+	fx.Invoke(func(deps nexusCommandHandlerDeps) error {
+		if deps.EndpointRegistry == nil || deps.Config == nil {
+			// Dependencies not available, skip registration.
+			// This happens in services that don't have nexus operation support.
+			return nil
+		}
+		return nexusoperation.RegisterCommandHandlers(
+			deps.Library.commandHandlers,
+			deps.EndpointRegistry,
+			deps.Config,
+		)
 	}),
 )
