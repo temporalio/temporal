4e3caa5fc03742f7c44d29396ba0a31e568e4c8d
Sean Kane <sean.kane@temporal.io>
2026-01-23 14:04:50 -0700

    Convert Unavailable to Aborted error in UpdateWithStart

--- service/history/workflow/update/errors_failures.go
+++ service/history/workflow/update/errors_failures.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	registryClearedErr          = errors.New("update registry was cleared")
-	AbortedByServerErr          = serviceerror.NewUnavailable("workflow update was aborted")
+	AbortedByServerErr          = serviceerror.NewAborted("workflow update was aborted")
 	AbortedByWorkflowClosingErr = serviceerror.NewNotFound("workflow update was aborted by closing workflow")
 	workflowTaskFailErr         = serviceerror.NewWorkflowNotReady("Unable to perform workflow execution update due to unexpected workflow task failure.")
 )
