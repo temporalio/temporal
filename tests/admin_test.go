3a564f18938125635128fb83541e690ccc69dee6
Stephan Behnke <stephanos@users.noreply.github.com>
2026-01-24 09:16:28 -0800

    Use TestEnv for all tests

--- tests/admin_test.go
+++ tests/admin_test.go
@@ -6,93 +6,109 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/suite"
 	commonpb "go.temporal.io/api/common/v1"
 	sdkclient "go.temporal.io/sdk/client"
+	sdkworker "go.temporal.io/sdk/worker"
 	"go.temporal.io/sdk/workflow"
 	"go.temporal.io/server/api/adminservice/v1"
 	persistencespb "go.temporal.io/server/api/persistence/v1"
 	"go.temporal.io/server/chasm"
 	"go.temporal.io/server/common/dynamicconfig"
+	"go.temporal.io/server/common/log"
 	"go.temporal.io/server/common/primitives/timestamp"
-	"go.temporal.io/server/common/testing/testvars"
 	"go.temporal.io/server/tests/testcore"
 )
 
-type AdminTestSuite struct {
-	testcore.FunctionalTestBase
-	testContext context.Context
-}
+func TestAdminRebuildMutableState(t *testing.T) {
+	t.Run("ChasmDisabled", func(t *testing.T) {
+		s := testcore.NewEnv(t)
+		testWithChasm := false
 
-func TestAdminTestSuite(t *testing.T) {
-	t.Parallel()
-	suite.Run(t, new(AdminTestSuite))
-}
+		// Set up SDK client and worker for the test-specific namespace
+		sdkClient, err := sdkclient.Dial(sdkclient.Options{
+			HostPort:  s.FrontendGRPCAddress(),
+			Namespace: s.Namespace().String(),
+			Logger:    log.NewSdkLogger(s.Logger),
+		})
+		s.NoError(err)
+		defer sdkClient.Close()
 
-func (s *AdminTestSuite) SetupSuite() {
-	// Call parent setup to initialize the test cluster
-	s.FunctionalTestBase.SetupSuite()
-	s.testContext = context.Background()
-}
+		taskQueue := s.Tv().TaskQueue().Name
+		worker := sdkworker.New(sdkClient, taskQueue, sdkworker.Options{})
 
-func (s *AdminTestSuite) TestAdminRebuildMutableState_ChasmDisabled() {
-	rebuildMutableStateWorkflowHelper(s, false)
-}
+		workflowFn := func(ctx workflow.Context) error {
+			var randomUUID string
+			err := workflow.SideEffect(
+				ctx,
+				func(workflow.Context) any { return uuid.New().String() },
+			).Get(&randomUUID)
+			if err != nil {
+				return err
+			}
 
-func (s *AdminTestSuite) TestAdminRebuildMutableState_ChasmEnabled() {
-	cleanup := s.OverrideDynamicConfig(dynamicconfig.EnableChasm, true)
-	defer cleanup()
+			_ = workflow.Sleep(ctx, 10*time.Minute)
+			return nil
+		}
 
-	configValues := s.GetTestCluster().Host().DcClient().GetValue(dynamicconfig.EnableChasm.Key())
-	s.NotEmpty(configValues, "EnableChasm config should be set")
-	configValue, _ := configValues[0].Value.(bool)
-	s.True(configValue, "EnableChasm config should be true")
-	rebuildMutableStateWorkflowHelper(s, true)
-}
+		worker.RegisterWorkflow(workflowFn)
+		s.NoError(worker.Start())
+		defer worker.Stop()
 
-// common test helper
-func rebuildMutableStateWorkflowHelper(s *AdminTestSuite, testWithChasm bool) {
-	tv := testvars.New(s.T())
-	workflowFn := func(ctx workflow.Context) error {
-		var randomUUID string
-		err := workflow.SideEffect(
-			ctx,
-			func(workflow.Context) any { return uuid.New().String() },
-		).Get(&randomUUID)
+		workflowID := s.Tv().Any().String()
+		workflowOptions := sdkclient.StartWorkflowOptions{
+			ID:                 workflowID,
+			TaskQueue:          taskQueue,
+			WorkflowRunTimeout: 20 * time.Second,
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		workflowRun, err := sdkClient.ExecuteWorkflow(context.Background(), workflowOptions, workflowFn)
+		s.NoError(err)
+		runID := workflowRun.GetRunID()
+
+		// there are total 6 events, 3 state transitions
+		//  1. WorkflowExecutionStarted
+		//  2. WorkflowTaskScheduled
+		//
+		//  3. WorkflowTaskStarted
+		//
+		//  4. WorkflowTaskCompleted
+		//  5. MarkerRecord
+		//  6. TimerStarted
+
+		var response1 *adminservice.DescribeMutableStateResponse
+		for {
+			response1, err = s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
+				Namespace: s.Namespace().String(),
+				Execution: &commonpb.WorkflowExecution{
+					WorkflowId: workflowID,
+					RunId:      runID,
+				},
+				Archetype: chasm.WorkflowArchetype,
+			})
+			s.NoError(err)
+			if response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount == 3 {
+				// Note: ChasmNodes may be empty even with CHASM enabled, so we only check if the rebuild can be performed,
+				// and not checking whether it is rebuildable because ChasmNodes are present.
+				if !testWithChasm {
+					s.Empty(response1.DatabaseMutableState.ChasmNodes, "CHASM-disabled workflows should not have ChasmNodes")
+				}
+				break
+			}
+			time.Sleep(20 * time.Millisecond) //nolint:forbidigo
+		}
+
+		_, err = s.AdminClient().RebuildMutableState(ctx, &adminservice.RebuildMutableStateRequest{
+			Namespace: s.Namespace().String(),
+			Execution: &commonpb.WorkflowExecution{
+				WorkflowId: workflowID,
+				RunId:      runID,
+			},
+		})
 		s.NoError(err)
 
-		_ = workflow.Sleep(ctx, 10*time.Minute)
-		return nil
-	}
-
-	s.Worker().RegisterWorkflow(workflowFn)
-
-	workflowID := tv.Any().String()
-	workflowOptions := sdkclient.StartWorkflowOptions{
-		ID:                 workflowID,
-		TaskQueue:          s.TaskQueue(),
-		WorkflowRunTimeout: 20 * time.Second,
-	}
-	ctx, cancel := context.WithTimeout(s.testContext, 30*time.Second)
-	defer cancel()
-
-	workflowRun, err := s.SdkClient().ExecuteWorkflow(s.testContext, workflowOptions, workflowFn)
-	s.NoError(err)
-	runID := workflowRun.GetRunID()
-
-	// there are total 6 events, 3 state transitions
-	//  1. WorkflowExecutionStarted
-	//  2. WorkflowTaskScheduled
-	//
-	//  3. WorkflowTaskStarted
-	//
-	//  4. WorkflowTaskCompleted
-	//  5. MarkerRecord
-	//  6. TimerStarted
-
-	var response1 *adminservice.DescribeMutableStateResponse
-	for {
-		response1, err = s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
+		response2, err := s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
 			Namespace: s.Namespace().String(),
 			Execution: &commonpb.WorkflowExecution{
 				WorkflowId: workflowID,
@@ -101,55 +117,153 @@ func rebuildMutableStateWorkflowHelper(s *AdminTestSuite, testWithChasm bool) {
 			Archetype: chasm.WorkflowArchetype,
 		})
 		s.NoError(err)
-		if response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount == 3 {
-			// Note: ChasmNodes may be empty even with CHASM enabled, so we only check if the rebuild can be performed,
-			// and not checking whether it is rebuildable because ChasmNodes are present.
-			if !testWithChasm {
-				s.Empty(response1.DatabaseMutableState.ChasmNodes, "CHASM-disabled workflows should not have ChasmNodes")
+		s.Equal(response1.DatabaseMutableState.ExecutionInfo.VersionHistories, response2.DatabaseMutableState.ExecutionInfo.VersionHistories)
+		s.Equal(response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount, response2.DatabaseMutableState.ExecutionInfo.StateTransitionCount)
+
+		s.Equal(response1.DatabaseMutableState.ExecutionState.CreateRequestId, response2.DatabaseMutableState.ExecutionState.CreateRequestId)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.RunId, response2.DatabaseMutableState.ExecutionState.RunId)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.State, response2.DatabaseMutableState.ExecutionState.State)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.Status, response2.DatabaseMutableState.ExecutionState.Status)
+
+		// From transition history perspective, Rebuild is considered as an update to the workflow and updates
+		// all sub state machines in the workflow, which includes the workflow ExecutionState.
+		s.Equal(&persistencespb.VersionedTransition{
+			NamespaceFailoverVersion: response1.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition.NamespaceFailoverVersion,
+			TransitionCount:          response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount + 1,
+		}, response2.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition)
+
+		// Rebuild explicitly sets start time, thus start time will change after rebuild.
+		s.NotNil(response1.DatabaseMutableState.ExecutionState.StartTime)
+		s.NotNil(response2.DatabaseMutableState.ExecutionState.StartTime)
+
+		timeBefore := timestamp.TimeValue(response1.DatabaseMutableState.ExecutionState.StartTime)
+		timeAfter := timestamp.TimeValue(response2.DatabaseMutableState.ExecutionState.StartTime)
+		s.False(timeAfter.Before(timeBefore))
+	})
+
+	t.Run("ChasmEnabled", func(t *testing.T) {
+		s := testcore.NewEnv(t, testcore.WithDynamicConfig(dynamicconfig.EnableChasm, true))
+		testWithChasm := true
+
+		configValues := s.GetTestCluster().Host().DcClient().GetValue(dynamicconfig.EnableChasm.Key())
+		s.NotEmpty(configValues, "EnableChasm config should be set")
+		configValue, _ := configValues[0].Value.(bool)
+		s.True(configValue, "EnableChasm config should be true")
+
+		// Set up SDK client and worker for the test-specific namespace
+		sdkClient, err := sdkclient.Dial(sdkclient.Options{
+			HostPort:  s.FrontendGRPCAddress(),
+			Namespace: s.Namespace().String(),
+			Logger:    log.NewSdkLogger(s.Logger),
+		})
+		s.NoError(err)
+		defer sdkClient.Close()
+
+		taskQueue := s.Tv().TaskQueue().Name
+		worker := sdkworker.New(sdkClient, taskQueue, sdkworker.Options{})
+
+		workflowFn := func(ctx workflow.Context) error {
+			var randomUUID string
+			err := workflow.SideEffect(
+				ctx,
+				func(workflow.Context) any { return uuid.New().String() },
+			).Get(&randomUUID)
+			if err != nil {
+				return err
 			}
-			break
+
+			_ = workflow.Sleep(ctx, 10*time.Minute)
+			return nil
 		}
-		time.Sleep(20 * time.Millisecond) //nolint:forbidigo
-	}
 
-	_, err = s.AdminClient().RebuildMutableState(ctx, &adminservice.RebuildMutableStateRequest{
-		Namespace: s.Namespace().String(),
-		Execution: &commonpb.WorkflowExecution{
-			WorkflowId: workflowID,
-			RunId:      runID,
-		},
+		worker.RegisterWorkflow(workflowFn)
+		s.NoError(worker.Start())
+		defer worker.Stop()
+
+		workflowID := s.Tv().Any().String()
+		workflowOptions := sdkclient.StartWorkflowOptions{
+			ID:                 workflowID,
+			TaskQueue:          taskQueue,
+			WorkflowRunTimeout: 20 * time.Second,
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		workflowRun, err := sdkClient.ExecuteWorkflow(context.Background(), workflowOptions, workflowFn)
+		s.NoError(err)
+		runID := workflowRun.GetRunID()
+
+		// there are total 6 events, 3 state transitions
+		//  1. WorkflowExecutionStarted
+		//  2. WorkflowTaskScheduled
+		//
+		//  3. WorkflowTaskStarted
+		//
+		//  4. WorkflowTaskCompleted
+		//  5. MarkerRecord
+		//  6. TimerStarted
+
+		var response1 *adminservice.DescribeMutableStateResponse
+		for {
+			response1, err = s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
+				Namespace: s.Namespace().String(),
+				Execution: &commonpb.WorkflowExecution{
+					WorkflowId: workflowID,
+					RunId:      runID,
+				},
+				Archetype: chasm.WorkflowArchetype,
+			})
+			s.NoError(err)
+			if response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount == 3 {
+				// Note: ChasmNodes may be empty even with CHASM enabled, so we only check if the rebuild can be performed,
+				// and not checking whether it is rebuildable because ChasmNodes are present.
+				if !testWithChasm {
+					s.Empty(response1.DatabaseMutableState.ChasmNodes, "CHASM-disabled workflows should not have ChasmNodes")
+				}
+				break
+			}
+			time.Sleep(20 * time.Millisecond) //nolint:forbidigo
+		}
+
+		_, err = s.AdminClient().RebuildMutableState(ctx, &adminservice.RebuildMutableStateRequest{
+			Namespace: s.Namespace().String(),
+			Execution: &commonpb.WorkflowExecution{
+				WorkflowId: workflowID,
+				RunId:      runID,
+			},
+		})
+		s.NoError(err)
+
+		response2, err := s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
+			Namespace: s.Namespace().String(),
+			Execution: &commonpb.WorkflowExecution{
+				WorkflowId: workflowID,
+				RunId:      runID,
+			},
+			Archetype: chasm.WorkflowArchetype,
+		})
+		s.NoError(err)
+		s.Equal(response1.DatabaseMutableState.ExecutionInfo.VersionHistories, response2.DatabaseMutableState.ExecutionInfo.VersionHistories)
+		s.Equal(response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount, response2.DatabaseMutableState.ExecutionInfo.StateTransitionCount)
+
+		s.Equal(response1.DatabaseMutableState.ExecutionState.CreateRequestId, response2.DatabaseMutableState.ExecutionState.CreateRequestId)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.RunId, response2.DatabaseMutableState.ExecutionState.RunId)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.State, response2.DatabaseMutableState.ExecutionState.State)
+		s.Equal(response1.DatabaseMutableState.ExecutionState.Status, response2.DatabaseMutableState.ExecutionState.Status)
+
+		// From transition history perspective, Rebuild is considered as an update to the workflow and updates
+		// all sub state machines in the workflow, which includes the workflow ExecutionState.
+		s.Equal(&persistencespb.VersionedTransition{
+			NamespaceFailoverVersion: response1.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition.NamespaceFailoverVersion,
+			TransitionCount:          response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount + 1,
+		}, response2.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition)
+
+		// Rebuild explicitly sets start time, thus start time will change after rebuild.
+		s.NotNil(response1.DatabaseMutableState.ExecutionState.StartTime)
+		s.NotNil(response2.DatabaseMutableState.ExecutionState.StartTime)
+
+		timeBefore := timestamp.TimeValue(response1.DatabaseMutableState.ExecutionState.StartTime)
+		timeAfter := timestamp.TimeValue(response2.DatabaseMutableState.ExecutionState.StartTime)
+		s.False(timeAfter.Before(timeBefore))
 	})
-	s.NoError(err)
-
-	response2, err := s.AdminClient().DescribeMutableState(ctx, &adminservice.DescribeMutableStateRequest{
-		Namespace: s.Namespace().String(),
-		Execution: &commonpb.WorkflowExecution{
-			WorkflowId: workflowID,
-			RunId:      runID,
-		},
-		Archetype: chasm.WorkflowArchetype,
-	})
-	s.NoError(err)
-	s.Equal(response1.DatabaseMutableState.ExecutionInfo.VersionHistories, response2.DatabaseMutableState.ExecutionInfo.VersionHistories)
-	s.Equal(response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount, response2.DatabaseMutableState.ExecutionInfo.StateTransitionCount)
-
-	s.Equal(response1.DatabaseMutableState.ExecutionState.CreateRequestId, response2.DatabaseMutableState.ExecutionState.CreateRequestId)
-	s.Equal(response1.DatabaseMutableState.ExecutionState.RunId, response2.DatabaseMutableState.ExecutionState.RunId)
-	s.Equal(response1.DatabaseMutableState.ExecutionState.State, response2.DatabaseMutableState.ExecutionState.State)
-	s.Equal(response1.DatabaseMutableState.ExecutionState.Status, response2.DatabaseMutableState.ExecutionState.Status)
-
-	// From transition history perspective, Rebuild is considered as an update to the workflow and updates
-	// all sub state machines in the workflow, which includes the workflow ExecutionState.
-	s.Equal(&persistencespb.VersionedTransition{
-		NamespaceFailoverVersion: response1.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition.NamespaceFailoverVersion,
-		TransitionCount:          response1.DatabaseMutableState.ExecutionInfo.StateTransitionCount + 1,
-	}, response2.DatabaseMutableState.ExecutionState.LastUpdateVersionedTransition)
-
-	// Rebuild explicitly sets start time, thus start time will change after rebuild.
-	s.NotNil(response1.DatabaseMutableState.ExecutionState.StartTime)
-	s.NotNil(response2.DatabaseMutableState.ExecutionState.StartTime)
-
-	timeBefore := timestamp.TimeValue(response1.DatabaseMutableState.ExecutionState.StartTime)
-	timeAfter := timestamp.TimeValue(response2.DatabaseMutableState.ExecutionState.StartTime)
-	s.False(timeAfter.Before(timeBefore))
 }
--- tests/relay_task_test.go
+++ tests/relay_task_test.go
@@ -5,100 +5,83 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/suite"
 	commandpb "go.temporal.io/api/command/v1"
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
-	taskqueuepb "go.temporal.io/api/taskqueue/v1"
 	"go.temporal.io/api/workflowservice/v1"
-	"go.temporal.io/server/common/log/tag"
 	"go.temporal.io/server/tests/testcore"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
 
-type RelayTaskTestSuite struct {
-	testcore.FunctionalTestBase
-}
+func TestRelayTask(t *testing.T) {
+	t.Run("WorkflowTaskTimeout", func(t *testing.T) {
+		s := testcore.NewEnv(t)
 
-func TestRelayTaskTestSuite(t *testing.T) {
-	t.Parallel()
-	suite.Run(t, new(RelayTaskTestSuite))
-}
-
-func (s *RelayTaskTestSuite) TestRelayWorkflowTaskTimeout() {
-	id := "functional-relay-workflow-task-timeout-test"
-	wt := "functional-relay-workflow-task-timeout-test-type"
-	tl := "functional-relay-workflow-task-timeout-test-taskqueue"
-	identity := "worker1"
-
-	// Start workflow execution
-	request := &workflowservice.StartWorkflowExecutionRequest{
-		RequestId:           uuid.NewString(),
-		Namespace:           s.Namespace().String(),
-		WorkflowId:          id,
-		WorkflowType:        &commonpb.WorkflowType{Name: wt},
-		TaskQueue:           &taskqueuepb.TaskQueue{Name: tl, Kind: enumspb.TASK_QUEUE_KIND_NORMAL},
-		Input:               nil,
-		WorkflowRunTimeout:  durationpb.New(100 * time.Second),
-		WorkflowTaskTimeout: durationpb.New(2 * time.Second),
-		Identity:            identity,
-	}
-
-	we, err0 := s.FrontendClient().StartWorkflowExecution(testcore.NewContext(), request)
-	s.NoError(err0)
-	s.Logger.Info("StartWorkflowExecution", tag.WorkflowRunID(we.RunId))
-
-	workflowExecution := &commonpb.WorkflowExecution{
-		WorkflowId: id,
-		RunId:      we.RunId,
-	}
-
-	workflowComplete, isFirst := false, true
-	wtHandler := func(task *workflowservice.PollWorkflowTaskQueueResponse) ([]*commandpb.Command, error) {
-		if isFirst {
-			isFirst = false
-			return []*commandpb.Command{{
-				CommandType: enumspb.COMMAND_TYPE_RECORD_MARKER,
-				Attributes: &commandpb.Command_RecordMarkerCommandAttributes{RecordMarkerCommandAttributes: &commandpb.RecordMarkerCommandAttributes{
-					MarkerName: "test-marker",
-				}},
-			}}, nil
+		request := &workflowservice.StartWorkflowExecutionRequest{
+			RequestId:           uuid.NewString(),
+			Namespace:           s.Namespace().String(),
+			WorkflowId:          s.Tv().WorkflowID(),
+			WorkflowType:        s.Tv().WorkflowType(),
+			TaskQueue:           s.Tv().TaskQueue(),
+			Input:               nil,
+			WorkflowRunTimeout:  durationpb.New(100 * time.Second),
+			WorkflowTaskTimeout: durationpb.New(2 * time.Second),
+			Identity:            s.Tv().WorkerIdentity(),
 		}
-		workflowComplete = true
-		return []*commandpb.Command{{
-			CommandType: enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION,
-			Attributes:  &commandpb.Command_CompleteWorkflowExecutionCommandAttributes{CompleteWorkflowExecutionCommandAttributes: &commandpb.CompleteWorkflowExecutionCommandAttributes{}}}}, nil
-	}
 
-	poller := &testcore.TaskPoller{
-		Client:              s.FrontendClient(),
-		Namespace:           s.Namespace().String(),
-		TaskQueue:           &taskqueuepb.TaskQueue{Name: tl, Kind: enumspb.TASK_QUEUE_KIND_NORMAL},
-		Identity:            identity,
-		WorkflowTaskHandler: wtHandler,
-		ActivityTaskHandler: nil,
-		Logger:              s.Logger,
-		T:                   s.T(),
-	}
+		we, err := s.FrontendClient().StartWorkflowExecution(testcore.NewContext(), request)
+		s.NoError(err)
 
-	// First workflow task complete with a marker command, and request to relay workflow task (immediately return a new workflow task)
-	res, err := poller.PollAndProcessWorkflowTask(
-		testcore.WithExpectedAttemptCount(0),
-		testcore.WithRetries(3),
-		testcore.WithForceNewWorkflowTask)
-	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
-	s.NoError(err)
-	newTask := res.NewTask
-	s.NotNil(newTask)
-	s.NotNil(newTask.WorkflowTask)
+		workflowExecution := &commonpb.WorkflowExecution{
+			WorkflowId: s.Tv().WorkflowID(),
+			RunId:      we.RunId,
+		}
 
-	//nolint:forbidigo
-	time.Sleep(time.Second * 2) // wait 2s for relay workflow task to timeout
-	workflowTaskTimeout := false
-	for i := 0; i < 3; i++ {
-		events := s.GetHistory(s.Namespace().String(), workflowExecution)
-		if len(events) == 8 {
-			s.EqualHistoryEvents(`
+		workflowComplete, isFirst := false, true
+		wtHandler := func(task *workflowservice.PollWorkflowTaskQueueResponse) ([]*commandpb.Command, error) {
+			if isFirst {
+				isFirst = false
+				return []*commandpb.Command{{
+					CommandType: enumspb.COMMAND_TYPE_RECORD_MARKER,
+					Attributes: &commandpb.Command_RecordMarkerCommandAttributes{RecordMarkerCommandAttributes: &commandpb.RecordMarkerCommandAttributes{
+						MarkerName: "test-marker",
+					}},
+				}}, nil
+			}
+			workflowComplete = true
+			return []*commandpb.Command{{
+				CommandType: enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION,
+				Attributes:  &commandpb.Command_CompleteWorkflowExecutionCommandAttributes{CompleteWorkflowExecutionCommandAttributes: &commandpb.CompleteWorkflowExecutionCommandAttributes{}}}}, nil
+		}
+
+		poller := &testcore.TaskPoller{
+			Client:              s.FrontendClient(),
+			Namespace:           s.Namespace().String(),
+			TaskQueue:           s.Tv().TaskQueue(),
+			Identity:            s.Tv().WorkerIdentity(),
+			WorkflowTaskHandler: wtHandler,
+			ActivityTaskHandler: nil,
+			Logger:              s.Logger,
+			T:                   s.T(),
+		}
+
+		// First workflow task complete with a marker command, and request to relay workflow task (immediately return a new workflow task)
+		res, err := poller.PollAndProcessWorkflowTask(
+			testcore.WithExpectedAttemptCount(0),
+			testcore.WithRetries(3),
+			testcore.WithForceNewWorkflowTask)
+		s.NoError(err)
+		newTask := res.NewTask
+		s.NotNil(newTask)
+		s.NotNil(newTask.WorkflowTask)
+
+		//nolint:forbidigo
+		time.Sleep(time.Second * 2) // wait 2s for relay workflow task to timeout
+		workflowTaskTimeout := false
+		for i := 0; i < 3; i++ {
+			events := s.GetHistory(s.Namespace().String(), workflowExecution)
+			if len(events) == 8 {
+				s.EqualHistoryEvents(`
   1 WorkflowExecutionStarted
   2 WorkflowTaskScheduled
   3 WorkflowTaskStarted
@@ -107,18 +90,18 @@ func (s *RelayTaskTestSuite) TestRelayWorkflowTaskTimeout() {
   6 WorkflowTaskScheduled
   7 WorkflowTaskStarted
   8 WorkflowTaskTimedOut {"ScheduledEventId":6,"StartedEventId":7,"TimeoutType":1} // TIMEOUT_TYPE_START_TO_CLOSE`, events)
-			workflowTaskTimeout = true
-			break
+				workflowTaskTimeout = true
+				break
+			}
+			time.Sleep(time.Second) //nolint:forbidigo
 		}
-		time.Sleep(time.Second) //nolint:forbidigo
-	}
-	// verify relay workflow task timeout
-	s.True(workflowTaskTimeout)
+		// verify relay workflow task timeout
+		s.True(workflowTaskTimeout)
 
-	// Now complete workflow
-	_, err = poller.PollAndProcessWorkflowTask(testcore.WithDumpHistory, testcore.WithExpectedAttemptCount(2))
-	s.Logger.Info("PollAndProcessWorkflowTask", tag.Error(err))
-	s.NoError(err)
+		// Now complete workflow
+		_, err = poller.PollAndProcessWorkflowTask(testcore.WithDumpHistory, testcore.WithExpectedAttemptCount(2))
+		s.NoError(err)
 
-	s.True(workflowComplete)
+		s.True(workflowComplete)
+	})
 }
