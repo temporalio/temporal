565246d30192f18bd834ffe0905eac1d00925df4
Stephan Behnke <stephanos@users.noreply.github.com>
2026-02-02 10:31:04 -0800

    CHASM workflow command handler for Nexus (#9109)
    - Added workflow command handler registry to CHASM's workflow library.
    - Integrated CHASM's workflow library into workflow completion handler.
    
    Migrating Nexus from HSM to CHASM.
    
    Tests will be ported over once actual command handler implementations
    are added.

--- service/history/api/respondworkflowtaskcompleted/api.go
+++ service/history/api/respondworkflowtaskcompleted/api.go
@@ -16,6 +16,7 @@ import (
 	"go.temporal.io/server/api/matchingservice/v1"
 	tokenspb "go.temporal.io/server/api/token/v1"
 	"go.temporal.io/server/chasm"
+	chasmcommand "go.temporal.io/server/chasm/lib/workflow/command"
 	"go.temporal.io/server/common"
 	"go.temporal.io/server/common/clock"
 	"go.temporal.io/server/common/collection"
@@ -61,6 +62,7 @@ type (
 		searchAttributesValidator      *searchattribute.Validator
 		persistenceVisibilityMgr       manager.VisibilityManager
 		commandHandlerRegistry         *workflow.CommandHandlerRegistry
+		chasmCommandRegistry           *chasmcommand.Registry
 		matchingClient                 matchingservice.MatchingServiceClient
 		versionMembershipCache         worker_versioning.VersionMembershipCache
 	}
@@ -71,6 +73,7 @@ func NewWorkflowTaskCompletedHandler(
 	tokenSerializer *tasktoken.Serializer,
 	eventNotifier events.Notifier,
 	commandHandlerRegistry *workflow.CommandHandlerRegistry,
+	chasmCommandRegistry *chasmcommand.Registry,
 	searchAttributesValidator *searchattribute.Validator,
 	visibilityManager manager.VisibilityManager,
 	workflowConsistencyChecker api.WorkflowConsistencyChecker,
@@ -97,6 +100,7 @@ func NewWorkflowTaskCompletedHandler(
 		searchAttributesValidator:      searchAttributesValidator,
 		persistenceVisibilityMgr:       visibilityManager,
 		commandHandlerRegistry:         commandHandlerRegistry,
+		chasmCommandRegistry:           chasmCommandRegistry,
 		matchingClient:                 matchingClient,
 		versionMembershipCache:         versionMembershipCache,
 	}
@@ -403,6 +407,7 @@ func (handler *WorkflowTaskCompletedHandler) Invoke(
 			handler.searchAttributesMapperProvider,
 			hasBufferedEventsOrMessages,
 			handler.commandHandlerRegistry,
+			handler.chasmCommandRegistry,
 			handler.matchingClient,
 			handler.versionMembershipCache,
 		)
