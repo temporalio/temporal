c930a214c0553859a0d0e2d0160635935edc75cd
Shivam Saraf <shivam.saraf@temporal.io>
2026-02-04 18:26:45 -0500

    fix comment

--- service/matching/matching_engine.go
+++ service/matching/matching_engine.go
@@ -1959,8 +1959,8 @@ func (e *matchingEngineImpl) SyncDeploymentUserData(
 	if err != nil {
 		return nil, err
 	}
-	// Simulating v1.29.2 validation: In v1.29.2, this checked `req.Deployment == nil && req.GetOperation() == nil`
-	// Since v1.30.0 never sets Deployment (field was removed), we simulate by checking just Operation
+	// Simulating v1.29.1 validation: In v1.29.1, this checked `req.Deployment == nil && req.GetOperation() == nil`
+	// Since cloud v1.30.0 never sets Deployment (field was removed), we simulate by checking just Operation
 	if req.GetOperation() == nil {
 		return nil, errMissingDeploymentVersion
 	}
