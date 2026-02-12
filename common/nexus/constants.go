fb39d0cc8a1b047407a40f5a56121ed7a8725c87
Kannan Rajah <kannan.rajah@temporal.io>
2026-02-12 11:53:23 -0800

    Use correct Nexus service name from proto definition

--- common/nexus/constants.go
+++ common/nexus/constants.go
@@ -7,7 +7,7 @@ const SystemCallbackURL = "temporal://system"
 // Worker control Nexus service and operations for server-to-worker communication.
 const (
 	// WorkerControlService is the Nexus service name for worker control operations.
-	WorkerControlService = "temporal-sys-worker-control"
+	WorkerControlService = "temporal.api.worker.v1.WorkerService"
 	// CancelActivitiesOperation is the operation to cancel running activities.
 	CancelActivitiesOperation = "cancel-activities"
 )
