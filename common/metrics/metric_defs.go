b3805c8403c9072dfc9fe71968e9f56989319b0e
Kannan Rajah <kannan.rajah@temporal.io>
2026-02-11 19:36:31 -0800

    Add metrics tag and low priority for CancelActivityNexusTask

--- common/metrics/metric_defs.go
+++ common/metrics/metric_defs.go
@@ -567,6 +567,7 @@ const (
 	TaskTypeTransferActiveTaskStartChildExecution         = "TransferActiveTaskStartChildExecution"
 	TaskTypeTransferActiveTaskResetWorkflow               = "TransferActiveTaskResetWorkflow"
 	TaskTypeTransferActiveTaskDeleteExecution             = "TransferActiveTaskDeleteExecution"
+	TaskTypeTransferActiveTaskCancelActivityNexus         = "TransferActiveTaskCancelActivityNexus"
 	TaskTypeTransferStandbyTaskActivity                   = "TransferStandbyTaskActivity"
 	TaskTypeTransferStandbyTaskWorkflowTask               = "TransferStandbyTaskWorkflowTask"
 	TaskTypeTransferStandbyTaskCloseExecution             = "TransferStandbyTaskCloseExecution"
