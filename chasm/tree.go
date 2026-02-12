d368ea49a69782bec1ef30a06698d18325c19d87
Yichao Yang <yichao@temporal.io>
2026-02-12 13:44:52 -0800

    CHASM: Avoid double map lookup in registry

--- chasm/tree.go
+++ chasm/tree.go
@@ -1788,7 +1788,7 @@ func (n *Node) closeTransactionUpdateComponentTasks(
 func (n *Node) deserializeComponentTask(
 	componentTask *persistencespb.ChasmComponentAttributes_Task,
 ) (any, error) {
-	registableTask, ok := n.registry.taskByID(componentTask.TypeId)
+	registableTask, ok := n.registry.TaskByID(componentTask.TypeId)
 	if !ok {
 		return nil, softassert.UnexpectedInternalErr(
 			n.logger,
@@ -2975,7 +2975,7 @@ func (n *Node) ValidateSideEffectTask(
 
 	taskInfo := chasmTask.Info
 	taskTypeID := taskInfo.TypeId
-	registrableTask, ok := n.registry.taskByID(taskTypeID)
+	registrableTask, ok := n.registry.TaskByID(taskTypeID)
 	if !ok {
 		return false, softassert.UnexpectedInternalErr(
 			n.logger,
@@ -3060,7 +3060,7 @@ func (n *Node) ExecuteSideEffectTask(
 
 	taskInfo := chasmTask.Info
 	taskTypeID := taskInfo.TypeId
-	registrableTask, ok := registry.taskByID(taskTypeID)
+	registrableTask, ok := registry.TaskByID(taskTypeID)
 	if !ok {
 		return softassert.UnexpectedInternalErr(
 			n.logger,
