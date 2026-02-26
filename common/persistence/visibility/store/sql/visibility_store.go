hanzoai/tasks

Ignore unknown search attributes in visibility (#9394)
## What changed?
Ignore unknown search attributes in visibility when encoding and
decoding.

## Why?
Unknown search attributes can only be possible on read/write path due to
a Predefined search attribute written by higher version server not
existing in lower version server constants.go that runs any Visibility
task.

This also fixes the read path discrepancy between ElasticSearch and SQL,
where ElasticSearch was dropping unknown search attribute field names,
but SQL was failing. Now, read path also ignores unknown search
attributes when reading instead of failing.

## How did you test it?
- [X] built
- [X] run locally and tested manually
- [X] covered by existing tests
- [X] added new unit test(s)
- [ ] added new functional test(s)

common/persistence/visibility/store/sql/visibility_store.go
@@ -14,6 +14,7 @@ import (
 	"go.temporal.io/server/common/config"
 	"go.temporal.io/server/common/dynamicconfig"
 	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/log/tag"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/namespace"
 	"go.temporal.io/server/common/persistence"
@@ -25,7 +26,6 @@ import (
 	"go.temporal.io/server/common/persistence/visibility/store/query"
 	"go.temporal.io/server/common/resolver"
 	"go.temporal.io/server/common/searchattribute"
-	"go.temporal.io/server/common/searchattribute/sadefs"
 )
 
 type (
@@ -34,6 +34,8 @@ type (
 		searchAttributesProvider       searchattribute.Provider
 		searchAttributesMapperProvider searchattribute.MapperProvider
 		chasmRegistry                  *chasm.Registry
+		metricsHandler                 metrics.Handler
+		logger                         log.Logger
 
 		enableUnifiedQueryConverter dynamicconfig.BoolPropertyFn
 	}
@@ -75,6 +77,8 @@ func NewSQLVisibilityStore(
 		searchAttributesProvider:       searchAttributesProvider,
 		searchAttributesMapperProvider: searchAttributesMapperProvider,
 		chasmRegistry:                  chasmRegistry,
+		metricsHandler:                 metricsHandler,
+		logger:                         logger,
 
 		enableUnifiedQueryConverter: enableUnifiedQueryConverter,
 	}, nil
@@ -790,6 +794,13 @@ func (s *VisibilityStore) prepareSearchAttributesForDb(
 	if err != nil {
 		return nil, err
 	}
+	if len(request.SearchAttributes.GetIndexedFields()) != len(searchAttributes) {
+		for name := range request.SearchAttributes.GetIndexedFields() {
+			if _, ok := searchAttributes[name]; !ok {
+				s.logger.Warn("Skipping unknown search attribute while generating visibility record", tag.String("search-attribute", name))
+			}
+		}
+	}
 	// This is to prevent existing tasks to fail indefinitely.
 	// If it's only invalid values error, then silently continue without them.
 	searchAttributes, err = s.ValidateCustomSearchAttributes(searchAttributes)
@@ -876,8 +887,7 @@ func (s *VisibilityStore) encodeRowSearchAttributes(
 	for name, value := range rowSearchAttributes {
 		tp, err := combinedTypeMap.GetType(name)
 		if err != nil {
-			// Silently ignore ErrInvalidName for unregistered chasm search attributes
-			if sadefs.IsChasmSearchAttribute(name) && errors.Is(err, searchattribute.ErrInvalidName) {
+			if errors.Is(err, searchattribute.ErrInvalidName) {
 				continue
 			}
 			return nil, err

common/searchattribute/encode_test.go
@@ -69,7 +69,7 @@ func Test_Encode_NilMap(t *testing.T) {
 	r.Equal("json/plain", string(sa.IndexedFields["key6"].GetMetadata()["encoding"]))
 }
 
-func Test_Encode_Error(t *testing.T) {
+func Test_Encode_SkipsUnknownSearchAttributes(t *testing.T) {
 	r := require.New(t)
 	sa, err := Encode(map[string]any{
 		"key1": "val1",
@@ -77,16 +77,15 @@ func Test_Encode_Error(t *testing.T) {
 		"key3": true,
 	}, &NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
 		"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
-		"key4": enumspb.INDEXED_VALUE_TYPE_INT,
 		"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
 	}})
 
-	r.Error(err)
-	r.ErrorIs(err, ErrInvalidName)
-	r.Len(sa.IndexedFields, 3)
+	// key2 is unknown and should be silently skipped.
+	r.NoError(err)
+	r.Len(sa.IndexedFields, 2)
 	r.Equal(`"val1"`, string(sa.IndexedFields["key1"].GetData()))
 	r.Equal("Text", string(sa.IndexedFields["key1"].GetMetadata()["type"]))
-	r.Equal("2", string(sa.IndexedFields["key2"].GetData()))
+	r.NotContains(sa.IndexedFields, "key2")
 	r.Equal("true", string(sa.IndexedFields["key3"].GetData()))
 	r.Equal("Bool", string(sa.IndexedFields["key3"].GetMetadata()["type"]))
 }
@@ -171,6 +170,37 @@ func Test_Decode_NilMap(t *testing.T) {
 	r.Nil(vals["key6"])
 }
 
+func Test_Decode_SkipsUnknownSearchAttributes(t *testing.T) {
+	r := require.New(t)
+
+	typeMap := &NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
+		"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
+		"key2": enumspb.INDEXED_VALUE_TYPE_INT,
+		"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
+	}}
+	sa, err := Encode(map[string]any{
+		"key1": "val1",
+		"key2": 2,
+		"key3": true,
+	}, typeMap)
+	r.NoError(err)
+
+	// Decode with a typeMap that doesn't include key2: key2 should be silently skipped.
+	vals, err := Decode(
+		sa,
+		&NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
+			"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
+			"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
+		}},
+		true,
+	)
+	r.NoError(err)
+	r.Len(vals, 2)
+	r.Equal("val1", vals["key1"])
+	r.NotContains(vals, "key2")
+	r.Equal(true, vals["key3"])
+}
+
 func Test_Decode_Error(t *testing.T) {
 	r := require.New(t)
 
@@ -186,27 +216,11 @@ func Test_Decode_Error(t *testing.T) {
 	}, typeMap)
 	r.NoError(err)
 
-	vals, err := Decode(
-		sa,
-		&NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
-			"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
-			"key4": enumspb.INDEXED_VALUE_TYPE_INT,
-			"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
-		}},
-		true,
-	)
-	r.Error(err)
-	r.ErrorIs(err, ErrInvalidName)
-	r.Len(sa.IndexedFields, 3)
-	r.Equal("val1", vals["key1"])
-	r.Equal(int64(2), vals["key2"])
-	r.Equal(true, vals["key3"])
-
 	delete(sa.IndexedFields["key1"].Metadata, "type")
 	delete(sa.IndexedFields["key2"].Metadata, "type")
 	delete(sa.IndexedFields["key3"].Metadata, "type")
 
-	vals, err = Decode(sa, nil, true)
+	vals, err := Decode(sa, nil, true)
 	r.Error(err)
 	r.ErrorIs(err, ErrInvalidType)
 	r.Len(vals, 3)

temporalio/temporal

common/persistence/visibility/store/sql/visibility_store.go
@@ -12,6 +12,7 @@ import (
 	"go.temporal.io/api/workflowservice/v1"
 	"go.temporal.io/server/common/config"
 	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/log/tag"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/namespace"
 	"go.temporal.io/server/common/persistence"
@@ -29,6 +30,8 @@ type (
 		sqlStore                       persistencesql.SqlStore
 		searchAttributesProvider       searchattribute.Provider
 		searchAttributesMapperProvider searchattribute.MapperProvider
+		metricsHandler                 metrics.Handler
+		logger                         log.Logger
 	}
 )
 
@@ -54,6 +57,8 @@ func NewSQLVisibilityStore(
 		sqlStore:                       persistencesql.NewSqlStore(db, logger),
 		searchAttributesProvider:       searchAttributesProvider,
 		searchAttributesMapperProvider: searchAttributesMapperProvider,
+		metricsHandler:                 metricsHandler,
+		logger:                         logger,
 	}, nil
 }
 
@@ -386,6 +391,13 @@ func (s *VisibilityStore) prepareSearchAttributesForDb(
 	if err != nil {
 		return nil, err
 	}
+	if len(request.SearchAttributes.GetIndexedFields()) != len(searchAttributes) {
+		for name := range request.SearchAttributes.GetIndexedFields() {
+			if _, ok := searchAttributes[name]; !ok {
+				s.logger.Warn("Skipping unknown search attribute while generating visibility record", tag.NewStringTag("search-attribute", name))
+			}
+		}
+	}
 	// This is to prevent existing tasks to fail indefinitely.
 	// If it's only invalid values error, then silently continue without them.
 	searchAttributes, err = s.ValidateCustomSearchAttributes(searchAttributes)
@@ -471,6 +483,9 @@ func (s *VisibilityStore) processRowSearchAttributes(
 	for name, value := range rowSearchAttributes {
 		tp, err := saTypeMap.GetType(name)
 		if err != nil {
+			if errors.Is(err, searchattribute.ErrInvalidName) {
+				continue
+			}
 			return nil, err
 		}
 		if tp == enumspb.INDEXED_VALUE_TYPE_KEYWORD_LIST {

common/searchattribute/encode_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	enumspb "go.temporal.io/api/enums/v1"
 )
 
@@ -69,7 +70,7 @@ func Test_Encode_NilMap(t *testing.T) {
 	assert.Equal("json/plain", string(sa.IndexedFields["key6"].GetMetadata()["encoding"]))
 }
 
-func Test_Encode_Error(t *testing.T) {
+func Test_Encode_SkipsUnknownSearchAttributes(t *testing.T) {
 	assert := assert.New(t)
 	sa, err := Encode(map[string]interface{}{
 		"key1": "val1",
@@ -77,16 +78,15 @@ func Test_Encode_Error(t *testing.T) {
 		"key3": true,
 	}, &NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
 		"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
-		"key4": enumspb.INDEXED_VALUE_TYPE_INT,
 		"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
 	}})
 
-	assert.Error(err)
-	assert.ErrorIs(err, ErrInvalidName)
-	assert.Len(sa.IndexedFields, 3)
+	// key2 is unknown and should be silently skipped.
+	assert.NoError(err)
+	assert.Len(sa.IndexedFields, 2)
 	assert.Equal(`"val1"`, string(sa.IndexedFields["key1"].GetData()))
 	assert.Equal("Text", string(sa.IndexedFields["key1"].GetMetadata()["type"]))
-	assert.Equal("2", string(sa.IndexedFields["key2"].GetData()))
+	assert.NotContains(sa.IndexedFields, "key2")
 	assert.Equal("true", string(sa.IndexedFields["key3"].GetData()))
 	assert.Equal("Bool", string(sa.IndexedFields["key3"].GetMetadata()["type"]))
 }
@@ -171,8 +171,8 @@ func Test_Decode_NilMap(t *testing.T) {
 	assert.Nil(vals["key6"])
 }
 
-func Test_Decode_Error(t *testing.T) {
-	assert := assert.New(t)
+func Test_Decode_SkipsUnknownSearchAttributes(t *testing.T) {
+	r := require.New(t)
 
 	typeMap := &NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
 		"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
@@ -184,33 +184,46 @@ func Test_Decode_Error(t *testing.T) {
 		"key2": 2,
 		"key3": true,
 	}, typeMap)
-	assert.NoError(err)
+	r.NoError(err)
 
+	// Decode with a typeMap that doesn't include key2: key2 should be silently skipped.
 	vals, err := Decode(
 		sa,
 		&NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
 			"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
-			"key4": enumspb.INDEXED_VALUE_TYPE_INT,
 			"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
 		}},
 		true,
 	)
-	assert.Error(err)
-	assert.ErrorIs(err, ErrInvalidName)
-	assert.Len(sa.IndexedFields, 3)
-	assert.Equal("val1", vals["key1"])
-	assert.Equal(int64(2), vals["key2"])
-	assert.Equal(true, vals["key3"])
+	r.NoError(err)
+	r.Len(vals, 2)
+	r.Equal("val1", vals["key1"])
+	r.NotContains(vals, "key2")
+	r.Equal(true, vals["key3"])
+}
+
+func Test_Decode_Error(t *testing.T) {
+	typeMap := &NameTypeMap{customSearchAttributes: map[string]enumspb.IndexedValueType{
+		"key1": enumspb.INDEXED_VALUE_TYPE_TEXT,
+		"key2": enumspb.INDEXED_VALUE_TYPE_INT,
+		"key3": enumspb.INDEXED_VALUE_TYPE_BOOL,
+	}}
+	sa, err := Encode(map[string]interface{}{
+		"key1": "val1",
+		"key2": 2,
+		"key3": true,
+	}, typeMap)
+	assert.NoError(t, err)
 
 	delete(sa.IndexedFields["key1"].Metadata, "type")
 	delete(sa.IndexedFields["key2"].Metadata, "type")
 	delete(sa.IndexedFields["key3"].Metadata, "type")
 
-	vals, err = Decode(sa, nil, true)
-	assert.Error(err)
-	assert.ErrorIs(err, ErrInvalidType)
-	assert.Len(vals, 3)
-	assert.Nil(vals["key1"])
-	assert.Nil(vals["key2"])
-	assert.Nil(vals["key3"])
+	vals, err := Decode(sa, nil, true)
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, ErrInvalidType)
+	assert.Len(t, vals, 3)
+	assert.Nil(t, vals["key1"])
+	assert.Nil(t, vals["key2"])
+	assert.Nil(t, vals["key3"])
 }