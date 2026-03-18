b63999e8c00bbe9845fd5537d6ab7d354ad72511
Chetan Gowda <gow@users.noreply.github.com>
2026-03-18 13:13:16 -0700

    [Nexus] Migrate registry and event definitions (#9491)
    ## What changed?
    Migrating Nexus history event Registry and Definition. I've also moved
    all the event implementations as well with commented out bodies. I will
    replace the implementations of `Apply()` and `CherryPick()` in follow
    PRs.
    Depends on https://github.com/temporalio/temporal/pull/9474
    
    ## Why?
    Migrating Nexus from HSM to CHASM.
    
    ## How did you test it?
    - [x] built
    - [ ] run locally and tested manually
    - [ ] covered by existing tests
    - [ ] added new unit test(s)
    - [ ] added new functional test(s)
    
    
    
    <!-- CURSOR_SUMMARY -->
    ---
    
    > [!NOTE]
    > **Medium Risk**
    > Introduces new history event registration and lookup paths for Nexus
    operations; since the `Apply` implementations are currently stubs,
    there’s some risk of silently skipping state transitions during
    replication/reset until follow-up PRs complete the logic.
    > 
    > **Overview**
    > Adds first-class **history event definitions** to
    `workflowregistry.Registry` via a new `EventDefinition` interface (with
    `Apply` and `CherryPick`) and
    `RegisterEventDefinition`/`EventDefinition` APIs.
    > 
    > Wires Nexus operation event registration into the `fx` module and
    introduces `events.go` with definitions for Nexus lifecycle events
    (scheduled/cancel/start/complete/fail/cancel/timeout), including basic
    workflow-task-trigger flags and `CherryPick` exclusion handling (notably
    `RESET_REAPPLY_EXCLUDE_TYPE_NEXUS`), while leaving `Apply` bodies as
    TODO stubs.
    > 
    > <sup>Written by [Cursor
    Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
    e4bd20b4009faf991b61586d9b31aea4901fc328. This will update automatically
    on new commits. Configure
    [here](https://cursor.com/dashboard?tab=bugbot).</sup>
    <!-- /CURSOR_SUMMARY -->

---
+++ chasm/lib/workflow/workflowregistry/events.go
@@ -0,0 +1,28 @@
+package workflowregistry
+
+import (
+	"errors"
+
+	enumspb "go.temporal.io/api/enums/v1"
+	historypb "go.temporal.io/api/history/v1"
+	"go.temporal.io/server/chasm"
+	chasmworkflow "go.temporal.io/server/chasm/lib/workflow"
+)
+
+// ErrEventNotCherryPickable should be returned by CherryPick if an event should not be cherry picked for whatever reason.
+var ErrEventNotCherryPickable = errors.New("event not cherry pickable")
+
+// EventDefinition is a definition for a history event for a given event type.
+type EventDefinition interface {
+	Type() enumspb.EventType
+	// IsWorkflowTaskTrigger returns a boolean indicating whether this event type should trigger a workflow task.
+	IsWorkflowTaskTrigger() bool
+	// Apply a history event to the state machine. Triggered during replication and workflow reset.
+	Apply(ctx chasm.MutableContext, wf *chasmworkflow.Workflow, event *historypb.HistoryEvent) error
+	// CherryPick (a.k.a "reapply") an event from a different history branch.
+	// Implementations should apply the event to the machine state and return nil in case the event is cherry-pickable.
+	// Command events should never be cherry picked as we rely on the workflow to reschedule them.
+	// Return [ErrEventNotCherryPickable] to skip cherry picking. Any other error is considered fatal and will abort the
+	// cherry pick process.
+	CherryPick(ctx chasm.MutableContext, wf *chasmworkflow.Workflow, event *historypb.HistoryEvent, resetReapplyExcludeTypes map[enumspb.ResetReapplyExcludeType]struct{}) error
+}
