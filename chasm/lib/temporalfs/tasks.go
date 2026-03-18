3eff7dacd1cb973ba3f76dee170ccaf1a1411616
Mohammad Dashti <mohammad.dashti@temporal.io>
2026-03-18 11:49:55 -0700

    Implement GC and quota task executors with temporal-fs
    ChunkGC executor now opens the FS store and runs f.RunGC() to process
    tombstones and delete orphaned chunks, then reschedules itself.
    QuotaCheck executor reads FS metrics to update stats and warns when
    size quota is exceeded. ManifestCompact remains a placeholder since
    compaction operates at the PebbleDB shard level.

--- chasm/lib/temporalfs/tasks.go
+++ chasm/lib/temporalfs/tasks.go
@@ -1,17 +1,22 @@
 package temporalfs
 
 import (
+	tfs "github.com/temporalio/temporal-fs/pkg/fs"
 	"go.temporal.io/server/chasm"
 	temporalfspb "go.temporal.io/server/chasm/lib/temporalfs/gen/temporalfspb/v1"
+	"go.temporal.io/server/common/log"
+	"go.temporal.io/server/common/log/tag"
 )
 
 // chunkGCTaskExecutor handles periodic garbage collection of orphaned chunks.
 type chunkGCTaskExecutor struct {
-	config *Config
+	config        *Config
+	logger        log.Logger
+	storeProvider FSStoreProvider
 }
 
-func newChunkGCTaskExecutor(config *Config) *chunkGCTaskExecutor {
-	return &chunkGCTaskExecutor{config: config}
+func newChunkGCTaskExecutor(config *Config, logger log.Logger, storeProvider FSStoreProvider) *chunkGCTaskExecutor {
+	return &chunkGCTaskExecutor{config: config, logger: logger, storeProvider: storeProvider}
 }
 
 func (e *chunkGCTaskExecutor) Validate(
@@ -29,26 +34,64 @@ func (e *chunkGCTaskExecutor) Execute(
 	_ chasm.TaskAttributes,
 	task *temporalfspb.ChunkGCTask,
 ) error {
-	// TODO: Implement GC using temporal-fs store when available.
-	// For now, reschedule the next GC run.
-	if gcInterval := fs.Config.GetGcInterval().AsDuration(); gcInterval > 0 {
+	key := ctx.ExecutionKey()
+
+	s, err := e.storeProvider.GetStore(0, key.NamespaceID, key.BusinessID)
+	if err != nil {
+		e.logger.Error("GC: failed to get store", tag.Error(err))
+		return e.rescheduleGC(ctx, fs, task.GetLastProcessedTxnId())
+	}
+
+	f, err := tfs.Open(s)
+	if err != nil {
+		_ = s.Close()
+		e.logger.Error("GC: failed to open FS", tag.Error(err))
+		return e.rescheduleGC(ctx, fs, task.GetLastProcessedTxnId())
+	}
+
+	gcStats := f.RunGC(tfs.GCConfig{
+		BatchSize:         100,
+		MaxChunksPerRound: 10000,
+	})
+	f.Close()
+
+	e.logger.Info("GC completed",
+		tag.NewStringTag("filesystem_id", key.BusinessID),
+		tag.NewInt64("tombstones_processed", int64(gcStats.TombstonesProcessed)),
+		tag.NewInt64("chunks_deleted", int64(gcStats.ChunksDeleted)),
+	)
+
+	// Update CHASM state stats from FS metrics.
+	if fs.Stats == nil {
+		fs.Stats = &temporalfspb.FSStats{}
+	}
+	fs.Stats.TransitionCount++
+	fs.Stats.ChunkCount -= uint64(gcStats.ChunksDeleted)
+
+	return e.rescheduleGC(ctx, fs, task.GetLastProcessedTxnId())
+}
+
+func (e *chunkGCTaskExecutor) rescheduleGC(ctx chasm.MutableContext, fs *Filesystem, lastTxnID uint64) error {
+	gcInterval := fs.Config.GetGcInterval().AsDuration()
+	if gcInterval > 0 {
 		ctx.AddTask(fs, chasm.TaskAttributes{
 			ScheduledTime: ctx.Now(fs).Add(gcInterval),
 		}, &temporalfspb.ChunkGCTask{
-			LastProcessedTxnId: task.GetLastProcessedTxnId(),
+			LastProcessedTxnId: lastTxnID,
 		})
 	}
-
 	return nil
 }
 
-// manifestCompactTaskExecutor handles compaction of manifest diff chains.
+// manifestCompactTaskExecutor handles compaction of the underlying PebbleDB store.
 type manifestCompactTaskExecutor struct {
-	config *Config
+	config        *Config
+	logger        log.Logger
+	storeProvider FSStoreProvider
 }
 
-func newManifestCompactTaskExecutor(config *Config) *manifestCompactTaskExecutor {
-	return &manifestCompactTaskExecutor{config: config}
+func newManifestCompactTaskExecutor(config *Config, logger log.Logger, storeProvider FSStoreProvider) *manifestCompactTaskExecutor {
+	return &manifestCompactTaskExecutor{config: config, logger: logger, storeProvider: storeProvider}
 }
 
 func (e *manifestCompactTaskExecutor) Validate(
@@ -66,17 +109,20 @@ func (e *manifestCompactTaskExecutor) Execute(
 	_ chasm.TaskAttributes,
 	_ *temporalfspb.ManifestCompactTask,
 ) error {
-	// TODO: Implement manifest compaction using temporal-fs store when available.
+	// Compaction is handled at the PebbleDB level per shard, not per filesystem.
+	// This task is a placeholder for future per-FS compaction triggers.
 	return nil
 }
 
 // quotaCheckTaskExecutor enforces storage quotas.
 type quotaCheckTaskExecutor struct {
-	config *Config
+	config        *Config
+	logger        log.Logger
+	storeProvider FSStoreProvider
 }
 
-func newQuotaCheckTaskExecutor(config *Config) *quotaCheckTaskExecutor {
-	return &quotaCheckTaskExecutor{config: config}
+func newQuotaCheckTaskExecutor(config *Config, logger log.Logger, storeProvider FSStoreProvider) *quotaCheckTaskExecutor {
+	return &quotaCheckTaskExecutor{config: config, logger: logger, storeProvider: storeProvider}
 }
 
 func (e *quotaCheckTaskExecutor) Validate(
@@ -89,13 +135,46 @@ func (e *quotaCheckTaskExecutor) Validate(
 }
 
 func (e *quotaCheckTaskExecutor) Execute(
-	_ chasm.MutableContext,
+	ctx chasm.MutableContext,
 	fs *Filesystem,
 	_ chasm.TaskAttributes,
 	_ *temporalfspb.QuotaCheckTask,
 ) error {
-	// TODO: Implement quota enforcement using temporal-fs store when available.
-	// Check fs.Stats.TotalSize against fs.Config.MaxSize.
-	// Check fs.Stats.InodeCount against fs.Config.MaxFiles.
+	key := ctx.ExecutionKey()
+
+	s, err := e.storeProvider.GetStore(0, key.NamespaceID, key.BusinessID)
+	if err != nil {
+		e.logger.Error("QuotaCheck: failed to get store", tag.Error(err))
+		return nil
+	}
+
+	f, err := tfs.Open(s)
+	if err != nil {
+		_ = s.Close()
+		e.logger.Error("QuotaCheck: failed to open FS", tag.Error(err))
+		return nil
+	}
+
+	m := f.Metrics()
+	f.Close()
+
+	if fs.Stats == nil {
+		fs.Stats = &temporalfspb.FSStats{}
+	}
+
+	// Update stats from FS metrics.
+	fs.Stats.TotalSize = uint64(m.BytesWritten.Load())
+	fs.Stats.FileCount = uint64(m.FilesCreated.Load() - m.FilesDeleted.Load())
+	fs.Stats.DirCount = uint64(m.DirsCreated.Load() - m.DirsDeleted.Load())
+
+	maxSize := fs.Config.GetMaxSize()
+	if maxSize > 0 && fs.Stats.TotalSize > maxSize {
+		e.logger.Warn("Filesystem exceeds size quota",
+			tag.NewStringTag("filesystem_id", key.BusinessID),
+			tag.NewInt64("total_size", int64(fs.Stats.TotalSize)),
+			tag.NewInt64("max_size", int64(maxSize)),
+		)
+	}
+
 	return nil
 }
