3c076d1fb602c28ede744f43ea0e399dfc70f110
Chetan Gowda <chetan.gowda@temporal.io>
2026-03-18 13:12:44 -0700

    Provide chasmworkflow.EventRegistry

--- chasm/lib/workflow/workflowregistry/fx.go
+++ chasm/lib/workflow/workflowregistry/fx.go
@@ -1,8 +1,12 @@
 package workflowregistry
 
-import "go.uber.org/fx"
+import (
+	chasmworkflow "go.temporal.io/server/chasm/lib/workflow"
+	"go.uber.org/fx"
+)
 
 var Module = fx.Module(
 	"chasm.lib.workflow.workflowregistry",
 	fx.Provide(NewRegistry),
+	fx.Provide(func(r *Registry) chasmworkflow.EventRegistry { return r }),
 )
