e535cb5d019f71c8533ddeee66afba390b542715
Lina Jodoin <lina.jodoin@temporal.io>
2026-03-19 14:01:45 -0700

    [Scheduled Actions] Attach Nexus callbacks to schedules migrated from V1/Legacy (#9560)
    ## What changed?
    - Nexus callbacks are attached to running workflows after migration from
    a V1 workflow-backed schedule. This is done through a new side-effect
    task.
    
    ## Why?
    - Drives workflow completion events.
    
    ## How did you test it?
    - [ ] built
    - [ ] run locally and tested manually
    - [ ] covered by existing tests
    - [ ] added new unit test(s)
    - [x] added new functional test(s)

--- chasm/lib/scheduler/gen/schedulerpb/v1/tasks.go-helpers.pb.go
+++ chasm/lib/scheduler/gen/schedulerpb/v1/tasks.go-helpers.pb.go
@@ -42,6 +42,43 @@ func (this *SchedulerIdleTask) Equal(that interface{}) bool {
 	return proto.Equal(this, that1)
 }
 
+// Marshal an object of type SchedulerCallbacksTask to the protobuf v3 wire format
+func (val *SchedulerCallbacksTask) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type SchedulerCallbacksTask from the protobuf v3 wire format
+func (val *SchedulerCallbacksTask) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *SchedulerCallbacksTask) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two SchedulerCallbacksTask values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *SchedulerCallbacksTask) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *SchedulerCallbacksTask
+	switch t := that.(type) {
+	case *SchedulerCallbacksTask:
+		that1 = t
+	case SchedulerCallbacksTask:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
 // Marshal an object of type GeneratorTask to the protobuf v3 wire format
 func (val *GeneratorTask) Marshal() ([]byte, error) {
 	return proto.Marshal(val)
