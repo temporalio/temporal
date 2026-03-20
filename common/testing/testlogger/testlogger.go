9b67adc5fafd6bfc9ac913908914a00650b94309
Sean Kane <sean.kane@temporal.io>
2026-03-20 17:17:47 -0600

    add a log to detect if the debug logs are showing up

--- common/testing/testlogger/testlogger.go
+++ common/testing/testlogger/testlogger.go
@@ -250,6 +250,7 @@ func NewTestLogger(t TestingT, mode Mode, opts ...LoggerOption) *TestLogger {
 		if debugFile := os.Getenv(log.TestDebugLogFileEnvVar); debugFile != "" {
 			f, err := os.OpenFile(debugFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 			if err == nil {
+				t.Logf("testlogger: debug logs → %s", debugFile)
 				// Debug+ goes to file; info+ goes to the test writer (visible in CI log).
 				fileCore := zapcore.NewCore(enc, zapcore.AddSync(f), zap.DebugLevel)
 				infoCore := zapcore.NewCore(enc, writer, zap.InfoLevel)
