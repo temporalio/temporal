c03bd512ebd01d96a9dc2fbb7b0ca15213ca56c4
Stephan Behnke <stephanos@users.noreply.github.com>
2026-04-02 15:32:35 -0700

    remove SetDefaultWorkflowIDPolicies from history

--- service/history/api/signalwithstartworkflow/api.go
+++ service/history/api/signalwithstartworkflow/api.go
@@ -8,7 +8,6 @@ import (
 	"go.temporal.io/server/api/historyservice/v1"
 	"go.temporal.io/server/api/matchingservice/v1"
 	"go.temporal.io/server/common/definition"
-	"go.temporal.io/server/common/enums"
 	"go.temporal.io/server/common/locks"
 	"go.temporal.io/server/common/metrics"
 	"go.temporal.io/server/common/namespace"
@@ -52,12 +51,6 @@ func Invoke(
 	default:
 		return nil, err
 	}
-
-	enums.SetDefaultWorkflowIDPolicies(
-		&signalWithStartRequest.SignalWithStartRequest.WorkflowIdReusePolicy,
-		&signalWithStartRequest.SignalWithStartRequest.WorkflowIdConflictPolicy,
-		enumspb.WORKFLOW_ID_CONFLICT_POLICY_USE_EXISTING,
-	)
 
 	startRequest := ConvertToStartRequest(
 		namespaceID,
