64eee5b0e4a7da1c36b66108e2ca787919007347
Shahab Tajik <shahab@temporal.io>
2026-04-02 13:40:59 -0700

    WorkerVersion field in WorkflowTaskStarted events should never be set… (#9794)
    … in WV3 (#9783)
    
    Cherry-picking #9783
    
    ## What changed?
    Make sure speculative tasks do not cause the field to be set if WV2 is
    not used.
    
    Also a test was added that reproduced the issue, but works fine with the
    change.
    
    ## Why?
    Speculative tasks when completed or failed previously were setting the
    WorkerVersion field in their WorkflowTaskStarted events. This is benign
    for the execution of WV3-versioned workflows, but if those workflows are
    reset, reapplying the events will cause the new workflow to have
    AssignedBuildId set WorkerVersion of the latest WorkflowTaskStarted
    event.
    
    ## How did you test it?
    - [ ] built
    - [ ] run locally and tested manually
    - [ ] covered by existing tests
    - [ ] added new unit test(s)
    - [x] added new functional test(s)
    
    ## Potential risks
    None.
    
    (cherry picked from commit ad68d7bb2be03db0af9d3740e84d62f7ba128750)

--- service/history/api/recordworkflowtaskstarted/api.go
+++ service/history/api/recordworkflowtaskstarted/api.go
@@ -165,7 +165,7 @@ func Invoke(
 				requestID,
 				pollerTaskQueue,
 				req.PollRequest.Identity,
-				worker_versioning.StampFromCapabilities(req.PollRequest.WorkerVersionCapabilities),
+				worker_versioning.StampFromCapabilities(req.PollRequest.WorkerVersionCapabilities, req.PollRequest.DeploymentOptions), //nolint:staticcheck // SA1019: WorkerVersionCapabilities is deprecated but still used for old versioning [cleanup-old-wv]
 				req.GetBuildIdRedirectInfo(),
 				workflowLease.GetContext().UpdateRegistry(ctx),
 				false,
