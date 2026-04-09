6fd3deac73493c41b0e26632b8e6550e841af329
Stephan Behnke <stephanos@users.noreply.github.com>
2026-04-09 08:15:05 -0700

    Nexus Standalone: translate "already started" err (#9832)
    ## What changed?
    
    Translate CHASM's "already started" error.
    
    ## Why?
    
    Provide better error response to user.
    
    ## How did you test it?
    - [ ] built
    - [ ] run locally and tested manually
    - [ ] covered by existing tests
    - [ ] added new unit test(s)
    - [x] added new functional test(s)

--- chasm/lib/nexusoperation/handler.go
+++ chasm/lib/nexusoperation/handler.go
@@ -51,6 +51,13 @@ func (h *handler) StartNexusOperation(
 		),
 	)
 	if err != nil {
+		if alreadyStartedErr, ok := errors.AsType[*chasm.ExecutionAlreadyStartedError](err); ok {
+			return nil, serviceerror.NewAlreadyExistsf(
+				"nexus operation execution already started: request_id=%s, run_id=%s",
+				alreadyStartedErr.CurrentRequestID,
+				alreadyStartedErr.CurrentRunID,
+			)
+		}
 		return nil, err
 	}
 
