6893d7894cbebfdb47c95f21065e32fbc8fc7a2a
Nick Beaumont <nicholas.beaumont@temporal.io>
2026-04-15 10:24:43 -0700

    Add allocations to benchmark results

--- common/stats/windowed_tdigest_performance_test.go
+++ common/stats/windowed_tdigest_performance_test.go
@@ -40,57 +40,70 @@ func BenchmarkNewWindow(b *testing.B) {
 	}
 }
 
-// -                                                 ms, us, ns
-// BenchmarkTenMillionRecordQuery/trimmed-mean       99,039,996 ns/op
-// BenchmarkTenMillionRecordQuery/quantile           98,738,240 ns/op
-// BenchmarkTenMillionRecordQuery/subwindow-quantile      1,509 ns/op
+// -                                                            s, ms, us, ns
+// BenchmarkTenMillionRecordQuery/trimmed-mean-size-10         11,655,230,875 ns/op
+// BenchmarkTenMillionRecordQuery/quantile-size-10             11,743,078,833 ns/op
+// BenchmarkTenMillionRecordQuery/subwindow-quantile-size-10        1,795,897 ns/op
+// BenchmarkTenMillionRecordQuery/trimmed-mean-size-100         5,297,755,167 ns/op
+// BenchmarkTenMillionRecordQuery/quantile-size-100             4,950,984,583 ns/op
+// BenchmarkTenMillionRecordQuery/subwindow-quantile-size-100         144,574 ns/op
+// BenchmarkTenMillionRecordQuery/trimmed-mean-size-1000        3,844,660,708 ns/op
+// BenchmarkTenMillionRecordQuery/quantile-size-1000            3,646,952,708 ns/op
+// BenchmarkTenMillionRecordQuery/subwindow-quantile-size-1000         13,883 ns/op
+// BenchmarkTenMillionRecordQuery/trimmed-mean-size-10000         473,836,570 ns/op
+// BenchmarkTenMillionRecordQuery/quantile-size-10000             473,955,278 ns/op
+// BenchmarkTenMillionRecordQuery/subwindow-quantile-size-10000       798,834 ns/op
 func BenchmarkTenMillionRecordQuery(b *testing.B) {
-	stats, _ := NewWindowedTDigest(WindowConfig{
-		WindowSize:  1 * time.Second,
-		WindowCount: 300,
-	})
-	datapoints := generateDatapoints(10_000_000, 10_000, rand.NewPCG(7, 42))
-	for _, dp := range datapoints {
-		stats.RecordMulti(dp.Value, dp.Time, dp.Count)
+	for windowSize := 10; windowSize <= 10_000; windowSize *= 10 {
+		stats, _ := NewWindowedTDigest(WindowConfig{
+			WindowSize:  time.Duration(windowSize) * time.Second,
+			WindowCount: 10_000_000 / windowSize,
+		})
+		datapoints := generateDatapoints(10_000_000, 1, rand.NewPCG(7, 42))
+		for _, dp := range datapoints {
+			stats.RecordMulti(dp.Value, dp.Time, dp.Count)
+		}
+		b.Run(fmt.Sprintf("trimmed-mean-size-%d", windowSize), func(b *testing.B) {
+			for b.Loop() {
+				stats.TrimmedMean(0, 1.0)
+			}
+		})
+		b.Run(fmt.Sprintf("quantile-size-%d", windowSize), func(b *testing.B) {
+			for b.Loop() {
+				stats.Quantile(0.9)
+			}
+		})
+		b.Run(fmt.Sprintf("subwindow-quantile-size-%d", windowSize), func(b *testing.B) {
+			for b.Loop() {
+				stats.SubWindowForTime(datapoints[9_000_000].Time).Quantile(0.9)
+			}
+		})
 	}
-	b.Run("trimmed-mean", func(b *testing.B) {
-		for b.Loop() {
-			stats.TrimmedMean(0, 1.0)
-		}
-	})
-	b.Run("quantile", func(b *testing.B) {
-		for b.Loop() {
-			stats.Quantile(0.9)
-		}
-	})
-	b.Run("subwindow-quantile", func(b *testing.B) {
-		for b.Loop() {
-			stats.SubWindowForTime(datapoints[9_000_000].Time).Quantile(0.9)
-		}
-	})
 }
 
 // How to read this chart: Lower numbers are better. There are two datasets tested,
 // 10k and 10M. Observe that increasing window size for a fixed datapoint count
 // has a nonlinear impact on latency. Observe that increasing datapoint count
 // for a fixed window size also has a somewhat super-linear impact on latency.
-// -                                                        s, ms, us, ns
-// BenchmarkInsert/datapoints-10,000-window-size-1             81,422,795 ns/op
-// BenchmarkInsert/datapoints-10,000-window-size-10             8,321,690 ns/op
-// BenchmarkInsert/datapoints-10,000-window-size-100            1,497,160 ns/op
-// BenchmarkInsert/datapoints-10,000-window-size-1000           1,697,073 ns/op
-// BenchmarkInsert/datapoints-10,000-window-size-10000          4,462,444 ns/op
-// BenchmarkInsert/datapoints-10,000-window-size-100000         <invalid> ns/op
-// -------------------------------------------------------------------------------
-// BenchmarkInsert/datapoints-10,000,000-window-size-1                DNF ns/op
-// BenchmarkInsert/datapoints-10,000,000-window-size-10     9,668,609,417 ns/op
-// BenchmarkInsert/datapoints-10,000,000-window-size-100    1,483,158,625 ns/op
-// BenchmarkInsert/datapoints-10,000,000-window-size-1000   1,716,553,958 ns/op
-// BenchmarkInsert/datapoints-10,000,000-window-size-10000  4,623,385,375 ns/op
-// BenchmarkInsert/datapoints-10,000,000-window-size-100000 5,567,272,666 ns/op
+// Increasing window rotation by reducing the total window count was tested.
+// It did not make a significant (<1%) impact on latency
+// -                                                        s, ms, us, ns       GB, MB, KB,  B
+// BenchmarkInsert/datapoints-10,000-window-size-1             79,594,851 ns/op    220,978,893 B/op     90,277 allocs/op
+// BenchmarkInsert/datapoints-10,000-window-size-10             8,262,293 ns/op     22,684,401 B/op     18,272 allocs/op
+// BenchmarkInsert/datapoints-10,000-window-size-100            1,496,739 ns/op      2,852,384 B/op     11,067 allocs/op
+// BenchmarkInsert/datapoints-10,000-window-size-1000           1,764,475 ns/op        867,330 B/op     10,344 allocs/op
+// BenchmarkInsert/datapoints-10,000-window-size-10000          4,493,649 ns/op      1,161,041 B/op     10,316 allocs/op
+// BenchmarkInsert/datapoints-10,000-window-size-100000     <Same as above. Did not saturate all the windows>
+// -                                                        s, ms, us, ns       GB, MB, KB,  B
+// BenchmarkInsert/datapoints-10,000,000-window-size-1      <Not tested due to test runtime>
+// BenchmarkInsert/datapoints-10,000,000-window-size-10     9,569,462,583 ns/op 22,680,717,288 B/op 18,302,020 allocs/op
+// BenchmarkInsert/datapoints-10,000,000-window-size-100    1,888,510,125 ns/op  2,851,910,864 B/op 11,101,984 allocs/op
+// BenchmarkInsert/datapoints-10,000,000-window-size-1000   1,782,219,708 ns/op    869,035,776 B/op 10,381,987 allocs/op
+// BenchmarkInsert/datapoints-10,000,000-window-size-10000  4,656,263,666 ns/op  1,162,586,672 B/op 10,353,995 allocs/op
+// BenchmarkInsert/datapoints-10,000,000-window-size-100000 5,698,841,500 ns/op  1,248,606,488 B/op 10,356,404 allocs/op
 func BenchmarkInsert(b *testing.B) {
 	slidingBenchmark("datapoints-10,000", b, 10_000, 1, 10_000)
-	slidingBenchmark("datapoints-10,000,000", b, 10_000_000, 10, 1000)
+	slidingBenchmark("datapoints-10,000,000", b, 10_000_000, 10, 100_000)
 }
 
 func generateDatapoints(datapoints int, pointsPerBucket uint64, random UIntGenerator) (output []RecordingValue) {
@@ -118,10 +131,11 @@ func slidingBenchmark(baseName string, b *testing.B, datapointCount, startWindow
 	datapoints := generateDatapoints(datapointCount, 1, rand.NewPCG(7, 42))
 	for windowSize := startWindowSize; windowSize <= maxWindowSize; windowSize *= 10 {
 		b.Run(fmt.Sprintf("%s-window-size-%d", baseName, windowSize), func(b *testing.B) {
+			b.ReportAllocs()
 			for b.Loop() {
 				stats, _ := NewWindowedTDigest(WindowConfig{
 					WindowSize:  time.Duration(windowSize) * time.Second,
-					WindowCount: datapointCount / windowSize,
+					WindowCount: datapointCount,
 				})
 				for _, dp := range datapoints {
 					stats.RecordMulti(dp.Value, dp.Time, dp.Count)
