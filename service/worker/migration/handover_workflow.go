352023b087141b5bbb33c2f06e581539ba9928bd
stuart-wells <stuart.wells@temporal.io>
2026-05-08 14:33:27 -0700

    Ensuring Normal state propagation on failover

--- service/worker/migration/handover_workflow.go
+++ service/worker/migration/handover_workflow.go
@@ -121,16 +121,6 @@ func NamespaceHandoverWorkflow(ctx workflow.Context, params NamespaceHandoverPar
 		return err
 	}
 
-	// ** Step 4: RecoverOrInitialize Handover (WARNING: Namespace cannot serve traffic while in this state)
-	handoverRequest := updateStateRequest{
-		Namespace: params.Namespace,
-		NewState:  enumspb.REPLICATION_STATE_HANDOVER,
-	}
-	err = workflow.ExecuteActivity(ctx, a.UpdateNamespaceState, handoverRequest).Get(ctx, nil)
-	if err != nil {
-		return err
-	}
-
 	defer func() {
 		// ** Final Step: Reset namespace state from Handover -> Registered. This helps ensure that whether
 		//                handover failed or succeeded, the namespace (for whichever cluster it is Active on)
@@ -154,6 +144,16 @@ func NamespaceHandoverWorkflow(ctx workflow.Context, params NamespaceHandoverPar
 			return
 		}
 	}()
+
+	// ** Step 4: RecoverOrInitialize Handover (WARNING: Namespace cannot serve traffic while in this state)
+	handoverRequest := updateStateRequest{
+		Namespace: params.Namespace,
+		NewState:  enumspb.REPLICATION_STATE_HANDOVER,
+	}
+	err = workflow.ExecuteActivity(ctx, a.UpdateNamespaceState, handoverRequest).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
 
 	// ** Step 5: Wait for Remote Cluster to completely drain its Replication Tasks
 	ao3 := workflow.ActivityOptions{
