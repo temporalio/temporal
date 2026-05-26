5e1cdb29cf890c62a70bda2821f08c458cff4f2c
Stephan Behnke <stephanos@users.noreply.github.com>
2026-05-26 09:16:03 -0700

    Use suite-scoped cluster pools

--- tests/versioning_3_test.go
+++ tests/versioning_3_test.go
@@ -82,7 +82,7 @@ type Versioning3Suite struct {
 }
 
 func TestVersioning3FunctionalSuite(t *testing.T) {
-	testcore.UseSuiteScopedCluster(t) //nolint:staticcheck // SA1019: suite still requires legacy sequential execution
+	testcore.UseSuiteScopedClusters(t, testcore.DefaultSuiteClusterPoolSize())
 	parallelsuite.Run(t, &Versioning3Suite{})
 }
 
