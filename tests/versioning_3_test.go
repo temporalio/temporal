7ff8912a83674daefcdd9d90e4d216fa2dc2d83a
Stephan Behnke <stephanos@users.noreply.github.com>
2026-05-26 11:06:46 -0700

    Limit child inheritance signal pause to inherit cases

--- tests/versioning_3_test.go
+++ tests/versioning_3_test.go
@@ -2302,6 +2302,7 @@ func (s *Versioning3Suite) testChildWorkflowInheritanceExpectNoInherit(crossTq b
 	}
 
 	wfStarted := make(chan struct{}, 10)
+	currentChanged := make(chan struct{}, 10)
 
 	childv1 := func(ctx workflow.Context) (string, error) {
 		panic("child should not run on v1")
@@ -2311,7 +2312,8 @@ func (s *Versioning3Suite) testChildWorkflowInheritanceExpectNoInherit(crossTq b
 	}
 	wf1 := func(ctx workflow.Context) (string, error) {
 		wfStarted <- struct{}{}
-		workflow.GetSignalChannel(ctx, "currentVersionChanged").Receive(ctx, nil)
+		// wait for current version to change
+		<-currentChanged
 
 		fut1 := workflow.ExecuteChildWorkflow(workflow.WithChildOptions(ctx, workflow.ChildWorkflowOptions{
 			TaskQueue:  tv2Child.TaskQueue().GetName(),
@@ -2321,6 +2323,7 @@ func (s *Versioning3Suite) testChildWorkflowInheritanceExpectNoInherit(crossTq b
 		var val1 string
 		s.NoError(fut1.Get(ctx, &val1))
 
+		env.verifyWorkflowVersioning(s, tv1, parentBehavior, tv1.Deployment(), nil, nil)
 		return val1, nil
 	}
 
@@ -2400,12 +2403,11 @@ func (s *Versioning3Suite) testChildWorkflowInheritanceExpectNoInherit(crossTq b
 	// wait for it to start on v1
 	env.WaitForChannel(wfStarted)
 	close(wfStarted)
-	env.verifyWorkflowVersioning(s, tv1, parentBehavior, tv1.Deployment(), nil, nil)
 
 	// make v2 current for both parent and child and unblock the wf to start the child
 	env.setCurrentDeployment(s, tv2)
 
-	s.NoError(sdkClient.SignalWorkflow(s.Context(), run.GetID(), run.GetRunID(), "currentVersionChanged", nil))
+	currentChanged <- struct{}{}
 
 	var out string
 	s.NoError(run.Get(s.Context(), &out))
