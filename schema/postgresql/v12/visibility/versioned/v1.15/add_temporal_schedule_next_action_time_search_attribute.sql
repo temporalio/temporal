d7ba822cef2eab6e32756e4825041b6c4129d2b0
David Porter <david.porter@temporal.io>
2026-05-29 11:30:03 -0700

    removing unneeded

--- schema/postgresql/v12/visibility/versioned/v1.15/add_temporal_schedule_next_action_time_search_attribute.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE executions_visibility
-ADD COLUMN TemporalScheduleNextActionTime TIMESTAMP GENERATED ALWAYS AS (convert_ts(search_attributes->>'TemporalScheduleNextActionTime')) STORED;
-
-CREATE INDEX by_temporal_schedule_next_action_time ON executions_visibility (namespace_id, TemporalScheduleNextActionTime, (COALESCE(close_time, '9999-12-31 23:59:59')) DESC, start_time DESC, run_id);
