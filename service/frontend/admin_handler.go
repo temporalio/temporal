4f440437911fbd87af04a9356761e72c844391e9
rodrigozhou <rodrigo.zhou@temporal.io>
2026-06-03 18:36:38 -0700

    Emit metric to track nil value in a KeywordList value

--- service/frontend/admin_handler.go
+++ service/frontend/admin_handler.go
@@ -220,6 +220,8 @@ func NewAdminHandler(
 				args.Config.VisibilityAllowList,
 			),
 			args.Config.SuppressErrorSetSystemSearchAttribute,
+			args.MetricsHandler,
+			args.Logger,
 		),
 		clusterMetadata:      args.ClusterMetadata,
 		healthServer:         args.HealthServer,
