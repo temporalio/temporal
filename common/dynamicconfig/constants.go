1e5e69466000f3686242fc7603fe6c8bd2c2a856
Quinn Klassen <klassenq@gmail.com>
2026-06-04 07:49:56 -0700

    Wire up SAA links (#10491)
    Wire up stand alone activity links. Allowing links to be recorded on the
    activity execution and links to the execution.

--- common/dynamicconfig/constants.go
+++ common/dynamicconfig/constants.go
@@ -1039,6 +1039,11 @@ so forwarding by endpoint ID will not work out of the box.`,
 		10,
 		`Maximum number of links allowed to be attached via a single API request.`,
 	)
+	MaxLinksPerComponent = NewNamespaceIntSetting(
+		"chasm.maxLinksPerComponent",
+		2000,
+		`MaxLinksPerComponent is the maximum number of links that can be attached to a single CHASM component (e.g. a standalone activity or standalone Nexus operation) across all start/attach calls.`,
+	)
 	FrontendMaxConcurrentBatchOperationPerNamespace = NewNamespaceIntSetting(
 		"frontend.MaxConcurrentBatchOperationPerNamespace",
 		1,
