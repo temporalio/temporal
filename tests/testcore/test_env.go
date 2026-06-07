66aa956b9ef4009e67042b77f92870a0917b37b9
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-07 10:18:38 -0700

    Fix cluster pool max-usage recycling (#10593)
    ## What changed
    
    Reworks the test cluster pool.
    
    - `clusterRouter`: routes cluster request a test to shared, dedicated,
    or suite-scoped cluster handling.
    - `clusterPool`: collection of `clusterPoolSlot`s
    - `clusterPoolSlot`: owns actual cluster`FunctionalTestBase`, plus
    metadata

--- tests/testcore/test_env.go
+++ tests/testcore/test_env.go
@@ -226,8 +226,8 @@ func NewEnv(t *testing.T, opts ...TestOption) *TestEnv {
 		}
 	}
 
-	// Obtain the test cluster from the pool.
-	base := testClusterPool.get(t, options.dedicatedCluster, startupConfig, options.clusterOptions)
+	// Obtain the test cluster from the router.
+	base := testClusterRouter.get(t, options.dedicatedCluster, startupConfig, options.clusterOptions)
 	cluster := base.GetTestCluster()
 
 	// Create a dedicated namespace for the test to help with test isolation.
