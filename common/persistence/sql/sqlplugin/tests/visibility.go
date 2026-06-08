087b192df69b997f07c5951592173b14d2d1aa75
Fred Tzeng <41805201+fretz12@users.noreply.github.com>
2026-06-08 09:37:04 -0700

    Cherry-pick: Encode duration as bigint for pgx simple protocol (#10529) (#10557)
    Backport of #10529 to release/v1.31.x for the v1.31.1 patch.
    
    Co-authored-by: Vladyslav Simonenko <vlad.simonenko@temporal.io>
    Co-authored-by: rodrigozhou <rodrigo.zhou@temporal.io>

--- common/persistence/sql/sqlplugin/tests/visibility.go
+++ common/persistence/sql/sqlplugin/tests/visibility.go
@@ -1341,19 +1341,24 @@ func (s *visibilitySuite) newRandomVisibilityRow(
 	historyLength *int64,
 ) sqlplugin.VisibilityRow {
 	s.version++
+	var executionDuration *time.Duration
+	if closeTime != nil {
+		executionDuration = new(closeTime.Sub(executionTime))
+	}
 	return sqlplugin.VisibilityRow{
-		NamespaceID:      namespaceID.String(),
-		RunID:            runID.String(),
-		WorkflowTypeName: workflowTypeName,
-		WorkflowID:       workflowID,
-		StartTime:        startTime,
-		ExecutionTime:    executionTime,
-		Status:           status,
-		CloseTime:        closeTime,
-		HistoryLength:    historyLength,
-		Memo:             shuffle.Bytes(testVisibilityData),
-		Encoding:         testVisibilityEncoding,
-		Version:          s.version,
+		NamespaceID:       namespaceID.String(),
+		RunID:             runID.String(),
+		WorkflowTypeName:  workflowTypeName,
+		WorkflowID:        workflowID,
+		StartTime:         startTime,
+		ExecutionTime:     executionTime,
+		Status:            status,
+		CloseTime:         closeTime,
+		HistoryLength:     historyLength,
+		ExecutionDuration: executionDuration,
+		Memo:              shuffle.Bytes(testVisibilityData),
+		Encoding:          testVisibilityEncoding,
+		Version:           s.version,
 	}
 }
 
