d9dd1878ccb68787127e4fac189ce397b1383b51
Chris Smith <aChrisSmith@gmail.com>
2026-06-10 11:09:46 -0700

    Remove unused FauxSDK

--- tests/workercallbacks/sdk.go
+++
@@ -1,5 +0,0 @@
-package workercallbacks
-
-// FauxSDK is a wrapper around raw gRPC calls, and is meant to approximate what
-// the user-facing SDK might look like when using the feature.
-type FauxSDK struct{}
