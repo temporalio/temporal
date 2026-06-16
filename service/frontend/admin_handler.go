ecaf98399f77f9514b15c8ce5fadb826b75b1834
alex.stanfield <13949480+chaptersix@users.noreply.github.com>
2026-06-16 14:20:46 -0500

    fix: log remaining sentinel idle time

--- service/frontend/admin_handler.go
+++ service/frontend/admin_handler.go
@@ -2594,10 +2594,14 @@ func (adh *AdminHandler) migrateScheduleToWorkflow(
 	case err != nil:
 		return nil, err
 	case descResp.GetWorkflowExecutionInfo().GetType().GetName() == dummy.DummyWFTypeName:
+		sentinelIdleTimeRemaining := time.Until(descResp.GetWorkflowExecutionInfo().GetStartTime().AsTime().Add(chasmscheduler.SentinelIdleTime))
+		if sentinelIdleTimeRemaining < 0 {
+			sentinelIdleTimeRemaining = 0
+		}
 		adh.logger.Warn(
 			"schedule migration to workflow blocked by workflow sentinel",
 			tag.ScheduleID(request.GetScheduleId()),
-			tag.Duration("sentinel-idle-time", chasmscheduler.SentinelIdleTime),
+			tag.Duration("sentinel-idle-time", sentinelIdleTimeRemaining),
 		)
 		return nil, chasmscheduler.ErrSentinelBlocked
 	default:
