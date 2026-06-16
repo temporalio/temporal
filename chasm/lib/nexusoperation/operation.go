ffbd311a1a99f6819c94873abc918f4b3a4c6ced
Chris Smith <aChrisSmith@gmail.com>
2026-06-16 15:34:08 -0700

    Make defensive copies of Nexus request headers

--- chasm/lib/nexusoperation/operation.go
+++ chasm/lib/nexusoperation/operation.go
@@ -327,7 +327,7 @@ func (o *Operation) loadStartArgs(
 		scheduleToStartTimeout: o.GetScheduleToStartTimeout().AsDuration(),
 		startToCloseTimeout:    o.GetStartToCloseTimeout().AsDuration(),
 		payload:                invocationData.Input,
-		header:                 invocationData.Header,
+		header:                 maps.Clone(invocationData.Header),
 		nexusLinks:             invocationData.NexusLinks,
 		serializedRef:          serializedRef,
 	}, nil
