c11d1227ef9b9395b69411cbcca85864dc4a2f11
rodrigozhou <rodrigo.zhou@temporal.io>
2026-06-16 12:54:25 -0700

    Add CHASM Visibility component to CHASM Workflow

--- service/history/interfaces/mutable_state.go
+++ service/history/interfaces/mutable_state.go
@@ -180,6 +180,7 @@ type (
 		GetLastEventVersion() (int64, error)
 		GetExecutionInfo() *persistencespb.WorkflowExecutionInfo
 		GetExecutionState() *persistencespb.WorkflowExecutionState
+		ExecutionStateUpdated() bool
 		GetStartedWorkflowTask() *WorkflowTaskInfo
 		GetPendingWorkflowTask() *WorkflowTaskInfo
 		GetLastFirstEventIDTxnID() (int64, int64)
