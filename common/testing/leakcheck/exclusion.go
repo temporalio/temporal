2800e3bacf2362823d6ecd122178b2e0f349dddb
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-18 10:40:29 -0700

    use regex

--- common/testing/leakcheck/exclusion.go
+++ common/testing/leakcheck/exclusion.go
@@ -2,8 +2,11 @@ package leakcheck
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
+
+var specificPathIndexPattern = regexp.MustCompile(`\[(?:key)?[0-9]+\]`)
 
 type exclusion struct {
 	pattern string
@@ -13,7 +16,7 @@ type exclusion struct {
 type exclusions []exclusion
 
 func newExclusion(pattern string) (exclusion, error) {
-	if hasSpecificPathIndex(pattern) {
+	if specificPathIndexPattern.MatchString(pattern) {
 		return exclusion{}, fmt.Errorf("object exclusion %q targets a specific index; use [*] or [key*]", pattern)
 	}
 	return exclusion{pattern: pattern}, nil
@@ -40,38 +43,4 @@ func (e exclusion) matchesValue(value string) bool {
 		return strings.HasPrefix(value, prefix)
 	}
 	return value == e.pattern
-}
-
-func hasSpecificPathIndex(path string) bool {
-	for i := 0; i < len(path); {
-		if path[i] != '[' {
-			i++
-			continue
-		}
-
-		end := strings.IndexByte(path[i:], ']')
-		if end < 0 {
-			return false
-		}
-		end += i
-		index := path[i+1 : end]
-		keyIndex, hasKeyPrefix := strings.CutPrefix(index, "key")
-		if allDigits(index) || hasKeyPrefix && allDigits(keyIndex) {
-			return true
-		}
-		i = end + 1
-	}
-	return false
-}
-
-func allDigits(s string) bool {
-	if s == "" {
-		return false
-	}
-	for _, r := range s {
-		if r < '0' || r > '9' {
-			return false
-		}
-	}
-	return true
 }
