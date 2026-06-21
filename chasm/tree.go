037baeea9b4e1e7186df80dbddc448f1ddc097f9
Stephan Behnke <stephanos@users.noreply.github.com>
2026-06-21 11:17:05 -0700

    Apply non-server lint fixes for oss-foundations

--- chasm/tree.go
+++ chasm/tree.go
@@ -1619,7 +1619,7 @@ func (n *Node) AddTask(
 		return
 	}
 
-	n.nodeBase.newTasks[component] = append(n.nodeBase.newTasks[component], taskWithAttributes{
+	n.newTasks[component] = append(n.newTasks[component], taskWithAttributes{
 		task:       task,
 		attributes: taskAttributes,
 	})
