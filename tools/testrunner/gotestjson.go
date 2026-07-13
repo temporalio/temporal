b274a3057e1a8ffa841f46083deac8512d814ac8
Stephan Behnke <stephanos@users.noreply.github.com>
2026-07-13 12:49:49 -0700

    Use go-junit-report JSON parser

--- tools/testrunner/gotestjson.go
+++ tools/testrunner/gotestjson.go
@@ -7,43 +7,25 @@ import (
 	"os"
 	"strings"
 
+	"github.com/jstemmer/go-junit-report/v2/gtr"
 	"github.com/jstemmer/go-junit-report/v2/junit"
+	"github.com/jstemmer/go-junit-report/v2/parser/gotest"
 )
 
 type goTestEvent struct {
-	Action  string  `json:"Action"`
-	Package string  `json:"Package"`
-	Test    string  `json:"Test"`
-	Output  string  `json:"Output"`
-	Elapsed float64 `json:"Elapsed"`
+	Output string `json:"Output"`
 }
 
 type goTestJSONOutput struct {
-	line     strings.Builder
-	output   strings.Builder
-	stdout   io.Writer
-	packages map[string]*goTestPackage
-	order    []string
-}
-
-type goTestPackage struct {
-	output  strings.Builder
-	tests   map[string]*goTestCase
-	order   []string
-	failed  bool
-	elapsed float64
-}
-
-type goTestCase struct {
-	output  strings.Builder
-	action  string
-	elapsed float64
+	line   strings.Builder
+	input  strings.Builder
+	output strings.Builder
+	stdout io.Writer
 }
 
 func newGoTestJSONOutput() *goTestJSONOutput {
 	return &goTestJSONOutput{
-		stdout:   os.Stdout,
-		packages: make(map[string]*goTestPackage),
+		stdout: os.Stdout,
 	}
 }
 
@@ -68,95 +50,37 @@ func (o *goTestJSONOutput) String() string {
 
 func (o *goTestJSONOutput) writeLine(line string) {
 	var event goTestEvent
-	if err := json.Unmarshal([]byte(line), &event); err != nil {
+	if err := o.decodeLine(line, &event); err != nil {
 		fmt.Fprintln(o.stdout, line)
 		o.output.WriteString(line)
 		o.output.WriteByte('\n')
 		return
 	}
-	o.record(event)
+	o.input.WriteString(line)
+	o.input.WriteByte('\n')
 	if event.Output != "" {
 		fmt.Fprint(o.stdout, event.Output)
 		o.output.WriteString(event.Output)
 	}
 }
 
-func (o *goTestJSONOutput) record(event goTestEvent) {
-	pkg := o.packages[event.Package]
-	if pkg == nil {
-		pkg = &goTestPackage{tests: make(map[string]*goTestCase)}
-		o.packages[event.Package] = pkg
-		o.order = append(o.order, event.Package)
-	}
-	if event.Output != "" {
-		pkg.output.WriteString(event.Output)
-	}
-	if event.Test != "" {
-		test := pkg.tests[event.Test]
-		if test == nil {
-			test = &goTestCase{}
-			pkg.tests[event.Test] = test
-			pkg.order = append(pkg.order, event.Test)
-		}
-		if event.Output != "" {
-			test.output.WriteString(event.Output)
-		}
-		if event.Action != "output" {
-			test.action = event.Action
-			test.elapsed = event.Elapsed
-		}
-	}
-	if event.Test == "" && event.Action == "fail" {
-		pkg.failed = true
-		pkg.elapsed = event.Elapsed
-	}
+func (o *goTestJSONOutput) decodeLine(line string, event *goTestEvent) error {
+	return json.Unmarshal([]byte(line), event)
 }
 
-func (o *goTestJSONOutput) junitReport() *junitReport {
-	report := &junitReport{}
-	for _, pkgName := range o.order {
-		pkg := o.packages[pkgName]
-		suite := junit.Testsuite{
-			Name: pkgName,
-			ID:   len(report.Suites),
-		}
-		if pkg.elapsed > 0 {
-			suite.Time = fmt.Sprintf("%.6f", pkg.elapsed)
-		}
-		var failedTests int
-		for _, testName := range pkg.order {
-			test := pkg.tests[testName]
-			switch test.action {
-			case "pass", "fail", "skip":
-			default:
-				continue
+func (o *goTestJSONOutput) junitReport() (*junitReport, error) {
+	report, err := gotest.NewJSONParser().Parse(strings.NewReader(o.input.String()))
+	if err != nil {
+		return &junitReport{}, err
+	}
+	for i := range report.Packages {
+		tests := report.Packages[i].Tests
+		report.Packages[i].Tests = tests[:0]
+		for _, test := range tests {
+			if test.Result != gtr.Unknown {
+				report.Packages[i].Tests = append(report.Packages[i].Tests, test)
 			}
-			tc := junit.Testcase{
-				Name:      testName,
-				Classname: pkgName,
-			}
-			if test.elapsed > 0 {
-				tc.Time = fmt.Sprintf("%.6f", test.elapsed)
-			}
-			switch test.action {
-			case "fail":
-				failedTests++
-				tc.Failure = generateFailure(failureTypeFailed, test.output.String())
-			case "skip":
-				tc.Skipped = generateFailure("Skipped", test.output.String())
-			}
-			suite.AddTestcase(tc)
-		}
-		if pkg.failed && failedTests == 0 {
-			suite.AddTestcase(junit.Testcase{
-				Name:      pkgName,
-				Classname: pkgName,
-				Failure:   generateFailure(failureTypeFailed, pkg.output.String()),
-			})
-		}
-		if len(suite.Testcases) > 0 {
-			report.AddSuite(suite)
 		}
 	}
-	return report
+	return &junitReport{Testsuites: junit.CreateFromReport(report, "")}, nil
 }
--- tools/testrunner/gotestjson_test.go
+++ tools/testrunner/gotestjson_test.go
@@ -16,13 +16,18 @@ func TestGoTestJSONOutput_ExcludesIncompleteTests(t *testing.T) {
 		`{"Action":"output","Package":"go.temporal.io/server/tests","Test":"TestWorkflowUpdateSuite/TestValidateWorkerMessages/success-case","Output":"=== RUN   TestWorkflowUpdateSuite/TestValidateWorkerMessages/success-case\n"}`,
 		`{"Action":"pause","Package":"go.temporal.io/server/tests","Test":"TestWorkflowUpdateSuite/TestValidateWorkerMessages/success-case"}`,
 		`{"Action":"run","Package":"go.temporal.io/server/tests","Test":"TestRealFailure"}`,
-		`{"Action":"output","Package":"go.temporal.io/server/tests","Test":"TestRealFailure","Output":"=== RUN   TestRealFailure\n    foo_test.go:99: boom\n"}`,
+		`{"Action":"output","Package":"go.temporal.io/server/tests","Test":"TestRealFailure","Output":"=== RUN   TestRealFailure\n"}`,
+		`{"Action":"output","Package":"go.temporal.io/server/tests","Test":"TestRealFailure","Output":"    foo_test.go:99: boom\n"}`,
+		`{"Action":"output","Package":"go.temporal.io/server/tests","Test":"TestRealFailure","Output":"--- FAIL: TestRealFailure (0.01s)\n"}`,
 		`{"Action":"fail","Package":"go.temporal.io/server/tests","Test":"TestRealFailure","Elapsed":0.01}`,
+		`{"Action":"output","Package":"go.temporal.io/server/tests","Output":"FAIL\n"}`,
+		`{"Action":"fail","Package":"go.temporal.io/server/tests","Elapsed":0.01}`,
 		"",
 	}, "\n")))
 	require.NoError(t, err)
 
-	report := output.junitReport()
+	report, err := output.junitReport()
+	require.NoError(t, err)
 
 	require.Equal(t, 1, report.Tests)
 	require.Equal(t, 1, report.Failures)
@@ -37,18 +42,20 @@ func TestGoTestJSONOutput_ReportsPackageFailureWithoutFailedTest(t *testing.T) {
 	output := newGoTestJSONOutput()
 	output.stdout = io.Discard
 	_, err := output.Write([]byte(strings.Join([]string{
-		`{"Action":"output","Package":"go.temporal.io/server/broken","Output":"# go.temporal.io/server/broken\n"}`,
-		`{"Action":"output","Package":"go.temporal.io/server/broken","Output":"broken_test.go:10: compile error\n"}`,
-		`{"Action":"fail","Package":"go.temporal.io/server/broken","Elapsed":0.01}`,
+		`{"ImportPath":"go.temporal.io/server/broken [go.temporal.io/server/broken.test]","Action":"build-output","Output":"# go.temporal.io/server/broken [go.temporal.io/server/broken.test]\n"}`,
+		`{"ImportPath":"go.temporal.io/server/broken [go.temporal.io/server/broken.test]","Action":"build-output","Output":"broken_test.go:10: compile error\n"}`,
+		`{"ImportPath":"go.temporal.io/server/broken [go.temporal.io/server/broken.test]","Action":"build-fail"}`,
+		`{"Action":"start","Package":"go.temporal.io/server/broken"}`,
+		`{"Action":"output","Package":"go.temporal.io/server/broken","Output":"FAIL\tgo.temporal.io/server/broken [build failed]\n"}`,
+		`{"Action":"fail","Package":"go.temporal.io/server/broken","Elapsed":0.01,"FailedBuild":"go.temporal.io/server/broken [go.temporal.io/server/broken.test]"}`,
 		"",
 	}, "\n")))
 	require.NoError(t, err)
 
-	report := output.junitReport()
+	report, err := output.junitReport()
+	require.NoError(t, err)
 
 	require.Equal(t, 1, report.Tests)
-	require.Equal(t, 1, report.Failures)
-	require.Len(t, report.Suites, 1)
-	require.Equal(t, "go.temporal.io/server/broken", report.Suites[0].Testcases[0].Name)
-	require.Contains(t, report.Suites[0].Testcases[0].Failure.Data, "compile error")
+	require.Equal(t, 1, report.Errors)
+	require.Contains(t, report.Suites[0].Testcases[0].Error.Data, "compile error")
 }
