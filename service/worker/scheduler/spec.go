e2083d8e7a4c16f6e866b9a57a774ef578bb6846
David Porter <david.porter@temporal.io>
2026-07-14 16:42:03 -0700

    Fix test

--- service/worker/scheduler/spec.go
+++ service/worker/scheduler/spec.go
@@ -16,7 +16,7 @@ import (
 	"go.temporal.io/server/common/util"
 )
 
-const defaultWarnIterations = 24 * 60 * 60
+const DefaultWarnIterations = 24 * 60 * 60
 
 type (
 	CompiledSpec struct {
@@ -299,7 +299,7 @@ func (cs *CompiledSpec) GetNextTime(jitterSeed string, after time.Time) (GetNext
 	}
 	warnIterations := cs.warnIterations()
 	if warnIterations <= 0 {
-		warnIterations = defaultWarnIterations
+		warnIterations = DefaultWarnIterations
 	}
 	maxIterations := cs.maxIterations()
 	if maxIterations <= 0 {
