dandavison/temporalio-temporal

Repro B2: SAA ignores NonRetryableErrorTypes for timeouts
The activity timeout retry handlers (chasm/lib/activity/activity_tasks.go)
reschedule unconditionally, never consulting the retry policy's
NonRetryableErrorTypes. A workflow activity denies a StartToClose/Heartbeat
timeout retry when that type is listed (service/history/workflow/retry.go
isRetryable), so the two products diverge. TestTimeoutNonRetryable_Repro drives
an activity to a StartToClose timeout with StartToClose marked non-retryable and
asserts the correct (workflow) behavior — terminal TimedOut — so it fails while
the bug is present. The fix is the next commit.

The claim's other leg — a worker submitting a retryable ServerFailure that SAA
treats non-retryable — is not worker-reachable: the frontend rejects any
RespondActivityTaskFailed whose failure is not an ApplicationFailure
(service/frontend/workflow_handler.go), so HandleFailed's non-Application branch
is unreachable via the worker RPC. TestServerFailureRejectedByFrontend confirms
this (it passes).

tests/activity_standalone_test.go
@@ -14421,3 +14421,55 @@ func (s *standaloneActivityTestSuite) TestTimeout_Declarative() {
 		})
 	})
 }
+
+// TestServerFailureRejectedByFrontend confirms why bug B2's ServerFailure path is not worker-reachable:
+// the frontend rejects any RespondActivityTaskFailed whose failure is not an ApplicationFailure
+// (workflow_handler.go), so a worker cannot submit a ServerFailure and HandleFailed's non-Application
+// branch (chasm/lib/activity/activity.go) is unreachable via the worker RPC — the divergence from a
+// workflow activity's isRetryable exists only for server-internally-generated failures.
+func (s *standaloneActivityTestSuite) TestServerFailureRejectedByFrontend() {
+	testcontext.For(s.T(), testcontext.WithTimeout(saaTraceBudget()))
+	env := s.newTestEnv()
+	t := s.T()
+
+	// Declarative prefix: start with retries remaining and dispatch attempt 1.
+	hd := s.runTrace(t, env, saaTrace{trace: []model.Event{saaPoll}, maxAttempts: 3})
+
+	// Manual: attempt to fail the attempt with a ServerFailure rather than an ApplicationFailure.
+	_, err := env.FrontendClient().RespondActivityTaskFailed(testcontext.For(t), &workflowservice.RespondActivityTaskFailedRequest{
+		Namespace: env.Namespace().String(),
+		TaskToken: hd.token,
+		Identity:  "worker",
+		Failure: &failurepb.Failure{
+			Message:     "server failure",
+			FailureInfo: &failurepb.Failure_ServerFailureInfo{ServerFailureInfo: &failurepb.ServerFailureInfo{NonRetryable: false}},
+		},
+	})
+	require.ErrorContains(t, err, "Failure must have ApplicationFailureInfo")
+}
+
+// TestTimeoutNonRetryable_Repro reproduces bug B2: the timeout retry handlers
+// (chasm/lib/activity/activity_tasks.go) reschedule unconditionally, never consulting the retry
+// policy's NonRetryableErrorTypes. A workflow activity denies a StartToClose/Heartbeat timeout retry
+// when that type is listed (service/history/workflow/retry.go isRetryable), so a StartToClose timeout
+// on an activity whose policy marks StartToClose non-retryable must fail terminally rather than
+// retry. This asserts the correct (workflow) behavior, so it fails while the bug is present.
+func (s *standaloneActivityTestSuite) TestTimeoutNonRetryable_Repro() {
+	testcontext.For(s.T(), testcontext.WithTimeout(saaTraceBudget()))
+	env := s.newTestEnv()
+	t := s.T()
+
+	// Declarative prefix: start with retries remaining, dispatch attempt 1, let its StartToClose
+	// timeout elapse — with StartToClose marked non-retryable in the policy.
+	hd := s.runTrace(t, env, saaTrace{
+		trace:                []model.Event{saaPoll, {Kind: model.StartToCloseElapses}},
+		maxAttempts:          3,
+		nonRetryableTimeouts: []model.EventKind{model.StartToCloseElapses},
+	})
+
+	// A non-retryable timeout must fail the activity terminally, not reschedule it.
+	st, err := hd.observed()
+	require.NoError(t, err)
+	require.Equalf(t, model.TimedOut, st.Status,
+		"a StartToClose timeout marked non-retryable must fail the activity, not retry it (got %s)", st.Status)
+}

temporalio/temporal

tests/activity_standalone_test.go
@@ -1767,6 +1767,61 @@ func (s *standaloneActivityTestSuite) TestFail() {
 		require.ErrorAs(t, err, &invalidArgErr)
 		require.Equal(t, "token does not match namespace", invalidArgErr.Message)
 	})
+
+	t.Run("WorkerMustSendApplicationFailure", func(t *testing.T) {
+		env := s.newTestEnv()
+		// Make an SAA with an attempt in progress
+		a := s.runTrace(t, env, saaTrace{trace: []model.Event{saaPoll}, maxAttempts: 3})
+		// Worker sends an invalid failure
+		_, err := env.FrontendClient().RespondActivityTaskFailed(testcontext.For(t), &workflowservice.RespondActivityTaskFailedRequest{
+			Namespace: env.Namespace().String(),
+			TaskToken: a.token,
+			Identity:  "worker",
+			Failure: &failurepb.Failure{
+				Message:     "server failure",
+				FailureInfo: &failurepb.Failure_ServerFailureInfo{ServerFailureInfo: &failurepb.ServerFailureInfo{NonRetryable: false}},
+			},
+		})
+		require.ErrorContains(t, err, "Failure must have ApplicationFailureInfo")
+	})
+
+	// StartToClose timeout can be marked non-retryable.
+	t.Run("StartToCloseTimeoutCanBeMarkedNonRetryable", func(t *testing.T) {
+		env := s.newTestEnv()
+
+		// Start attempt, then fail non-retryably
+		a := s.runTrace(t, env, saaTrace{
+			trace:       []model.Event{saaPoll, {Kind: model.StartToCloseElapses}},
+			maxAttempts: 3,
+			customizeStart: func(req *workflowservice.StartActivityExecutionRequest) {
+				req.RetryPolicy.NonRetryableErrorTypes = []string{
+					retrypolicy.TimeoutFailureTypePrefix + enumspb.TIMEOUT_TYPE_START_TO_CLOSE.String()}
+			},
+		})
+		st, err := a.observed()
+		require.NoError(t, err)
+		require.Equalf(t, model.TimedOut, st.Status,
+			"a StartToClose timeout marked non-retryable must fail the activity, not retry it (got %s)", st.Status)
+	})
+
+	// Heartbeat timeout can be marked non-retryable.
+	t.Run("HeartbeatTimeoutCanBeMarkedNonRetryable", func(t *testing.T) {
+		env := s.newTestEnv()
+
+		// Start attempt, then fail non-retryably
+		a := s.runTrace(t, env, saaTrace{
+			trace:       []model.Event{saaPoll, {Kind: model.HeartbeatElapses}},
+			maxAttempts: 3,
+			customizeStart: func(req *workflowservice.StartActivityExecutionRequest) {
+				req.RetryPolicy.NonRetryableErrorTypes = []string{
+					retrypolicy.TimeoutFailureTypePrefix + enumspb.TIMEOUT_TYPE_HEARTBEAT.String()}
+			},
+		})
+		st, err := a.observed()
+		require.NoError(t, err)
+		require.Equalf(t, model.TimedOut, st.Status,
+			"a Heartbeat timeout marked non-retryable must fail the activity, not retry it (got %s)", st.Status)
+	})
 }
 
 func (s *standaloneActivityTestSuite) TestRequestCancel() {
@@ -14263,6 +14318,7 @@ func (s *standaloneActivityTestSuite) runTrace(t *testing.T, env *standaloneActi
 		// "Dispatchable" must mean "dispatches promptly", so bound the positive poll below the delay
 		// window — that is how a reset that discards a backoff (immediate) is told from still-delayed.
 		positivePollTimeout: saaNegativePollTimeout,
+		customizeStart:      tr.customizeStart,
 	}
 	return h.driveTrace(t, tr.trace)
 }