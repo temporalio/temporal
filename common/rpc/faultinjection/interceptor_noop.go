928f7e6b4c0213de52f54458e3990528e0475a9b
Stephan Behnke <stephanos@users.noreply.github.com>
2026-07-24 13:35:16 -0700

    Always install grpc fault interceptor

--- common/rpc/faultinjection/interceptor_noop.go
+++ common/rpc/faultinjection/interceptor_noop.go
@@ -3,11 +3,15 @@
 package faultinjection
 
 import (
+	"context"
+
 	"go.temporal.io/server/common/testing/testhooks"
 	"google.golang.org/grpc"
 )
 
-// GRPCUnaryServerInterceptor returns nil when test hooks are disabled.
+// GRPCUnaryServerInterceptor returns a pass-through interceptor when test hooks are disabled.
 func GRPCUnaryServerInterceptor(testhooks.TestHooks) grpc.UnaryServerInterceptor {
-	return nil
+	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		return handler(ctx, req)
+	}
 }
