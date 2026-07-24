3af48f45436b0477f3dec6a08af0784a34f572e9
Stephan Behnke <stephanos@users.noreply.github.com>
2026-07-24 13:37:07 -0700

    Omit grpc fault interceptor from production chains

--- common/rpc/faultinjection/interceptor_noop.go
+++ common/rpc/faultinjection/interceptor_noop.go
@@ -3,15 +3,11 @@
 package faultinjection
 
 import (
-	"context"
-
 	"go.temporal.io/server/common/testing/testhooks"
 	"google.golang.org/grpc"
 )
 
-// GRPCUnaryServerInterceptor returns a pass-through interceptor when test hooks are disabled.
+// GRPCUnaryServerInterceptor returns nil when test hooks are disabled.
 func GRPCUnaryServerInterceptor(testhooks.TestHooks) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-		return handler(ctx, req)
-	}
+	return nil
 }
