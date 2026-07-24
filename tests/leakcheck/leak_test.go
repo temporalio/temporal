fb9aca36aff37af67e677b65666c29120f300fa4
Tom Dyas <tom.dyas@temporal.io>
2026-07-24 11:36:46 -0700

    update leak detector ignores

--- tests/leakcheck/leak_test.go
+++ tests/leakcheck/leak_test.go
@@ -36,13 +36,14 @@ var goleakOpts = []goleak.Option{
 	goleak.IgnoreTopFunction("google.golang.org/grpc/internal/grpcsync.(*CallbackSerializer).run"),
 	goleak.IgnoreAnyFunction("google.golang.org/grpc.(*addrConn).resetTransportAndUnlock"),
 	goleak.IgnoreTopFunction("google.golang.org/grpc/internal/balancer/gracefulswitch.(*Balancer).updateSubConnState"),
-	goleak.IgnoreTopFunction("go.temporal.io/server/common/membership.(*grpcResolver).listen"),
+	goleak.IgnoreAnyFunction("go.temporal.io/server/common/membership.(*grpcResolver).listen"),
 
 	// TODO: worker-service and persistence goroutine leaks.
 	goleak.IgnoreTopFunction("net/http.(*persistConn).readLoop"),
 	goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
 
 	// TODO: SDK worker goroutines not fully stopped on cluster shutdown.
+	goleak.IgnoreAnyFunction("go.temporal.io/sdk/internal.(*baseWorker).Start.func2"),
 	goleak.IgnoreTopFunction("go.temporal.io/sdk/internal.(*baseWorker).runEagerTaskDispatcher"),
 	goleak.IgnoreTopFunction("go.temporal.io/sdk/internal.(*baseWorker).runTaskDispatcher"),
 	goleak.IgnoreTopFunction("go.temporal.io/sdk/internal.(*localActivityTunnel).getTask"),
