351805cd39a953519ab714a48d3c33f21b6e85ac
Shivam Saraf <shivam.saraf@temporal.io>
2026-07-29 21:09:42 -0400

    Test task queue family limit through poll APIs

--- tests/worker_deployment_version_test.go
+++ tests/worker_deployment_version_test.go
@@ -104,7 +104,7 @@ func (s *DeploymentVersionSuite) pollFromDeployment(ctx context.Context, env *te
 	})
 }
 
-func (s *DeploymentVersionSuite) pollActivityFromDeployment(ctx context.Context, env *testcore.TestEnv, tv *testvars.TestVars) {
+func pollActivityFromDeployment(ctx context.Context, env *testcore.TestEnv, tv *testvars.TestVars) {
 	_, _ = env.FrontendClient().PollActivityTaskQueue(ctx, &workflowservice.PollActivityTaskQueueRequest{
 		Namespace:         env.Namespace().String(),
 		TaskQueue:         tv.TaskQueue(),
@@ -343,7 +343,7 @@ func (s *DeploymentVersionSuite) TestDescribeVersion_RegisterTaskQueue_Concurren
 		tv2 := env.Tv().WithTaskQueue(root.TaskQueue().NormalPartition(p).RpcName())
 		for range 3 {
 			go s.pollFromDeployment(s.Context(), env, tv2)
-			go s.pollActivityFromDeployment(s.Context(), env, tv2)
+			go pollActivityFromDeployment(s.Context(), env, tv2)
 		}
 	}
 
