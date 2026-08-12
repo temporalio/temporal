d92d48f097fc2ab5d2e848717bc1bec6a2ae81a3
Maruthi ChandraSekhar Vemuri <maruthi.vemuri@temporal.io>
2026-08-12 16:28:39 -0700

    add a wrapper for ns rate-limit interceptor

--- service/frontend/fx.go
+++ service/frontend/fx.go
@@ -131,6 +131,7 @@ var Module = fx.Options(
 	fx.Provide(NewVersionChecker),
 	fx.Provide(ServiceResolverProvider),
 	fx.Provide(newNexusForwardingInterceptor),
+	fx.Provide(interceptor.NewNexusNamespaceRateLimitInterceptor),
 	fx.Provide(newNexusCompletionHandler),
 	fx.Provide(NewNexusOperationHTTPHandler),
 	fx.Provide(newNexusCompletionHTTPHandler),
