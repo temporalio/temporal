2d4b24d4d3c51c845a69a38c986a62a40f62fdf5
Stephan Behnke <stephanos@users.noreply.github.com>
2026-08-11 17:27:50 -0700

    Close system namespace metadata manager

--- temporal/server_impl.go
+++ temporal/server_impl.go
@@ -127,7 +127,7 @@ func initSystemNamespaces(
 	if err != nil {
 		return fmt.Errorf("unable to initialize metadata manager: %w", err)
 	}
-	// Do not close the manager here because the factory owns and closes its persistence resources.
+	defer metadataManager.Close()
 	ctx, cancel := context.WithTimeout(
 		headers.SetCallerInfo(ctx, headers.SystemBackgroundHighCallerInfo),
 		30*time.Second,
