157e21c32f877a23ba1b07df1043d0ea00791afe
akhokhar <akhokhar@nvidia.com>
2026-08-21 01:37:54 -0700

    Preserve SQL plugin update compatibility

--- common/persistence/sql/sqlplugin/sqlite/execution.go
+++ common/persistence/sql/sqlplugin/sqlite/execution.go
@@ -17,7 +17,9 @@ const (
 
 	updateExecutionQuery = `UPDATE executions SET
  db_record_version = :db_record_version, next_event_id = :next_event_id, last_write_version = :last_write_version, data = :data, data_encoding = :data_encoding, state = :state, state_encoding = :state_encoding
- WHERE shard_id = :shard_id AND namespace_id = :namespace_id AND workflow_id = :workflow_id AND run_id = :run_id
+ WHERE shard_id = :shard_id AND namespace_id = :namespace_id AND workflow_id = :workflow_id AND run_id = :run_id`
+
+	updateExecutionWithConditionQuery = updateExecutionQuery + `
  AND ((:db_record_version != 0 AND db_record_version = :db_record_version - 1)
       OR (:db_record_version = 0 AND next_event_id = :condition))`
 
@@ -197,10 +199,21 @@ func (mdb *db) InsertIntoExecutions(
 // UpdateExecutions updates a single row in executions table
 func (mdb *db) UpdateExecutions(
 	ctx context.Context,
-	update *sqlplugin.ExecutionsUpdate,
+	row *sqlplugin.ExecutionsRow,
 ) (sql.Result, error) {
 	return mdb.conn.NamedExecContext(ctx,
 		updateExecutionQuery,
+		row,
+	)
+}
+
+// UpdateExecutionsWithCondition conditionally updates a single row in executions table
+func (mdb *db) UpdateExecutionsWithCondition(
+	ctx context.Context,
+	update *sqlplugin.ExecutionsUpdate,
+) (sql.Result, error) {
+	return mdb.conn.NamedExecContext(ctx,
+		updateExecutionWithConditionQuery,
 		update,
 	)
 }
