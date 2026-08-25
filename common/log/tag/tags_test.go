f0d71add880d4c5b7b530d4bcb762bad2515b9a3
Stephan Behnke <stephanos@users.noreply.github.com>
2026-08-24 19:36:06 -0700

    Remove pagination log tag tests

--- common/log/tag/tags_test.go
+++ common/log/tag/tags_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"go.temporal.io/api/serviceerror"
 )
 
@@ -20,22 +20,6 @@ func TestErrorType(t *testing.T) {
 	}
 
 	for id, data := range testData {
-		require.Equal(t, data.expectedResult, ServiceErrorType(data.err).Value().(string), "Unexpected error type in index %d", id)
+		assert.Equal(t, data.expectedResult, ServiceErrorType(data.err).Value().(string), "Unexpected error type in index %d", id)
 	}
-}
-
-func TestNextPageToken(t *testing.T) {
-	nextPageToken := []byte("next-page-token")
-
-	actual := NextPageToken(nextPageToken)
-
-	require.Equal(t, "next-page-token", actual.Key())
-	require.Equal(t, nextPageToken, actual.Value())
-}
-
-func TestPageSize(t *testing.T) {
-	actual := PageSize(42)
-
-	require.Equal(t, "page-size", actual.Key())
-	require.Equal(t, int64(42), actual.Value())
 }
