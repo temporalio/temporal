790527277e5c404eeb33e56c83d66517189f7bab
Stephan Behnke <stephanos@users.noreply.github.com>
2026-08-27 11:20:43 -0700

    fix govuln

--- common/testing/grpcfaultstest/hooks_noop.go
+++ common/testing/grpcfaultstest/hooks_noop.go
@@ -7,6 +7,11 @@ import (
 	commontesthooks "go.temporal.io/server/common/testing/testhooks"
 )
 
+// NewCallbackGenerator creates a CallbackGenerator without test hooks.
+func NewCallbackGenerator(commontesthooks.TestHooks) *grpcfaults.CallbackGenerator {
+	return grpcfaults.NewCallbackGenerator()
+}
+
 // NewGenerator returns nil when test hooks are disabled.
 func NewGenerator(commontesthooks.TestHooks) grpcfaults.Generator {
 	return nil
