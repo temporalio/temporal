0069ffe47c4952c649419a52f2db0bdeb5fe658a
Terence Li <terence.li@temporal.io>
2026-09-04 15:40:19 -0500

    add client timeout

--- common/persistence/visibility/store/elasticsearch/client/client_v7.go
+++ common/persistence/visibility/store/elasticsearch/client/client_v7.go
@@ -46,6 +46,11 @@ func NewEsClient(cfg *Config, httpClient *http.Client, logger log.Logger) (*elas
 			return nil, err
 		}
 	}
+	if httpClient == nil || httpClient == http.DefaultClient {
+		httpClient = &http.Client{Timeout: 60 * time.Second}
+	} else {
+		httpClient.Timeout = 60 * time.Second
+	}
 
 	// TODO (alex): Remove this when https://github.com/olivere/elastic/pull/1507 is merged.
 	if cfg.CloseIdleConnectionsInterval != time.Duration(0) {
