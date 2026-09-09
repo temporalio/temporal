2ba0651ef439463357fd16859284a7ec205d8fa7
thestephenstanton <stephen.stanton@temporal.io>
2026-09-09 09:48:37 -0400

    bring back declined

--- api/enums/v1/cluster.go-helpers.pb.go
+++ api/enums/v1/cluster.go-helpers.pb.go
@@ -28,10 +28,11 @@ func ClusterMemberRoleFromString(s string) (ClusterMemberRole, error) {
 
 var (
 	HealthState_shorthandValue = map[string]int32{
-		"Unspecified":   0,
-		"Serving":       1,
-		"NotServing":    2,
-		"InternalError": 4,
+		"Unspecified":     0,
+		"Serving":         1,
+		"NotServing":      2,
+		"DeclinedServing": 3,
+		"InternalError":   4,
 	}
 )
 
