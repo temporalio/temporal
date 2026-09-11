5b6d8a638453d8fc25cd77fe1fea17dc163074ec
Carly de Frondeville <cdefrondeville@berkeley.edu>
2026-09-10 21:21:28 -0700

    checl pc.valid for write LB

--- client/matching/loadbalancer.go
+++ client/matching/loadbalancer.go
@@ -90,7 +90,7 @@ func (lb *defaultLoadBalancer) PickWritePartition(
 	pc PartitionCounts,
 ) (*tqid.NormalPartition, int) {
 	var partitionCount int
-	if pc.Write > 0 {
+	if pc.Valid() {
 		partitionCount = int(pc.Write)
 	} else {
 		nsName, err := lb.namespaceIDToName(namespace.ID(taskQueue.NamespaceId()))
