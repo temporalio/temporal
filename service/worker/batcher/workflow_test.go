217461a7890030922b311e164a72f2cffa12b0b3
chaptersix <13949480+chaptersix@users.noreply.github.com>
2026-09-12 17:00:47 -0500

    Prevent batch workflows from sharing activity options

--- service/worker/batcher/workflow_test.go
+++ service/worker/batcher/workflow_test.go
@@ -1,18 +1,24 @@
 package batcher
 
 import (
+	"context"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	batchpb "go.temporal.io/api/batch/v1"
 	commonpb "go.temporal.io/api/common/v1"
 	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/testsuite"
 	batchspb "go.temporal.io/server/api/batch/v1"
 	"go.uber.org/mock/gomock"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 type batcherSuite struct {
@@ -35,6 +41,49 @@ func (s *batcherSuite) SetupTest() {
 func (s *batcherSuite) TearDownTest() {
 	s.controller.Finish()
 	s.env.AssertExpectations(s.T())
+}
+
+func TestBatchWorkflowActivityOptionsAreIndependent(t *testing.T) {
+	const workflowCount = 32
+
+	expectedTimeouts := make([]time.Duration, workflowCount)
+	actualTimeouts := make([]time.Duration, workflowCount)
+	errs := make([]error, workflowCount)
+	var wg sync.WaitGroup
+	for i := range workflowCount {
+		expectedTimeout := time.Duration(i+10) * time.Second
+		var heartbeatTimeout *durationpb.Duration
+		if i == 0 {
+			expectedTimeout = defaultActivityHeartBeatTimeout
+		} else {
+			heartbeatTimeout = durationpb.New(expectedTimeout)
+		}
+		expectedTimeouts[i] = expectedTimeout
+
+		wg.Go(func() {
+			var workflowTestSuite testsuite.WorkflowTestSuite
+			env := workflowTestSuite.NewTestWorkflowEnvironment()
+			env.RegisterWorkflow(BatchWorkflowProtobuf)
+			var ac *activities
+			env.OnActivity(ac.BatchActivityWithProtobuf, mock.Anything, mock.Anything).
+				Return(func(ctx context.Context, _ *batchspb.BatchOperationInput) (HeartBeatDetails, error) {
+					actualTimeouts[i] = activity.GetInfo(ctx).HeartbeatTimeout
+					return HeartBeatDetails{}, nil
+				}).Once()
+			env.OnUpsertMemo(mock.Anything).Return(nil).Once()
+			env.ExecuteWorkflow(BatchWorkflowProtobuf, &batchspb.BatchOperationInput{
+				ActivityHeartbeatTimeout: heartbeatTimeout,
+			})
+			errs[i] = env.GetWorkflowError()
+			env.AssertExpectations(t)
+		})
+	}
+	wg.Wait()
+
+	for i := range workflowCount {
+		require.NoError(t, errs[i])
+		require.Equal(t, expectedTimeouts[i], actualTimeouts[i])
+	}
 }
 
 func (s *batcherSuite) TestBatchWorkflow_ValidParams_Query_Protobuf() {
